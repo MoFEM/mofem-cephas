96ced40be603bbd17804a9979fb3d4d7a295eb6a
Lukasz Kaczmarczyk <likask@wp.pl>
2013-08-06 14:04:41 +0100

    new mesh and some quick cleaning for interface class

--- mofem_v0.0.1/examples/ElasticFEMethodForInterface.hpp
+++ mofem_v0.0.1/examples/ElasticFEMethodForInterface.hpp
@@ -66,17 +66,6 @@ struct InterfaceElasticFEMethod: public ElasticFEMethod {
   PetscErrorCode operator()() {
       PetscFunctionBegin;
       ierr = OpStudentStart_TET(g_NTET); CHKERRQ(ierr);
-      ierr = GetMatrices(); CHKERRQ(ierr);
-
-      //Dirihlet Boundary Condition
-      ApplyDirihletBC();
-      if(Diagonal!=PETSC_NULL) {
-	if(DirihletBC.size()>0) {
-	  DirihletBCDiagVal.resize(DirihletBC.size());
-	  fill(DirihletBCDiagVal.begin(),DirihletBCDiagVal.end(),1);
-	  ierr = VecSetValues(Diagonal,DirihletBC.size(),&(DirihletBC[0]),&DirihletBCDiagVal[0],INSERT_VALUES); CHKERRQ(ierr);
-	}
-      }
 
       //Assembly Aij and F
       ierr = RhsAndLhs(); CHKERRQ(ierr);
