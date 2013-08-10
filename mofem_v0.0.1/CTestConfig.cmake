a64c0016a67341c1da5adc97ed1ec682f037acb3
Lukasz Kaczmarczyk <likask@wp.pl>
2013-08-10 23:06:24 +0100

    quick fixes for automatic ctests

--- mofem_v0.0.1/CTestConfig.cmake
+++ mofem_v0.0.1/CTestConfig.cmake
@@ -7,9 +7,12 @@
 ##   INCLUDE(CTest)
 
 set(CTEST_PROJECT_NAME "mofem")
-set(CTEST_NIGHTLY_START_TIME "00:00:00 EST")
+set(CTEST_NIGHTLY_START_TIME "00:00:00 UTC")
 
 set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE "my.cdash.org")
 set(CTEST_DROP_LOCATION "/submit.php?project=mofem")
 set(CTEST_DROP_SITE_CDASH TRUE)
+
+
+
