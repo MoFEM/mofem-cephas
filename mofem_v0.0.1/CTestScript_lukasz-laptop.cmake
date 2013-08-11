6d417e4f1162efacf3febc3a5550db909d3bffea
Lukasz Kaczmarczyk <likask@wp.pl>
2013-08-11 01:06:22 +0100

    crest script setup

--- mofem_v0.0.1/CTestScript_lukasz-laptop.cmake
+++ mofem_v0.0.1/CTestScript_lukasz-laptop.cmake
@@ -1,5 +1,8 @@
 set(CTEST_BUILD_OPTIONS "-DCMAKE_CXX_FLAGS=-lstdc++ -DPETSC_DIR=/opt/build_for_gcc-mp-4.4/petsc-3.3-p3 -DPETSC_ARCH=arch-darwin-c-opt -DMOAB_DIR=/opt/build_for_gcc-mp-4.4/local-moab-4.6.0/ -DBUILD_SHARED_LIBS=FALSE -DCMAKE_CXX_COMPILER=/opt/build_for_gcc-mp-4.4/local/bin/mpicxx")
 
+set(CTEST_SITE "lukaszs-laptop.lan")
+set(CTEST_BUILD_NAME "Darwin-mpicxx")
+
 include(CTestScript.cmake)
 
 
