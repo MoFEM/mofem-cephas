fca781a62dac31cd4584a6f19f3c7f57b5886c36
Zahur Ullah <zahur.ullah@glasgow.ac.uk>
2013-08-23 13:58:53 +0100

    add new test to atom tests, printing data on elements

---
+++ mofem_v0.0.1/atom_tests/print_data_on_elements.cpp
@@ -0,0 +1,176 @@
+/* Copyright (C) 2013, Lukasz Kaczmarczyk (likask AT wp.pl)
+ * --------------------------------------------------------------
+ * FIXME: DESCRIPTION
+ */
+
+/* This file is part of MoFEM.
+ * MoFEM is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * MoFEM is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with MoFEM. If not, see <http://www.gnu.org/licenses/>. */
+
+#include "moabField.hpp"
+#include "moabField_Core.hpp"
+#include "moabFEMethod_UpLevelStudent.hpp"
+#include "cholesky.hpp"
+#include <petscksp.h>
+
+#include "ElasticFEMethod.hpp"
+#include "PostProcVertexMethod.hpp"
+#include "PostProcDisplacementAndStrainOnRefindedMesh.hpp"
+
+using namespace MoFEM;
+
+ErrorCode rval;
+PetscErrorCode ierr;
+
+static char help[] = "...\n\n";
+
+int main(int argc, char *argv[]) {
+
+  PetscInitialize(&argc,&argv,(char *)0,help);
+
+  Core mb_instance;
+  Interface& moab = mb_instance;
+  int rank;
+  MPI_Comm_rank(PETSC_COMM_WORLD,&rank);
+
+  //Reade parameters from line command
+  PetscBool flg = PETSC_TRUE;
+  char mesh_file_name[255];
+  ierr = PetscOptionsGetString(PETSC_NULL,"-my_file",mesh_file_name,255,&flg); CHKERRQ(ierr);
+  if(flg != PETSC_TRUE) {
+    SETERRQ(PETSC_COMM_SELF,1,"*** ERROR -my_file (MESH FILE NEEDED)");
+  }
+
+  //Read mesh to MOAB
+  const char *option;
+  option = "";//"PARALLEL=BCAST;";//;DEBUG_IO";
+  rval = moab.load_file(mesh_file_name, 0, option); CHKERR_PETSC(rval); 
+  ParallelComm* pcomm = ParallelComm::get_pcomm(&moab,MYPCOMM_INDEX);
+  if(pcomm == NULL) pcomm =  new ParallelComm(&moab,PETSC_COMM_WORLD);
+
+  //We need that for code profiling
+  PetscLogDouble t1,t2;
+  PetscLogDouble v1,v2;
+  ierr = PetscGetTime(&v1); CHKERRQ(ierr);
+  ierr = PetscGetCPUTime(&t1); CHKERRQ(ierr);
+
+  //Create MoFEM (Joseph) database
+  moabField_Core core(moab);
+  moabField& mField = core;
+
+  //ref meshset ref level 0
+  ierr = mField.seed_ref_level_3D(0,0); CHKERRQ(ierr);
+
+  // stl::bitset see for more details
+  BitRefLevel bit_level0;
+  bit_level0.set(0);
+  EntityHandle meshset_level0;
+  rval = moab.create_meshset(MESHSET_SET,meshset_level0); CHKERR_PETSC(rval);
+  ierr = mField.seed_ref_level_3D(0,bit_level0); CHKERRQ(ierr);
+  ierr = mField.refine_get_ents(bit_level0,meshset_level0); CHKERRQ(ierr);
+
+  /***/
+  //Define problem
+
+  //Fields
+  ierr = mField.add_field("DISPLACEMENT",H1,3); CHKERRQ(ierr);
+    
+  //FE
+  ierr = mField.add_finite_element("ELASTIC"); CHKERRQ(ierr);
+
+  //Define rows/cols and element data
+  ierr = mField.modify_finite_element_add_field_row("ELASTIC","DISPLACEMENT"); CHKERRQ(ierr);
+  ierr = mField.modify_finite_element_add_field_col("ELASTIC","DISPLACEMENT"); CHKERRQ(ierr);
+  ierr = mField.modify_finite_element_add_field_data("ELASTIC","DISPLACEMENT"); CHKERRQ(ierr);
+
+  //define problems
+  ierr = mField.add_problem("ELASTIC_MECHANICS"); CHKERRQ(ierr);
+
+  //set finite elements for problem
+  ierr = mField.modify_problem_add_finite_element("ELASTIC_MECHANICS","ELASTIC"); CHKERRQ(ierr);
+
+  //set refinment level for problem
+  ierr = mField.modify_problem_ref_level_add_bit("ELASTIC_MECHANICS",bit_level0); CHKERRQ(ierr);
+
+  /***/
+  //Declare problem
+
+  //add entitities (by tets) to the field
+  ierr = mField.add_ents_to_field_by_TETs(0,"DISPLACEMENT"); CHKERRQ(ierr);
+
+  //add finite elements entities
+  ierr = mField.add_ents_to_finite_element_EntType_by_bit_ref(bit_level0,"ELASTIC",MBTET); CHKERRQ(ierr);
+
+  //set app. order
+  //see Hierarchic Finite Element Bases on Unstructured Tetrahedral Meshes (Mark Ainsworth & Joe Coyle)
+  ierr = mField.set_field_order(0,MBTET,"DISPLACEMENT",5); CHKERRQ(ierr);
+  ierr = mField.set_field_order(0,MBTRI,"DISPLACEMENT",5); CHKERRQ(ierr);
+  ierr = mField.set_field_order(0,MBEDGE,"DISPLACEMENT",5); CHKERRQ(ierr);
+  ierr = mField.set_field_order(0,MBVERTEX,"DISPLACEMENT",1); CHKERRQ(ierr);
+
+  /****/
+  //build database
+
+  //build field
+  ierr = mField.build_fields(); CHKERRQ(ierr);
+
+  //build finite elemnts
+  ierr = mField.build_finite_elements(); CHKERRQ(ierr);
+
+  //build adjacencies
+  ierr = mField.build_adjacencies(bit_level0); CHKERRQ(ierr);
+
+  //build problem
+  ierr = mField.build_problems(); CHKERRQ(ierr);
+
+  /****/
+  //mesh partitioning 
+
+  //partition
+  ierr = mField.partition_problem("ELASTIC_MECHANICS"); CHKERRQ(ierr);
+  ierr = mField.partition_finite_elements("ELASTIC_MECHANICS"); CHKERRQ(ierr);
+  //what are ghost nodes, see Petsc Manual
+  ierr = mField.partition_ghost_dofs("ELASTIC_MECHANICS"); CHKERRQ(ierr);
+
+    struct MyFiniteElement_For_Printing: public moabField::FEMethod {
+        MyFiniteElement_For_Printing() {};
+        
+        PetscErrorCode preProcess() {
+            PetscFunctionBegin;
+            PetscPrintf(PETSC_COMM_WORLD,"Start Loop\n");
+            PetscFunctionReturn(0);
+        }
+        PetscErrorCode operator()() {
+            PetscFunctionBegin;
+            PetscSynchronizedPrintf(PETSC_COMM_WORLD,"FE name %s proc %u\n",fe_name.c_str(),fe_ptr->get_part());
+            PetscFunctionReturn(0);
+        }
+        PetscErrorCode postProcess() {
+            PetscFunctionBegin;
+            PetscSynchronizedFlush(PETSC_COMM_WORLD);
+            PetscPrintf(PETSC_COMM_WORLD,"End Loop\n");
+            PetscFunctionReturn(0);
+        }
+
+    };
+    
+  ierr = PetscGetTime(&v2);CHKERRQ(ierr);
+  ierr = PetscGetCPUTime(&t2);CHKERRQ(ierr);
+
+  PetscSynchronizedPrintf(PETSC_COMM_WORLD,"Total Rank %d Time = %f CPU Time = %f\n",pcomm->rank(),v2-v1,t2-t1);
+  PetscSynchronizedFlush(PETSC_COMM_WORLD);
+
+  PetscFinalize();
+
+}
+
