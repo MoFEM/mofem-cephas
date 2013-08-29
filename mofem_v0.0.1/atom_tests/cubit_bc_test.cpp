240b4e9bbbed110124eb217cb3eb60c0bd59b6fe
Dimitrios Kourepinis <dimitrios.kourepinis@glasgow.ac.uk>
2013-08-29 09:41:34 +0100

    cubit_bc_test for review - work in progress
    - Modified bc data structures
    - added function which identifies the type of bc and appropriate struct to improve re-usability
    - Testing force_cubit_bc_data. Does not work properly yet

--- mofem_v0.0.1/atom_tests/cubit_bc_test.cpp
+++ mofem_v0.0.1/atom_tests/cubit_bc_test.cpp
@@ -27,41 +27,72 @@ PetscErrorCode ierr;
 
 static char help[] = "...\n\n";
 
-/*! \struct bc_type
- *  \brief Identify type of BC
+/*! \struct xxxxxxxxx
+ *  \brief xxxxxxxxxx
  *
- *   Defines a string of characters which is used for comparison against the leading characters in bc_data.
- *   Once the type of BC is identified, the corresponding bc data structure can be used.
+ *   Details ...
  *
  */
 
-struct __attribute__((packed)) bc_type { // pack the structure to eliminate padding
+
+// generic bc data structure
+struct generic_cubit_bc_data {
+    PetscErrorCode ierr;
     
-    char name [20]; // BC name
+    virtual PetscErrorCode fill_data(const vector<char>& bc_data) {
+        PetscFunctionBegin;
+        SETERRQ(PETSC_COMM_SELF,1,"It makes no sense for the generic bc type");
+        PetscFunctionReturn(0);
+    }
     
 };
 
-
-// Temperature (NodeSets)
-struct __attribute__((packed)) bc_temp {  // pack the structure in order to get rid of padding
-  char name[11]; // 11 characters for "Temperature"
-  char zero[2]; //
-  char flag1; // Flag for X-Translation (0: not used, 1: applied)
-  char flag2; // Flag for Y-Translation (0: not used, 1: applied)
-  char flag3; // Flag for Z-Translation (0: not used, 1: applied)
-  char flag4; // Flag for X-Rotation (0: not used, 1: applied)
-  char flag5; // Flag for Y-Rotation (0: not used, 1: applied)
-  char flag6; // Flag for Z-Rotation (0: not used, 1: applied)
-  double value1; // Value of X-Translation
-  double value2; // Value of Y-Translation
-  double value3; // Value of Z-Translation
-  double value4; // Value of X-Rotation
-  double value5; // Value of Y-Rotation
-  double value6; // Value of Z-Rotation
+struct displacement_cubit_bc_data: public generic_cubit_bc_data {
+    char name[12]; // 12 characters for "Displacement"
+    char zero;
+    char four;
+    char flag1; // Flag for X-Translation (0: not used, 1: applied)
+    char flag2; // Flag for Y-Translation (0: not used, 1: applied)
+    char flag3; // Flag for Z-Translation (0: not used, 1: applied)
+    char flag4; // Flag for X-Rotation (0: not used, 1: applied)
+    char flag5; // Flag for Y-Rotation (0: not used, 1: applied)
+    char flag6; // Flag for Z-Rotation (0: not used, 1: applied)
+    double value1; // Value of X-Translation
+    double value2; // Value of Y-Translation
+    double value3; // Value of Z-Translation
+    double value4; // Value of X-Rotation
+    double value5; // Value of Y-Rotation
+    double value6; // Value of Z-Rotation
+    virtual PetscErrorCode fill_data(const vector<char>& bc_data) {
+        PetscFunctionBegin;
+        PetscFunctionReturn(0);
+    }
+    
 };
 
-// Velocity (NodeSets)
-struct __attribute__((packed)) bc_veloc {  // pack the structure in order to get rid of padding
+struct force_cubit_bc_data: public generic_cubit_bc_data {
+    struct __attribute__ ((packed)) data{
+    char name[5]; // 5 characters for "Force"
+    char zero[3]; //
+    double value1; // Value of Force
+    double value2; // Value of Moment
+    double value3; // X-component of force direction vector
+    double value4; // Y-component of force direction vector
+    double value5; // Z-component of force direction vector
+    double value6; // X-component of moment direction vector
+    double value7; // Y-component of moment direction vector
+    double value8; // Z-component of moment direction vector
+    char zero2; // 0
+    };
+    
+    virtual PetscErrorCode fill_data(const vector<char>& bc_data) {
+        PetscFunctionBegin;
+        PetscFunctionReturn(0);
+    }
+    
+};
+
+struct velocity_cubit_bc_data: public generic_cubit_bc_data {
     char name[8]; // 8 characters for "Velocity"
     char zero[2];
     char flag1; // Flag for X-Translation (0: not used, 1: applied)
@@ -76,84 +107,170 @@ struct __attribute__((packed)) bc_veloc {  // pack the structure in order to get
     double value4; // Value of X-Rotation
     double value5; // Value of Y-Rotation
     double value6; // Value of Z-Rotation
-};
-
-// Acceleration (NodeSets)
-struct __attribute__((packed)) bc_accel {  // pack the structure in order to get rid of padding
-  char name[12]; // 12 characters for "Acceleration"
-  char zero[2]; //
-  char flag1; // Flag for X-Translation (0: not used, 1: applied)
-  char flag2; // Flag for Y-Translation (0: not used, 1: applied)
-  char flag3; // Flag for Z-Translation (0: not used, 1: applied)
-  char flag4; // Flag for X-Rotation (0: not used, 1: applied)
-  char flag5; // Flag for Y-Rotation (0: not used, 1: applied)
-  char flag6; // Flag for Z-Rotation (0: not used, 1: applied)
-  double value1; // Value of X-Translation
-  double value2; // Value of Y-Translation
-  double value3; // Value of Z-Translation
-  double value4; // Value of X-Rotation
-  double value5; // Value of Y-Rotation
-  double value6; // Value of Z-Rotation
-};
-
-// Force (NodeSets)
-struct __attribute__((packed)) bc_force {  // pack the structure in order to get rid of padding
-  char name[5]; // 5 characters for "Force"
-  char zero[3]; //
-  double value1; // Value of Force
-  double value2; // Value of Moment
-  double value3; // X-component of force direction vector
-  double value4; // Y-component of force direction vector
-  double value5; // Z-component of force direction vector
-  double value6; // X-component of moment direction vector
-  double value7; // Y-component of moment direction vector
-  double value8; // Z-component of moment direction vector
-  char zero2; // 0
-};
-
-// Displacement (NodeSets)
-struct __attribute__((packed)) bc_disp {  // need to pack the structure in order to get rid of padding
-  char name[12]; // 12 characters for "Displacement"
-  char zero;
-  char four;
-  char flag1; // Flag for X-Translation (0: not used, 1: applied)
-  char flag2; // Flag for Y-Translation (0: not used, 1: applied)
-  char flag3; // Flag for Z-Translation (0: not used, 1: applied)
-  char flag4; // Flag for X-Rotation (0: not used, 1: applied)
-  char flag5; // Flag for Y-Rotation (0: not used, 1: applied)
-  char flag6; // Flag for Z-Rotation (0: not used, 1: applied)
-  double value1; // Value of X-Translation
-  double value2; // Value of Y-Translation
-  double value3; // Value of Z-Translation
-  double value4; // Value of X-Rotation
-  double value5; // Value of Y-Rotation
-  double value6; // Value of Z-Rotation
-};
-
-// Pressure (SideSets)
-struct __attribute__((packed)) bc_press {  // pack the structure in order to get rid of padding
-    char name[8]; // 8 characters for "Pressure"
-    char flag1; //
-    char flag2; //    
-    double value1; // Pressure value
-    char zero; //
+    virtual PetscErrorCode fill_data(const vector<char>& bc_data) {
+        PetscFunctionBegin;
+        PetscFunctionReturn(0);
+    }
     
 };
 
-// Heat Flux (SideSets)
-struct __attribute__((packed)) bc_heatflux {  // need to pack the structure in order to get rid of padding
+struct acceleration_cubit_bc_data: public generic_cubit_bc_data {
+    char name[12]; // 12 characters for "Acceleration"
+    char zero[2]; //
+    char flag1; // Flag for X-Translation (0: not used, 1: applied)
+    char flag2; // Flag for Y-Translation (0: not used, 1: applied)
+    char flag3; // Flag for Z-Translation (0: not used, 1: applied)
+    char flag4; // Flag for X-Rotation (0: not used, 1: applied)
+    char flag5; // Flag for Y-Rotation (0: not used, 1: applied)
+    char flag6; // Flag for Z-Rotation (0: not used, 1: applied)
+    double value1; // Value of X-Translation
+    double value2; // Value of Y-Translation
+    double value3; // Value of Z-Translation
+    double value4; // Value of X-Rotation
+    double value5; // Value of Y-Rotation
+    double value6; // Value of Z-Rotation
+    virtual PetscErrorCode fill_data(const vector<char>& bc_data) {
+        PetscFunctionBegin;
+        PetscFunctionReturn(0);
+    }
+    
+};
+
+struct temperature_cubit_bc_data: public generic_cubit_bc_data {
+    char name[11]; // 11 characters for "Temperature"
+    char zero[2]; //
+    char flag1; // Flag for X-Translation (0: not used, 1: applied)
+    char flag2; // Flag for Y-Translation (0: not used, 1: applied)
+    char flag3; // Flag for Z-Translation (0: not used, 1: applied)
+    char flag4; // Flag for X-Rotation (0: not used, 1: applied)
+    char flag5; // Flag for Y-Rotation (0: not used, 1: applied)
+    char flag6; // Flag for Z-Rotation (0: not used, 1: applied)
+    double value1; // Value of X-Translation
+    double value2; // Value of Y-Translation
+    double value3; // Value of Z-Translation
+    double value4; // Value of X-Rotation
+    double value5; // Value of Y-Rotation
+    double value6; // Value of Z-Rotation
+    virtual PetscErrorCode fill_data(const vector<char>& bc_data) {
+        PetscFunctionBegin;
+        PetscFunctionReturn(0);
+    }
+    
+};
+
+struct pressure_cubit_bc_data: public generic_cubit_bc_data {
+    char name[8]; // 8 characters for "Pressure"
+    char flag1; //
+    char flag2; //
+    double value1; // Pressure value
+    char zero; //
+    virtual PetscErrorCode fill_data(const vector<char>& bc_data) {
+        PetscFunctionBegin;
+        PetscFunctionReturn(0);
+    }
+    
+};
+
+struct heatflux_cubit_bc_data: public generic_cubit_bc_data {
     char name[8]; // 8 characters for "HeatFlux" (no space)
     char flag1; //
     char flag2; //
     char flag3; //
     char flag4; //
     char flag5; //
-
     double value1; //
     double value2; //
     double value3; //
-
+    virtual PetscErrorCode fill_data(const vector<char>& bc_data) {
+        PetscFunctionBegin;
+        PetscFunctionReturn(0);
+    }
+    
 };
+
+
+PetscErrorCode func(const vector<char> &bc_data,generic_cubit_bc_data *ptr_cubit_bc_data) {
+    PetscFunctionBegin;
+    
+    PetscErrorCode ierr;
+    
+    int scom;
+    if (strcmp (&bc_data[0],"Displacement") == 0)
+        scom = 1;
+    else if (strcmp (&bc_data[0],"Force") == 0)
+        scom = 2;
+    else if (strcmp (&bc_data[0],"Velocity") == 0)
+        scom = 3;
+    else if (strcmp (&bc_data[0],"Acceleration") == 0)
+        scom = 4;
+    else if (strcmp (&bc_data[0],"Temperature") == 0)
+        scom = 5;
+    else if (strcmp (&bc_data[0],"Pressure") == 0)
+        scom = 6;
+    else if (strcmp (&bc_data[0],"HeatFlux") == 0)
+        scom = 7;
+    else SETERRQ(PETSC_COMM_SELF,1,"Error: Unrecognizable BC type");
+    
+    switch (scom)
+    {
+        case 1: {
+            //Displacement
+            //FILL displacement_cubit_bc_data
+            cout << "<Displacement>";
+            ierr = ptr_cubit_bc_data->fill_data(bc_data); CHKERRQ(ierr);
+            
+        }
+            break;
+        case 2: {
+            //Force
+            //FILL force_cubit_bc_data
+            ierr = ptr_cubit_bc_data->fill_data(bc_data); CHKERRQ(ierr);
+            
+        }
+            break;
+        case 3: {
+            //Velocity
+            //FILL velocity_cubit_bc_data
+            cout << "Velocity" << endl;
+            ierr = ptr_cubit_bc_data->fill_data(bc_data); CHKERRQ(ierr);
+            
+        }
+            break;
+        case 4: {
+            //Acceleration
+            //FILL acceleration_cubit_bc_data
+            cout << "Acceleration" << endl;
+            ierr = ptr_cubit_bc_data->fill_data(bc_data); CHKERRQ(ierr);
+        }
+            break;
+        case 5: {
+            //Temperature
+            //FILL temperature_cubit_bc_data
+            cout << "Temperature" << endl;
+            ierr = ptr_cubit_bc_data->fill_data(bc_data); CHKERRQ(ierr);
+        }
+            break;
+        case 6: {
+            //Pressure
+            //FILL pressure_cubit_bc_data
+            cout << "Pressure" << endl;
+            ierr = ptr_cubit_bc_data->fill_data(bc_data); CHKERRQ(ierr);
+        }
+            break;
+        case 7: {
+            //Heat Flux
+            //FILL heatflux_cubit_bc_data
+            cout << "Heat Flux" << endl;
+            ierr = ptr_cubit_bc_data->fill_data(bc_data); CHKERRQ(ierr);
+        }
+            break;
+    
+        default:
+            SETERRQ(PETSC_COMM_SELF,1,"Something went wrong filling cubit_bc_data");
+    }
+    
+    PetscFunctionReturn(0);
+}
 
 
 int main(int argc, char *argv[]) {
@@ -198,57 +315,24 @@ int main(int argc, char *argv[]) {
     vector<char> bc_data;
     ierr = it->get_Cubit_bc_data(bc_data); CHKERRQ(ierr);
       
-      //DK work in progress
-      bc_type mydata;
-      memcpy(&mydata,&bc_data[0],sizeof(bc_type));
       
-      int scom;
-      if (strcmp (mydata.name,"Temperature") == 0)
-          scom = 1;
-      else if (strcmp (mydata.name,"Velocity") == 0)
-          scom = 2;
-      else if (strcmp (mydata.name,"Acceleration") == 0)
-          scom = 3;
-      else if (strcmp (mydata.name,"Force") == 0)
-          scom = 4;
-      else if (strcmp (mydata.name,"Displacement") == 0)
-          scom = 5;
-      else if (strcmp (mydata.name,"Pressure") == 0)
-          scom = 6;
-      else if (strcmp (mydata.name,"HeatFlux") == 0)
-          scom = 7;
+      //displacement_cubit_bc_data disp_cubit_bc_struct;
+      force_cubit_bc_data force_cubit_bc_struct;
+      
+      ierr = func(bc_data,&force_cubit_bc_struct); CHKERRQ(ierr);
       
       
-      switch (scom)
-      {
-          case 1:
-              cout << "Temperature";
-              break;
-          case 2:
-              cout << "Velocity";
-              break;
-          case 3:
-              cout << "Acceleration";
-              break;
-          case 4:
-              cout << "Force";
-              break;
-          case 5:
-              cout << "Displacement";
-              break;
-          case 6:
-              cout << "Pressure";
-              break;
-          case 7:
-              cout << "Heat Flux";
-              break;
-              
-          default:
-              cout << "BC not recognized";
-              
-      }
+      //testing
+      int z;
+      z=sizeof(force_cubit_bc_struct); cout << z;
       
- }
+      //memcpy(&force_cubit_bc_struct,&bc_data[0],sizeof(z));
+
+
+       //printf("---> value1 %f\n",force_cubit_bc_struct.value1);
+
+      
+  }
 
   cout << "<<<< SideSets >>>>>" << endl;
   //SideSets
