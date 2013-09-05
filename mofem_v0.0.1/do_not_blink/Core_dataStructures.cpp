e6ecebb628d8558cefe0199983e905c2e299dde9
Lukasz Kaczmarczyk <likask@wp.pl>
2013-09-05 14:12:57 +0100

    more bugs in sidenumers of degenerated prisms elements

--- mofem_v0.0.1/do_not_blink/Core_dataStructures.cpp
+++ mofem_v0.0.1/do_not_blink/Core_dataStructures.cpp
@@ -297,8 +297,8 @@ SideNumber* RefMoFEMElement_PRISM::get_side_number_ptr(Interface &moab,EntityHan
 	  ( conn_ent[0] == edges[ee][0] )&&( conn_ent[1] == edges[ee][1] )||
 	  ( conn_ent[0] == edges[ee][1] )&&( conn_ent[1] == edges[ee][0] ) ) {
 	  side_number = ee;
-	  if(ee>2) {
-	    side_number += 3;
+	  if(ee>=3) {
+	    side_number += 6;
 	    EntityHandle* conn0_4_ptr = find( face4, &face4[3], conn_ent[0] );
 	    offset = distance( face4, conn0_4_ptr ) + 3;
 	  } else {
