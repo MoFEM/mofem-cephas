bb03f4f895159798ff6f4cba96e6cef45d436e91
Lukasz Kaczmarczyk <likask@wp.pl>
2013-10-17 18:17:38 +0100

    credit for micheal

--- mofem_v0.0.1/atom_tests/trans_isotropic_test.cpp
+++ mofem_v0.0.1/atom_tests/trans_isotropic_test.cpp
@@ -1,4 +1,4 @@
-/* Copyright (C) 2013, Lukasz Kaczmarczyk (likask AT wp.pl)
+/* Copyright (C) 2013, Micheal Cortis
  * --------------------------------------------------------------
  * FIXME: DESCRIPTION
  */
--- mofem_v0.0.1/student_body/ElasticFEMethodTransIso.hpp
+++ mofem_v0.0.1/student_body/ElasticFEMethodTransIso.hpp
@@ -1,4 +1,4 @@
-/* Copyright (C) 2013, Lukasz Kaczmarczyk (likask AT wp.pl)
+/* Copyright (C) 2013, Michel Cortis
  * --------------------------------------------------------------
  * FIXME: DESCRIPTION
  */
