e6e97b0a4403fc85ad76a133ab3c61f20bdf67c9
Lukasz Kaczmarczyk <likask@wp.pl>
2013-10-29 19:51:30 +0000

    -snes_type ls to -snes_type newtonls

--- mofem_v0.0.1/name_change.sh
+++ mofem_v0.0.1/name_change.sh
@@ -4,5 +4,7 @@ find . -name "*.h" -exec sed -i .org "s/$1/$2/g" {} \;
 find . -name "*.c" -exec sed -i .org "s/$1/$2/g" {} \;
 find . -name "*.hpp" -exec sed -i .org "s/$1/$2/g" {} \;
 find . -name "*.cpp" -exec sed -i .org "s/$1/$2/g" {} \;
+find . -name "*.txt" -exec sed -i .org "s/$1/$2/g" {} \;
+find . -name "README" -exec sed -i .org "s/$1/$2/g" {} \;
 
 
