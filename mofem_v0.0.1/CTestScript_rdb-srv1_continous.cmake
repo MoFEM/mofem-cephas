52d4a29e747c145abb6d393f3c59f115b358a4db
Lukasz Kaczmarczyk <likask@wp.pl>
2013-10-31 19:45:36 +0000

    bug in scripts for rdb valgrind testing

--- mofem_v0.0.1/CTestScript_rdb-srv1_continous.cmake
+++ mofem_v0.0.1/CTestScript_rdb-srv1_continous.cmake
@@ -10,7 +10,8 @@ endif(NOT DASHBOARDTEST)
 find_program(CTEST_MEMORYCHECK_COMMAND NAMES valgrind)
 SET(CTEST_MEMORYCHECK_COMMAND_OPTIONS 
   "--trace-children=yes --quiet --tool=memcheck --leak-check=yes --show-reachable=yes --num-callers=50 --verbose --demangle=yes --gen-suppressions=all")
-SET(CTEST_MEMORYCHECK_SUPPRESSIONS_FILE ${CTEST_SOURCE_DIRECTORY}/CTestScript_rdb-srv1.cmake)
+SET(CTEST_MEMORYCHECK_SUPPRESSIONS_FILE "$ENV{HOME}/tmp/mofem/source/CTestScript_rdb-srv1.cmake")
+
 
 include(CTestScript.cmake)
 
