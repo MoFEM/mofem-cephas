39b3a5aca20cb6866509405fb2740620c6ed2256
likask <likask@wp.pl>
2013-11-12 19:00:03 +0000

    continous build for cdash testing on live-cd

--- mofem_v0.0.1/CTestScript_live_cd.cmake
+++ mofem_v0.0.1/CTestScript_live_cd.cmake
@@ -3,12 +3,12 @@ set(CTEST_BUILD_OPTIONS "-DCMAKE_CXX_FLAGS=-I/opt/local/include\\ -L/opt/local/l
 set(CTEST_SITE "live-cd")
 set(CTEST_BUILD_NAME "Linux-mpicxx")
 
-if(NOT DASHBOARDTEST) 
-  set(FORCETESTING "YES")
-  set(DASHBOARDTEST "Nightly")
+if(NOT DASHBOARDTEST)
+  # set(FORCETESTING "YES")
+  set(DASHBOARDTEST "Continuous")
 endif(NOT DASHBOARDTEST)
 
-#valgrind set up
+# valgrind set up
 find_program(CTEST_MEMORYCHECK_COMMAND NAMES valgrind)
 SET(CTEST_MEMORYCHECK_COMMAND_OPTIONS 
   "--trace-children=yes --quiet --tool=memcheck --leak-check=yes --show-reachable=yes --num-callers=50 --verbose --demangle=yes --gen-suppressions=all")
