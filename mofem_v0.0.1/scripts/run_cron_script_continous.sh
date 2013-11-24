b5440402242a7a4c950d49d85d75cd5355144250
Lukasz Kaczmarczyk <likask@wp.pl>
2013-11-24 10:12:13 +0000

    more run scripts

--- mofem_v0.0.1/scripts/run_cron_script_continous.sh
+++ mofem_v0.0.1/scripts/run_cron_script_continous.sh
@@ -2,12 +2,15 @@
 
 CTEST_SCRIPTS_FILE_PATH=/home/lukasz/bitbucket/mofem-joseph/mofem_v0.0.1/cmake
 CTSET_SCRIPT=CTestScript_rdb-srv1_continous.cmake
+CWD=`pwd`
 
 if test -f /home/lukasz/tests.lock
 then
   echo "lock"
 else
   touch /home/lukasz/tests.lock
-  /usr/bin/ctest -VV --http1.0 -S $CTEST_SCRIPTS_FILE_PATH/$CTSET_SCRIPT >> /home/lukasz/tests.log 2>&1
+  cd $CTEST_SCRIPTS_FILE_PATH
+  /usr/bin/ctest -VV --http1.0 -S $CTSET_SCRIPT >> /home/lukasz/tests.log 2>&1
+  cd $CWD
   rm -v /home/lukasz/tests.lock
 fi
--- mofem_v0.0.1/scripts/run_cron_script_experimental.sh
+++ mofem_v0.0.1/scripts/run_cron_script_experimental.sh
@@ -2,6 +2,8 @@
 
 CTEST_SCRIPTS_FILE_PATH=/home/lukasz/bitbucket/mofem-joseph/mofem_v0.0.1/cmake
 CTSET_SCRIPT=CTestScript_rdb-srv1_experimental.cmake
+CWD=`pwd`
 
-/usr/bin/ctest -VV --http1.0 -S $CTEST_SCRIPTS_FILE_PATH/$CTSET_SCRIPT >> /home/lukasz/tests.log 2>&1
-
+cd $CTEST_SCRIPTS_FILE_PATH
+/usr/bin/ctest -VV --http1.0 -S $CTSET_SCRIPT >> /home/lukasz/tests.log 2>&1
+cd $CWD
