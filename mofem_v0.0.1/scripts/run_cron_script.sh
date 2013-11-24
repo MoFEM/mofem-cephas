3bacc5ff1b75529ad5383ab2791ef0ddb82f5239
Lukasz Kaczmarczyk <likask@wp.pl>
2013-11-24 09:52:37 +0000

    run scripts

--- mofem_v0.0.1/scripts/run_cron_script.sh
+++ mofem_v0.0.1/scripts/run_cron_script.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
+CTEST_SCRIPTS_FILE_PATH=/home/lukasz/bitbucket/mofem-joseph/mofem_v0.0.1/cmake
+CTSET_SCRIPT=CTestScript_rdb-srv1.cmake
+
 if test -f /home/lukasz/tests.lock
 then
   echo "lock"
 else
   touch /home/lukasz/tests.lock
-  cd /home/lukasz/bitbucket/mofem-joseph/mofem_v0.0.1/
-  /usr/bin/ctest -VV --http1.0 -S CTestScript_rdb-srv1.cmake >> /home/lukasz/tests.log 2>&1
+  /usr/bin/ctest -VV --http1.0 -S $CTEST_SCRIPTS_FILE_PATH/$CTSET_SCRIPT >> /home/lukasz/tests.log 2>&1
   rm -v /home/lukasz/tests.lock
 fi
--- mofem_v0.0.1/scripts/run_cron_script_experimental.sh
+++ mofem_v0.0.1/scripts/run_cron_script_experimental.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-cd /home/lukasz/bitbucket/mofem-joseph/mofem_v0.0.1/
-/usr/bin/ctest -VV --http1.0 -S CTestScript_rdb-srv1_experimental.cmake  >> /home/lukasz/tests.log 2>&1
+CTEST_SCRIPTS_FILE_PATH=/home/lukasz/bitbucket/mofem-joseph/mofem_v0.0.1/cmake
+CTSET_SCRIPT=CTestScript_rdb-srv1_experimental.cmake
+
+/usr/bin/ctest -VV --http1.0 -S $CTEST_SCRIPTS_FILE_PATH/$CTSET_SCRIPT >> /home/lukasz/tests.log 2>&1
 
