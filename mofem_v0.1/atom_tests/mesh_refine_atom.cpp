bd702ef55b89d885b2fd92e4520d0b21500eed4f
Lukasz Kaczmarczyk <likask@wp.pl>
2014-02-02 14:04:17 +0000

    add mesh refine atom test

--- mofem_v0.1/atom_tests/mesh_refine_atom.cpp
+++ mofem_v0.1/atom_tests/mesh_refine_atom.cpp
@@ -37,11 +37,6 @@ int main(int argc, char *argv[]) {
   if(flg != PETSC_TRUE) {
     SETERRQ(PETSC_COMM_SELF,1,"*** ERROR -my_file (MESH FILE NEEDED)");
   }
-  char mesh_out_file_name[255];
-  ierr = PetscOptionsGetString(PETSC_NULL,"-my_out_file",mesh_out_file_name,255,&flg); CHKERRQ(ierr);
-  if(flg != PETSC_TRUE) {
-    SETERRQ(PETSC_COMM_SELF,1,"*** ERROR -my_out_file (MESH FILE NEEDED)");
-  }
 
   Core mb_instance;
   Interface& moab = mb_instance;
@@ -51,61 +46,61 @@ int main(int argc, char *argv[]) {
   rval = moab.load_file(mesh_file_name, 0, option); CHKERR(rval); 
   ParallelComm* pcomm = ParallelComm::get_pcomm(&moab,0);
 
-  PetscLogDouble t1,t2;
-  PetscLogDouble v1,v2;
-  ierr = PetscGetTime(&v1); CHKERRQ(ierr);
-  ierr = PetscGetCPUTime(&t1); CHKERRQ(ierr);
-
   FieldCore core(moab);
   FieldInterface& mField = core;
 
-  ierr = mField.seed_ref_level_3D(0,0); CHKERRQ(ierr);
   BitRefLevel bit_level0;
   bit_level0.set(0);
-
-  for(_IT_CUBITMESHSETS_BY_BCDATA_TYPE_FOR_LOOP_(mField,SideSet|InterfaceSet,cit)) {
-    EntityHandle meshset = cit->get_meshset();
-    ierr = mField.get_msId_3dENTS_sides(*mit,true,0); CHKERRQ(ierr);
-    ierr = mField.get_msId_3dENTS_split_sides(0,bit_level0,*mit,true,true,0); CHKERRQ(ierr);
-  }
-  EntityHandle meshset_level0;
-  rval = moab.create_meshset(MESHSET_SET,meshset_level0); CHKERR_PETSC(rval);
-  ierr = mField.refine_get_ents(bit_level0,BitRefLevel().set(),meshset_level0); CHKERRQ(ierr);
+  ierr = mField.seed_ref_level_3D(0,bit_level0); CHKERRQ(ierr);
 
   BitRefLevel bit_level1;
   bit_level1.set(1);
-  ierr = mField.seed_ref_level_3D(meshset_level0,bit_level1); CHKERRQ(ierr);
 
-  EntityHandle meshset_level1;
-  rval = moab.create_meshset(MESHSET_SET,meshset_level1); CHKERR_PETSC(rval);
-  ierr = mField.refine_get_ents(bit_level1,BitRefLevel().set(),meshset_level1); CHKERRQ(ierr);
+  EntityHandle meshset_level0;
+  rval = moab.create_meshset(MESHSET_SET,meshset_level0); CHKERR_PETSC(rval);
+  ierr = mField.refine_get_ents(bit_level0,BitRefLevel().set(),meshset_level0); CHKERRQ(ierr);
 
   // random mesh refinment
   EntityHandle meshset_ref_edges;
   rval = moab.create_meshset(MESHSET_SET,meshset_ref_edges); CHKERR_PETSC(rval);
   Range edges_to_refine;
-  rval = moab.get_entities_by_type(meshset_level1,MBEDGE,edges_to_refine);  CHKERR_PETSC(rval);
-  for(Range::iterator eit = edges_to_refine.begin();eit!=edges_to_refine.end();eit++) {
-    int numb = rand() % 3;
+  rval = moab.get_entities_by_type(meshset_level0,MBEDGE,edges_to_refine);  CHKERR_PETSC(rval);
+  int ii = 0;
+  for(Range::iterator eit = edges_to_refine.begin();
+    eit!=edges_to_refine.end();eit++,ii++) {
+    int numb = ii % 2;
     if(numb == 0) {
       ierr = moab.add_entities(meshset_ref_edges,&*eit,1); CHKERRQ(ierr);
     }
   }
-  BitRefLevel bit_level2;
-  bit_level2.set(2);
-  ierr = mField.add_verices_in_the_middel_of_edges(meshset_ref_edges,bit_level2); CHKERRQ(ierr);
-  ierr = mField.refine_TET(meshset_level1,bit_level2); CHKERRQ(ierr);
-  ierr = mField.refine_PRISM(meshset_level1,bit_level2,0); CHKERRQ(ierr);
+  ierr = mField.add_verices_in_the_middel_of_edges(meshset_ref_edges,bit_level1); CHKERRQ(ierr);
+  ierr = mField.refine_TET(meshset_level0,bit_level1); CHKERRQ(ierr);
 
-  ierr = PetscGetTime(&v2);CHKERRQ(ierr);
-  ierr = PetscGetCPUTime(&t2);CHKERRQ(ierr);
-  PetscSynchronizedPrintf(PETSC_COMM_WORLD,"SNESSolve:: Rank %d Time = %f CPU Time = %f\n",pcomm->rank(),v2-v1,t2-t1);
-  PetscSynchronizedFlush(PETSC_COMM_WORLD);
+  ofstream myfile;
+  myfile.open("mesh_refine.txt");
 
-  EntityHandle out_meshset_ref;
-  rval = moab.create_meshset(MESHSET_SET,out_meshset_ref); CHKERR_PETSC(rval);
-  ierr = mField.refine_get_finite_elements(bit_level2,out_meshset_ref); CHKERRQ(ierr);
-  rval = moab.write_file(mesh_out_file_name,"VTK","",&out_meshset_ref,1); CHKERR_PETSC(rval);
+  EntityHandle out_meshset_tet;
+  rval = moab.create_meshset(MESHSET_SET,out_meshset_tet); CHKERR_PETSC(rval);
+  ierr = mField.refine_get_ents(bit_level1,BitRefLevel().set(),MBTET,out_meshset_tet); CHKERRQ(ierr);
+  Range tets;
+  rval = moab.get_entities_by_handle(out_meshset_tet,tets); CHKERR_PETSC(rval);
+  for(Range::iterator tit = tets.begin();tit!=tets.end();tit++) {
+    int num_nodes; 
+    const EntityHandle* conn;
+    rval = moab.get_connectivity(*tit,conn,num_nodes,true); CHKERR_PETSC(rval);
+    
+    for(int nn = 0;nn<num_nodes;nn++) {
+      cout << conn[nn] << " ";
+      myfile << conn[nn] << " ";
+    }
+    cout << endl;
+    myfile << endl;
+
+  }
+
+  myfile.close();
+
+  //rval = moab.write_file("out.vtk","VTK","",&out_meshset_tet,1); CHKERR_PETSC(rval);
 
   PetscFinalize();
 
---
+++ mofem_v0.1/student_body/PostProcTemperatureOnRefindedMesh.hpp
@@ -0,0 +1,111 @@
+/* Copyright (C) 2013, Lukasz Kaczmarczyk (likask AT wp.pl)
+ * --------------------------------------------------------------
+ * FIXME: DESCRIPTION
+ */
+
+/* This file is part of MoFEM.
+ * MoFEM is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * MoFEM is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with MoFEM. If not, see <http://www.gnu.org/licenses/>. */
+
+#ifndef __POSTPROCTERMOERATUREONREFINDEDMESH_HPP__
+#define __POSTPROCTERMOERATUREONREFINDEDMESH_HPP__
+
+#include "PostProcDisplacementAndStrainOnRefindedMesh.hpp"
+
+using namespace MoFEM;
+
+struct PostProcTemperatureOnRefMesh: public PostProcOnRefMesh_Base,FEMethod_UpLevelStudent {
+
+    Tag th_themp,th_grad_themp;
+    PostProcTemperatureOnRefMesh(Interface& _moab): 
+      FEMethod_UpLevelStudent(_moab),PostProcOnRefMesh_Base() 
+      {
+
+      double def_VAL[3] = {0,0,0};
+      rval = moab_post_proc.tag_get_handle("TEMPERATURE_VAL",1,
+	MB_TYPE_DOUBLE,th_themp,MB_TAG_CREAT|MB_TAG_SPARSE,def_VAL); CHKERR_THROW(rval);
+      rval = moab_post_proc.tag_get_handle("TEMPERATURE_GARDIENT_VAL",3,
+	MB_TYPE_DOUBLE,th_grad_themp,MB_TAG_CREAT|MB_TAG_SPARSE,def_VAL); CHKERR_THROW(rval);
+
+    }
+
+
+    map<EntityHandle,EntityHandle> node_map;
+
+    PetscErrorCode operator()() {
+      PetscFunctionBegin;
+
+      ierr = OpStudentStart_TET(g_NTET); CHKERRQ(ierr);
+
+      Range ref_nodes;
+      rval = moab_ref.get_entities_by_type(meshset_level[max_level],MBVERTEX,ref_nodes); CHKERR_PETSC(rval);
+      if(4*ref_nodes.size()!=g_NTET.size()) SETERRQ(PETSC_COMM_SELF,1,"data inconsistency");
+      if(ref_nodes.size()!=coords_at_Gauss_nodes.size()) SETERRQ(PETSC_COMM_SELF,1,"data inconsistency");
+      Range::iterator nit = ref_nodes.begin();
+      node_map.clear();
+      for(int nn = 0;nit!=ref_nodes.end();nit++,nn++) {
+	EntityHandle &node = node_map[*nit];
+	rval = moab_post_proc.create_vertex(&(coords_at_Gauss_nodes[nn]).data()[0],node); CHKERR_PETSC(rval);
+      }
+      Range ref_tets;
+      rval = moab_ref.get_entities_by_type(meshset_level[max_level],MBTET,ref_tets); CHKERR_PETSC(rval);
+      Range::iterator tit = ref_tets.begin();
+      for(;tit!=ref_tets.end();tit++) {
+	const EntityHandle *conn_ref;
+        int num_nodes;
+	rval = moab_ref.get_connectivity(*tit,conn_ref,num_nodes,true); CHKERR_PETSC(rval);
+	EntityHandle conn_post_proc[num_nodes];
+	for(int nn = 0;nn<num_nodes;nn++) {
+	  conn_post_proc[nn] = node_map[conn_ref[nn]];
+	}
+	EntityHandle ref_tet;
+	rval = moab_post_proc.create_element(MBTET,conn_post_proc,4,ref_tet); CHKERR_PETSC(rval);
+      }
+
+      //Get displacements at Gauss points
+      {
+
+	string filed_name("TEMPERATURE");
+	H1L2_Data_at_Gauss_pt::iterator diit = h1l2_data_at_gauss_pt.find("TEMPEREATURE");
+	if(diit==h1l2_data_at_gauss_pt.end()) SETERRQ(PETSC_COMM_SELF,1,"no field_name TEMPERATURE !!!");
+	vector< ublas::vector<FieldData> > &data = diit->second;
+	vector< ublas::vector<FieldData> >::iterator vit = data.begin();
+	map<EntityHandle,EntityHandle>::iterator mit = node_map.begin();
+	for(;vit!=data.end();vit++,mit++) {
+	  rval = moab_post_proc.tag_set_data(th_themp,&mit->second,1,&vit->data()[0]); CHKERR_PETSC(rval);
+	}
+
+      }
+
+      {
+  
+	H1_DiffData_at_Gauss_pt::iterator  giit = h1_diff_data_at_gauss_pt.find("TEMPEREATURE");
+	if(giit == h1_diff_data_at_gauss_pt.end()) SETERRQ(PETSC_COMM_SELF,1,"no field_name TEMPERATURE !!!");
+	vector< ublas::matrix<FieldData> > &data = giit->second;
+	vector< ublas::matrix<FieldData> >::iterator m_dit = data.begin();
+	map<EntityHandle,EntityHandle>::iterator mit = node_map.begin();
+	for(;m_dit!=data.end();m_dit++,mit++) {
+	  rval = moab_post_proc.tag_set_data(th_grad_themp,&mit->second,1,&m_dit->data()[0]); CHKERR_PETSC(rval);
+	}
+
+      }
+
+      PetscFunctionReturn(0);
+    }
+
+};
+
+
+#endif //__POSTPROCTERMOERATUREONREFINDEDMESH_HPP__
+
+
