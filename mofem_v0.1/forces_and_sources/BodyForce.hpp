b8117128d1f9fdebde090bf3db6ab27373e90184
Lukasz Kaczmarczyk <likask@wp.pl>
2014-05-27 21:49:06 +0100

    body forces

--- mofem_v0.1/forces_and_sources/BodyForce.hpp
+++ mofem_v0.1/forces_and_sources/BodyForce.hpp
@@ -37,32 +37,64 @@ struct BodyFroce: public CoreForcesAndSurces {
   CoreForcesAndSurces(FiedInterface& _mField,const int _msId):
     CoreForcesAndSurces(mField),msId(_msId) {};
 
+  struct opBodyForce: public dataOperator {
+
+      Block_BodyForces data;
+
+      ublas::vector<FieldData> f;
+      PetscErrorCode doWork(
+	int side,int side,
+	EntityType type
+	ublas::vector<DofIdx> &indices
+	ublas::matrix<FieldData> &N) {
+
+      PetscFunctionBegin;
+
+      f.resize(indices.size());
+      for(int gg = 0;gg<N.size1();gg++) {
+	
+      }
+
+
+      PetscFunctionReturn(0);
+
+      }
+  }
+
+  opBodyForce op;
+
   const CubitMeshSets *cubit_meshset_ptr;
-  Block_BodyForces data;
   EntityHandle meshset;
 
   PetscErroCode getBodyElements() {
     PetscFunctionBegin;
     ierr = mField.get_Cubit_msId(msId,BlockSet|Body_Force,&cubit_meshset_ptr); CHKERRQ(ierr);
-    ierr = cubit_meshset_ptr->get_attribute_data_structure(mydata); CHKERRQ(ierr);
+    ierr = cubit_meshset_ptr->get_attribute_data_structure(op.data); CHKERRQ(ierr);
     meshset = cubit_meshset_ptr->get_meshset();
     PetscFunctionReturn(0);
   }
 
-  PetscErrorCode preProcess() {
-    PetscFunctionBegin;
-    PetscFunctionReturn(0);
-  }
+  dataForcesAndSurcesCore data;
 
   PetscErrorCode operator()() {
     PetscFunctionBegin;
 
+    ierr = getEdgesSense(data); CHKERRQ(ierr);
+    ierr = getFacesSense(data); CHKERRQ(ierr);
 
-    PetscFunctionReturn(0);
-  }
+    ierr = getEdgesOrder(data,"DISPLACEMENT"); CHKERRQ(ierr);
+    ierr = getFacesOrder(data,"DISPLACEMENT"); CHKERRQ(ierr);
+    ierr = getOrderVolume(data,"DISPLACEMENT"); CHKERRQ(ierr);
+    ierr = getRowNodesIndices(data,"DISPLACEMENT"); CHKERRQ(ierr);
+    ierr = getEdgeRowIndices(data,"DISPLACEMENT"); CHKERRQ(ierr);
+    ierr = getFacesRowIndices(data,"DISPLACEMENT"); CHKERRQ(ierr);
+    ierr = getTetRowIndices(data,"DISPLACEMENT"); CHKERRQ(ierr);
+    ierr = getFaceNodes(data); CHKERRQ(ierr);
 
-  PetscErrorCode postProcess() {
-    PetscFunctionBegin;
+    ierr = shapeTETFunctions_H1(data,G_TET_X4,G_TET_Y4,G_TET_Z4,4); CHKERRQ(ierr);
+
+    ierr = op.opNH1NH1(data); CHKERRQ(ierr);
+
     PetscFunctionReturn(0);
   }
 
