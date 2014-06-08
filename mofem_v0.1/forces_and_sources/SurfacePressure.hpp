8cdfd3e967896f397b4d998136de6f24d8b9e2d1
Lukasz Kaczmarczyk <likask@wp.pl>
2014-06-08 21:41:42 +0100

    squash this

---
+++ mofem_v0.1/forces_and_sources/SurfacePressure.hpp
@@ -0,0 +1,37 @@
+/* Copyright (C) 2013, Lukasz Kaczmarczyk (likask AT wp.pl)
+ * --------------------------------------------------------------
+ *
+ * Test for linar elastic dynamics.
+ *
+ * This is not exactly procedure for linear elatic dynamics, since jacobian is
+ * evaluated at every time step and snes procedure is involved. However it is
+ * implemented like that, to test methodology for general nonlinear problem.
+ *
+ */
+
+/* This file is part of MoFEM.
+ * MoFEM is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * MoFEM is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with MoFEM. If not, see <http://www.gnu.org/licenses/>. */
+
+
+#ifndef __NEUMANM_FORCES_HPP
+#define __NEUMANN_FORCES_HPP
+
+#include "ForcesAndSurcesCore.hpp"
+
+namespace MoFEM {
+
+}
+
+#endif //__NEUMAN_FORCES_HPP
+
