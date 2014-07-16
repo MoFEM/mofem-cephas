ef61b3099cdeda57d0e6b4edda9784cdf729d1bb
Lukasz Kaczmarczyk <likask@wp.pl>
2014-07-16 23:30:12 +0100

    correct Zhur definitions

--- mofem_v0.1/homogenisation/ElasticFE_RVELagrange_RigidBodyTranslation.hpp
+++ mofem_v0.1/homogenisation/ElasticFE_RVELagrange_RigidBodyTranslation.hpp
@@ -17,8 +17,8 @@
  * You should have received a copy of the GNU Lesser General Public
  * License along with MoFEM. If not, see <http://www.gnu.org/licenses/>. */
 
-#ifndef __ElasticFE_RVELagrange_RigidBodyTranslation_HPP__
-#define __ElasticFE_RVELagrange_RigidBodyTranslation__
+#ifndef __ELASTICFE_RVELAGRANGE_RIGIDBODYTRANSLATION_HPP__
+#define __ELASTICFE_RVELAGRANGE_RIGIDBODYTRANSLATION_HPP__
 
 #include "ElasticFE_RVELagrange_Disp.hpp"
 
