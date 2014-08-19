7d3577937ce62ee72bf6fec06f250d290768a396
Zahur Ullah <zahur.ullah@glasgow.ac.uk>
2014-08-19 18:53:22 +0100

    Adding first order stochastic (work in progress)

--- mofem_v0.1/stochastic/K_rPoissonFEMethod.hpp
+++ mofem_v0.1/stochastic/K_rPoissonFEMethod.hpp
@@ -26,322 +26,185 @@ extern "C" {
 }
 
 namespace MoFEM {
-
-struct K_rFEMethod: public ElasticFEMethod {
-
+  
+  struct K_rPoissonFEMethod: public ElasticFEMethod {
+    
     double young;
     double pois;
-  
-    K_rFEMethod( FieldInterface& _mField,Mat &_Aij,Vec _X,Vec _F,double _young,double _pois):
-      ElasticFEMethod( _mField,_Aij,_X,_F,0,0,"DISP_r"),young(_young),pois(_pois) {
-        
+    Vec dF;
+    
+    K_rPoissonFEMethod( FieldInterface& _mField,Mat &_Aij,Vec _X,Vec _F,double _young,double _pois):
+    ElasticFEMethod( _mField,_Aij,_X,_F,0,0,"DISPLACEMENT"),young(_young),pois(_pois),dF(_F) {
+      
     }
-
+    
     PetscErrorCode calculateD(double young, double nu) {
       PetscFunctionBegin;
-
+      D.resize(6,6);
       D.clear();
-      cout<<"D = "<<D;
-      D00=(2*young*(nu^2 + 2*nu - 2))/(nu + 1)^2;
-      D01=-(young*(2*nu^2 + 4*nu - 1))/(nu + 1)^2;
-      D33=(young*(4*nu^2 + 8*nu - 5))/(2*(nu + 1)^2);
-
+      double D00,D01,D33;
+      D00=(2*young*(nu*nu + 2*nu - 2)) /pow((nu + 1),2);
+      D01=-(young*(2*nu*nu + 4*nu - 1))/pow((nu + 1),2);
+      D33=(young*(4*nu*nu + 8*nu - 5))/(2*pow((nu + 1),2));
+      
       D(0,0)=D00;  D(0,1)=D01;  D(0,2)=D01;
-      D(1,0)=nu;   D(1,1)=1-nu; D(1,2)=nu;
-      D(2,0)=nu;   D(2,1)=nu;   D(2,2)=1-nu;
-                                              D(3,3)=(1-2*nu)/2; D(3,4)=0;
-                                                                 D(4,4)=(1-2*nu)/2; 
-                                                                                    D(5,5)=(1-2*nu)/2;
-
-      
-      
-      //cerr << D_lambda << endl;
-      //cerr << D_mu << endl;
-      //cerr << D << endl;
-
+      D(1,0)=D01;  D(1,1)=D00;  D(1,2)=nu;
+      D(2,0)=D01;  D(2,1)=D01;   D(2,2)=D00;
+      D(3,3)=D33;
+      D(4,4)=D33;
+      D(5,5)=D33;
+      //      cout<<"D = "<<D;
       PetscFunctionReturn(0);
     }
-
+    
     virtual PetscErrorCode GetMatParameters(double *_young,double *_pois) {
       PetscFunctionBegin;
-
+      
       *_young = young;
       *_pois = pois;
-
-
+      
+      
       if(propeties_from_BLOCKSET_MAT_ELASTICSET) {
-	EntityHandle ent = fePtr->get_ent();
-	for(_IT_CUBITMESHSETS_BY_BCDATA_TYPE_FOR_LOOP_(mField,BLOCKSET|MAT_ELASTICSET,it)) {
-
-	  Mat_Elastic mydata;
-	  ierr = it->get_attribute_data_structure(mydata); CHKERRQ(ierr);
-
-	  Range meshsets;
-	  rval = moab.get_entities_by_type(it->meshset,MBENTITYSET,meshsets,true); CHKERR_PETSC(rval);
-	  meshsets.insert(it->meshset);
-	  for(Range::iterator mit = meshsets.begin();mit != meshsets.end(); mit++) {
-	    if( moab.contains_entities(*mit,&ent,1) ) {
-	      *_young = mydata.data.Young;
-	      *_pois  = mydata.data.Poisson;
-	    PetscFunctionReturn(0);  
-	  }
-	}
-
+        EntityHandle ent = fePtr->get_ent();
+        for(_IT_CUBITMESHSETS_BY_BCDATA_TYPE_FOR_LOOP_(mField,BLOCKSET|MAT_ELASTICSET,it)) {
+          
+          Mat_Elastic mydata;
+          ierr = it->get_attribute_data_structure(mydata); CHKERRQ(ierr);
+          
+          Range meshsets;
+          rval = moab.get_entities_by_type(it->meshset,MBENTITYSET,meshsets,true); CHKERR_PETSC(rval);
+          meshsets.insert(it->meshset);
+          for(Range::iterator mit = meshsets.begin();mit != meshsets.end(); mit++) {
+            if( moab.contains_entities(*mit,&ent,1) ) {
+              *_young = mydata.data.Young;
+              *_pois  = mydata.data.Poisson;
+              PetscFunctionReturn(0);
+            }
+          }
+          
+        }
+        
+        SETERRQ(PETSC_COMM_SELF,1,
+                "Element is not in elestic block, however you run linear elastic analysis with that element\n"
+                "top tip: check if you update block sets after mesh refinments or interface insertion");
+        
       }
-
-      SETERRQ(PETSC_COMM_SELF,1,
-	"Element is not in elestic block, however you run linear elastic analysis with that element\n"
-	"top tip: check if you update block sets after mesh refinments or interface insertion");
-
-      }
-
+      
       PetscFunctionReturn(0);
     }
-
-  
-
-
+    
+    
     ublas::matrix<ublas::matrix<FieldData> > K;
     ublas::matrix<FieldData> BD;
     virtual PetscErrorCode Stiffness() {
       PetscFunctionBegin;
-
+      
       double _young,_pois;
       ierr = GetMatParameters(&_young,&_pois); CHKERRQ(ierr);
-//      ierr = calculateD(_young,_pois); CHKERRQ(ierr);
-
-//      K.resize(row_mat,col_mat);
-//      int g_dim = g_NTET.size()/4;
-//      for(int rr = 0;rr<row_mat;rr++) {
-//	if(RowGlob[rr].size()==0) continue;
-//	for(int gg = 0;gg<g_dim;gg++) {
-//	  ublas::matrix<FieldData> &row_Mat = (rowBMatrices[rr])[gg];
-//	  double w = V*G_TET_W[gg];
-//	  if(detH.size()>0) {
-//	    w *= detH[gg];
-//	  }
-//	  BD.resize(6,row_Mat.size2());
-//	  //ublas::noalias(BD) = prod( w*D,row_Mat );
-//	  cblas_dsymm(CblasRowMajor,CblasLeft,CblasUpper,
-//	    BD.size1(),BD.size2(),
-//	    w,&*D.data().begin(),D.size2(),
-//	    &*row_Mat.data().begin(),row_Mat.size2(),
-//	    0.,&*BD.data().begin(),BD.size2());
-//	  for(int cc = rr;cc<col_mat;cc++) {
-//	    if(ColGlob[cc].size()==0) continue;
-//	    ublas::matrix<FieldData> &col_Mat = (colBMatrices[cc])[gg];
-//	    if(gg == 0) {
-//	      K(rr,cc).resize(BD.size2(),col_Mat.size2());
-//	      //ublas::noalias(K(rr,cc)) = prod(trans(BD) , col_Mat ); // int BT*D*B
-//	      cblas_dgemm(CblasRowMajor,CblasTrans,CblasNoTrans,
-//		BD.size2(),col_Mat.size2(),BD.size1(),
-//		1.,&*BD.data().begin(),BD.size2(),
-//		&*col_Mat.data().begin(),col_Mat.size2(),
-//		0.,&*K(rr,cc).data().begin(),K(rr,cc).size2());
-//	    } else {
-//	      //ublas::noalias(K(rr,cc)) += prod(trans(BTD) , col_Mat ); // int BT*D*B
-//	      cblas_dgemm(CblasRowMajor,CblasTrans,CblasNoTrans,
-//		BD.size2(),col_Mat.size2(),BD.size1(),
-//		1.,&*BD.data().begin(),BD.size2(),
-//		&*col_Mat.data().begin(),col_Mat.size2(),
-//		1.,&*K(rr,cc).data().begin(),K(rr,cc).size2());
-//	    }
-//	  }
-//	}
-//      }
+      ierr = calculateD(_young,_pois); CHKERRQ(ierr);
+      //      cout<<" D "<<D<<endl;
+      //      cout<<" row_mat "<<row_mat<<endl;
+      //      cout<<" col_mat "<<col_mat<<endl;
+      K.resize(row_mat,col_mat);
+      int g_dim = g_NTET.size()/4;
+      //      cout<<" g_dim "<<g_dim<<endl;
+      for(int rr = 0;rr<row_mat;rr++) {
+        if(RowGlob[rr].size()==0) continue;
+        for(int gg = 0;gg<g_dim;gg++) {
+          ublas::matrix<FieldData> &row_Mat = (rowBMatrices[rr])[gg];
+          double w = V*G_TET_W[gg];
+          if(detH.size()>0) {
+            w *= detH[gg];
+          }
+          BD.resize(6,row_Mat.size2());
+          //ublas::noalias(BD) = prod( w*D,row_Mat );
+          cblas_dsymm(CblasRowMajor,CblasLeft,CblasUpper,
+                      BD.size1(),BD.size2(),
+                      w,&*D.data().begin(),D.size2(),
+                      &*row_Mat.data().begin(),row_Mat.size2(),
+                      0.,&*BD.data().begin(),BD.size2());
+          for(int cc = rr;cc<col_mat;cc++) {
+            if(ColGlob[cc].size()==0) continue;
+            ublas::matrix<FieldData> &col_Mat = (colBMatrices[cc])[gg];
+            if(gg == 0) {
+              K(rr,cc).resize(BD.size2(),col_Mat.size2());
+              //ublas::noalias(K(rr,cc)) = prod(trans(BD) , col_Mat ); // int BT*D*B
+              cblas_dgemm(CblasRowMajor,CblasTrans,CblasNoTrans,
+                          BD.size2(),col_Mat.size2(),BD.size1(),
+                          1.,&*BD.data().begin(),BD.size2(),
+                          &*col_Mat.data().begin(),col_Mat.size2(),
+                          0.,&*K(rr,cc).data().begin(),K(rr,cc).size2());
+            } else {
+              //ublas::noalias(K(rr,cc)) += prod(trans(BTD) , col_Mat ); // int BT*D*B
+              cblas_dgemm(CblasRowMajor,CblasTrans,CblasNoTrans,
+                          BD.size2(),col_Mat.size2(),BD.size1(),
+                          1.,&*BD.data().begin(),BD.size2(),
+                          &*col_Mat.data().begin(),col_Mat.size2(),
+                          1.,&*K(rr,cc).data().begin(),K(rr,cc).size2());
+            }
+          }
+        }
+      }
       PetscFunctionReturn(0);
     }
-
-    virtual PetscErrorCode Lhs() {
+    
+    
+    vector<ublas::vector<FieldData> > f_re;
+    virtual PetscErrorCode Rhs() {
       PetscFunctionBegin;
+      //      cout<<" Rhs() "<<endl;
       ierr = Stiffness(); CHKERRQ(ierr);
-//      for(int rr = 0;rr<row_mat;rr++) {
-//	if(RowGlob[rr].size()==0) continue;
-//	for(int cc = rr;cc<col_mat;cc++) {
-//	  if(ColGlob[cc].size()==0) continue;
-//	  if(RowGlob[rr].size()!=K(rr,cc).size1()) SETERRQ(PETSC_COMM_SELF,1,"data inconsistency");
-//	  if(ColGlob[cc].size()!=K(rr,cc).size2()) SETERRQ(PETSC_COMM_SELF,1,"data inconsistency");
-//	  ierr = MatSetValues(*snes_B,RowGlob[rr].size(),&(RowGlob[rr])[0],ColGlob[cc].size(),&(ColGlob[cc])[0],&(K(rr,cc).data())[0],ADD_VALUES); CHKERRQ(ierr);
-//	  if(rr!=cc) {
-//	    K(cc,rr) = trans(K(rr,cc));
-//	    ierr = MatSetValues(*snes_B,ColGlob[cc].size(),&(ColGlob[cc])[0],RowGlob[rr].size(),&(RowGlob[rr])[0],&(K(cc,rr).data())[0],ADD_VALUES); CHKERRQ(ierr);
-//	  }
-//	}
-//      }
+      
+      vector<ublas::vector<FieldData> > D_elm;
+      //       cout<<"col_mat = "<< col_mat << endl;
+      
+      D_elm.resize(col_mat);
+      
+      int col_mat1 = 0;  //only nodes (1st order)
+      ierr = GetDataVector("DISPLACEMENT",D_elm[col_mat1]); CHKERRQ(ierr);
+      //       cout<<"D_elm[col_mat] = "<< D_elm[col_mat1] << endl;
+      
+      f_re.resize(row_mat);
+      for(int rr = 0;rr<row_mat;rr++) {
+        if(RowGlob[rr].size()==0) continue;
+        for(int cc = rr;cc<col_mat;cc++) {
+          if(ColGlob[cc].size()==0) continue;
+          //                  cout<<""<<prod( K(rr,cc), D_elm[cc])<<endl;
+          f_re[rr] = -prod( K(rr,cc), D_elm[cc] );
+        }
+      }
+      for(int rr = 0;rr<row_mat;rr++) {
+        if(RowGlob[rr].size()==0) continue;
+        if(RowGlob[rr].size()!=f_re[rr].size()) SETERRQ(PETSC_COMM_SELF,1,"data inconsistency");
+        ierr = VecSetValues(dF,RowGlob[rr].size(),&(RowGlob[rr])[0],&(f_re[rr].data()[0]),ADD_VALUES); CHKERRQ(ierr);
+      }
       PetscFunctionReturn(0);
     }
-
-//    virtual PetscErrorCode Rhs() {
-//      PetscFunctionBegin;
-//      ierr = Fint(snes_f); CHKERRQ(ierr);
-//      PetscFunctionReturn(0);
-//    }
-
-//    vector<ublas::vector<FieldData> > f_int;
-//    virtual PetscErrorCode Fint() {
-//      PetscFunctionBegin;
-//
-//      try {
-//
-//      //Higher order approximation of geometry
-//      ierr = GetHierarchicalGeometryApproximation(invH,detH); CHKERRQ(ierr);
-//
-//      double _lambda,_mu;
-//      ierr = GetMatParameters(&_lambda,&_mu); CHKERRQ(ierr);
-//      ierr = calculateD(_lambda,_mu); CHKERRQ(ierr);
-//
-//      //Gradient at Gauss points; 
-//      vector< ublas::matrix< FieldData > > GradU_at_GaussPt;
-//      ierr = GetGaussDiffDataVector("DISP_r",GradU_at_GaussPt); CHKERRQ(ierr);
-//      unsigned int g_dim = g_NTET.size()/4;
-//      assert(GradU_at_GaussPt.size() == g_dim);
-//      NOT_USED(g_dim);
-//      vector< ublas::matrix< FieldData > >::iterator viit = GradU_at_GaussPt.begin();
-//      int gg = 0;
-//      for(;viit!=GradU_at_GaussPt.end();viit++,gg++) {
-//	try {
-//	  ublas::matrix< FieldData > GradU = *viit;
-//	  if(!invH.empty()) {
-//	    //GradU = 
-//	      //[ dU/dChi1 dU/dChi2 dU/dChi3 ]
-//	      //[ dV/dChi1 dV/dChi2 dU/dChi3 ]
-//	      //[ dW/dChi1 dW/dChi2 dW/dChi3 ]
-//	    //H = 
-//	      //[ dX1/dChi1 dX1/dChi2 dX1/dChi3 ]
-//	      //[ dX2/dChi1 dX2/dChi2 dX2/dChi3 ]
-//	      //[ dX3/dChi1 dX3/dChi2 dX3/dChi3 ]    
-//	    //invH = 
-//	      //[ dChi1/dX1 dChi1/dX2 dChi1/dX3 ]
-//	      //[ dChi2/dX1 dChi2/dX2 dChi2/dX3 ]
-//	      //[ dChi3/dX1 dChi3/dX2 dChi3/dX3 ]
-//	    //GradU = 
-//	      //[ dU/dX1 dU/dX2 dU/dX3 ]
-//	      //[ dV/dX1 dV/dX2 dV/dX3 ] = GradU * invH
-//	      //[ dW/dX1 dW/dX2 dW/dX3 ] 
-//	    GradU = prod( GradU, invH[gg] ); 
-//	  }
-//	  ublas::matrix< FieldData > Strain = 0.5*( GradU + trans(GradU) );
-//	  ublas::vector< FieldData > VoightStrain(6);
-//	  VoightStrain[0] = Strain(0,0);
-//	  VoightStrain[1] = Strain(1,1);
-//	  VoightStrain[2] = Strain(2,2);
-//	  VoightStrain[3] = 2*Strain(0,1);
-//	  VoightStrain[4] = 2*Strain(1,2);
-//	  VoightStrain[5] = 2*Strain(2,0);
-//	  double w = V*G_TET_W[gg];
-//	  ublas::vector<FieldData> VoightStress = prod(w*D,VoightStrain);
-//	  //BT * VoigtStress
-//	  f_int.resize(row_mat);
-//	  for(int rr = 0;rr<row_mat;rr++) {
-//	    if(RowGlob[rr].size()==0) continue;
-//	    ublas::matrix<FieldData> &B = (rowBMatrices[rr])[gg];
-//	    if(gg == 0) {
-//	      f_int[rr] = prod( trans(B), VoightStress );
-//	    } else {
-//	      f_int[rr] += prod( trans(B), VoightStress );
-//	    }
-//	  }
-//	} catch (const std::exception& ex) {
-//	  ostringstream ss;
-//	  ss << "thorw in method: " << ex.what() << " at line " << __LINE__ << " in file " << __FILE__ << endl;
-//	  SETERRQ(PETSC_COMM_SELF,1,ss.str().c_str());
-//	} 
-//      }
-//
-//      } catch (const std::exception& ex) {
-//	ostringstream ss;
-//	ss << "thorw in method: " << ex.what() << " at line " << __LINE__ << " in file " << __FILE__ << endl;
-//	SETERRQ(PETSC_COMM_SELF,1,ss.str().c_str());
-//      }
-//
-//      PetscFunctionReturn(0);
-//    }
-
-//    virtual PetscErrorCode Fint(Vec F_int) {
-//      PetscFunctionBegin;
-//      try {
-//	ierr = Fint(); CHKERRQ(ierr);
-//	for(int rr = 0;rr<row_mat;rr++) {
-//	  if(RowGlob[rr].size()==0) continue;
-//	  if(RowGlob[rr].size()!=f_int[rr].size()) SETERRQ(PETSC_COMM_SELF,1,"data inconsistency");
-//	  ierr = VecSetValues(F_int,RowGlob[rr].size(),&(RowGlob[rr])[0],&(f_int[rr].data()[0]),ADD_VALUES); CHKERRQ(ierr);
-//	}
-//      } catch (const std::exception& ex) {
-//	ostringstream ss;
-//	ss << "thorw in method: " << ex.what() << " at line " << __LINE__ << " in file " << __FILE__ << endl;
-//	SETERRQ(PETSC_COMM_SELF,1,ss.str().c_str());
-//      }
-//      PetscFunctionReturn(0);
-//    }
-
-
-//    virtual PetscErrorCode RhsAndLhs() {
-//      PetscFunctionBegin;
-//
-//      ierr = Rhs(); CHKERRQ(ierr);
-//      ierr = Lhs(); CHKERRQ(ierr);
-//
-//      PetscFunctionReturn(0);
-//    }
-
-//    ublas::matrix<double> gaussPts;
-//    virtual PetscErrorCode Get_g_NTET() {
-//      PetscFunctionBegin;
-//
-//      int order = 1;
-//      for(_IT_GET_FEDATA_BY_NAME_DOFS_FOR_LOOP_(this,"DISP_r",dof)) {
-//	order = max(order,dof->get_max_order());
-//      }
-//
-//      int rule = max(0,order-1);
-//      if( 2*rule + 1 < 2*(order-1) ) {
-//	SETERRQ2(PETSC_COMM_SELF,1,"wrong rule %d %d",order,rule);
-//      }
-//      int nb_gauss_pts = gm_rule_size(rule,3);
-//      if(gaussPts.size2() == (unsigned int)nb_gauss_pts) {
-//	PetscFunctionReturn(0);
-//      }
-//      gaussPts.resize(4,nb_gauss_pts);
-//      ierr = Grundmann_Moeller_integration_points_3D_TET(
-//	rule,&gaussPts(0,0),&gaussPts(1,0),&gaussPts(2,0),&gaussPts(3,0)); CHKERRQ(ierr);
-//
-//      g_NTET.resize(4*nb_gauss_pts);
-//      ierr = ShapeMBTET(&g_NTET[0],&gaussPts(0,0),&gaussPts(1,0),&gaussPts(2,0),nb_gauss_pts); CHKERRQ(ierr);
-//      G_TET_W = &gaussPts(3,0);
-//
-//      PetscFunctionReturn(0);
-//    }
-
-//    PetscErrorCode preProcess() {
-//      PetscFunctionBegin;
-//
-//      g_NTET.resize(4*45);
-//      ierr = ShapeMBTET(&g_NTET[0],G_TET_X45,G_TET_Y45,G_TET_Z45,45); CHKERRQ(ierr);
-//      G_TET_W = G_TET_W45;
-//			
-//      // See FEAP - - A Finite Element Analysis Program
-//      D_lambda.resize(6,6);
-//      D_lambda.clear();
-//      for(int rr = 0;rr<3;rr++) {
-//	for(int cc = 0;cc<3;cc++) {
-//	  D_lambda(rr,cc) = 1;
-//	}
-//      }
-//      D_mu.resize(6,6);
-//      D_mu.clear();
-//      for(int rr = 0;rr<6;rr++) {
-//	D_mu(rr,rr) = rr<3 ? 2 : 1;
-//      }
-//
-//      PetscFunctionReturn(0);
-//    }
-
-
-
-
-};
+    
+    
+    PetscErrorCode postProcess() {
+      PetscFunctionBegin;
+      PetscFunctionReturn(0);
+    }
 
     
+     PetscErrorCode operator()() {
+      PetscFunctionBegin;
+      ierr = OpStudentStart_TET(g_NTET); CHKERRQ(ierr);
+//      cout<<"Hi from K_rPoissonFEMethod "<<endl;
+      //      cout<<"fieldName "<<fieldName<<endl;
+      ierr = GetMatrices(); CHKERRQ(ierr);
+      
+      ierr = Rhs(); CHKERRQ(ierr);
+      
+      ierr = OpStudentEnd(); CHKERRQ(ierr);
+      PetscFunctionReturn(0);
+    }
+    
+    
+  };
+  
+  
 }
 
 #endif //__K_RFEMETHOD_HPP__
