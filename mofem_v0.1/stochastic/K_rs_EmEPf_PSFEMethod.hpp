4f468d4412db9f638fdd2dad216521640afd1d5d
zhou <xiaoyi.zhou@ncl.ac.uk>
2014-09-02 16:21:46 +0100

    Adding stochastic RVE homogenization for cube with inclusion
    The implementation of stochastic homogenization method for RVE of two-
    phase composite comprising isotropic matrix and isotropic
    inclusion/fibre has completed by combining previous codes. The
    verification study showed that the MoFEM simulation results matched well
    with those from theoretical formula.

---
+++ mofem_v0.1/stochastic/K_rs_EmEPf_PSFEMethod.hpp
@@ -0,0 +1,80 @@
+/* Copyright (C) 2014, Xiao-Yi Zhou (xiaoyi.zhou@ncl.ac.uk)
+ * --------------------------------------------------------------
+ * This routine calculates the second-order partial derivative of stiffness 
+ * matrix, K_rs, with respect to Young's modulus of matrix and Young's modulus
+ * or Poisson's ratio of inclusion/fibre for two-phase material for the
+ * stochastic finite element.
+ */
+
+/* This file is part of MoFEM.
+ * MoFEM is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * MoFEM is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with MoFEM. If not, see <http://www.gnu.org/licenses/>. */
+
+#ifndef __K_RS_EMEPF_PSFEMETHOD_HPP__
+#define __K_RS_EMEPF_PSFEMETHOD_HPP__
+
+#include <boost/numeric/ublas/symmetric.hpp>
+#include "K_rsPoissonFEMethod.hpp"
+
+extern "C" {
+#include <gm_rule.h>
+}
+
+namespace MoFEM {
+
+struct K_rs_EmEPf_PSFEMethod: public K_rs_PSFEMethod {
+
+    K_rs_EmEPf_PSFEMethod( FieldInterface& _mField,Mat &_Aij,Vec _X,Vec _F,double _young,double _pois,const string& _second_field):
+      K_rs_PSFEMethod( _mField,_Aij,_X,_F,_young,_pois,_second_field){
+        
+    }
+    //F: calculate the first-order derivative of constitutive matrix  
+    //   with respect to Young's modulus
+    virtual PetscErrorCode calculateD_r(double young, double nu) {
+      PetscFunctionBegin;
+      D.resize(6,6);
+      D.clear();
+
+      double D_r00,D_r01,D_r33;
+      D_r00 = (1-nu)/((1+nu)*(1-2*nu));
+      D_r01 = (nu)/((1+nu)*(1-2*nu));
+      D_r33 = (1-2*nu)/(2*(1+nu)*(1-2*nu));
+      
+      D(0,0)=D_r00;  D(0,1)=D_r01;   D(0,2)=D_r01;
+      D(1,0)=D_r01;  D(1,1)=D_r00;   D(1,2)=D_r01;
+      D(2,0)=D_r01;  D(2,1)=D_r01;   D(2,2)=D_r00;
+      D(3,3)=D_r33;
+      D(4,4)=D_r33;
+      D(5,5)=D_r33;
+      // cout<<"D_r = "<<D;
+      PetscFunctionReturn(0);
+    }
+
+    //F: calculate the second-order derivative of constitutive matrix 
+    //   with respect to Young's modulus of matrix and Young's modulus of fibre
+    virtual PetscErrorCode calculateD_rs(double young, double nu) {
+      PetscFunctionBegin;
+      D.resize(6,6);
+      D.clear(); // Initiate D_rs to zero
+
+      // cout<<"D_rs = "<<D;
+
+      PetscFunctionReturn(0);
+    }
+
+};
+
+    
+}
+
+#endif //__K_RS_EMEPF_PSFEMETHOD_HPP__
