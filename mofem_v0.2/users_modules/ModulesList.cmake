1f3461867bd3345abde6c932000d36d0cf11cda6
Lukasz Kaczmarczyk <likask@wp.pl>
2014-11-26 21:07:31 +0000

    clean directory organisation

--- mofem_v0.2/users_modules/ModulesList.cmake
+++ mofem_v0.2/users_modules/ModulesList.cmake
@@ -23,18 +23,12 @@ add_library(complex_for_lazy_obsolete
   ${UM_SOURCE_DIR}/obsolete/c_impl/complex_for_lazy.c
 )
 
-include_directories("${UM_SOURCE_DIR}/analytical_dirihlet_boundary_conditions/src")
 add_subdirectory(analytical_dirihlet_boundary_conditions)
-
-include_directories("${UM_SOURCE_DIR}/convective_mass_element/src")
 add_subdirectory(convective_mass_element)
-
-add_subdirectory(atom_tests)
-
-include_directories("${UM_SOURCE_DIR}/elasticity")
 add_subdirectory(elasticity)
 add_subdirectory(nonlinear_elasticity)
 add_subdirectory(fracture_mechanics)
 add_subdirectory(homogenisation)
 
+add_subdirectory(atom_tests)
 
