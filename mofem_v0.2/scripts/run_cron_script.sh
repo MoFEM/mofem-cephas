92017921df7533aa6b788a68deb7ac052c99d35d
Lukasz Kaczmarczyk <likask@wp.pl>
2014-12-02 12:11:33 +0000

    improving cdash testing scripts

--- mofem_v0.2/scripts/run_cron_script.sh
+++ mofem_v0.2/scripts/run_cron_script.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
 
-if [ -e /home/lukasz/tests_cephas.lock ]
+if [ -e $HOME/tests_cephas.lock ]
 then
   echo "lock"
 else
   CWD=`pwd`
-  CTEST_SCRIPTS_FILE_PATH=/home/lukasz/mofem-cephas/mofem_v0.2/cmake
-  CTEST_USER_MODULES_PATH=/home/lukasz/tmp/cephas_users_modules/users_modules/
+  CTEST_SCRIPTS_FILE_PATH=$HOME/mofem-cephas/mofem_v0.2/cmake
+  CTEST_USER_MODULES_PATH=$HOME/tmp/cephas_users_modules/users_modules/
   CTSET_SCRIPT=CTestScript_rdb-srv1.cmake
-  BUILD_DIR=/home/lukasz/tmp/cephas/build
-  touch /home/lukasz/tests_cephas.lock
+  BUILD_DIR=$HOME/tmp/cephas/build
+  touch $HOME/tests_cephas.lock
   cd $CTEST_SCRIPTS_FILE_PATH
-  /usr/bin/ctest -VV --http1.0 -S $CTSET_SCRIPT >> /home/lukasz/tests_cephas.log 2>&1
-  if [ -e /home/lukasz/tmp/cephas/source/has_bin_build ]; then
-    rm /home/lukasz/tmp/cephas/source/has_bin_build
+  /usr/bin/ctest -VV --http1.0 -S $CTSET_SCRIPT >> $HOME/tests_cephas.log 2>&1
+  if [ -e $HOME/tmp/cephas/source/has_bin_build ]; then
+    rm $HOME/tmp/cephas/source/has_bin_build
     cd $BUILD_DIR
     /usr/bin/make install
     chmod u+x $CTEST_USER_MODULES_PATH/scripts/run_cron_script.sh
     $CTEST_USER_MODULES_PATH/scripts/run_cron_script.sh
   fi  
   cd $CWD  
-  rm -v /home/lukasz/tests_cephas.lock
+  rm -v $HOME/tests_cephas.lock
 fi
