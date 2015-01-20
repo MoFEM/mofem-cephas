6efb92534d0909a7094581426adb963ec549e78b
Lukasz Kaczmarczyk <likask@wp.pl>
2015-01-20 22:04:55 +0000

    fix compilation issue

--- mofem_v0.2/users_modules/cmake/CTestScript_rdb-srv1.cmake
+++ mofem_v0.2/users_modules/cmake/CTestScript_rdb-srv1.cmake
@@ -1,4 +1,4 @@
-set(CTEST_BUILD_OPTIONS "-DSTAND_ALLONE_USERS_MODULES=ON -DCMAKE_CXX_FLAGS=\"-I/opt/local_boost_1_54_0/include -L/opt/local_boost_1_54_0/lib\" /home/lukasz/tmp/cephas_users_modules")
+set(CTEST_BUILD_OPTIONS "-DSTAND_ALLONE_USERS_MODULES=ON -DCMAKE_CXX_FLAGS=-I/opt/local_boost_1_54_0/include -DCMAKE_EXE_LINKER_FLAGS=-L/opt/local_boost_1_54_0/lib /home/lukasz/tmp/cephas_users_modules/users_modules")
 
 set(CTEST_SITE "rdb-srv1")
 set(CTEST_BUILD_NAME "Linux-mpicxx")
