21666a487c6ad0b22f5f43188d21e8b88d286782
Zahur Ullah <zahur.ullah@glasgow.ac.uk>
2015-01-20 11:01:39 +0000

    quick fix in implementation BCs form blocksets. change in moisture-transport element, to work it with conductivity matrix.

--- mofem_v0.2/finite_element_library/impl/DirichletBC.cpp
+++ mofem_v0.2/finite_element_library/impl/DirichletBC.cpp
@@ -353,14 +353,15 @@ PetscErrorCode FixBcAtEntities::preProcess() {
   PetscFunctionReturn(0);
 }
 
-  
+
+
 PetscErrorCode DirichletBCFromBlockSetFEMethodPreAndPostProc::iNitalize() {
   PetscFunctionBegin;
   if(map_zero_rows.empty()) {
     ParallelComm* pcomm = ParallelComm::get_pcomm(&mField.get_moab(),MYPCOMM_INDEX);
     
     for(_IT_CUBITMESHSETS_BY_SET_TYPE_FOR_LOOP_(mField,BLOCKSET,it)) {
-      if(it->get_Cubit_name().compare(0,14,blocksetName) == 0) {
+      if(it->get_Cubit_name().compare(0,blocksetName.length(),blocksetName) == 0) {
         vector<double> mydata;
         ierr = it->get_Cubit_attributes(mydata); CHKERRQ(ierr);
         for(int dim = 0;dim<3;dim++) {
