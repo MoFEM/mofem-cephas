e3057da0acfb89580eedac9a9f833a9bef4f8610
Lukasz Kaczmarczyk <likask@wp.pl>
2015-03-11 23:48:18 +0000

    documentation and data for Zamira

--- mofem_v0.2/users_modules/nonlinear_elasticity/doc/user_doc.h
+++ mofem_v0.2/users_modules/nonlinear_elasticity/doc/user_doc.h
@@ -1,15 +1,11 @@
-/*! \page elastic_dynamics Elastic dynamic (Usage example)
+/*! \page elastic_dynamics Nonlinear Elasticity (Usage example)
 
-\section dynamic_elastic_bar Prismatic 3d bar
+\section dynamic_elastic_bar Dynamic analysis of prismatic 3d bar
 
 \subsection dynamic_elastic_bar_input_file Input files
 
 All files for this example can be found in directory
 \em users_modules/nonlinear_elasticity/examples/prismatic_bar
-
-  - mesh input file, .cub, .h5m. .vtk and more.
-  - time data history, f.e. history_data.in
-
 
 \subsubsection journal Journal file
 
@@ -20,23 +16,27 @@ set duplicate block elements on
 
 brick x 1 y 1 z 10
 
-create Displacement  on surface 1 dof 1 dof 2 dof 3 fix 0
-create pressure  on surface 12 magnitude 2
+create Displacement  on surface 1 dof 3 fix 0
+create Displacement  on vertex 1  dof 3 dof 2 fix 0 
+create Displacement  on vertex 3  dof 3 dof 1 fix 0 
+create Displacement  on vertex 4  dof 1 dof 2 dof 3 fix 0
+
+create pressure  on surface 2 magnitude 2
 #create force  on surface 2 force value 1 direction   y 
 
 block 1 volume all 
 block 1 name 'MAT_ELASTIC'
 block 1 attribute count 2
-block 1 attribute index 1 1 #young modulus
-block 1 attribute index 2 0 #poisson ratio
+block 1 attribute index 1 1e2 	#young modulus
+block 1 attribute index 2 0.25 #poisson ratio
 
 block 2 volume 1
 block 2 name "BODY_FORCES")
 block 2 attribute count 4
-block 2 attribute index 1 0.01	#material density
-block 2 attribute index 2 0.	#constant acceleration in x-direction
-block 2 attribute index 3 0.	#constant acceleration in y-direction
-block 2 attribute index 4 0.	#constant acceleration in z-direction
+block 2 attribute index 1 0.01		#material density
+block 2 attribute index 2 0.		#constant acceleration in x-direction
+block 2 attribute index 3 0.		#constant acceleration in y-direction
+block 2 attribute index 4 0.		#constant acceleration in z-direction
 
 volume all scheme Tetmesh
 volume all size auto factor 9
@@ -47,41 +47,62 @@ mesh volume all
 
 Time data history file (rod_history.in),
 \code
-0.0 	0.0
-0.1 	1.0
-0.1001	0.0
-100000	0.0
+0.0             0.0
+1.0             1.0
+1.1001          0.0
+100000          0.0
 \endcode
 
 where first column represents time and second column represent force multiplier.
 
 \subsection execution Executing code
 
-If bar material model is NeoHookean (note that uisng key word KIRCHOFF a St Venant Kirchoff material is used)
+If bar material model is NEOHOOKEAN (note that using key word KIRCHOFF a St Venant Kirchoff material is used)
 \code
-mpirun -np 4 ../../nonlinear_dynamics \
+mpirun -np 4  ../../nonlinear_dynamics \
   -my_file rod.cub \
-  -ksp_type fgmres -pc_type lu -pc_factor_mat_solver_package superlu_dist -ksp_atol 1e-10 -ksp_rtol 1e-10 \
-  -snes_monitor -snes_type newtonls -snes_linesearch_type basic -snes_max_it 100 -snes_atol 1e-7 -snes_rtol 1e-7 \
-  -ts_monitor -ts_type alpha -ts_dt 0.001 -ts_final_time 0.9 \
-  -my_output_prt -10 -my_max_post_proc_ref_level 0 \
-  -my_disp_order 2 \
-  -default_material NEOHOOKEAN -my_time_data_file rod_history.in 2>&1 | tee log
+  -ksp_type fgmres -pc_type lu -pc_factor_mat_solver_package superlu_dist -ksp_atol 1e-12 -ksp_rtol 1e-12 \
+  -snes_monitor -snes_type newtonls -snes_linesearch_type basic -snes_max_it 10 -snes_atol 1e-8 -snes_rtol 1e-8 \
+  -ts_monitor -ts_type alpha -ts_dt 0.01 -ts_final_time 10 -ts_max_snes_failures -1  \
+  -my_output_prt -1 -my_max_post_proc_ref_level 0  \
+  -my_disp_order 2 -my_time_data_file rod_history.in -default_material NEOHOOKEAN 2>&1 | tee log
 \endcode
 
 If bar is made form Hooke material subjected to small strains and displacements,
 \code
-mpirun -np 4 ../../nonlinear_dynamics \
+mpirun -np 4  ../../nonlinear_dynamics \
   -my_file rod.cub \
-  -ksp_type fgmres -pc_type lu -pc_factor_mat_solver_package superlu_dist -ksp_atol 1e-10 -ksp_rtol 1e-10 \
-  -snes_monitor -snes_type newtonls -snes_linesearch_type basic -snes_max_it 100 -snes_atol 1e-7 -snes_rtol 1e-7 \
-  -ts_monitor -ts_type alpha -ts_dt 0.001 -ts_final_time 0.9 \
-  -my_output_prt -10 -my_max_post_proc_ref_level 0 \
-  -my_disp_order 2 \
-  -snes_lag_jacobian -2 \
-  -default_material HOOKE -is_linear -my_time_data_file rod_history.in 2>&1 | tee log
+  -ksp_type fgmres -pc_type lu -pc_factor_mat_solver_package superlu_dist -ksp_atol 1e-12 -ksp_rtol 1e-12 \
+  -snes_monitor -snes_type newtonls -snes_linesearch_type basic -snes_max_it 10 -snes_atol 1e-8 -snes_rtol 1e-8 \
+  -ts_monitor -ts_type alpha -ts_dt 0.01 -ts_final_time 10 -ts_max_snes_failures -1  \
+  -my_output_prt -1 -my_max_post_proc_ref_level 0  \
+  -my_disp_order 2 -my_time_data_file rod_history.in -default_material HOOKE -is_linear -snes_lag_jacobian -2 2>&1 | tee log
 \endcode
 
+Notes:
+  - Approximation order can be set using \em -my_disp_order. 
+  - Time integration scheme is controlled by \em -ts_type, look to PETSc manual for more details. 
+  - If linear analysis i.e. \em -is_linear then Jacobian is calculated only once for 1st step by setting \em -snes_lag_jacobian -2
+  - Resolution of post-processing mesh is set by \em -my_max_post_proc_ref_level 
 
+\subsection gnuplot Generating data for plots
+
+Each of above command executing dynamic analysis generate log file. Filtering lines starting from key word \em Energy, data file for plots of elatic energy, potential energy and total energy in time are generated. 
+
+For example data for making plots in excel, gnuplot and any other plotting software can be generated by flowing line command. 
+\code
+awk '/Energy/ { print $3,$6,$9,$11 }' log | tee plot_data
+\endcode
+
+\subsection paraview VTK files for paraview
+
+Running dynamic analysis out_values_1.h5m, out_values_2.h5m, ... are generated
+for each time step, Post-processing mesh is stored in native MoAB data format
+using standard h5m.
+
+VTK files can be generated using script located in \em users_modules/nonlinear_elasticity/do_vtk.sh. For example
+\code
+../../do_vtk.sh out_values_*h5m
+\endcode
 
 */
