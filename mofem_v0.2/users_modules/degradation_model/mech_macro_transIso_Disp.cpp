a5120817382429ccf47d87bc4cb7c8dc4233c41f
Zahur Ullah <zahur.ullah@glasgow.ac.uk>
2015-04-24 17:09:26 +0100

    reorganising meshes in degradation directory before pulling from CDashTesting

--- mofem_v0.2/users_modules/degradation_model/mech_macro_transIso_Disp.cpp
+++ mofem_v0.2/users_modules/degradation_model/mech_macro_transIso_Disp.cpp
@@ -452,6 +452,7 @@ int main(int argc, char *argv[]) {
     cout<<"============== Wt_SERIES exists =============== "<<endl;
     for(_IT_SERIES_STEPS_BY_NAME_FOR_LOOP_(recorder_ptr,"Wt_SERIES",sit)) {
       if(count%10==0){
+        if(count==0){
         PetscPrintf(PETSC_COMM_WORLD,"Process step %d\n",sit->get_step_number());
         ierr = recorder_ptr->load_series_data("Wt_SERIES",sit->get_step_number()); CHKERRQ(ierr);
 
@@ -540,6 +541,7 @@ int main(int argc, char *argv[]) {
         PetscPrintf(PETSC_COMM_WORLD,"End of step %d\n",sit->get_step_number());
 //        string wait;
 //        cin>>wait;
+        }
       }
       count++;
       
