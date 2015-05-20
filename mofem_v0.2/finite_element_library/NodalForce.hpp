68c082f1e0af198c70b728d166692d4f09f02b59
Lukasz Kaczmarczyk <likask@wp.pl>
2015-05-20 22:58:20 +0100

    Removing code form header files to libraries. Speeding up compilation.

--- mofem_v0.2/finite_element_library/NodalForce.hpp
+++ mofem_v0.2/finite_element_library/NodalForce.hpp
@@ -26,7 +26,7 @@ struct NodalForce {
   NodalForce(FieldInterface &m_field): mField(m_field),fe(m_field) {}
 
   struct MyFE: public VertexElementForcesAndSourcesCore {
-    MyFE(FieldInterface &_m_field): VertexElementForcesAndSourcesCore(_m_field) {}
+    MyFE(FieldInterface &_m_field);
   };
 
   MyFE fe;
@@ -40,6 +40,7 @@ struct NodalForce {
 
   boost::ptr_vector<MethodsForOp> methodsOp;
 
+  /// \brief Operator to assemble nodal force into right hand side vector
   struct OpNodalForce: public VertexElementForcesAndSourcesCore::UserDataOperator {
 
     Vec &F;
@@ -48,100 +49,41 @@ struct NodalForce {
     boost::ptr_vector<MethodsForOp> &methodsOp;
 
     OpNodalForce(const string field_name,Vec &_F,bCForce &data,
-      boost::ptr_vector<MethodsForOp> &methods_op,bool use_snes_f = false):
-      VertexElementForcesAndSourcesCore::UserDataOperator(field_name,ForcesAndSurcesCore::UserDataOperator::OPROW),
-      F(_F),useSnesF(use_snes_f),dAta(data),methodsOp(methods_op) {}
+      boost::ptr_vector<MethodsForOp> &methods_op,
+      bool use_snes_f = false);
 
     ublas::vector<FieldData> Nf;
-    PetscErrorCode doWork(int side,EntityType type,DataForcesAndSurcesCore::EntData &data) {
-      PetscFunctionBegin;
 
-      if(data.getIndices().size()==0) PetscFunctionReturn(0);
-      EntityHandle ent = getMoFEMFEPtr()->get_ent();
-      if(dAta.nOdes.find(ent)==dAta.nOdes.end()) PetscFunctionReturn(0);
-
-      PetscErrorCode ierr;
-
-      const FENumeredDofMoFEMEntity *dof_ptr;
-      ierr = getMoFEMFEPtr()->get_row_dofs_by_petsc_gloabl_dof_idx(data.getIndices()[0],&dof_ptr); CHKERRQ(ierr);
-      int rank = dof_ptr->get_max_rank();
-      if(rank != 3) {
-	SETERRQ(PETSC_COMM_SELF,1,"wrong field rank");
-      }
-      if(data.getIndices().size()!=(unsigned int)rank) {
-	SETERRQ(PETSC_COMM_SELF,1,"data inconsistency");
-      }
-
-      Nf.resize(3);
-      for(int rr = 0;rr<rank;rr++) {
-	if(rr == 0) {
-	  Nf[0] = dAta.data.data.value3*dAta.data.data.value1;
-	} else if(rr == 1) {
-	  Nf[1] = dAta.data.data.value4*dAta.data.data.value1;
-	} else if(rr == 2) {
-	  Nf[2] = dAta.data.data.value5*dAta.data.data.value1;
-	} else {
-	  SETERRQ(PETSC_COMM_SELF,1,"data inconsistency");
-	}
-      }
-
-      ierr = MethodsForOp::applyScale(getFEMethod(),methodsOp,Nf); CHKERRQ(ierr);
-      Vec myF = F;
-      if(useSnesF) {
-	myF = getFEMethod()->snes_f;
-      }
-      ierr = VecSetValues(myF,data.getIndices().size(),
-	&data.getIndices()[0],&Nf[0],ADD_VALUES); CHKERRQ(ierr);
-
-      PetscFunctionReturn(0);
-    }
+    /** Exeuted for each entity on element, i.e. in this case Vertex element
+      has only one entity, that is vertex
+    */
+    PetscErrorCode doWork(int side,EntityType type,DataForcesAndSurcesCore::EntData &data);
 
   };
 
-  PetscErrorCode addForce(const string field_name,Vec &F,int ms_id,bool use_snes_f = false) {
-    PetscFunctionBegin;
-    PetscErrorCode ierr;
-    ErrorCode rval;
-    const CubitMeshSets *cubit_meshset_ptr;
-    ierr = mField.get_cubit_msId(ms_id,NODESET,&cubit_meshset_ptr); CHKERRQ(ierr);
-    ierr = cubit_meshset_ptr->get_bc_data_structure(mapForce[ms_id].data); CHKERRQ(ierr);
-    rval = mField.get_moab().get_entities_by_type(cubit_meshset_ptr->meshset,MBVERTEX,mapForce[ms_id].nOdes,true); CHKERR_PETSC(rval);
-    fe.getOpPtrVector().push_back(new OpNodalForce(field_name,F,mapForce[ms_id],methodsOp,use_snes_f));
-    PetscFunctionReturn(0);
-  }
+  PetscErrorCode addForce(const string field_name,Vec &F,int ms_id,bool use_snes_f = false);
 
 };
 
 struct MetaNodalForces {
 
-  //nodal forces
+  /** \brief Scale force based on tag value "_LoadFactor_Scale_"
+
+    This is obsolete, is kept to have back compatibility with fracture code.
+
+  */
   struct TagForceScale: public MethodsForOp {
     FieldInterface &mField;
     double *sCale;
     Tag thScale;
-    TagForceScale(FieldInterface &m_field): mField(m_field) {
-      ErrorCode rval;
-      double def_scale = 1.;
-      const EntityHandle root_meshset = mField.get_moab().get_root_set();
-      rval = mField.get_moab().tag_get_handle("_LoadFactor_Scale_",1,MB_TYPE_DOUBLE,thScale,MB_TAG_CREAT|MB_TAG_EXCL|MB_TAG_MESH,&def_scale);
-      if(rval == MB_ALREADY_ALLOCATED) {
-	rval = mField.get_moab().tag_get_by_ptr(thScale,&root_meshset,1,(const void**)&sCale); CHKERR_THROW(rval);
-      } else {
-	CHKERR_THROW(rval);
-	rval = mField.get_moab().tag_set_data(thScale,&root_meshset,1,&def_scale); CHKERR_THROW(rval);
-	rval = mField.get_moab().tag_get_by_ptr(thScale,&root_meshset,1,(const void**)&sCale); CHKERR_THROW(rval);
-      }
-    }
-    PetscErrorCode scaleNf(const FEMethod *fe,ublas::vector<FieldData> &Nf) {
-      PetscFunctionBegin;
-      Nf *= *sCale;
-      PetscFunctionReturn(0);
-    }
+
+    TagForceScale(FieldInterface &m_field);
+    PetscErrorCode scaleNf(const FEMethod *fe,ublas::vector<FieldData> &Nf);
+
   };
 
-  static PetscErrorCode addNodalForceElement (
-    FieldInterface &mField,
-    const string field_name) {
+  /// Add element taking information from NODESET
+  static PetscErrorCode addNodalForceElement (FieldInterface &mField,const string field_name) {
     PetscFunctionBegin;
     PetscErrorCode ierr;
     ErrorCode rval;
@@ -167,24 +109,26 @@ struct MetaNodalForces {
     PetscFunctionReturn(0);
   }
 
+
+  /// Set inergration point operators
   static PetscErrorCode setNodalForceElementOperators(
     FieldInterface &mField,
     boost::ptr_map<string,NodalForce> &nodal_forces,
     Vec &F,const string field_name) {
-    PetscFunctionBegin;
-    PetscErrorCode ierr;
-    string fe_name;
-    fe_name = "FORCE_FE";
-    nodal_forces.insert(fe_name,new NodalForce(mField));
-    for(_IT_CUBITMESHSETS_BY_BCDATA_TYPE_FOR_LOOP_(mField,NODESET|FORCESET,it)) {
-      ierr = nodal_forces.at(fe_name).addForce(field_name,F,it->get_msId());  CHKERRQ(ierr);
-      /*ForceCubitBcData data;
-      ierr = it->get_bc_data_structure(data); CHKERRQ(ierr);
-      my_split << *it << endl;
-      my_split << data << endl;*/
+      PetscFunctionBegin;
+      PetscErrorCode ierr;
+      string fe_name;
+      fe_name = "FORCE_FE";
+      nodal_forces.insert(fe_name,new NodalForce(mField));
+      for(_IT_CUBITMESHSETS_BY_BCDATA_TYPE_FOR_LOOP_(mField,NODESET|FORCESET,it)) {
+        ierr = nodal_forces.at(fe_name).addForce(field_name,F,it->get_msId());  CHKERRQ(ierr);
+        /*ForceCubitBcData data;
+        ierr = it->get_bc_data_structure(data); CHKERRQ(ierr);
+        my_split << *it << endl;
+        my_split << data << endl;*/
+      }
+      PetscFunctionReturn(0);
     }
-    PetscFunctionReturn(0);
-  }
 
 };
 
--- mofem_v0.2/finite_element_library/SurfacePressure.hpp
+++ mofem_v0.2/finite_element_library/SurfacePressure.hpp
@@ -3,7 +3,6 @@
 
 */
 
-
 /* This file is part of MoFEM.
  * MoFEM is free software: you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the
@@ -24,6 +23,7 @@
 
 namespace MoFEM {
 
+/// Class used to scale loads, f.e. in arc-length control
 struct MethodsForOp {
 
   virtual PetscErrorCode scaleNf(const FEMethod *fe,ublas::vector<FieldData> &Nf) = 0;
@@ -52,7 +52,7 @@ struct NeummanForcesSurface {
   FieldInterface &mField;
 
   struct MyTriangleFE: public FaceElementForcesAndSourcesCore {
-    MyTriangleFE(FieldInterface &_mField): FaceElementForcesAndSourcesCore(_mField) {}
+    MyTriangleFE(FieldInterface &m_field);
     int getRule(int order) { return order; };
   };
 
@@ -76,67 +76,24 @@ struct NeummanForcesSurface {
 
   boost::ptr_vector<MethodsForOp> methodsOp;
 
+  /// Operator for force element
   struct OpNeumannForce: public FaceElementForcesAndSourcesCore::UserDataOperator {
 
     Vec &F;
     bCForce &dAta;
     boost::ptr_vector<MethodsForOp> &methodsOp;
 
-    OpNeumannForce(const string field_name,Vec &_F,bCForce &data,
-      boost::ptr_vector<MethodsForOp> &methods_op):
-      FaceElementForcesAndSourcesCore::UserDataOperator(field_name,UserDataOperator::OPROW),
-      F(_F),dAta(data),methodsOp(methods_op) {}
+    OpNeumannForce(
+      const string field_name,Vec &_F,bCForce &data,
+      boost::ptr_vector<MethodsForOp> &methods_op);
 
     ublas::vector<FieldData> Nf;
 
-    PetscErrorCode doWork(
-      int side,EntityType type,DataForcesAndSurcesCore::EntData &data) {
-      PetscFunctionBegin;
-
-      if(data.getIndices().size()==0) PetscFunctionReturn(0);
-      EntityHandle ent = getMoFEMFEPtr()->get_ent();
-      if(dAta.tRis.find(ent)==dAta.tRis.end()) PetscFunctionReturn(0);
-
-      PetscErrorCode ierr;
-
-      const FENumeredDofMoFEMEntity *dof_ptr;
-      ierr = getMoFEMFEPtr()->get_row_dofs_by_petsc_gloabl_dof_idx(data.getIndices()[0],&dof_ptr); CHKERRQ(ierr);
-      int rank = dof_ptr->get_max_rank();
-      int nb_row_dofs = data.getIndices().size()/rank;
-
-      Nf.resize(data.getIndices().size());
-      Nf.clear();
-
-      for (unsigned int gg = 0;gg<data.getN().size1();gg++) {
-
-        double val = getArea()*getGaussPts()(2,gg);
-        for (int rr = 0;rr<rank;rr++) {
-
-          double force;
-          if(rr == 0) {
-            force = dAta.data.data.value3;
-          } else if(rr == 1) {
-            force = dAta.data.data.value4;
-          } else if(rr == 2) {
-            force = dAta.data.data.value5;
-          } else {
-            SETERRQ(PETSC_COMM_SELF,1,"data inconsistency");
-          }
-          force *= dAta.data.data.value1;
-          cblas_daxpy(nb_row_dofs,val*force,&data.getN()(gg,0),1,&Nf[rr],rank);
-
-        }
-
-      }
-
-      ierr = MethodsForOp::applyScale(getFEMethod(), methodsOp, Nf); CHKERRQ(ierr);
-      ierr = VecSetValues(F,data.getIndices().size(), &data.getIndices()[0], &Nf[0], ADD_VALUES); CHKERRQ(ierr);
-
-      PetscFunctionReturn(0);
-    }
+    PetscErrorCode doWork(int side,EntityType type,DataForcesAndSurcesCore::EntData &data);
 
   };
 
+  /// Operator for pressure element
   struct OpNeumannPreassure:public FaceElementForcesAndSourcesCore::UserDataOperator {
 
     Vec &F;
@@ -144,167 +101,55 @@ struct NeummanForcesSurface {
     boost::ptr_vector<MethodsForOp> &methodsOp;
     bool hoGeometry;
 
-    OpNeumannPreassure(const string field_name,Vec &_F,
+    OpNeumannPreassure(
+      const string field_name,Vec &_F,
       bCPreassure &data,boost::ptr_vector<MethodsForOp> &methods_op,
-      bool ho_geometry = false):
-      FaceElementForcesAndSourcesCore::UserDataOperator(field_name,UserDataOperator::OPROW),
-      F(_F),dAta(data),methodsOp(methods_op),hoGeometry(ho_geometry) {}
+      bool ho_geometry = false);
 
     ublas::vector<FieldData> Nf;
 
-    PetscErrorCode doWork(
-      int side,EntityType type,DataForcesAndSurcesCore::EntData &data) {
-      PetscFunctionBegin;
-
-      if(data.getIndices().size()==0) PetscFunctionReturn(0);
-      if(dAta.tRis.find(getMoFEMFEPtr()->get_ent())==dAta.tRis.end()) PetscFunctionReturn(0);
-
-      PetscErrorCode ierr;
-
-      const FENumeredDofMoFEMEntity *dof_ptr;
-      ierr = getMoFEMFEPtr()->get_row_dofs_by_petsc_gloabl_dof_idx(data.getIndices()[0],&dof_ptr); CHKERRQ(ierr);
-      int rank = dof_ptr->get_max_rank();
-
-      int nb_row_dofs = data.getIndices().size()/rank;
-
-      Nf.resize(data.getIndices().size());
-      bzero(&*Nf.data().begin(),data.getIndices().size()*sizeof(FieldData));
-
-      //cerr << getNormal() << endl;
-      //cerr << getNormals_at_GaussPt() << endl;
-
-      for(unsigned int gg = 0;gg<data.getN().size1();gg++) {
-
-        double val = getGaussPts()(2,gg);
-        for(int rr = 0;rr<rank;rr++) {
-
-          double force;
-          if(hoGeometry) {
-            force = dAta.data.data.value1*getNormals_at_GaussPt()(gg,rr);
-          } else {
-            force = dAta.data.data.value1*getNormal()[rr];
-          }
-          cblas_daxpy(nb_row_dofs,0.5*val*force,&data.getN()(gg,0),1,&Nf[rr],rank);
-
-        }
-
-      }
-
-      /*cerr << "VecSetValues\n";
-      cerr << Nf << endl;
-      cerr << data.getIndices() << endl;*/
-      ierr = MethodsForOp::applyScale(getFEMethod(),methodsOp,Nf); CHKERRQ(ierr);
-      ierr = VecSetValues(F,data.getIndices().size(),
-	      &data.getIndices()[0],&Nf[0],ADD_VALUES); CHKERRQ(ierr);
-
-      PetscFunctionReturn(0);
-    }
+    PetscErrorCode doWork(int side,EntityType type,DataForcesAndSurcesCore::EntData &data);
 
   };
 
-  struct OpNeumannPreassureFlux:public FaceElementForcesAndSourcesCore::UserDataOperator {
+
+
+  /// Operator for flux element
+  struct OpNeumannFlux:public FaceElementForcesAndSourcesCore::UserDataOperator {
 
     Vec &F;
     bCPreassure &dAta;
     boost::ptr_vector<MethodsForOp> &methodsOp;
     bool hoGeometry;
 
-    OpNeumannPreassureFlux(const string field_name,Vec &_F,
+    OpNeumannFlux(
+      const string field_name,Vec &_F,
       bCPreassure &data,boost::ptr_vector<MethodsForOp> &methods_op,
-      bool ho_geometry = false):
-      FaceElementForcesAndSourcesCore::UserDataOperator(field_name,UserDataOperator::OPROW),
-      F(_F),dAta(data),methodsOp(methods_op),hoGeometry(ho_geometry) {}
+      bool ho_geometry);
 
     ublas::vector<FieldData> Nf;
 
-    PetscErrorCode doWork(
-      int side,EntityType type,DataForcesAndSurcesCore::EntData &data) {
-      PetscFunctionBegin;
-
-      if(data.getIndices().size()==0) PetscFunctionReturn(0);
-      if(dAta.tRis.find(getMoFEMFEPtr()->get_ent())==dAta.tRis.end()) PetscFunctionReturn(0);
-
-      PetscErrorCode ierr;
-
-      const FENumeredDofMoFEMEntity *dof_ptr;
-      ierr = getMoFEMFEPtr()->get_row_dofs_by_petsc_gloabl_dof_idx(data.getIndices()[0],&dof_ptr); CHKERRQ(ierr);
-      int rank = dof_ptr->get_max_rank();
-
-      int nb_row_dofs = data.getIndices().size()/rank;
-
-      Nf.resize(data.getIndices().size());
-      Nf.clear();
-      //cerr << getNormal() << endl;
-      //cerr << getNormals_at_GaussPt() << endl;
-
-      for(unsigned int gg = 0;gg<data.getN().size1();gg++) {
-
-        double val = getGaussPts()(2,gg);
-        double flux;
-        if(hoGeometry) {
-          double area = cblas_dnrm2(3,&getNormals_at_GaussPt()(gg,0),1);
-          flux = dAta.data.data.value1*area;
-        } else {
-          flux = dAta.data.data.value1*getArea();
-        }
-        cblas_daxpy(nb_row_dofs,val*flux,&data.getN()(gg,0),1,&*Nf.data().begin(),1);
-
-      }
-
-      //cerr << "VecSetValues\n";
-      //cerr << Nf << endl;
-      //cerr << data.getIndices() << endl;
-      ierr = MethodsForOp::applyScale(getFEMethod(), methodsOp, Nf); CHKERRQ(ierr);
-      ierr = VecSetValues(F, data.getIndices().size(), &data.getIndices()[0], &Nf[0], ADD_VALUES); CHKERRQ(ierr);
-
-      PetscFunctionReturn(0);
-    }
+    PetscErrorCode doWork(int side,EntityType type,DataForcesAndSurcesCore::EntData &data);
 
   };
 
-
-  PetscErrorCode addForce(const string field_name,Vec &F,int ms_id) {
-    PetscFunctionBegin;
-    PetscErrorCode ierr;
-    ErrorCode rval;
-    const CubitMeshSets *cubit_meshset_ptr;
-    ierr = mField.get_cubit_msId(ms_id,NODESET,&cubit_meshset_ptr); CHKERRQ(ierr);
-    ierr = cubit_meshset_ptr->get_bc_data_structure(mapForce[ms_id].data); CHKERRQ(ierr);
-    rval = mField.get_moab().get_entities_by_type(cubit_meshset_ptr->meshset,MBTRI,mapForce[ms_id].tRis,true); CHKERR_PETSC(rval);
-    fe.getOpPtrVector().push_back(new OpNeumannForce(field_name,F,mapForce[ms_id],methodsOp));
-    PetscFunctionReturn(0);
-  }
-
-   PetscErrorCode addPreassure(const string field_name,Vec &F,int ms_id,bool ho_geometry = false) {
-    PetscFunctionBegin;
-    PetscErrorCode ierr;
-    ErrorCode rval;
-    const CubitMeshSets *cubit_meshset_ptr;
-    ierr = mField.get_cubit_msId(ms_id,SIDESET,&cubit_meshset_ptr); CHKERRQ(ierr);
-    ierr = cubit_meshset_ptr->get_bc_data_structure(mapPreassure[ms_id].data); CHKERRQ(ierr);
-    rval = mField.get_moab().get_entities_by_type(cubit_meshset_ptr->meshset,MBTRI,mapPreassure[ms_id].tRis,true); CHKERR_PETSC(rval);
-    fe.getOpPtrVector().push_back(new OpNeumannPreassure(field_name,F,mapPreassure[ms_id],methodsOp,ho_geometry));
-    PetscFunctionReturn(0);
-  }
-
-  PetscErrorCode addFlux(const string field_name,Vec &F,int ms_id,bool ho_geometry = false) {
-    PetscFunctionBegin;
-    PetscErrorCode ierr;
-    ErrorCode rval;
-    const CubitMeshSets *cubit_meshset_ptr;
-    ierr = mField.get_cubit_msId(ms_id,SIDESET,&cubit_meshset_ptr); CHKERRQ(ierr);
-    ierr = cubit_meshset_ptr->get_bc_data_structure(mapPreassure[ms_id].data); CHKERRQ(ierr);
-    rval = mField.get_moab().get_entities_by_type(cubit_meshset_ptr->meshset,MBTRI,mapPreassure[ms_id].tRis,true); CHKERR_PETSC(rval);
-    fe.getOpPtrVector().push_back(new OpNeumannPreassureFlux(field_name,F,mapPreassure[ms_id],methodsOp,ho_geometry));
-    PetscFunctionReturn(0);
-  }
+  DEPRECATED typedef OpNeumannFlux OpNeumannPreassureFlux;
 
 
+  /// Add force element operator  (integration on face)
+  PetscErrorCode addForce(const string field_name,Vec &F,int ms_id);
+
+  /// Add pressure element operator (integration on face)
+  PetscErrorCode addPreassure(const string field_name,Vec &F,int ms_id,bool ho_geometry = false);
+
+  /// Add flux element operator (integration on face)
+  PetscErrorCode addFlux(const string field_name,Vec &F,int ms_id,bool ho_geometry = false);
 
 };
 
+/// Meta functions to add elements from blocksets
 struct MetaNeummanForces {
-
+  
   static PetscErrorCode addNeumannBCElements(
     FieldInterface &mField,
     const string field_name,
