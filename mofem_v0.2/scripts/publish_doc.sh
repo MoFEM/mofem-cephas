b3bb9612cb79770e04adb28cb540ca6913f9e289
Lukasz Kaczmarczyk <likask@wp.pl>
2015-06-09 00:12:13 +0100

    delete

--- mofem_v0.2/scripts/publish_doc.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-SFTP=/usr/bin/sftp
-PROJECT_SOURCE_DIR=/mnt/home/Documents/mofem-cephas/mofem_v0.2
-
-$SFTP -C -b $PROJECT_SOURCE_DIR/scripts/publish_doc "campus\lk58p"@userweb.eng.gla.ac.uk
