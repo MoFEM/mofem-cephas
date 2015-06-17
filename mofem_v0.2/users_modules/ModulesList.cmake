6b970461b37508c4688f9c8021d0f301b7d1a594
Lukasz Kaczmarczyk <likask@wp.pl>
2015-06-17 05:25:46 +0100

    Cleaning CMake for user modules and remove mess made by Thomas

--- mofem_v0.2/users_modules/ModulesList.cmake
+++ mofem_v0.2/users_modules/ModulesList.cmake
@@ -1,17 +1,6 @@
-# Users modules
-
-# Users modules library, common for all programs
-#add_library(users_modules
-#)
-
-add_subdirectory(thermal)
-add_subdirectory(ultraweak)
-add_subdirectory(convective_mass_element)
-add_subdirectory(ground_surface_temperature)
-add_subdirectory(analytical_dirichlet_boundary_conditions)
-
 # Obsolete, i.e. implementation is obsolete and need to be changed or replaced
-# by alternative classes or functions
+# by alternative classes or functions. Use of oboslete implementation should be
+# avoided. This code will be removed in futuer versions of MoFEM
 
 include_directories(${UM_SOURCE_DIR}/obsolete)
 include_directories(${UM_SOURCE_DIR}/obsolete/c)
@@ -29,15 +18,24 @@ add_library(complex_for_lazy_obsolete
   ${UM_SOURCE_DIR}/obsolete/impl/SurfacePressureComplexForLazy.cpp
 )
 
+
+# Users modules
+
+add_subdirectory(thermal)
+add_subdirectory(ultraweak)
+add_subdirectory(convective_mass_element)
+add_subdirectory(ground_surface_temperature)
+add_subdirectory(analytical_dirichlet_boundary_conditions)
 add_subdirectory(elasticity)
 add_subdirectory(cohesive_interface)
 add_subdirectory(nonlinear_elasticity)
 add_subdirectory(fracture_mechanics)
-
 add_subdirectory(helmholtz)
-
 add_subdirectory(homogenisation)
 add_subdirectory(moisture_transport)
-
 add_subdirectory(atom_tests)
 
+
+# Users modules library, common for all programs
+#add_library(users_modules
+#)
