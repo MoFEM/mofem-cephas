821bc5f44d7cad868950cfd00168d71c07d4a0d1
Lukasz Kaczmarczyk <likask@wp.pl>
2015-08-17 13:21:04 +0100

    fixing bug with deadlock

--- mofem_v0.2/users_modules/basic_finite_elements/src/impl/DirichletBC.cpp
+++ mofem_v0.2/users_modules/basic_finite_elements/src/impl/DirichletBC.cpp
@@ -203,28 +203,26 @@ PetscErrorCode DisplacementBCFEMethodPreAndPostProc::postProcess() {
     case CTX_SNESNONE:
     break;
     case CTX_SNESSETFUNCTION: {
-      if(!dofsIndices.empty()) {
-        dofsXValues.resize(dofsIndices.size());
-        ierr = VecGetValues(
-          snes_x,dofsIndices.size(),&dofsIndices[0],&dofsXValues[0]
-        ); CHKERRQ(ierr);
-        ierr = VecAssemblyBegin(snes_f); CHKERRQ(ierr);
-        ierr = VecAssemblyEnd(snes_f); CHKERRQ(ierr);
-        int ii = 0;
-        for(vector<int>::iterator vit = dofsIndices.begin();vit!=dofsIndices.end();vit++,ii++) {
-          double val = 0;
-          if(!dofsXValues.empty()) {
-            val += dofsXValues[ii];
-            val += -mapZeroRows[*vit]; // in snes it is on the left hand side, that way -1
-            dofsXValues[ii] = val;
-          }
+      dofsXValues.resize(dofsIndices.size());
+      ierr = VecGetValues(
+        snes_x,dofsIndices.size(),&*dofsIndices.begin(),&*dofsXValues.begin()
+      ); CHKERRQ(ierr);
+      ierr = VecAssemblyBegin(snes_f); CHKERRQ(ierr);
+      ierr = VecAssemblyEnd(snes_f); CHKERRQ(ierr);
+      int ii = 0;
+      for(vector<int>::iterator vit = dofsIndices.begin();vit!=dofsIndices.end();vit++,ii++) {
+        double val = 0;
+        if(!dofsXValues.empty()) {
+          val += dofsXValues[ii];
+          val += -mapZeroRows[*vit]; // in snes it is on the left hand side, that way -1
+          dofsXValues[ii] = val;
         }
-        ierr = VecSetValues(
-          snes_f,dofsIndices.size(),&dofsIndices[0],&dofsXValues[0],INSERT_VALUES
-        ); CHKERRQ(ierr);
-        ierr = VecAssemblyBegin(snes_f); CHKERRQ(ierr);
-        ierr = VecAssemblyEnd(snes_f); CHKERRQ(ierr);
       }
+      ierr = VecSetValues(
+        snes_f,dofsIndices.size(),&*dofsIndices.begin(),&*dofsXValues.begin(),INSERT_VALUES
+      ); CHKERRQ(ierr);
+      ierr = VecAssemblyBegin(snes_f); CHKERRQ(ierr);
+      ierr = VecAssemblyEnd(snes_f); CHKERRQ(ierr);
     }
     break;
     case CTX_SNESSETJACOBIAN: {
