d7072e3351a4d002816a0c78861919873b91068c
Lukasz Kaczmarczyk <likask@wp.pl>
2015-08-18 16:51:56 +0100

    fix ignored files

--- mofem_v0.2/scripts/git_contributors.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-GIT=/usr/bin/git
-PROJECT_SOURCE_DIR=/mnt/home/Documents/moFEM/mofem-cephas/mofem_v0.2
-
-$GIT shortlog -s -e -n  | \
-  sed 's/\@/ at /g' | \
-  tee $PROJECT_SOURCE_DIR/doc/contributors_list
