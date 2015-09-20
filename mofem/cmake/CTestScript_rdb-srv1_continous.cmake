bfab9467f59523b3fe6d1c7f7bf1caf856705e7f
Lukasz Kaczmarczyk <likask@wp.pl>
2015-09-20 15:47:48 +0100

    move mofem to nee dir

---
+++ mofem/cmake/CTestScript_rdb-srv1_continous.cmake
@@ -0,0 +1,16 @@
+set(CTEST_BUILD_OPTIONS "-DPETSC_DIR=/opt/petsc-3.5.1 -DPETSC_ARCH=arch-linux2-c-debug -DMOAB_DIR=/opt/local_new_moab -DADOL-C_DIR=/opt/local_adol-c-2.5.2 -DTETGEN_DIR=/opt/tetgen1.5.0 -DSLEPC_DIR=/opt/slepc-3.5.3 -DCMAKE_INSTALL_PREFIX=/home/lukasz/tmp/cephas_users_modules /home/lukasz/mofem-cephas/mofem -DCMAKE_CXX_FLAGS=-I/opt/local_boost_1_54_0/include -DCMAKE_EXE_LINKER_FLAGS=-L/opt/local_boost_1_54_0/lib")
+set(CTEST_SITE "rdb-srv1")
+set(CTEST_BUILD_NAME "Linux-mpicxx")
+set(CTEST_BRANCH "CDashTesting")
+
+if(NOT DASHBOARDTEST)
+  set(DASHBOARDTEST "Continuous")
+endif(NOT DASHBOARDTEST)
+
+#valgrind set up
+find_program(CTEST_MEMORYCHECK_COMMAND NAMES valgrind)
+set(CTEST_MEMORYCHECK_COMMAND_OPTIONS
+  "--trace-children=yes --quiet --tool=memcheck --leak-check=yes --show-reachable=yes --num-callers=50 --verbose --demangle=yes --gen-suppressions=all")
+set(CTEST_MEMORYCHECK_SUPPRESSIONS_FILE "$ENV{HOME}/tmp/cephas/source/mofem/cmake/rdb-srv1-valgrind.supp")
+
+include(CTestScript.cmake)
