8b30799dcd15f7c776c58036ef2f21f30a4d377f
Lukasz Kaczmarczyk <likask@wp.pl>
2016-02-20 14:58:35 +0000

    fix configure

--- mofem/users_modules/cmake/CTestScript_rdb-srv1.cmake
+++ mofem/users_modules/cmake/CTestScript_rdb-srv1.cmake
@@ -1,9 +1,9 @@
 set(
   CTEST_BUILD_OPTIONS
-  "-DSTAND_ALLONE_USERS_MODULES=ON"
-  "-DCMAKE_CXX_FLAGS=-I/opt/local_boost_1_54_0/include"
-  "-DCMAKE_EXE_LINKER_FLAGS=-L/opt/local_boost_1_54_0/lib"
-  "/home/lukasz/tmp/cephas_users_modules/users_modules"
+  "-DSTAND_ALLONE_USERS_MODULES=ON \
+  -DCMAKE_CXX_FLAGS=-I/opt/local_boost_1_54_0/include \
+  -DCMAKE_EXE_LINKER_FLAGS=-L/opt/local_boost_1_54_0/lib \
+  /home/lukasz/tmp/cephas_users_modules/users_modules"
 )
 
 set(CTEST_SITE "rdb-srv1")
