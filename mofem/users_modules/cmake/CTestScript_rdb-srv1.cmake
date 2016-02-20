97d84881f6a77cf901a9ca4628f3189f18fd8388
Lukasz Kaczmarczyk <likask@wp.pl>
2016-02-20 14:14:02 +0000

    allow to download users modules while config

--- mofem/users_modules/cmake/CTestScript_rdb-srv1.cmake
+++ mofem/users_modules/cmake/CTestScript_rdb-srv1.cmake
@@ -1,12 +1,13 @@
-set(CTEST_BUILD_OPTIONS "-DSTAND_ALLONE_USERS_MODULES=ON -DCMAKE_CXX_FLAGS=-I/opt/local_boost_1_54_0/include -DCMAKE_EXE_LINKER_FLAGS=-L/opt/local_boost_1_54_0/lib /home/lukasz/tmp/cephas_users_modules/users_modules")
+set(
+  CTEST_BUILD_OPTIONS
+  "-DSTAND_ALLONE_USERS_MODULES=ON -DCMAKE_CXX_FLAGS=-I/opt/local_boost_1_54_0/include -DCMAKE_EXE_LINKER_FLAGS=-L/opt/local_boost_1_54_0/lib /home/lukasz/tmp/cephas_users_modules/users_modules"
+)
 
 set(CTEST_SITE "rdb-srv1")
 set(CTEST_BUILD_NAME "Linux-mpicxx")
 
-if(NOT DASHBOARDTEST) 
+if(NOT DASHBOARDTEST)
   set(DASHBOARDTEST "Continuous")
 endif(NOT DASHBOARDTEST)
 
 include(CTestScript.cmake)
-
-
