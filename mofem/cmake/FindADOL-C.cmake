f3e3cf01cd88bcbd6474d62f9071c6a25508f568
Lukasz Kaczmarczyk <likask@wp.pl>
2016-02-21 02:42:20 +0000

    add external packages install method for adol-c and tetgen

--- mofem/cmake/FindADOL-C.cmake
+++ mofem/cmake/FindADOL-C.cmake
@@ -1,23 +1,27 @@
 # - Try to find ADOL-C
 # Once done this will define
 #
-#  ADOL-C_DIR - directory in which MOAB resides
+#  ADOL-C_DIR - directory in which ADOL-C resides
 
-# If unset, try environment
-if(ADOL-C_DIR)
-  set(ADOL-C_DIR $ENV{ADOL-C_DIR})
-endif(ADOL-C_DIR)
-
-find_library(ADOL-C_LIBRARY NAMES adolc PATHS "${ADOL-C_DIR}/lib")
-message(STATUS ${ADOL-C_LIBRARY})
-
-if(ADOL-C_LIBRARY) 
-  include_directories("${ADOL-C_DIR}/include")
-  add_definitions( -DWITH_ADOL_C )
-endif(ADOL-C_LIBRARY)
-
-
-
-
-
-
+if(WITH_ADOL-C)
+  ExternalProject_Add(
+    adolc
+    PREFIX ${PROJECT_BINARY_DIR}/external/
+    URL http://www.coin-or.org/download/source/ADOL-C/ADOL-C-2.5.2.tgz
+    CONFIGURE_COMMAND ${PROJECT_BINARY_DIR}/external/src/adolc/configure CC=${CMAKE_C_COMPILER} CXX=${CMAKE_CXX_COMPILER} --disable-shared --prefix=${PROJECT_BINARY_DIR}/external
+  )
+  include_directories(${PROJECT_BINARY_DIR}/external/include)
+  link_directories(${PROJECT_BINARY_DIR}/external/lib64)
+  set(ADOL-C_DIR ${PROJECT_BINARY_DIR}/external)
+  set(ADOL-C_LIBRARY ${ADOL-C_DIR}/lib64/libadolc.a)
+  message(STATUS ${ADOL-C_LIBRARY})
+  add_library(ADOL-C_LIBRARY STATIC IMPORTED)
+  add_definitions(-DWITH_ADOL_C)
+elseif(ADOL-C_DIR)
+  find_library(ADOL-C_LIBRARY NAMES adolc PATHS ${ADOL-C_DIR}/lib)
+  message(STATUS ${ADOL-C_LIBRARY})
+  if(ADOL-C_LIBRARY)
+    include_directories("${ADOL-C_DIR}/include")
+    add_definitions(-DWITH_ADOL_C)
+  endif(ADOL-C_LIBRARY)
+endif(WITH_ADOL-C)
--- mofem/cmake/FindTetGen.cmake
+++ mofem/cmake/FindTetGen.cmake
@@ -1,19 +1,32 @@
 # - Try to find TETGEN
 
-# If unset, try environment
-
-if(NOT TETGEN_DIR)
-  set(TETGEN_DIR $ENV{TETGEN_DIR})
-endif(NOT TETGEN_DIR)
-
-find_library(TETGEN_LIBRARY NAMES tet PATHS "${TETGEN_DIR}/lib")
-message(STATUS ${TETGEN_LIBRARY})
-
-if(TETGEN_LIBRARY) 
-  include_directories("${TETGEN_DIR}/include")
-  add_definitions( -DWITH_TETGEN )
-endif(TETGEN_LIBRARY)
-
-
-
-
+if(WITH_TETGEN)
+  ExternalProject_Add(
+    tetgen
+    PREFIX ${PROJECT_BINARY_DIR}/external/
+    URL https://bitbucket.org/likask/mofem-joseph/downloads/tetgen1.5.0.tgz
+    INSTALL_COMMAND ""
+  )
+  add_custom_target(
+    install_tetegen
+    ALL
+    COMMAND ${CMAKE_COMMAND} -E copy_if_different ${PROJECT_BINARY_DIR}/external/src/tetgen/include/tetgen.h ${PROJECT_BINARY_DIR}/external/include/
+    COMMAND ${CMAKE_COMMAND} -E copy_if_different ${PROJECT_BINARY_DIR}/external/src/tetgen-build/libtet* ${PROJECT_BINARY_DIR}/external/lib/
+    COMMAND ${CMAKE_COMMAND} -E copy_if_different ${PROJECT_BINARY_DIR}/external/src/tetgen-build/tetgen ${PROJECT_BINARY_DIR}/external/bin/
+    DEPENDS tetgen
+  )
+  include_directories(${PROJECT_BINARY_DIR}/external/include)
+  link_directories(${PROJECT_BINARY_DIR}/external/lib)
+  add_definitions(-DWITH_TETGEN)
+  set(TETGEN_DIR ${PROJECT_BINARY_DIR}/external)
+  set(TETGEN_LIBRARY ${TETGEN_DIR}/lib/libtet.a)
+  add_library(TETGEN_LIBRARY STATIC IMPORTED)
+  message(STATUS ${TETGEN_LIBRARY})
+elseif(TETGEN_DIR)
+  find_library(TETGEN_LIBRARY NAMES tet PATHS ${TETGEN_DIR}/lib)
+  message(STATUS ${TETGEN_LIBRARY})
+  if(TETGEN_LIBRARY)
+    include_directories(${TETGEN_DIR}/include)
+    add_definitions(-DWITH_TETGEN)
+  endif(TETGEN_LIBRARY)
+endif(WITH_TETGEN)
