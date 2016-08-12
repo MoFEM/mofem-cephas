5afe2c1a3a381a17bc018c797425a046c6422ba2
Lukasz Kaczmarczyk <likask@wp.pl>
2016-08-12 23:05:41 +0100

    limit maximal number of cores used to compilation to 12

--- mofem/docker_build_mofem_script.sh
+++ mofem/docker_build_mofem_script.sh
@@ -8,6 +8,17 @@ echo MOFEM_BUILD_DIR $MOFEM_BUILD_DIR
 mkdir -p $MOFEM_BUILD_DIR
 cd $MOFEM_BUILD_DIR
 
+# Determine number of cores to compile code
+
+NBCORES=`cat /proc/cpuinfo | grep processor | wc -l`
+NBMAX=12
+if (($NBCORES > $NBMAX)); then
+  NB=$NBMAX
+else
+  NB=$NBCORES
+fi
+echo Nb. of cores $NBCORES and nb. of cores used to compilation $NB
+
 echo "Configure"
 /opt/local/bin/cmake \
   -DCMAKE_BUILD_TYPE=MinSizeRel \
@@ -19,7 +30,7 @@ echo "Configure"
   -DWITH_ADOL-C=1 \
   -DCMAKE_INSTALL_PREFIX=$MOFEM_INSTALL_DIR $MOFEM_SRC_DIR
 echo "Build"
-make install
+make -j $NB install
 ctest --output-on-failure
 make clean
 
@@ -28,7 +39,7 @@ cd $MOFEM_INSTALL_DIR
 /opt/local/bin/cmake -DBUILD_SHARED_LIBS=yes -DCMAKE_CXX_FLAGS="-Wall" users_modules
 echo "Build users modules"
 cd $MOFEM_INSTALL_DIR/basic_finite_elements/atom_tests
-make
+make -j $NB
 ctest --output-on-failure
 make clean
 echo "All done"
