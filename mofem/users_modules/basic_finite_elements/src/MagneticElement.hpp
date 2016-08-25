ac1fdabf1110a705a7c4b58a3c70fc0a33401724
Lukasz Kaczmarczyk <likask@wp.pl>
2016-08-25 21:40:31 +0100

    add magnetostatic problem as example

---
+++ mofem/users_modules/basic_finite_elements/src/MagneticElement.hpp
@@ -0,0 +1,450 @@
+/** \file MagneticElement.hpp
+ * \brief Implementation of magnetic element
+ *
+ */
+
+/*
+ * This file is part of MoFEM.
+ * MoFEM is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * MoFEM is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with MoFEM. If not, see <http://www.gnu.org/licenses/>. */
+
+#ifndef __MAGNETICELEMENT_HPP__
+#define __MAGNETICELEMENT_HPP__
+
+struct MagneticElement {
+
+  MoFEM::Interface &mField;
+
+  /// \brief  definition of volume element
+  struct VolumeFE: public MoFEM::VolumeElementForcesAndSourcesCore {
+    VolumeFE(MoFEM::Interface &m_field):
+    MoFEM::VolumeElementForcesAndSourcesCore(m_field) {}
+    int getRule(int order) { return 2*order; };
+  };
+  VolumeFE feVol;
+
+  /** \brief define surface element
+    *
+    */
+  struct TriFE: public MoFEM::FaceElementForcesAndSourcesCore {
+    TriFE(MoFEM::Interface &m_field): MoFEM::FaceElementForcesAndSourcesCore(m_field) {}
+    int getRule(int order) { return 2*order; };
+  };
+  TriFE feTri;
+
+  MagneticElement(MoFEM::Interface &m_field):
+  mField(m_field),
+  feVol(m_field),
+  feTri(m_field) {
+
+  };
+  virtual ~MagneticElement() {}
+
+
+
+  /**
+   * \brief data structure storing material constants, model parameters, matrices, etc.
+   *
+   */
+  struct BlockData {
+
+    // field
+    const string fieldName;
+    const string feName;
+    const string feNaturalBCName;
+
+    // material parameters
+    double mu;  ///< magnetic constant  N / A2
+
+    // Natural boundary conditions
+    Range naturalBc;
+
+    // Essential boundary conditions
+    Range essentialBc;
+
+    int oRder; ///< approximation order
+
+    // Petsc data
+    DM dM;
+    Mat A;
+    Vec D,F;
+
+    BlockData():
+    fieldName("MAGNETIC_POTENTIAL"),
+    feName("MAGNETIC"),
+    feNaturalBCName("MAGENTIC_NATURAL_BC"),
+    mu(M_PI*4e-7) {
+    }
+    ~BlockData() {}
+  };
+
+  BlockData blockData;
+
+  /**
+   * \brief get natural boundary conditions
+   * @return      error code
+   */
+  PetscErrorCode getNaturalBc() {
+    MoABErrorCode rval;
+    PetscFunctionBegin;
+    for(_IT_CUBITMESHSETS_BY_SET_TYPE_FOR_LOOP_(mField,BLOCKSET,bit)) {
+      if(bit->getName().compare(0,9,"NATURALBC") == 0) {
+        Range faces;
+        rval = mField.get_moab().get_entities_by_type(bit->meshset,MBTRI,faces,true); CHKERRQ_MOAB(rval);
+        rval = mField.get_moab().get_adjacencies(faces,1,true,blockData.naturalBc,moab::Interface::UNION); CHKERRQ_MOAB(rval);
+        blockData.naturalBc.merge(faces);
+      }
+    }
+    PetscFunctionReturn(0);
+  }
+
+  /**
+   * \brief get essential boundary conditions (only homogenous case is considered)
+   * @return      error code
+   */
+  PetscErrorCode getEssentialBc() {
+    MoABErrorCode rval;
+    PetscFunctionBegin;
+    for(_IT_CUBITMESHSETS_BY_SET_TYPE_FOR_LOOP_(mField,BLOCKSET,bit)) {
+      if(bit->getName().compare(0,10,"ESSENTIALBC") == 0) {
+        Range faces;
+        rval = mField.get_moab().get_entities_by_type(bit->meshset,MBTRI,faces,true); CHKERRQ_MOAB(rval);
+        rval = mField.get_moab().get_adjacencies(faces,1,true,blockData.essentialBc,moab::Interface::UNION); CHKERRQ_MOAB(rval);
+        blockData.essentialBc.merge(faces);
+      }
+    }
+    if(blockData.essentialBc.empty()) {
+      Range tets;
+      rval = mField.get_moab().get_entities_by_type(0,MBTET,tets); CHKERRQ_MOAB(rval);
+      Skinner skin(&mField.get_moab());
+      Range skin_faces; // skin faces from 3d ents
+      rval = skin.find_skin(0,tets,false,blockData.essentialBc); CHKERR_MOAB(rval);
+    }
+    PetscFunctionReturn(0);
+  }
+
+  /**
+   * \brief build problem data structures
+   * @return error code
+   */
+  PetscErrorCode createFields() {
+    PetscErrorCode ierr;
+    PetscFunctionBegin;
+    //Fields
+    ierr = mField.add_field(blockData.fieldName,HCURL,1); CHKERRQ(ierr);
+    ierr = mField.add_field("MESH_NODE_POSITIONS",H1,3); CHKERRQ(ierr);
+    //meshset consisting all entities in mesh
+    EntityHandle root_set = mField.get_moab().get_root_set();
+    //add entities to field
+    ierr = mField.add_ents_to_field_by_TETs(root_set,blockData.fieldName); CHKERRQ(ierr);
+    ierr = mField.set_field_order(root_set,MBTET,blockData.fieldName,blockData.oRder); CHKERRQ(ierr);
+    ierr = mField.set_field_order(root_set,MBTRI,blockData.fieldName,blockData.oRder); CHKERRQ(ierr);
+    ierr = mField.set_field_order(root_set,MBEDGE,blockData.fieldName,blockData.oRder); CHKERRQ(ierr);
+    ierr = mField.add_ents_to_field_by_TETs(root_set,"MESH_NODE_POSITIONS"); CHKERRQ(ierr);
+    ierr = mField.set_field_order(root_set,MBTET,"MESH_NODE_POSITIONS",2); CHKERRQ(ierr);
+    ierr = mField.set_field_order(root_set,MBTRI,"MESH_NODE_POSITIONS",2); CHKERRQ(ierr);
+    ierr = mField.set_field_order(root_set,MBEDGE,"MESH_NODE_POSITIONS",2); CHKERRQ(ierr);
+    ierr = mField.set_field_order(root_set,MBVERTEX,"MESH_NODE_POSITIONS",1); CHKERRQ(ierr);
+    //build field
+    ierr = mField.build_fields(); CHKERRQ(ierr);
+    PetscFunctionReturn(0);
+  }
+
+  PetscErrorCode createElements() {
+    PetscErrorCode ierr;
+    PetscFunctionBegin;
+    // //Elements
+    ierr = mField.add_finite_element(blockData.feName); CHKERRQ(ierr);
+    ierr = mField.modify_finite_element_add_field_row(blockData.feName,blockData.fieldName); CHKERRQ(ierr);
+    ierr = mField.modify_finite_element_add_field_col(blockData.feName,blockData.fieldName); CHKERRQ(ierr);
+    ierr = mField.modify_finite_element_add_field_data(blockData.feName,blockData.fieldName); CHKERRQ(ierr);
+    ierr = mField.modify_finite_element_add_field_data(blockData.feName,"MESH_NODE_POSITIONS"); CHKERRQ(ierr);
+    ierr = mField.add_finite_element(blockData.feNaturalBCName); CHKERRQ(ierr);
+    ierr = mField.modify_finite_element_add_field_row(blockData.feNaturalBCName,blockData.fieldName); CHKERRQ(ierr);
+    ierr = mField.modify_finite_element_add_field_col(blockData.feNaturalBCName,blockData.fieldName); CHKERRQ(ierr);
+    ierr = mField.modify_finite_element_add_field_data(blockData.feNaturalBCName,blockData.fieldName); CHKERRQ(ierr);
+    ierr = mField.modify_finite_element_add_field_data(blockData.feNaturalBCName,"MESH_NODE_POSITIONS"); CHKERRQ(ierr);
+    ierr = mField.add_ents_to_finite_element_by_TETs(0,blockData.feName); CHKERRQ(ierr);
+    ierr = mField.add_ents_to_finite_element_by_TRIs(blockData.naturalBc,blockData.feNaturalBCName); CHKERRQ(ierr);
+    //build finite elemnts
+    ierr = mField.build_finite_elements(); CHKERRQ(ierr);
+    //build adjacencies
+    ierr = mField.build_adjacencies(BitRefLevel().set(0)); CHKERRQ(ierr);
+    PetscFunctionReturn(0);
+  }
+
+  PetscErrorCode createProblem() {
+    PetscErrorCode ierr;
+    PetscFunctionBegin;
+    // set up DM
+    DMType dm_name = "MAGNETIC_PROBLEM";
+    ierr = DMRegister_MoFEM(dm_name); CHKERRQ(ierr);
+    ierr = DMCreate(PETSC_COMM_WORLD,&blockData.dM);CHKERRQ(ierr);
+    ierr = DMSetType(blockData.dM,dm_name);CHKERRQ(ierr);
+    ierr = DMMoFEMCreateMoFEM(blockData.dM,&mField,dm_name,BitRefLevel().set(0)); CHKERRQ(ierr);
+    ierr = DMSetFromOptions(blockData.dM); CHKERRQ(ierr);
+    //add elements to blockData.dM
+    ierr = DMMoFEMAddElement(blockData.dM,blockData.feName.c_str()); CHKERRQ(ierr);
+    ierr = DMMoFEMAddElement(blockData.dM,blockData.feNaturalBCName.c_str()); CHKERRQ(ierr);
+    ierr = DMSetUp(blockData.dM); CHKERRQ(ierr);
+    // create matrices and vectors
+    ierr = DMCreateGlobalVector(blockData.dM,&blockData.D); CHKERRQ(ierr);
+    PetscFunctionReturn(0);
+  }
+
+  PetscErrorCode destroyProblem() {
+    PetscErrorCode ierr;
+    PetscFunctionBegin;
+    ierr = DMDestroy(&blockData.dM); CHKERRQ(ierr);
+    PetscFunctionReturn(0);
+  }
+
+  PetscErrorCode solveProblem() {
+    PetscErrorCode ierr;
+    PetscFunctionBegin;
+    ierr = DMCreateMatrix(blockData.dM,&blockData.A); CHKERRQ(ierr);
+    ierr = DMCreateGlobalVector(blockData.dM,&blockData.F); CHKERRQ(ierr);
+    ierr = VecDuplicate(blockData.F,&blockData.D); CHKERRQ(ierr);
+
+    VolumeFE vol_fe(mField);
+    vol_fe.getOpPtrVector().push_back(new OpCurlCurl(blockData));
+    TriFE tri_fe(mField);
+    tri_fe.getOpPtrVector().push_back(new OpNaturalBC(blockData));
+
+    std::vector<int> dofs_bc_indices;
+    const MoFEM::MoFEMProblem *problem_ptr;
+    ierr = DMMoFEMGetProblemPtr(blockData.dM,&problem_ptr); CHKERRQ(ierr);
+    for(Range::iterator eit = blockData.essentialBc.begin();eit!=blockData.essentialBc.end();eit++) {
+      for(_IT_NUMEREDDOFMOFEMENTITY_ROW_BY_NAME_ENT_PART_FOR_LOOP_(
+        problem_ptr,blockData.fieldName,*eit,mField.getCommRank(),dof_ptr
+      )) {
+        dofs_bc_indices.push_back(dof_ptr->get()->getPetscGlobalDofIdx());
+      }
+    }
+
+    const double diag = 1;
+    ierr = MatZeroRowsColumns(
+      blockData.A,
+      dofs_bc_indices.size(),
+      &*dofs_bc_indices.begin(),
+      diag,
+      PETSC_NULL,
+      blockData.F
+    ); CHKERRQ(ierr);
+
+    ierr = MatZeroEntries(blockData.A); CHKERRQ(ierr);
+    ierr = DMoFEMLoopFiniteElements(blockData.dM,blockData.feName.c_str(),&vol_fe); CHKERRQ(ierr);
+    ierr = DMoFEMLoopFiniteElements(blockData.dM,blockData.feNaturalBCName.c_str(),&tri_fe); CHKERRQ(ierr);
+
+    ierr = MatAssemblyBegin(blockData.A,MAT_FINAL_ASSEMBLY); CHKERRQ(ierr);
+    ierr = MatAssemblyEnd(blockData.A,MAT_FINAL_ASSEMBLY); CHKERRQ(ierr);
+    ierr = VecGhostUpdateBegin(blockData.F,ADD_VALUES,SCATTER_REVERSE); CHKERRQ(ierr);
+    ierr = VecGhostUpdateEnd(blockData.F,ADD_VALUES,SCATTER_REVERSE); CHKERRQ(ierr);
+    ierr = VecAssemblyBegin(blockData.F); CHKERRQ(ierr);
+    ierr = VecAssemblyEnd(blockData.F); CHKERRQ(ierr);
+
+    KSP solver;
+    ierr = KSPCreate(PETSC_COMM_WORLD,&solver); CHKERRQ(ierr);
+    ierr = KSPSetDM(solver,blockData.dM); CHKERRQ(ierr);
+    ierr = KSPSetOperators(solver,blockData.A,blockData.A); CHKERRQ(ierr);
+    ierr = KSPSetFromOptions(solver); CHKERRQ(ierr);
+    ierr = KSPSetUp(solver); CHKERRQ(ierr);
+    ierr = KSPSolve(solver,blockData.F,blockData.D); CHKERRQ(ierr);
+    ierr = VecGhostUpdateBegin(blockData.D,INSERT_VALUES,SCATTER_FORWARD); CHKERRQ(ierr);
+    ierr = VecGhostUpdateEnd(blockData.D,INSERT_VALUES,SCATTER_FORWARD); CHKERRQ(ierr);
+    ierr = KSPDestroy(&solver); CHKERRQ(ierr);
+    ierr = VecDestroy(&blockData.F); CHKERRQ(ierr);
+    ierr = MatDestroy(&blockData.A); CHKERRQ(ierr);
+    ierr = DMoFEMMeshToLocalVector(
+      blockData.dM,blockData.D,INSERT_VALUES,SCATTER_REVERSE
+    ); CHKERRQ(ierr);
+    ierr = VecDestroy(&blockData.D); CHKERRQ(ierr);
+    PetscFunctionReturn(0);
+  }
+
+  PetscErrorCode postProcessResults() {
+    PetscErrorCode ierr;
+    PetscFunctionBegin;
+    PostProcVolumeOnRefinedMesh post_proc(mField);
+    ierr = post_proc.generateReferenceElementMesh(); CHKERRQ(ierr);
+    ierr = post_proc.addFieldValuesPostProc(blockData.fieldName); CHKERRQ(ierr);
+    ierr = post_proc.addFieldValuesGradientPostProc(blockData.fieldName); CHKERRQ(ierr);
+    ierr = DMoFEMLoopFiniteElements(blockData.dM,blockData.feName.c_str(),&post_proc); CHKERRQ(ierr);
+    ierr = post_proc.writeFile("out_values.h5m"); CHKERRQ(ierr);
+    PetscFunctionReturn(0);
+  }
+
+
+  /** \brief calculate and assemble CurlCurl matrix
+  */
+  struct OpCurlCurl: public MoFEM::VolumeElementForcesAndSourcesCore::UserDataOperator {
+
+    BlockData &blockData;
+    OpCurlCurl(
+      BlockData &data
+    ):
+    MoFEM::VolumeElementForcesAndSourcesCore::UserDataOperator(
+      data.fieldName,UserDataOperator::OPROWCOL
+    ),
+    blockData(data) {
+    }
+    virtual ~OpCurlCurl() {}
+
+    MatrixDouble entityLocalMatrix;
+
+    PetscErrorCode doWork(
+      int row_side,int col_side,
+      EntityType row_type,EntityType col_type,
+      DataForcesAndSurcesCore::EntData &row_data,
+      DataForcesAndSurcesCore::EntData &col_data
+    ) {
+      PetscErrorCode ierr;
+      PetscFunctionBegin;
+
+      const int nb_row_dofs = row_data.getHcurlN().size2()/3;
+      if(nb_row_dofs==0) PetscFunctionReturn(0);
+      const int nb_col_dofs = col_data.getHcurlN().size2()/3;
+      if(nb_col_dofs==0) PetscFunctionReturn(0);
+      entityLocalMatrix.resize(nb_row_dofs,nb_col_dofs,false);
+
+      MatrixDouble row_curl_mat,col_curl_mat;
+      FTensor::Index<'i',3> i;
+
+      const int nb_gauss_pts = row_data.getHcurlN().size1();
+      for(int gg = 0;gg!=nb_gauss_pts;gg++) {
+
+        // get integration weight scaled by volume
+        double w = getGaussPts()(3,gg)*getVolume();
+        if(getHoGaussPtsDetJac().size()>0) {
+          // if ho geometry is given
+          w *= getHoGaussPtsDetJac()(gg);
+        }
+        ierr = getCurlOfHCurlBaseFunctions(
+          row_side,row_type,row_data,gg,row_curl_mat
+        ); CHKERRQ(ierr);
+        ierr = getCurlOfHCurlBaseFunctions(
+          col_side,col_type,col_data,gg,col_curl_mat
+        ); CHKERRQ(ierr);
+
+        FTensor::Tensor1<double*,3> t_row_curl(
+          &row_curl_mat(0,HCURL0),&row_curl_mat(0,HCURL1),&row_curl_mat(0,HCURL2),3
+        );
+        for(int aa = 0;aa!=nb_row_dofs;aa++) {
+          FTensor::Tensor0<double*> t_local_mat(&entityLocalMatrix(aa,0));
+          FTensor::Tensor1<double*,3> t_col_curl(
+            &col_curl_mat(0,HCURL0),&col_curl_mat(0,HCURL1),&col_curl_mat(0,HCURL2),3
+          );
+          for(int bb = 0;bb!=nb_col_dofs;bb++) {
+            t_local_mat += w*t_row_curl(i)*t_col_curl(i);
+            ++t_col_curl;
+            ++t_local_mat;
+          }
+          ++t_row_curl;
+        }
+
+      }
+
+      ierr = MatSetValues(
+        blockData.A,
+        nb_row_dofs,&row_data.getIndices()[0],
+        nb_col_dofs,&col_data.getIndices()[0],
+        &entityLocalMatrix(0,0),ADD_VALUES
+      ); CHKERRQ(ierr);
+
+      if(row_side != col_side || row_type != col_type) {
+        entityLocalMatrix = trans(entityLocalMatrix);
+        ierr = MatSetValues(
+          blockData.A,
+          nb_col_dofs,&col_data.getIndices()[0],
+          nb_row_dofs,&row_data.getIndices()[0],
+          &entityLocalMatrix(0,0),ADD_VALUES
+        ); CHKERRQ(ierr);
+      }
+
+      PetscFunctionReturn(0);
+    }
+
+  };
+
+  /** \brief calculate essential boundary conditions
+    */
+  struct OpNaturalBC: public MoFEM::FaceElementForcesAndSourcesCore::UserDataOperator {
+
+    BlockData &blockData;
+
+    OpNaturalBC(BlockData &data):
+    MoFEM::FaceElementForcesAndSourcesCore::UserDataOperator(data.fieldName,UserDataOperator::OPROW),
+    blockData(data) {
+    }
+
+    VectorDouble naturalBC;
+    PetscErrorCode doWork(
+      int row_side,EntityType row_type,DataForcesAndSurcesCore::EntData &row_data
+    ) {
+      PetscErrorCode ierr;
+      PetscFunctionBegin;
+
+      const int nb_row_dofs = row_data.getHcurlN().size2()/3;
+      if(nb_row_dofs==0) PetscFunctionReturn(0);
+      naturalBC.resize(nb_row_dofs,false);
+
+      FTensor::Index<'i',3> i;
+
+      const int nb_gauss_pts = row_data.getHcurlN().size1();
+      FTensor::Tensor1<double*,3> t_row_base = row_data.getFTensor1HcurlN<3>();
+
+      for(int gg = 0;gg!=nb_gauss_pts;gg++) {
+        // get integration weight scaled by volume
+        double area;
+        if(getNormals_at_GaussPt().size1() == (unsigned int)nb_gauss_pts) {
+          area = norm_2(getNormals_at_GaussPt(gg))*0.5;
+        } else {
+          area = getArea();
+        }
+        double w = getGaussPts()(2,gg)*area;
+
+        // Current is on surface where natural bc are applied. It is set that
+        // current is in XY plane, circular, around the coil.
+        FTensor::Tensor1<double,3> t_j;
+        const double x = getHoCoordsAtGaussPts()(gg,0);
+        const double y = getHoCoordsAtGaussPts()(gg,1);
+        const double r = sqrt(x*x+y*y);
+        t_j(0) = -y/r;
+        t_j(1) = +x/r;
+        t_j(2) = 0;
+
+        FTensor::Tensor0<double*> t_f(&naturalBC[0]);
+        for(int aa = 0;aa!=nb_row_dofs;aa++) {
+          t_f = w*t_row_base(i)*t_j(i);
+          ++t_row_base;
+          ++t_f;
+        }
+
+      }
+
+      ierr = VecSetValues(
+        blockData.F,
+        row_data.getIndices().size(),
+        &row_data.getIndices()[0],
+        &naturalBC[0],
+        ADD_VALUES
+      ); CHKERRQ(ierr);
+
+      PetscFunctionReturn(0);
+    }
+
+  };
+
+};
+
+#endif //__MAGNETICELEMENT_HPP__
