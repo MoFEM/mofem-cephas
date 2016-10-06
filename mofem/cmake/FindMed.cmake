5d6045de2ba1605f5432ebd71c47fccc3ba54191
Lukasz Kaczmarczyk <likask@wp.pl>
2016-10-06 23:31:55 +0100

    add med to docker images and testing on server

--- mofem/cmake/FindMed.cmake
+++ mofem/cmake/FindMed.cmake
@@ -12,3 +12,24 @@ if(MED_DIR)
     add_definitions(-DWITH_MED)
   endif(MED_LIBRARY)
 endif(MED_DIR)
+
+
+if(WITH_MED)
+  ExternalProject_Add(
+    med
+    PREFIX ${PROJECT_BINARY_DIR}/external/
+    URL http://files.salome-platform.org/Salome/other/med-3.2.0.tar.gz
+    CONFIGURE_COMMAND ${PROJECT_BINARY_DIR}/external/src/med/configure CPPFLAGS=${MOAB_CPPFLAGS} LDFLAGS=${MOAB_LDFLAGS} CC=${CMAKE_C_COMPILER} CFLAGS=${MOAB_CFLAGS} CXX=${CMAKE_CXX_COMPILER} CXXFLAGS=${MOAB_CXXFLAGS} --disable-shared --disable-python --disable-fortran --prefix=${PROJECT_BINARY_DIR}/external
+  )
+  add_dependencies(install_prerequisites med)
+endif(WITH_MED)
+
+if(WITH_MED)
+  if(NOT MED_LIBRARY)
+    include_directories(${PROJECT_BINARY_DIR}/external/include)
+    set(MED_DIR ${PROJECT_BINARY_DIR}/external CACHE FILEPATH "path to med" FORCE)
+    set(MED_LIBRARY ${MED_DIR}/lib/libmed.a CACHE FILEPATH "med lib" FORCE)
+    add_definitions(-DWITH_MED)
+    message(STATUS ${MED_LIBRARY})
+  endif(NOT MED_LIBRARY)
+endif(WITH_MED)
