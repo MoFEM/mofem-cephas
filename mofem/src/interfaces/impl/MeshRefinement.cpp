6ef5c5177a13184879bed781f575883381572cc2
Lukasz Kaczmarczyk <likask@wp.pl>
2016-10-13 11:00:55 +0100

    fix includes in right order

--- mofem/src/interfaces/impl/MeshRefinement.cpp
+++ mofem/src/interfaces/impl/MeshRefinement.cpp
@@ -40,11 +40,11 @@
 
 #include <LoopMethods.hpp>
 #include <Interface.hpp>
-#include <MeshRefinement.hpp>
 #include <PrismInterface.hpp>
 #include <SeriesRecorder.hpp>
 #include <Core.hpp>
 
+#include <MeshRefinement.hpp>
 #include <EntityRefine.hpp>
 
 namespace MoFEM {
