3b86e77ceb62fbaeda6a41e2021e227c613b1626
Lukasz Kaczmarczyk <likask@wp.pl>
2016-11-08 16:56:58 +0000

    small change in test

--- mofem/atom_tests/dm_build_partitioned_mesh.cpp
+++ mofem/atom_tests/dm_build_partitioned_mesh.cpp
@@ -111,11 +111,18 @@ int main(int argc, char *argv[]) {
   ierr = DMMoFEMAddElement(dm,"FE"); CHKERRQ(ierr);
   ierr = DMSetUp(dm); CHKERRQ(ierr);
 
-  ierr = m_field.partition_check_matrix_fill_in("DMMOFEM",-1,-1,1); CHKERRQ(ierr);
-
   // dump data to file, just to check if something was changed
   Mat m;
   ierr = DMCreateMatrix(dm,&m); CHKERRQ(ierr);
+
+  // if(1) {
+  //   MatView(m,PETSC_VIEWER_DRAW_WORLD);
+  //   std::string wait;
+  //   std::cin >> wait;
+  // }
+
+  ierr = m_field.partition_check_matrix_fill_in("DMMOFEM",-1,-1,1); CHKERRQ(ierr);
+
 
   PetscBool save_file = PETSC_TRUE;
 
