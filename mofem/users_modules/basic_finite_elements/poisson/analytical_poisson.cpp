5c520114365908e9b640b7d689f6eca296e82057
Lukasz Kaczmarczyk <likask@wp.pl>
2017-06-28 13:27:49 +0100

    fix problem with compilation of poisson example without precompiled headers

--- mofem/users_modules/basic_finite_elements/poisson/analytical_poisson.cpp
+++ mofem/users_modules/basic_finite_elements/poisson/analytical_poisson.cpp
@@ -23,8 +23,7 @@
 * You should have received a copy of the GNU Lesser General Public
 * License along with MoFEM. If not, see <http://www.gnu.org/licenses/>. */
 
-#include <MoFEM.hpp>
-using namespace MoFEM;
+#include <BasicFiniteElements.hpp>
 #include <PoissonOperators.hpp>
 using namespace PoissonOperators;
 
