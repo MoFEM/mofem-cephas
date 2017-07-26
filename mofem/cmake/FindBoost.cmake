0cbbadd1b836e531d762439649194f27e75c8c78
Lukasz Kaczmarczyk <likask@wp.pl>
2017-07-26 13:53:16 +0100

    Improving boost configuration and set to search include files before system files. Fixes problem when boost installed in the system but one like to use own boost compilation in local directory.

---
+++ mofem/cmake/FindBoost.cmake
@@ -0,0 +1,28 @@
+
+# Add BOOST directory
+if(NOT BOOST_DIR)
+  set(BOOST_DIR $ENV{BOOST_DIR})
+endif(NOT BOOST_DIR)
+
+find_library(
+  BOOST_PROGRAM_OPTIONS_LIB NAMES boost_program_options
+  HINTS
+  ${BOOST_DIR}/lib
+  ${PETSC_DIR}/${PETSC_ARCH}/lib
+)
+message(STATUS "Boost found: " ${BOOST_PROGRAM_OPTIONS_LIB})
+if(NOT BOOST_PROGRAM_OPTIONS_LIB)
+  message(FATAL_ERROR "boost program options library not found")
+endif(NOT BOOST_PROGRAM_OPTIONS_LIB)
+find_path(
+  BOOST_INCLUDE_DIR
+  NAMES boost/multi_index_container.hpp
+  HINTS
+  ${BOOST_DIR}/include
+  ${PETSC_DIR}/${PETSC_ARCH}/include
+)
+message(STATUS "Boost include found: " ${BOOST_INCLUDE_DIR})
+if(NOT BOOST_INCLUDE_DIR)
+  message(FATAL_ERROR "boost program incoude dir not found")
+endif(NOT BOOST_INCLUDE_DIR)
+include_directories(${BOOST_INCLUDE_DIR})
