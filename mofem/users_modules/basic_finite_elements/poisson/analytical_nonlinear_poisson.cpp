18adb3fe7f86753af72c8d3292337db182bb333d
Karol Lewandowski <k.lewandowski.1@research.gla.ac.uk>
2017-07-27 13:06:58 +0100

    I think it is better to change the name of this structure, because now every single letter A in entire MoFEM documentation is a link to this.

--- mofem/users_modules/basic_finite_elements/poisson/analytical_nonlinear_poisson.cpp
+++ mofem/users_modules/basic_finite_elements/poisson/analytical_nonlinear_poisson.cpp
@@ -76,7 +76,7 @@ struct ExactLaplacianFunction {
   }
 };
 
-struct A {
+struct FunctionA {
   double operator()(const double u) { return 1+u*u; }
 };
 
@@ -142,7 +142,7 @@ int main(int argc, char *argv[]) {
       // Add problem specific operators the generic finite elements to calculate matrices and vectors.
       ierr = PoissonExample::CreateFiniteElements(m_field).createFEToAssmbleMatrceAndVectorForNonlinearProblem(
         ExactFunction(),ExactLaplacianFunction(),
-        A(),DiffA(),
+        FunctionA(),DiffA(),
         domain_lhs_fe,boundary_lhs_fe,domain_rhs_fe,boundary_rhs_fe,
         VolRuleNonlinear()
       ); CHKERRQ(ierr);
