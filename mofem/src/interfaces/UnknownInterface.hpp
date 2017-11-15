f26f59e73c41364fc980805d3541643cdf9ccb1e
Lukasz Kaczmarczyk <likask@wp.pl>
2017-11-15 11:24:42 +0000

    add comments

--- mofem/src/interfaces/UnknownInterface.hpp
+++ mofem/src/interfaces/UnknownInterface.hpp
@@ -71,7 +71,7 @@ struct UnknownInterface {
    *
    * @param uuid
    * @param true
-   * @return template <class IFACE> MoFEMErrorCode
+   * @return MoFEMErrorCode
    */
   template <class IFACE>
   MoFEMErrorCode registerInterface(const MOFEMuuid &uuid,
@@ -91,9 +91,12 @@ struct UnknownInterface {
   /**
    * @brief Get interface by uuid and return reference to pointer of interface
    *
+   * \note uuid of interface and interface are verified, if second template
+   * parameter is true. Function throw error if both do not match.
+   *
    * @param uuid
-   * @param iface
-   * @return template <class IFACE, bool VERIFY>  MoFEMErrorCode
+   * @param iface reference to a interface pointer
+   * @return MoFEMErrorCode
    */
   template <class IFACE, bool VERIFY /* =false C++11 needed to have this */ >
   inline MoFEMErrorCode getInterface(const MOFEMuuid &uuid,
@@ -115,8 +118,28 @@ struct UnknownInterface {
   /**
    * @brief Get interface refernce to pointer of interface
    *
-   * @param iface
-   * @return template <class IFACE>  MoFEMErrorCode
+   * \code
+   * // Create moab database
+   * moab::Core mb_instance;
+   * // Access moab database by interface
+   * moab::Interface &moab = mb_instance;
+   *
+   * // Create MoFEM database
+   * MoFEM::Core core(moab);
+   * // Acces MoFEM database by Interface
+   * MoFEM::Interface &m_field = core;
+   *
+   * // Get interface
+   * // Get simple interface is simplified version enabling quick and
+   * // easy construction of problem.
+   * Simple *simple_interface;
+   * // Query interface and get pointer to Simple interface
+   *  CHKERR m_field.getInterface(simple_interface);
+   *
+   * \endcode
+   *
+   * @param iface reference to a interface pointer
+   * @return MoFEMErrorCode
    */
   template <class IFACE>
   inline MoFEMErrorCode getInterface(IFACE *&iface) const {
@@ -127,8 +150,29 @@ struct UnknownInterface {
   /**
    * @brief Get interface pointer to pointer of interface
    *
-   * @param iface
-   * @return template <class IFACE>  MoFEMErrorCode
+   * \code
+   * // Create moab database
+   * moab::Core mb_instance;
+   * // Access moab database by interface
+   * moab::Interface &moab = mb_instance;
+   *
+   * // Create MoFEM database
+   * MoFEM::Core core(moab);
+   * // Acces MoFEM database by Interface
+   * MoFEM::Interface &m_field = core;
+   *
+   * // Get interface
+   * // Get simple interface is simplified version enabling quick and
+   * // easy construction of problem.
+   * Simple *simple_interface;
+   * // Query interface and get pointer to Simple interface
+   *  CHKERR m_field.getInterface(&simple_interface);
+   *
+   * \endcode
+   *
+   *
+   * @param iface const pointer to a interface pointer
+   * @return MoFEMErrorCode
    */
   template <class IFACE>
   inline MoFEMErrorCode getInterface(IFACE **const iface) const {
@@ -136,6 +180,29 @@ struct UnknownInterface {
                                       *iface);
   }
 
+  /**
+   * @brief Get interface pointer to pointer of interface
+   *
+   * \code
+   * // Create moab database
+   * moab::Core mb_instance;
+   * // Access moab database by interface
+   * moab::Interface &moab = mb_instance;
+   *
+   * // Create MoFEM database
+   * MoFEM::Core core(moab);
+   * // Acces MoFEM database by Interface
+   * MoFEM::Interface &m_field = core;
+   *
+   * // Get interface
+   * // Get simple interface is simplified version enabling quick and
+   * // easy construction of problem.
+   * Simple *simple_interface = m_field.getInterface<Simple*,0>();
+   *
+   * \endcode
+   *
+   * @return *IFACE
+   */
   template <
     class IFACE, 
     typename boost::enable_if<boost::is_pointer<IFACE>, int>::type /* =0 C++11 needed to have this */  
@@ -149,6 +216,29 @@ struct UnknownInterface {
     return iface;
   }
 
+ /**
+   * @brief Get reference to interface 
+   *
+   * \code
+   * // Create moab database
+   * moab::Core mb_instance;
+   * // Access moab database by interface
+   * moab::Interface &moab = mb_instance;
+   *
+   * // Create MoFEM database
+   * MoFEM::Core core(moab);
+   * // Acces MoFEM database by Interface
+   * MoFEM::Interface &m_field = core;
+   *
+   * // Get interface
+   * // Get simple interface is simplified version enabling quick and
+   * // easy construction of problem.
+   * Simple &simple_interface = m_field.getInterface<Simple&,0>();
+   *
+   * \endcode
+   *
+   * @return &IFACE
+   */
   template <
     class IFACE, 
     typename boost::enable_if<boost::is_reference<IFACE>, int>::type /* =0 C++11 needed to have this */  
@@ -165,7 +255,25 @@ struct UnknownInterface {
   /**
    * @brief Function returning pointer to interface
    *
-   * @return template <class IFACE>  IFACE*
+   * \code
+   * // Create moab database
+   * moab::Core mb_instance;
+   * // Access moab database by interface
+   * moab::Interface &moab = mb_instance;
+   *
+   * // Create MoFEM database
+   * MoFEM::Core core(moab);
+   * // Acces MoFEM database by Interface
+   * MoFEM::Interface &m_field = core;
+   *
+   * // Get interface
+   * // Get simple interface is simplified version enabling quick and
+   * // easy construction of problem.
+   * Simple *simple_interface = m_field.getInterface<Simple,0>();
+   *
+   * \endcode
+   *
+   * @return IFACE*
    */
   template <class IFACE>
   inline IFACE* getInterface() const {
