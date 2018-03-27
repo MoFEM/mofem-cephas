f4e004ef988abab57101a80945371992b6cbc998
Karol Lewandowski <k.lewandowski.1@research.gla.ac.uk>
2018-03-27 14:22:03 +0100

    clean up the code, check  for H1 space

--- mofem/users_modules/tools/field_to_vertices.cpp
+++ mofem/users_modules/tools/field_to_vertices.cpp
@@ -59,31 +59,15 @@ int main(int argc, char *argv[]) {
       SETERRQ(PETSC_COMM_SELF, MOFEM_INVALID_DATA,
               "*** ERROR -my_file (MESH FILE NEEDED)");
     }
-    // if (flg_list_of_sidesets != PETSC_TRUE) {
-    //   SETERRQ(PETSC_COMM_SELF, MOFEM_INVALID_DATA,
-    //           "List of sidesets not given -my_side_sets ...");
-    // }
     BitRefLevel bit_level0;
     bit_level0.set(0);
     ierr = m_field.seed_ref_level_3D(0,bit_level0); CHKERRQ(ierr);
-
     ierr = m_field.build_fields();
-
-    //ierr = m_field.list_fields(); CHKERRQ(ierr);
-
-    // Projection10NodeCoordsOnField ents_method(m_field,"MESH_NODE_POSITIONS");
-    // CHKERR m_field.loop_dofs("MESH_NODE_POSITIONS",ents_method);
-
-    if(m_field.check_field("rho")) {
-      field_name = "rho";
-    }
 
     bool field_flg = false;
     const Field_multiIndex *fields_ptr;
     CHKERR m_field.get_fields(&fields_ptr);
     for(auto field : (*fields_ptr)) {
-      // cerr << field->getName() << "\n";
-      // cerr << field->getNbOfCoeffs() << "\n";
       bool check_space = field->getSpace() == H1;
       if(field->getName() == field_name && check_space) field_flg = true;
     }
@@ -91,32 +75,12 @@ int main(int argc, char *argv[]) {
       SETERRQ(PETSC_COMM_SELF, MOFEM_INVALID_DATA,
               "*** ERROR -my_field (FIELD (in H1 space) is NOT FOUND)");
     }
-    // const FieldEntity_multiIndex *field_ents;
-    // CHKERR m_field.get_field_ents(field_ents);
-    // auto low_it = field_ents.get<FieldName_mi_tag>.lower_bound("RHO");
-    // auto hi_it = field_ents.get<FieldName_mi_tag>.upper_bound("RHO");
-    // for(;low_it!=hi_it;++low_it) {
-    //   if(low_it->get()->getEntType()==MBVERTEX) {
-    //     double data = low_it->get()->getEntFieldData()[0];
-    //     EntityHandle ent = low_it->get()->getEnt();
-    //     CHKERR moab.tag_set_data(th,&ent,1,&data);
 
-    //   }
-    // }
 
-    // PostProcVertexMethod ent_method(m_field, field_name.c_str());
     SaveVertexDofOnTag ent_method(m_field, field_name.c_str());
 
     CHKERR m_field.loop_dofs(field_name.c_str(),ent_method);
-    // CHKERR m_field.loop_dofs("MESH_NODE_POSITIONS",ent_method);
     PetscPrintf(PETSC_COMM_WORLD, "\nDone. Saving files... \n");
-            // Range nodes_tets;
-            // CHKERR moab.get_entities_by_type(0, MBVERTEX, nodes_tets, true);
-            // for ( auto it : nodes_tets){
-            //   double var;
-            //   moab.tag_get_data(ent_method.tH,&it,1,&var);
-            //   cerr << "DATA on " << it << " is: " << var << '\n';
-            // }
 
     //TODO: Higher order field mapping
     CHKERR m_field.getInterface<BitRefManager>()->writeBitLevelByType(bit_level0, BitRefLevel().set(), MBTET,
