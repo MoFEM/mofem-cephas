8a957834e875ea31dc3537d92b4ef8b730fc264f
Lukasz Kaczmarczyk <likask@wp.pl>
2018-04-29 10:19:11 +0100

    format file

--- mofem/atom_tests/loops_test.cpp
+++ mofem/atom_tests/loops_test.cpp
@@ -16,94 +16,94 @@
 
 using namespace MoFEM;
 
-
-
-
 static char help[] = "...\n\n";
 
 int main(int argc, char *argv[]) {
 
-  MoFEM::Core::Initialize(&argc,&argv,(char *)0,help);
+  MoFEM::Core::Initialize(&argc, &argv, (char *)0, help);
 
   try {
 
     moab::Core mb_instance;
-    moab::Interface& moab = mb_instance;
+    moab::Interface &moab = mb_instance;
     int rank;
-    MPI_Comm_rank(PETSC_COMM_WORLD,&rank);
+    MPI_Comm_rank(PETSC_COMM_WORLD, &rank);
 
-    //Read parameters from line command
+    // Read parameters from line command
     PetscBool flg = PETSC_TRUE;
     char mesh_file_name[255];
-    #if PETSC_VERSION_GE(3,6,4)
-    ierr = PetscOptionsGetString(PETSC_NULL,"","-my_file",mesh_file_name,255,&flg); CHKERRG(ierr);
-    #else
-    ierr = PetscOptionsGetString(PETSC_NULL,PETSC_NULL,"-my_file",mesh_file_name,255,&flg); CHKERRG(ierr);
-    #endif
-    if(flg != PETSC_TRUE) {
-      SETERRQ(PETSC_COMM_SELF,1,"*** ERROR -my_file (MESH FILE NEEDED)");
+#if PETSC_VERSION_GE(3, 6, 4)
+    ierr = PetscOptionsGetString(PETSC_NULL, "", "-my_file", mesh_file_name,
+                                 255, &flg);
+    CHKERRG(ierr);
+#else
+    ierr = PetscOptionsGetString(PETSC_NULL, PETSC_NULL, "-my_file",
+                                 mesh_file_name, 255, &flg);
+    CHKERRG(ierr);
+#endif
+    if (flg != PETSC_TRUE) {
+      SETERRQ(PETSC_COMM_SELF, 1, "*** ERROR -my_file (MESH FILE NEEDED)");
     }
 
-    //Read mesh to MOAB
+    // Read mesh to MOAB
     const char *option;
     option = "";
-    rval = moab.load_file(mesh_file_name, 0, option); CHKERRG(rval);
-    ParallelComm* pcomm = ParallelComm::get_pcomm(&moab,MYPCOMM_INDEX);
-    if(pcomm == NULL) pcomm =  new ParallelComm(&moab,PETSC_COMM_WORLD);
+    rval = moab.load_file(mesh_file_name, 0, option);
+    CHKERRG(rval);
+    ParallelComm *pcomm = ParallelComm::get_pcomm(&moab, MYPCOMM_INDEX);
+    if (pcomm == NULL)
+      pcomm = new ParallelComm(&moab, PETSC_COMM_WORLD);
 
-    //Create MoFEM (Joseph) database
+    // Create MoFEM (Joseph) database
     MoFEM::Core core(moab);
-    MoFEM::Interface& m_field = core;
+    MoFEM::Interface &m_field = core;
 
-    //Open mesh_file_name.txt for writing
+    // Open mesh_file_name.txt for writing
     std::ofstream myfile;
-    myfile.open ((std::string(mesh_file_name)+".txt").c_str());
+    myfile.open((std::string(mesh_file_name) + ".txt").c_str());
 
     std::cout << "<<<< All BLOCKSETs, SIDESETs and NODESETs >>>>>" << std::endl;
-    for(_IT_CUBITMESHSETS_FOR_LOOP_(m_field,it)) {
-      std::cout<< it->getName() << std::endl;
+    for (_IT_CUBITMESHSETS_FOR_LOOP_(m_field, it)) {
+      std::cout << it->getName() << std::endl;
       myfile << it->getName() << std::endl;
     }
     std::cout << "<<<< BLOCKSETs >>>>>" << std::endl;
-    for(_IT_CUBITMESHSETS_BY_SET_TYPE_FOR_LOOP_(m_field,BLOCKSET,it)) {
-      std::cout<< it->getName() << std::endl;
+    for (_IT_CUBITMESHSETS_BY_SET_TYPE_FOR_LOOP_(m_field, BLOCKSET, it)) {
+      std::cout << it->getName() << std::endl;
       myfile << it->getName() << std::endl;
     }
     std::cout << "<<<< NODESETs >>>>>" << std::endl;
-    for(_IT_CUBITMESHSETS_BY_SET_TYPE_FOR_LOOP_(m_field,NODESET,it)) {
-      std::cout<< it->getName() << std::endl;
+    for (_IT_CUBITMESHSETS_BY_SET_TYPE_FOR_LOOP_(m_field, NODESET, it)) {
+      std::cout << it->getName() << std::endl;
       myfile << it->getName() << std::endl;
     }
     std::cout << "<<<< SIDESETs >>>>>" << std::endl;
-    for(_IT_CUBITMESHSETS_BY_SET_TYPE_FOR_LOOP_(m_field,SIDESET,it)) {
-      std::cout<< it->getName() << std::endl;
-      myfile << it->getName() << std::endl;
-    }
-    std::cout <<"<<<< MeshSet of Name Moon >>>>" << std::endl;
-    for (_IT_CUBITMESHSETS_BY_NAME_FOR_LOOP_(m_field,"Moon",it)){
+    for (_IT_CUBITMESHSETS_BY_SET_TYPE_FOR_LOOP_(m_field, SIDESET, it)) {
       std::cout << it->getName() << std::endl;
       myfile << it->getName() << std::endl;
-      if(it->getBcTypeULong() & BLOCKSET) {
+    }
+    std::cout << "<<<< MeshSet of Name Moon >>>>" << std::endl;
+    for (_IT_CUBITMESHSETS_BY_NAME_FOR_LOOP_(m_field, "Moon", it)) {
+      std::cout << it->getName() << std::endl;
+      myfile << it->getName() << std::endl;
+      if (it->getBcTypeULong() & BLOCKSET) {
         std::cout << "BLOCKSET" << std::endl;
         myfile << "BLOCKSET" << std::endl;
       }
-      if(it->getBcTypeULong() & SIDESET) {
+      if (it->getBcTypeULong() & SIDESET) {
         std::cout << "SIDESET" << std::endl;
         myfile << "SIDESET" << std::endl;
       }
-      if(it->getBcTypeULong() & NODESET) {
+      if (it->getBcTypeULong() & NODESET) {
         std::cout << "NODESET" << std::endl;
         myfile << "NODESET" << std::endl;
       }
     }
 
-    //Close mesh_file_name.txt
+    // Close mesh_file_name.txt
     myfile.close();
-
-  } catch (MoFEMException const &e) {
-    SETERRQ(PETSC_COMM_SELF,e.errorCode,e.errorMessage);
   }
+  CATCH_ERRORS;
 
   MoFEM::Core::Finalize();
-
 }
