9fbc38f34f97e0d44d35586ff5040b0b11185330
Lukasz Kaczmarczyk <likask@wp.pl>
2018-06-05 20:32:14 +0100

    fix tetegen install

--- mofem/cmake/FindTetGen.cmake
+++ mofem/cmake/FindTetGen.cmake
@@ -17,8 +17,8 @@ if(WITH_TETGEN)
   ExternalProject_Add(
     tetgen
     PREFIX ${PROJECT_BINARY_DIR}/external/
-    URL https://bitbucket.org/likask/mofem-joseph/downloads/tetgen1.5.0.tgz
-    CONFIGURE_COMMAND cmake ${PROJECT_BINARY_DIR}/external/src/tetgen/tetgen1.5.0
+    URL https://bitbucket.org/likask/mofem-joseph/downloads/tetgen-1.5.0.tgz
+    CONFIGURE_COMMAND cmake ${PROJECT_BINARY_DIR}/external/src/tetgen
     BUILD_COMMAND make
     INSTALL_COMMAND ""
   )
@@ -26,7 +26,7 @@ if(WITH_TETGEN)
   add_custom_target(
     copy_tetgen_fields
     ALL
-    COMMAND ${CMAKE_COMMAND} -E copy_if_different ${PROJECT_BINARY_DIR}/external/src/tetgen/tetgen1.5.0/include/tetgen.h ${PROJECT_BINARY_DIR}/external/include
+    COMMAND ${CMAKE_COMMAND} -E copy_if_different ${PROJECT_BINARY_DIR}/external/src/tetgen/include/tetgen.h ${PROJECT_BINARY_DIR}/external/include
     COMMAND ${CMAKE_COMMAND} -E copy_if_different ${PROJECT_BINARY_DIR}/external/src/tetgen-build/libtet* ${PROJECT_BINARY_DIR}/external/lib
     COMMAND ${CMAKE_COMMAND} -E copy_if_different ${PROJECT_BINARY_DIR}/external/src/tetgen-build/tetgen ${PROJECT_BINARY_DIR}/external/bin
     DEPENDS tetgen
