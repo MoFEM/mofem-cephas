1e37c5308e241ef1a1f2480fadbf65b41ceb657d
Lukasz Kaczmarczyk <likask@wp.pl>
2018-07-03 17:32:21 +0100

    installation with spack and many other cmake fixes

--- mofem/cmake/FindTetGen.cmake
+++ mofem/cmake/FindTetGen.cmake
@@ -5,12 +5,27 @@ if(NOT TETGEN_DIR)
 endif(NOT TETGEN_DIR)
 
 if(TETGEN_DIR)
-  find_library(TETGEN_LIBRARY NAMES tet PATHS ${TETGEN_DIR}/lib)
+  find_library(
+    TETGEN_LIBRARY 
+    NAMES tet 
+    PATHS ${TETGEN_DIR}/lib
+    NO_DEFAULT_PATH
+    NO_CMAKE_ENVIRONMENT_PATH
+    NO_CMAKE_PATH
+    NO_SYSTEM_ENVIRONMENT_PATH
+    NO_CMAKE_SYSTEM_PATH
+    CMAKE_FIND_ROOT_PATH_BOTH
+  )
+  find_path(TETGEN_HEADER
+    NAMES tetgen.h
+    PATHS ${TETGEN_DIR}/include
+  )
   message(STATUS ${TETGEN_LIBRARY})
-  if(TETGEN_LIBRARY)
-    include_directories(${TETGEN_DIR}/include)
+  message(STATUS ${TETGEN_HEADER})
+  if(TETGEN_LIBRARY AND TETGEN_HEADER)
+    include_directories(${TETGEN_HEADER})
     add_definitions(-DWITH_TETGEN)
-  endif(TETGEN_LIBRARY)
+  endif(TETGEN_LIBRARY AND TETGEN_HEADER)
 endif(TETGEN_DIR)
 
 if(WITH_TETGEN)
@@ -36,10 +51,12 @@ endif(WITH_TETGEN)
 
 if(WITH_TETGEN)
   if(NOT TETGEN_LIBRARY)
-    include_directories(${PROJECT_BINARY_DIR}/external/include)
-    add_definitions(-DWITH_TETGEN)
     set(TETGEN_DIR ${PROJECT_BINARY_DIR}/external CACHE FILEPATH "path to tetgen dir"  FORCE)
+    set(TETGEN_HEADER ${TETGEN_DIR}/include)
+    include_directories(${TETGEN_HEADER})
     set(TETGEN_LIBRARY ${TETGEN_DIR}/lib/libtet.a)
+    add_definitions(-DWITH_TETGEN)
     message(STATUS ${TETGEN_LIBRARY})
+    message(STATUS ${TETGEN_HEADER})
   endif(NOT TETGEN_LIBRARY)
 endif(WITH_TETGEN)
