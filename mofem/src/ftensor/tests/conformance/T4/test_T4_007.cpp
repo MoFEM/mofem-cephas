34e6f78f452dd53df3082ec2ce6a2f88e51fada0
Lukasz Kaczmarczyk <likask@wp.pl>
2018-09-03 08:27:44 +0100

    add test for tensor4 times tensor 3 yielding tensor 3

--- mofem/src/ftensor/tests/conformance/T4/test_T4_007.cpp
+++ mofem/src/ftensor/tests/conformance/T4/test_T4_007.cpp
@@ -137,7 +137,7 @@ void test_T4_007(const Tensor4<double, 1, 2, 3, 4> &t4,
         }
   }
 
-  // /* Tensor4 to a Tensor4, yielding a Ddg. */
+  //  Tensor4 to a Tensor4, yielding a Ddg.
   {
     Index<'i', 3> i;
     Index<'j', 3> j;
@@ -146,21 +146,54 @@ void test_T4_007(const Tensor4<double, 1, 2, 3, 4> &t4,
     Ddg<double, 3, 3> t_ddg;
     Tensor4<double, 3, 3, 2, 2> t_4;
     for (int ii = 0; ii != 3; ++ii)
-      for (int jj = 0; jj != 3;++jj)
-        for (int kk = 0; kk != 2;++kk)
-          for (int ll = 0; ll != 2;++ll) {
+      for (int jj = 0; jj != 3; ++jj)
+        for (int kk = 0; kk != 2; ++kk)
+          for (int ll = 0; ll != 2; ++ll) {
             t_4(ii, jj, kk, ll) = ii + 10. * jj + 100 * kk + 1000 * ll;
           }
     t_ddg(i, j, k, l) = t_4(i, j, k, l) || t_4(j, i, l, k);
     for (int ii = 0; ii != 3; ++ii)
-      for (int jj = 0; jj != 3;++jj)
-        for (int kk = 0; kk != 2;++kk)
-          for (int ll = 0; ll != 2;++ll) {
+      for (int jj = 0; jj != 3; ++jj)
+        for (int kk = 0; kk != 2; ++kk)
+          for (int ll = 0; ll != 2; ++ll) {
             test_for_zero(t_ddg(ii, jj, ll, kk) - t_4(ii, jj, kk, ll) -
                               t_4(jj, ii, ll, kk),
                           "T4(i,j,k,l)||T4(j,i,l,k)(" + to_string(ii) + "," +
                               to_string(jj) + "," + to_string(ll) + "," +
                               to_string(kk) + ")");
-          } 
+          }
+  }
+
+  //  Tensor4 times tensor 3 yields tensor 3
+  {
+    Tensor4<double, 1, 2, 3, 4> t_4;
+    Tensor2<double, 1, 2> t_2_1;
+    Tensor2<double, 3, 4> t_2_2;
+    for (int ii = 0; ii != 1; ++ii)
+      for (int jj = 0; jj != 2; ++jj) {
+        t_2_1(ii, jj) = ii + 10. * jj;
+      }
+    for (int kk = 0; kk != 2; ++kk)
+      for (int ll = 0; ll != 2; ++ll) {
+        t_2_2(kk, ll) = 100 * kk + 1000 * ll;
+      }
+    t_4(i, j, k, l) = t_2_1(i, j) * t_2_2(k, l);
+    Tensor3<double, 3, 4, 3> t_3_1;
+    for (int kk = 0; kk != 2; ++kk)
+      for (int ll = 0; ll != 2; ++ll)
+        for (int mm = 0; mm != 3; ++mm) {
+          t_3_1(kk, ll, mm) = 100 * kk + 1000 * ll + 10000 * mm;
+        }
+    Tensor3<double, 1, 2, 3> t_3_2;
+    Index<'m', 3> m;
+    t_3_2(i, j, m) = t_4(i, j, k, l) * t_3_1(k, l, m) -
+                     t_2_1(i, j) * (t_3_1(k, l, m) * t_2_2(k, l));
+    for (int ii = 0; ii != 1; ++ii)
+      for (int jj = 0; jj != 2; ++jj)
+        for (int mm = 0; mm != 3; ++mm) {
+          test_for_zero(t_3_2(ii, jj, mm),
+                        "T4(i,j,k,l)*T3(k,l,m)(" + to_string(ii) + "," +
+                            to_string(jj) + "," + to_string(mm) + ")");
+        }
   }
 }
