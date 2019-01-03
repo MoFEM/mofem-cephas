b282dee80c9657c0050b74bc3c94fc5692e6c87c
Lukasz Kaczmarczyk <likask@wp.pl>
2019-01-03 22:41:15 +0000

    format code

--- mofem/atom_tests/build_problems.cpp
+++ mofem/atom_tests/build_problems.cpp
@@ -27,140 +27,140 @@ static char help[] = "...\n\n";
 
 int main(int argc, char *argv[]) {
 
-
-
-
-  MoFEM::Core::Initialize(&argc,&argv,(char *)0,help);
+  MoFEM::Core::Initialize(&argc, &argv, (char *)0, help);
 
   try {
 
     moab::Core mb_instance;
-    moab::Interface& moab = mb_instance;
+    moab::Interface &moab = mb_instance;
     int rank;
-    MPI_Comm_rank(PETSC_COMM_WORLD,&rank);
+    MPI_Comm_rank(PETSC_COMM_WORLD, &rank);
 
     PetscBool flg = PETSC_TRUE;
     char mesh_file_name[255];
-    #if PETSC_VERSION_GE(3,6,4)
-    ierr = PetscOptionsGetString(PETSC_NULL,"","-my_file",mesh_file_name,255,&flg); CHKERRG(ierr);
-    #else
-    ierr = PetscOptionsGetString(PETSC_NULL,PETSC_NULL,"-my_file",mesh_file_name,255,&flg); CHKERRG(ierr);
-    #endif
-    if(flg != PETSC_TRUE) {
-      SETERRQ(PETSC_COMM_SELF,1,"*** ERROR -my_file (MESH FILE NEEDED)");
+#if PETSC_VERSION_GE(3, 6, 4)
+    CHKERR PetscOptionsGetString(PETSC_NULL, "", "-my_file", mesh_file_name,
+                                 255, &flg);
+#else
+    CHKERR PetscOptionsGetString(PETSC_NULL, PETSC_NULL, "-my_file",
+                                 mesh_file_name, 255, &flg);
+#endif
+    if (flg != PETSC_TRUE) {
+      SETERRQ(PETSC_COMM_SELF, 1, "*** ERROR -my_file (MESH FILE NEEDED)");
     }
 
-    ParallelComm* pcomm = ParallelComm::get_pcomm(&moab,MYPCOMM_INDEX);
-    if(pcomm == NULL) pcomm =  new ParallelComm(&moab,PETSC_COMM_WORLD);
+    ParallelComm *pcomm = ParallelComm::get_pcomm(&moab, MYPCOMM_INDEX);
+    if (pcomm == NULL)
+      pcomm = new ParallelComm(&moab, PETSC_COMM_WORLD);
 
     const char *option;
-    option = "";//"PARALLEL=BCAST;";//;DEBUG_IO";
-    rval = moab.load_file(mesh_file_name, 0, option); CHKERRG(rval);
+    option = ""; //"PARALLEL=BCAST;";//;DEBUG_IO";
+    CHKERR moab.load_file(mesh_file_name, 0, option);
 
-    //Create MoFEM (Joseph) database
+    // Create MoFEM (Joseph) database
     MoFEM::Core core(moab);
-    MoFEM::Interface& m_field = core;
+    MoFEM::Interface &m_field = core;
 
-    //set entitities bit level
+    // set entitities bit level
     BitRefLevel bit_level0;
     bit_level0.set(0);
     EntityHandle meshset_level0;
-    rval = moab.create_meshset(MESHSET_SET,meshset_level0); CHKERRG(rval);
-    ierr = m_field.getInterface<BitRefManager>()->setBitRefLevelByDim(0,3,bit_level0); CHKERRG(ierr);
+    CHKERR moab.create_meshset(MESHSET_SET, meshset_level0);
+    CHKERR m_field.getInterface<BitRefManager>()->setBitRefLevelByDim(
+        0, 3, bit_level0);
 
-    //Fields
-    ierr = m_field.add_field("F1",L2,AINSWORTH_LEGENDRE_BASE,1); CHKERRG(ierr);
-    ierr = m_field.add_field("F2",HDIV,AINSWORTH_LEGENDRE_BASE,1); CHKERRG(ierr);
+    // Fields
+    CHKERR m_field.add_field("F1", L2, AINSWORTH_LEGENDRE_BASE, 1);
+    CHKERR m_field.add_field("F2", HDIV, AINSWORTH_LEGENDRE_BASE, 1);
 
-    //meshset consisting all entities in mesh
+    // meshset consisting all entities in mesh
     EntityHandle root_set = moab.get_root_set();
-    //add entities to field
-    ierr = m_field.add_ents_to_field_by_type(root_set,MBTET,"F1"); CHKERRG(ierr);
-    ierr = m_field.add_ents_to_field_by_type(root_set,MBTET,"F2"); CHKERRG(ierr);
+    // add entities to field
+    CHKERR m_field.add_ents_to_field_by_type(root_set, MBTET, "F1");
+    CHKERR m_field.add_ents_to_field_by_type(root_set, MBTET, "F2");
 
-    //set app. order
-    //see Hierarchic Finite Element Bases on Unstructured Tetrahedral Meshes (Mark Ainsworth & Joe Coyle)
+    // set app. order
+    // see Hierarchic Finite Element Bases on Unstructured Tetrahedral Meshes
+    // (Mark Ainsworth & Joe Coyle)
     int order = 2;
-    ierr = m_field.set_field_order(root_set,MBTET,"F1",order-1); CHKERRG(ierr);
-    ierr = m_field.set_field_order(root_set,MBTET,"F2",order); CHKERRG(ierr);
-    ierr = m_field.set_field_order(root_set,MBTRI,"F2",order); CHKERRG(ierr);
+    CHKERR m_field.set_field_order(root_set, MBTET, "F1", order - 1);
+    CHKERR m_field.set_field_order(root_set, MBTET, "F2", order);
+    CHKERR m_field.set_field_order(root_set, MBTRI, "F2", order);
 
-    ierr = m_field.build_fields(); CHKERRG(ierr);
+    CHKERR m_field.build_fields();
 
-    //add elements
-    ierr = m_field.add_finite_element("E1"); CHKERRG(ierr);
-    ierr = m_field.add_finite_element("E2"); CHKERRG(ierr);
-    ierr = m_field.add_finite_element("E3"); CHKERRG(ierr);
+    // add elements
+    CHKERR m_field.add_finite_element("E1");
+    CHKERR m_field.add_finite_element("E2");
+    CHKERR m_field.add_finite_element("E3");
 
-    ierr = m_field.modify_finite_element_add_field_row("E1","F1"); CHKERRG(ierr);
-    ierr = m_field.modify_finite_element_add_field_col("E1","F1"); CHKERRG(ierr);
-    ierr = m_field.modify_finite_element_add_field_row("E2","F2"); CHKERRG(ierr);
-    ierr = m_field.modify_finite_element_add_field_col("E2","F2"); CHKERRG(ierr);
-    //To build composite problem
-    ierr = m_field.modify_finite_element_add_field_row("E3","F1"); CHKERRG(ierr);
-    ierr = m_field.modify_finite_element_add_field_col("E3","F2"); CHKERRG(ierr);
+    CHKERR m_field.modify_finite_element_add_field_row("E1", "F1");
+    CHKERR m_field.modify_finite_element_add_field_col("E1", "F1");
+    CHKERR m_field.modify_finite_element_add_field_row("E2", "F2");
+    CHKERR m_field.modify_finite_element_add_field_col("E2", "F2");
+    // To build composite problem
+    CHKERR m_field.modify_finite_element_add_field_row("E3", "F1");
+    CHKERR m_field.modify_finite_element_add_field_col("E3", "F2");
 
-    ierr = m_field.add_ents_to_finite_element_by_type(root_set,MBTET,"E1"); CHKERRG(ierr);
-    ierr = m_field.add_ents_to_finite_element_by_type(root_set,MBTET,"E2"); CHKERRG(ierr);
-    ierr = m_field.add_ents_to_finite_element_by_type(root_set,MBTET,"E3"); CHKERRG(ierr);
+    CHKERR m_field.add_ents_to_finite_element_by_type(root_set, MBTET, "E1");
+    CHKERR m_field.add_ents_to_finite_element_by_type(root_set, MBTET, "E2");
+    CHKERR m_field.add_ents_to_finite_element_by_type(root_set, MBTET, "E3");
 
-    ierr = m_field.build_finite_elements(); CHKERRG(ierr);
-    ierr = m_field.build_adjacencies(bit_level0); CHKERRG(ierr);
+    CHKERR m_field.build_finite_elements();
+    CHKERR m_field.build_adjacencies(bit_level0);
 
-    //Problems
-    ierr = m_field.add_problem("P1"); CHKERRG(ierr);
-    ierr = m_field.add_problem("P2"); CHKERRG(ierr);
+    // Problems
+    CHKERR m_field.add_problem("P1");
+    CHKERR m_field.add_problem("P2");
 
-    //set refinement level for problem
-    ierr = m_field.modify_problem_ref_level_add_bit("P1",bit_level0); CHKERRG(ierr);
-    ierr = m_field.modify_problem_ref_level_add_bit("P2",bit_level0); CHKERRG(ierr);
+    // set refinement level for problem
+    CHKERR m_field.modify_problem_ref_level_add_bit("P1", bit_level0);
+    CHKERR m_field.modify_problem_ref_level_add_bit("P2", bit_level0);
 
-    ierr = m_field.modify_problem_add_finite_element("P1","E1"); CHKERRG(ierr);
-    ierr = m_field.modify_problem_add_finite_element("P2","E2"); CHKERRG(ierr);
+    CHKERR m_field.modify_problem_add_finite_element("P1", "E1");
+    CHKERR m_field.modify_problem_add_finite_element("P2", "E2");
 
-    //build problems
+    // build problems
     ProblemsManager *prb_mng_ptr;
-    ierr = m_field.getInterface(prb_mng_ptr); CHKERRG(ierr);
-    ierr = prb_mng_ptr->buildProblem("P1",true); CHKERRG(ierr);
-    ierr = prb_mng_ptr->buildProblem("P2",true); CHKERRG(ierr);
-    ierr = prb_mng_ptr->partitionProblem("P1"); CHKERRG(ierr);
-    ierr = prb_mng_ptr->partitionProblem("P2"); CHKERRG(ierr);
+    CHKERR m_field.getInterface(prb_mng_ptr);
+    CHKERR prb_mng_ptr->buildProblem("P1", true);
+    CHKERR prb_mng_ptr->buildProblem("P2", true);
+    CHKERR prb_mng_ptr->partitionProblem("P1");
+    CHKERR prb_mng_ptr->partitionProblem("P2");
 
-    ierr = prb_mng_ptr->partitionFiniteElements("P1"); CHKERRG(ierr);
-    ierr = prb_mng_ptr->partitionGhostDofs("P1"); CHKERRG(ierr);
-    ierr = m_field.partition_check_matrix_fill_in("P1",-1,-1,0); CHKERRG(ierr);
-    ierr = prb_mng_ptr->partitionFiniteElements("P2"); CHKERRG(ierr);
-    ierr = prb_mng_ptr->partitionGhostDofs("P2"); CHKERRG(ierr);
-    ierr = m_field.partition_check_matrix_fill_in("P2",-1,-1,0); CHKERRG(ierr);
+    CHKERR prb_mng_ptr->partitionFiniteElements("P1");
+    CHKERR prb_mng_ptr->partitionGhostDofs("P1");
+    CHKERR m_field.partition_check_matrix_fill_in("P1", -1, -1, 0);
+    CHKERR prb_mng_ptr->partitionFiniteElements("P2");
+    CHKERR prb_mng_ptr->partitionGhostDofs("P2");
+    CHKERR m_field.partition_check_matrix_fill_in("P2", -1, -1, 0);
 
-    //compose problem
-    ierr = m_field.add_problem("P3"); CHKERRG(ierr);
-    ierr = m_field.modify_problem_ref_level_add_bit("P3",bit_level0); CHKERRG(ierr);
-    ierr = m_field.modify_problem_add_finite_element("P3","E3"); CHKERRG(ierr);
+    // compose problem
+    CHKERR m_field.add_problem("P3");
+    CHKERR m_field.modify_problem_ref_level_add_bit("P3", bit_level0);
+    CHKERR m_field.modify_problem_add_finite_element("P3", "E3");
 
-    ierr = prb_mng_ptr->buildProblem("P3",false); CHKERRG(ierr);
-    ierr = prb_mng_ptr->inheritPartition("P3","P1",false,"P2",true); CHKERRG(ierr);
-    ierr = prb_mng_ptr->partitionFiniteElements("P3"); CHKERRG(ierr);
-    ierr = prb_mng_ptr->partitionGhostDofs("P3"); CHKERRG(ierr);
-    ierr = m_field.partition_check_matrix_fill_in("P3",-1,-1,0); CHKERRG(ierr);
+    CHKERR prb_mng_ptr->buildProblem("P3", false);
+    CHKERR prb_mng_ptr->inheritPartition("P3", "P1", false, "P2", true);
+    CHKERR prb_mng_ptr->partitionFiniteElements("P3");
+    CHKERR prb_mng_ptr->partitionGhostDofs("P3");
+    CHKERR m_field.partition_check_matrix_fill_in("P3", -1, -1, 0);
 
     /*Mat m;
-    ierr = m_field.MatCreateMPIAIJWithArrays("P3",&m); CHKERRG(ierr);
+    CHKERR m_field.MatCreateMPIAIJWithArrays("P3",&m);
     PetscViewer viewer;
-    ierr = PetscViewerASCIIOpen(PETSC_COMM_WORLD,"build_composite_problem.txt",&viewer); CHKERRG(ierr);
+    CHKERR
+    PetscViewerASCIIOpen(PETSC_COMM_WORLD,"build_composite_problem.txt",&viewer);
     MatView(m,viewer);
-    ierr = PetscViewerDestroy(&viewer); CHKERRG(ierr);
-    ierr = MatDestroy(&m); CHKERRG(ierr);*/
-
-    
+    CHKERR PetscViewerDestroy(&viewer);
+    CHKERR MatDestroy(&m); */
 
   } catch (MoFEMException const &e) {
-    SETERRQ(PETSC_COMM_SELF,e.errorCode,e.errorMessage);
+    SETERRQ(PETSC_COMM_SELF, e.errorCode, e.errorMessage);
   }
 
-  //finish work cleaning memory, getting statistics, etc.
-  ierr = MoFEM::Core::Finalize(); CHKERRG(ierr);
+  // finish work cleaning memory, getting statistics, etc.
+  CHKERR MoFEM::Core::Finalize();
 
   return 0;
-
 }
