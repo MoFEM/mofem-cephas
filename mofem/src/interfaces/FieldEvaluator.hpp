605ca6fde6dec668aa90641f40760af252e1d181
Lukasz Kaczmarczyk <lukasz.kaczmarczyk@glasgow.ac.uk>
2019-05-07 22:12:16 +0100

    fix doc

--- mofem/src/interfaces/FieldEvaluator.hpp
+++ mofem/src/interfaces/FieldEvaluator.hpp
@@ -127,7 +127,7 @@ struct FieldEvaluatorInterface : public UnknownInterface {
     // iterate over elemnts with evaluated points
     CHKERR m_field.getInterface<FieldEvaluatorInterface>()
           ->evalFEAtThePoint3D(point.data(), dist, prb_ptr->getName(),
-                               simple_interface->getDomainFEName(), 
+                               "FINITE_ELEMENT_NAME", 
                                data_ptr, m_field.get_comm_rank(),
                                m_field.get_comm_rank(), MF_EXIST, QUIET);
 
