6092af1fc4566f212feb86c947d10242cc5634ce
Lukasz Kaczmarczyk <lukasz.kaczmarczyk@glasgow.ac.uk>
2019-09-06 13:00:07 +0100

    fix test

--- mofem/atom_tests/forces_and_sources_integration_on_skeleton.cpp
+++ mofem/atom_tests/forces_and_sources_integration_on_skeleton.cpp
@@ -1,7 +1,7 @@
 /** \file forces_and_sources_integration_on_skeleton.cpp
  * \example forces_and_sources_integration_on_skeleton.cpp
  * \brief Testing integration on skeleton
-*/
+ */
 
 /* This file is part of MoFEM.
  * MoFEM is free software: you can redistribute it and/or modify it under
@@ -51,7 +51,7 @@ int main(int argc, char *argv[]) {
       pcomm = new ParallelComm(&moab, PETSC_COMM_WORLD);
 
     const char *option;
-    option = ""; 
+    option = "";
     CHKERR moab.load_file(mesh_file_name, 0, option);
 
     // Create MoFEM database
@@ -114,38 +114,78 @@ int main(int argc, char *argv[]) {
     CHKERR prb_mng_ptr->partitionFiniteElements("P1");
     CHKERR prb_mng_ptr->partitionGhostDofs("P1");
 
+    struct CommonData {
+      MatrixDouble dotNormalFace;
+      MatrixDouble dotNormalEleLeft;
+      MatrixDouble dotNormalEleRight;
+    };
+
+
     struct SkeletonFE
         : public FaceElementForcesAndSourcesCore::UserDataOperator {
 
       VolumeElementForcesAndSourcesCoreOnSide volSideFe;
       struct OpVolSide
           : public VolumeElementForcesAndSourcesCoreOnSide::UserDataOperator {
-        OpVolSide()
+
+        CommonData &elemData;
+        OpVolSide(CommonData &elem_data)
             : VolumeElementForcesAndSourcesCoreOnSide::UserDataOperator(
-                  "F2", UserDataOperator::OPROW) {}
+                  "F2", UserDataOperator::OPROW),
+              elemData(elem_data) {}
         MoFEMErrorCode doWork(int side, EntityType type,
                               DataForcesAndSourcesCore::EntData &data) {
           MoFEMFunctionBeginHot;
-          std::cout << "\tVolume" << getFEMethod()->nInTheLoop << std::endl;
-          std::cout << "\tGauss pts " << getGaussPts() << std::endl;
-          std::cout << "\tCoords " << getCoordsAtGaussPts() << endl;
-          MatrixDouble diff = getCoordsAtGaussPts() - getFaceCoordsAtGaussPts();
-          std::cout << std::fixed << std::setprecision(3) << "\tDiff coords "
-                    << diff << endl;
-          const double eps = 1e-12;
-          if (norm_inf(diff) > eps) {
-            SETERRQ(PETSC_COMM_WORLD, MOFEM_ATOM_TEST_INVALID,
-                    "coordinates at integration pts are different");
+
+          if (type == MBTRI && side == getFaceSideNumber()) {
+
+            // std::cout << "\tVolume" << getFEMethod()->nInTheLoop <<
+            // std::endl; std::cout << "\tGauss pts " << getGaussPts() <<
+            // std::endl; std::cout << "\tCoords " << getCoordsAtGaussPts() <<
+            // endl;
+            MatrixDouble diff =
+                getCoordsAtGaussPts() - getFaceCoordsAtGaussPts();
+            // std::cout << std::fixed << std::setprecision(3) << "\tDiff coords
+            // "
+            //           << diff << endl;
+            const double eps = 1e-12;
+            if (norm_inf(diff) > eps)
+              SETERRQ(PETSC_COMM_WORLD, MOFEM_ATOM_TEST_INVALID,
+                      "coordinates at integration pts are different");
+
+            const size_t nb_dofs = data.getN().size2() / 3;
+            const size_t nb_integration_pts = data.getN().size1();
+
+            auto t_normal = getFTensor1Normal();
+            auto t_hdiv_base = data.getFTensor1N<3>();
+            FTensor::Index<'i', 3> i;
+            MatrixDouble *ptr_dot_elem_data = nullptr;
+            if (getFEMethod()->nInTheLoop == 0)
+              ptr_dot_elem_data = &elemData.dotNormalEleLeft;
+            else
+              ptr_dot_elem_data = &elemData.dotNormalEleRight;
+            MatrixDouble &dot_elem_data = *ptr_dot_elem_data;
+            dot_elem_data.resize(nb_integration_pts, nb_dofs, false);
+
+            for (size_t gg = 0; gg != nb_integration_pts; ++gg) {
+              for (size_t bb = 0; bb != nb_dofs; ++bb) {
+                dot_elem_data(gg, bb) = t_normal(i) * t_hdiv_base(i);
+                ++t_hdiv_base;
+              }
+            }
+
           }
           MoFEMFunctionReturnHot(0);
         }
       };
 
-      SkeletonFE(MoFEM::Interface &m_field)
+      CommonData &elemData;
+      SkeletonFE(MoFEM::Interface &m_field, CommonData &elem_data)
           : FaceElementForcesAndSourcesCore::UserDataOperator(
                 "F2", UserDataOperator::OPROW),
-            volSideFe(m_field) {
-        volSideFe.getOpPtrVector().push_back(new SkeletonFE::OpVolSide());
+            volSideFe(m_field), elemData(elem_data) {
+        volSideFe.getOpPtrVector().push_back(
+            new SkeletonFE::OpVolSide(elemData));
       }
 
       int getRule(int order) { return order; };
@@ -155,19 +195,70 @@ int main(int argc, char *argv[]) {
 
         MoFEMFunctionBeginHot;
         if (type == MBTRI && side == 0) {
-          std::cout << "Face" << std::endl;
-          std::cout << "Gauss pts " << getGaussPts() << std::endl;
-          std::cout << "Coords " << getCoordsAtGaussPts() << endl;
+          // std::cout << "Face" << std::endl;
+          // std::cout << "Gauss pts " << getGaussPts() << std::endl;
+          // std::cout << "Coords " << getCoordsAtGaussPts() << endl;
+
+          const size_t nb_dofs = data.getN().size2() / 3;
+          const size_t nb_integration_pts = data.getN().size1();
+
+          auto t_normal = getFTensor1Normal();
+          auto t_hdiv_base = data.getFTensor1N<3>();
+          FTensor::Index<'i', 3> i;
+          elemData.dotNormalFace.resize(nb_integration_pts, nb_dofs, false);
+          elemData.dotNormalEleLeft.resize(nb_integration_pts, 0, false);
+          elemData.dotNormalEleRight.resize(nb_integration_pts, 0, false);
+
+          for (size_t gg = 0; gg != nb_integration_pts; ++gg) {
+            for (size_t bb = 0; bb != nb_dofs; ++bb) {
+              elemData.dotNormalFace(gg, bb) = t_normal(i) * t_hdiv_base(i);
+              ++t_hdiv_base;
+            }
+          }
+
           CHKERR loopSideVolumes("V1", volSideFe);
+
+          auto check_continuity_of_base = [&](auto &vol_dot_data) {
+            MoFEMFunctionBegin;
+
+            if (vol_dot_data.size1() != elemData.dotNormalFace.size1())
+              SETERRQ(PETSC_COMM_SELF, MOFEM_ATOM_TEST_INVALID,
+                      "Inconsistent number of integration points");
+
+            if (vol_dot_data.size2() != elemData.dotNormalFace.size2())
+              SETERRQ(PETSC_COMM_SELF, MOFEM_ATOM_TEST_INVALID,
+                      "Inconsistent number of base functions");
+            const double eps = 1e-12;
+            for (size_t gg = 0; gg != vol_dot_data.size1(); ++gg)
+              for (size_t bb = 0; bb != vol_dot_data.size2(); ++bb) {
+                const double error = std::abs(vol_dot_data(gg, bb) -
+                             elemData.dotNormalFace(gg, bb));
+                if (error > eps)
+                  SETERRQ2(PETSC_COMM_SELF, MOFEM_ATOM_TEST_INVALID,
+                           "Inconsistency %3.4e != %3.4e", vol_dot_data(gg, bb),
+                           elemData.dotNormalFace(gg, bb));
+              }
+            MoFEMFunctionReturn(0);
+          };
+
+          // std::cout << elemData.dotNormalFace << std::endl;
+          // std::cout << elemData.dotNormalEleLeft << std::endl;
+          // std::cout << elemData.dotNormalEleRight << std::endl;
+          // std::cout << std::endl;
+
+          if (elemData.dotNormalEleLeft.size2() != 0)
+            CHKERR check_continuity_of_base(elemData.dotNormalEleLeft);
+          else if (elemData.dotNormalEleRight.size2() != 0)
+            CHKERR check_continuity_of_base(elemData.dotNormalEleRight);
         }
         MoFEMFunctionReturnHot(0);
       }
     };
 
+    CommonData elem_data;
     FaceElementForcesAndSourcesCore face_fe(m_field);
-    face_fe.getOpPtrVector().push_back(new SkeletonFE(m_field));
+    face_fe.getOpPtrVector().push_back(new SkeletonFE(m_field, elem_data));
     CHKERR m_field.loop_finite_elements("P1", "S2", face_fe);
-
   }
   CATCH_ERRORS;
 
