29c3cf27d67007cabc0e0f4b6ec522bbcd986484
Lukasz Kaczmarczyk <lukasz.kaczmarczyk@glasgow.ac.uk>
2019-10-01 17:36:35 +0100

    calc area only at one point

--- mofem/src/finite_elements/impl/FaceElementForcesAndSourcesCore.cpp
+++ mofem/src/finite_elements/impl/FaceElementForcesAndSourcesCore.cpp
@@ -130,7 +130,6 @@ FaceElementForcesAndSourcesCoreBase::calculateAreaAndNormalAtIntegrationPts() {
     FTensor::Number<0> N0;
     FTensor::Number<1> N1;
 
-    aRea = 0;
     for (int gg = 0; gg != nb_gauss_pts; ++gg) {
       auto t_coords = get_ftensor_from_vec_3d(coords);
 
@@ -143,13 +142,11 @@ FaceElementForcesAndSourcesCoreBase::calculateAreaAndNormalAtIntegrationPts() {
       t_normal(j) = FTensor::levi_civita(i, j, k) * t_t1(k) * t_t2(i);
 
       double w = gaussPts(2, gg);
-      aRea += w * sqrt(t_normal(i) * t_normal(i));
 
       ++t_t1;
       ++t_t2;
       ++t_normal;
     }
-
   }
 
   MoFEMFunctionReturn(0);
@@ -157,6 +154,7 @@ FaceElementForcesAndSourcesCoreBase::calculateAreaAndNormalAtIntegrationPts() {
 
 MoFEMErrorCode FaceElementForcesAndSourcesCoreBase::calculateAreaAndNormal() {
   MoFEMFunctionBegin;
+
   EntityHandle ent = numeredEntFiniteElementPtr->getEnt();
   CHKERR mField.get_moab().get_connectivity(ent, conn, num_nodes, true);
   coords.resize(num_nodes * 3, false);
@@ -208,6 +206,29 @@ MoFEMErrorCode FaceElementForcesAndSourcesCoreBase::calculateAreaAndNormal() {
   case MBQUAD:
     diff_ptr = Tools::diffShapeFunMBQUADAtCenter.data();
     CHKERR calc_normal(diff_ptr);
+    {
+      EntityHandle ent = numeredEntFiniteElementPtr->getEnt();
+      CHKERR mField.get_moab().get_connectivity(ent, conn, num_nodes, true);
+      coords.resize(num_nodes * 3, false);
+      CHKERR mField.get_moab().get_coords(conn, num_nodes,
+                                          &*coords.data().begin());
+      FTensor::Tensor1<double, 3> t_a(coords[0], coords[1], coords[2]);
+      FTensor::Tensor1<double, 3> t_b(coords[3], coords[4], coords[5]);
+      FTensor::Tensor1<double, 3> t_c(coords[6], coords[7], coords[8]);
+      FTensor::Tensor1<double, 3> t_d(coords[9], coords[10], coords[11]);
+
+      FTensor::Index<'i', 3> i;
+      FTensor::Index<'j', 3> j;
+      FTensor::Index<'k', 3> k;
+
+      FTensor::Tensor1<double, 3> coross;
+      coross(k) =
+          FTensor::levi_civita(i, j, k) * (t_a(i) - t_c(i)) * (t_b(j) - t_d(j));
+      double test_area = sqrt(coross(k) * coross(k));
+      cerr << aRea << " " << test_area << " " << aRea/test_area << endl;
+
+    }
+
     break;
   default:
     SETERRQ(PETSC_COMM_SELF, MOFEM_NOT_IMPLEMENTED, "Element type not implemented");
@@ -422,7 +443,7 @@ MoFEMErrorCode FaceElementForcesAndSourcesCoreBase::calculateHoNormal() {
     CHKERR opHOCoordsAndNormals.opRhs(dataH1);
     CHKERR opHOCoordsAndNormals.calculateNormals();
 
-  } else {
+  } else if(numeredEntFiniteElementPtr->getEntType() == MBTRI) {
     hoCoordsAtGaussPts.resize(0, 0, false);
     normalsAtGaussPts.resize(0, 0, false);
     tangentOneAtGaussPts.resize(0, 0, false);
