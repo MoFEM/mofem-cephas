413c019a01fdd95497a2807fdd9dd265b10356ea
Lukasz Kaczmarczyk <likask@wp.pl>
2019-10-24 10:40:42 +0100

    add  jenkins files

--- jenkins._spack_install.sh
+++ jenkins._spack_install.sh
@@ -6,10 +6,10 @@ mkdir -p mofem_mirror &&
 curl -s -L https://bitbucket.org/likask/mofem-cephas/downloads/mirror_v0.9.0.tar.gz \
 | tar xzC $PWD/mofem_mirror  --strip 1
 
-spack --config-scope $WORKSPACE mirror remove mofem_mirror  || true
-spack --config-scope $WORKSPACE mirror add mofem_mirror $PWD/mofem_mirror
-spack --config-scope $WORKSPACE bootstrap
-spack --config-scope $WORKSPACE install --only dependencies mofem-cephas+slepc
+spack mirror remove mofem_mirror  || true
+spack mirror add mofem_mirror $PWD/mofem_mirror
+spack bootstrap
+spack install --only dependencies mofem-cephas+slepc
 
 rm -f lock_spack
 
