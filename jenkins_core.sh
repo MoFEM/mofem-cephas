ff34f593f3db4f054d6410be657606e79e756736
Lukasz Kaczmarczyk <likask@wp.pl>
2019-10-27 15:09:07 +0000

    add curll

--- jenkins_core.sh
+++ jenkins_core.sh
@@ -109,9 +109,17 @@ EOF
 
 #fi
 
-
-spack load cmake
-ctest -V -S $WORKSPACE/script.cmake
+curl \
+  -X POST -H 'Content-type: application/json' \
+  --data '{"text":"Jenkins start job for branch $GIT_BRANCH"}' \
+  https://hooks.slack.com/services/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
 
 cd $WORKSPACE/build
+spack load cmake
+ctest -V -S $WORKSPACE/script.cmake
 make install
+
+curl \
+  -X POST -H 'Content-type: application/json' \
+  --data '{"text":"Jenkins end job for branch $GIT_BRANCH (See http://cdash.eng.gla.ac.uk/cdash/)"}' \
+  https://hooks.slack.com/services/T06180CDN/BPWLDQK71/KfDtJMLrecQOVZ4NKDXhPiA
