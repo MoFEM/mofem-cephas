95307422cbc71f84f37499280aba298a41ad8095
Lukasz Kaczmarczyk <likask@wp.pl>
2019-12-30 01:39:38 +0000

    refactor code

--- mofem/src/ftensor/src/FTensor/Tensor4/Tensor4_Expr.hpp
+++ mofem/src/ftensor/src/FTensor/Tensor4/Tensor4_Expr.hpp
@@ -15,120 +15,172 @@
 
 #include "Tensor4_times_Tensor1.hpp"
 
-namespace FTensor
-{
-  template <class A, class T, int Dim0, int Dim1, int Dim2, int Dim3, char i,
-            char j, char k, char l>
-  class Tensor4_Expr
-  {
-    A iter;
+namespace FTensor {
+template <class A, class T, int Dim0, int Dim1, int Dim2, int Dim3, char i,
+          char j, char k, char l>
+class Tensor4_Expr {
+  A iter;
 
-  public:
-    Tensor4_Expr(const A &a) : iter(a) {}
-    T operator()(const int N1, const int N2, const int N3, const int N4) const
-    {
-      return iter(N1, N2, N3, N4);
-    }
-  };
+public:
+  Tensor4_Expr(const A &a) : iter(a) {}
+  T operator()(const int N1, const int N2, const int N3, const int N4) const {
+    return iter(N1, N2, N3, N4);
+  }
+};
 
-  template <class A, class T, int Dim0, int Dim1, int Dim2, int Dim3, char i,
-            char j, char k, char l>
-  class Tensor4_Expr<Tensor4<A, Dim0, Dim1, Dim2, Dim3>, T, Dim0, Dim1, Dim2,
-                     Dim3, i, j, k, l>
-  {
-    Tensor4<A, Dim0, Dim1, Dim2, Dim3> &iter;
+template <class A, class T, int Dim0, int Dim1, int Dim2, int Dim3, char i,
+          char j, char k, char l>
+class Tensor4_Expr<Tensor4<A, Dim0, Dim1, Dim2, Dim3>, T, Dim0, Dim1, Dim2,
+                   Dim3, i, j, k, l> {
+  Tensor4<A, Dim0, Dim1, Dim2, Dim3> &iter;
 
-  public:
-    Tensor4_Expr(Tensor4<A, Dim0, Dim1, Dim2, Dim3> &a) : iter(a) {}
-    T &operator()(const int N1, const int N2, const int N3, const int N4)
-    {
-      return iter(N1, N2, N3, N4);
-    }
-    T operator()(const int N1, const int N2, const int N3, const int N4) const
-    {
-      return iter(N1, N2, N3, N4);
-    }
+public:
+  Tensor4_Expr(Tensor4<A, Dim0, Dim1, Dim2, Dim3> &a) : iter(a) {}
+  T &operator()(const int N1, const int N2, const int N3, const int N4) {
+    return iter(N1, N2, N3, N4);
+  }
+  T operator()(const int N1, const int N2, const int N3, const int N4) const {
+    return iter(N1, N2, N3, N4);
+  }
 
-    /* Various assignment operators.  I have to explicitly declare the
-       second operator= because otherwise the compiler will generate its
-       own and not use the template code. */
+  /* Various assignment operators.  I have to explicitly declare the
+     second operator= because otherwise the compiler will generate its
+     own and not use the template code. */
 
-    template <class B, class U, int Dim1_0, int Dim1_1, int Dim1_2, int Dim1_3,
-              char i_1, char j_1, char k_1, char l_1>
-    auto &equals(const Tensor4_Expr<B, U, Dim1_0, Dim1_1, Dim1_2, Dim1_3, i_1,
-                                    j_1, k_1, l_1> &rhs)
-    {
-      for(int ii = 0; ii < Dim0; ++ii)
-        for(int jj = 0; jj < Dim1; ++jj)
-          for(int kk = 0; kk < Dim2; ++kk)
-            for(int ll = 0; ll < Dim3; ++ll)
-              {
-                iter(ii, jj, kk, ll) = permute(*this, rhs, ii, jj, kk, ll);
-              }
-      return *this;
-    }
+  template <class B, class U, int Dim1_0, int Dim1_1, int Dim1_2, int Dim1_3,
+            char i_1, char j_1, char k_1, char l_1>
+  auto &equals(const Tensor4_Expr<B, U, Dim1_0, Dim1_1, Dim1_2, Dim1_3, i_1,
+                                  j_1, k_1, l_1> &rhs) {
+    auto index_sequence0 = std::make_index_sequence<Dim0>();
+    auto index_sequence1 = std::make_index_sequence<Dim1>();
+    auto index_sequence2 = std::make_index_sequence<Dim2>();
+    auto index_sequence3 = std::make_index_sequence<Dim3>();
 
-    template <class B, class U, int Dim1_0, int Dim1_1, int Dim1_2, int Dim1_3,
-              char i_1, char j_1, char k_1, char l_1>
-    auto &operator=(const Tensor4_Expr<B, U, Dim1_0, Dim1_1, Dim1_2, Dim1_3,
-                                       i_1, j_1, k_1, l_1> &rhs)
-    {
-      return equals(rhs);
-    }
+    auto l0 = [&](auto N0) {
+      auto l1 = [&](auto N1) {
+        auto l2 = [&](auto N2) {
+          auto l3 = [&](auto N3) {
+            iter(N0, N1, N2, N3) = permute(*this, rhs, N0, N1, N2, N3);
+          };
+          boost::hana::for_each(index_sequence3, l3);
+        };
+        boost::hana::for_each(index_sequence2, l2);
+      };
+      boost::hana::for_each(index_sequence1, l1);
+    };
+    boost::hana::for_each(index_sequence0, l0);
 
-    auto &
-    operator=(const Tensor4_Expr<Tensor4<A, Dim0, Dim1, Dim2, Dim3>, T, Dim0,
-                                 Dim1, Dim2, Dim3, i, j, k, l> &rhs)
-    {
-      return equals(rhs);
-    }
+    return *this;
+  }
 
-    template <class B, class U, int Dim1_0, int Dim1_1, int Dim1_2, int Dim1_3,
-              char i_1, char j_1, char k_1, char l_1>
-    auto &operator+=(const Tensor4_Expr<B, U, Dim1_0, Dim1_1, Dim1_2, Dim1_3,
-                                        i_1, j_1, k_1, l_1> &rhs)
-    {
-      for(int ii = 0; ii < Dim0; ++ii)
-        for(int jj = 0; jj < Dim1; ++jj)
-          for(int kk = 0; kk < Dim2; ++kk)
-            for(int ll = 0; ll < Dim3; ++ll)
-              {
-                iter(ii, jj, kk, ll) += permute(*this, rhs, ii, jj, kk, ll);
-              }
-      return *this;
-    }
+  template <class B, class U, int Dim1_0, int Dim1_1, int Dim1_2, int Dim1_3,
+            char i_1, char j_1, char k_1, char l_1>
+  auto &operator=(const Tensor4_Expr<B, U, Dim1_0, Dim1_1, Dim1_2, Dim1_3, i_1,
+                                     j_1, k_1, l_1> &rhs) {
+    return equals(rhs);
+  }
 
-    template <class B, class U, int Dim1_0, int Dim1_1, int Dim1_2, int Dim1_3,
-              char i_1, char j_1, char k_1, char l_1>
-    auto &operator-=(const Tensor4_Expr<B, U, Dim1_0, Dim1_1, Dim1_2, Dim1_3,
-                                        i_1, j_1, k_1, l_1> &rhs)
-    {
-      for(int ii = 0; ii < Dim0; ++ii)
-        for(int jj = 0; jj < Dim1; ++jj)
-          for(int kk = 0; kk < Dim2; ++kk)
-            for(int ll = 0; ll < Dim3; ++ll)
-              {
-                iter(ii, jj, kk, ll) += permute(*this, rhs, ii, jj, kk, ll);
-              }
-      return *this;
-    }
+  auto &operator=(const Tensor4_Expr<Tensor4<A, Dim0, Dim1, Dim2, Dim3>, T,
+                                     Dim0, Dim1, Dim2, Dim3, i, j, k, l> &rhs) {
+    return equals(rhs);
+  }
 
-    template <class U> auto &operator=(const U &d) {
-      for (int ii = 0; ii != Dim0; ++ii)
-        for (int jj = 0; jj != Dim1; ++jj)
-          for (int kk = 0; kk != Dim2; ++kk)
-            for (int ll = 0; ll != Dim3; ++ll)
-              iter(ii, jj, kk, ll) = d;
-      return *this;
-    }
+  template <class B, class U, int Dim1_0, int Dim1_1, int Dim1_2, int Dim1_3,
+            char i_1, char j_1, char k_1, char l_1>
+  auto &operator+=(const Tensor4_Expr<B, U, Dim1_0, Dim1_1, Dim1_2, Dim1_3, i_1,
+                                      j_1, k_1, l_1> &rhs) {
 
-    template <class U> auto &operator*=(const U &d) {
-      for (int ii = 0; ii != Dim0; ++ii)
-        for (int jj = 0; jj != Dim1; ++jj)
-          for (int kk = 0; kk != Dim2; ++kk)
-            for (int ll = 0; ll != Dim3; ++ll)
-              iter(ii, jj, kk, ll) *= d;
-      return *this;
-    }
-  };
-}
+    auto index_sequence0 = std::make_index_sequence<Dim0>();
+    auto index_sequence1 = std::make_index_sequence<Dim1>();
+    auto index_sequence2 = std::make_index_sequence<Dim2>();
+    auto index_sequence3 = std::make_index_sequence<Dim3>();
+
+    auto l0 = [&](auto N0) {
+      auto l1 = [&](auto N1) {
+        auto l2 = [&](auto N2) {
+          auto l3 = [&](auto N3) {
+            iter(N0, N1, N2, N3) += permute(*this, rhs, N0, N1, N2, N3);
+          };
+          boost::hana::for_each(index_sequence3, l3);
+        };
+        boost::hana::for_each(index_sequence2, l2);
+      };
+      boost::hana::for_each(index_sequence1, l1);
+    };
+    boost::hana::for_each(index_sequence0, l0);
+
+    return *this;
+  }
+
+  template <class B, class U, int Dim1_0, int Dim1_1, int Dim1_2, int Dim1_3,
+            char i_1, char j_1, char k_1, char l_1>
+  auto &operator-=(const Tensor4_Expr<B, U, Dim1_0, Dim1_1, Dim1_2, Dim1_3, i_1,
+                                      j_1, k_1, l_1> &rhs) {
+
+    auto index_sequence0 = std::make_index_sequence<Dim0>();
+    auto index_sequence1 = std::make_index_sequence<Dim1>();
+    auto index_sequence2 = std::make_index_sequence<Dim2>();
+    auto index_sequence3 = std::make_index_sequence<Dim3>();
+
+    auto l0 = [&](auto N0) {
+      auto l1 = [&](auto N1) {
+        auto l2 = [&](auto N2) {
+          auto l3 = [&](auto N3) {
+            iter(N0, N1, N2, N3) -= permute(*this, rhs, N0, N1, N2, N3);
+          };
+          boost::hana::for_each(index_sequence3, l3);
+        };
+        boost::hana::for_each(index_sequence2, l2);
+      };
+      boost::hana::for_each(index_sequence1, l1);
+    };
+    boost::hana::for_each(index_sequence0, l0);
+
+    return *this;
+  }
+
+  template <class U> auto &operator=(const U &d) {
+
+    auto index_sequence0 = std::make_index_sequence<Dim0>();
+    auto index_sequence1 = std::make_index_sequence<Dim1>();
+    auto index_sequence2 = std::make_index_sequence<Dim2>();
+    auto index_sequence3 = std::make_index_sequence<Dim3>();
+
+    auto l0 = [&](auto N0) {
+      auto l1 = [&](auto N1) {
+        auto l2 = [&](auto N2) {
+          auto l3 = [&](auto N3) { iter(N0, N1, N2, N3) = d; };
+          boost::hana::for_each(index_sequence3, l3);
+        };
+        boost::hana::for_each(index_sequence2, l2);
+      };
+      boost::hana::for_each(index_sequence1, l1);
+    };
+    boost::hana::for_each(index_sequence0, l0);
+
+    return *this;
+  }
+
+  template <class U> auto &operator*=(const U &d) {
+
+    auto index_sequence0 = std::make_index_sequence<Dim0>();
+    auto index_sequence1 = std::make_index_sequence<Dim1>();
+    auto index_sequence2 = std::make_index_sequence<Dim2>();
+    auto index_sequence3 = std::make_index_sequence<Dim3>();
+
+    auto l0 = [&](auto N0) {
+      auto l1 = [&](auto N1) {
+        auto l2 = [&](auto N2) {
+          auto l3 = [&](auto N3) { iter(N0, N1, N2, N3) *= d; };
+          boost::hana::for_each(index_sequence3, l3);
+        };
+        boost::hana::for_each(index_sequence2, l2);
+      };
+      boost::hana::for_each(index_sequence1, l1);
+    };
+    boost::hana::for_each(index_sequence0, l0);
+
+    return *this;
+  }
+};
+} // namespace FTensor
