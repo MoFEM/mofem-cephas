7b7d5e7a4e166c85baae8d69c3734f49d4e65e4d
Lukasz Kaczmarczyk <likask@wp.pl>
2020-04-18 16:28:57 +0100

    add kronecker delta

---
+++ mofem/src/ftensor/src/FTensor/Kronecker_Delta.hpp
@@ -0,0 +1,61 @@
+/**
+ * @file Kronecker_Delta.hpp.
+ *
+ */
+
+#pragma once
+
+namespace FTensor {
+
+/**
+ * @brief Kronecker Delta class
+ *
+ * @tparam int
+ */
+template <class T = int> class Kronecker_Delta {
+public:
+  constexpr T operator()(const int N1, const int N2) const {
+    return (N1 == N2) ? T(1) : T(0);
+  }
+
+  template <char i, char j, int Dim0, int Dim1>
+  Tensor2_Expr<Kronecker_Delta<T>, T, Dim0, Dim1, i, j>
+  operator()(const Index<i, Dim0> &, const Index<j, Dim1> &) {
+    return Tensor2_Expr<Kronecker_Delta<T>, T, Dim0, Dim1, i, j>(*this);
+  };
+
+  template <char i, int Dim0>
+  constexpr auto operator()(const Index<i, Dim0> &, const int &N1) {
+    auto TensorExpr = [this, N1](const int &N0) {
+      return this->operator()(N0, N1);
+    };
+    return Tensor1_Expr<decltype(TensorExpr), T, Dim0, i>(TensorExpr);
+  };
+
+  template <char j, int Dim1>
+  constexpr auto operator()(const int &N0, const Index<j, Dim1> &) {
+    auto TensorExpr = [this, N0](const int &N1) {
+      return this->operator()(N0, N1);
+    };
+    return Tensor1_Expr<decltype(TensorExpr), T, Dim1, j>{TensorExpr};
+  };
+};
+
+/// Rank 2
+template <class T = int, char i, char j, int Dim0, int Dim1>
+Tensor2_Expr<Kronecker_Delta<T>, T, Dim0, Dim1, i, j>
+kronecker_delta(const Index<i, Dim0> &, const Index<j, Dim1> &) {
+  return Kronecker_Delta<T>()(Index<i, Dim0>(), Index<j, Dim1>());
+}
+
+template <class T = int, char i, int Dim0>
+constexpr auto kronecker_delta(const Index<i, Dim0> &, const int &N1) {
+  return Kronecker_Delta<T>()(Index<i, Dim0>(), N1);
+}
+
+template <class T = int, char j, int Dim1>
+constexpr auto kronecker_delta(const int &N0, const Index<j, Dim1> &) {
+  return Kronecker_Delta<T>()(N0, Index<j, Dim1>());
+}
+
+} // namespace FTensor
