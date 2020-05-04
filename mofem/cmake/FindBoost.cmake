1b277ad6522a1522ae7c28f662023c8663cbc415
Lukasz Kaczmarczyk <likask@wp.pl>
2020-05-04 22:21:40 +0100

    work in progress

--- mofem/cmake/FindBoost.cmake
+++ mofem/cmake/FindBoost.cmake
@@ -14,6 +14,18 @@ message(STATUS "Boost found: " ${BOOST_PROGRAM_OPTIONS_LIB})
 if(NOT BOOST_PROGRAM_OPTIONS_LIB)
   message(FATAL_ERROR "boost program options library not found")
 endif(NOT BOOST_PROGRAM_OPTIONS_LIB)
+
+find_library(
+  BOOST_LOG_LIB NAMES boost_log
+  HINTS
+  ${BOOST_DIR}/lib
+  ${PETSC_DIR}/${PETSC_ARCH}/lib
+)
+message(STATUS "Boost found: " ${BOOST_LOG_LIB})
+if(NOT BOOST_LOG_LIB)
+  message(FATAL_ERROR "boost log library not found")
+endif(NOT BOOST_LOG_LIB)
+
 find_path(
   BOOST_INCLUDE_DIR
   NAMES boost/multi_index_container.hpp
