7717e820e786eb3f5523734ec18c5f78a0950980
Lukasz Kaczmarczyk <likask@wp.pl>
2020-10-12 20:59:06 +0100

    work in progress

--- mofem/atom_tests/matrix_function.cpp
+++ mofem/atom_tests/matrix_function.cpp
@@ -24,8 +24,48 @@ int main(int argc, char *argv[]) {
   LogManager::setLog("ATOM_TEST");
 
   try {
-  }
-  CATCH_ERRORS;
 
-  CHKERR MoFEM::Core::Finalize();
+    FTensor::Index<'i', 3> i;
+    FTensor::Index<'j', 3> j;
+
+    // Test matrix
+    {
+      FTensor::Tensor2<double, 3, 3> t_A{
+
+          1.,   0.1, -0.5,
+
+          0.1,  2.,  0.,
+
+          -0.5, 0.,  3.};
+
+      FTensor::Tensor2<double, 3, 3> t_N{
+
+          0.969837,  -0.0860972, 0.228042,
+
+          0.0790574, 0.996073,   0.0398449,
+
+          -0.230577, -0.0206147, 0.972836};
+
+      FTensor::Tensor1<double, 3> t_L{0.873555, 2.00794, 3.11851};
+
+      MatrixDouble a_sym_mat(6, 1);
+      auto t_sym_a = getFTensor2SymmetricFromMat<3>(a_sym_mat);
+      t_sym_a(i, j) = (t_A(i, j) || t_A(i, j)) / 2;
+
+      MatrixDouble n_mat(9, 1);
+      auto t_n = getFTensor2FromMat<3, 3>(n_mat);
+      t_n(i, j) = t_N(i, j);
+
+      MatrixDouble l_mat(3, 1);
+      auto t_l = getFTensor1FromMat<3>(n_mat);
+      t_l(i) = t_L(i);
+
+      auto f = [](double) { return 1; };
+
+      auto t_b = EigenProjection<double, 3, 1>::getMat<3>(t_l, t_n, f);
+  }
+}
+CATCH_ERRORS;
+
+CHKERR MoFEM::Core::Finalize();
 }
--- mofem/src/ftensor/src/MatrixFunction.hpp
+++ mofem/src/ftensor/src/MatrixFunction.hpp
@@ -20,48 +20,46 @@
 
 #pragma once
 
-namespace MatrixFunction {
+template <typename T, int Dim, int Shift> struct EigenProjection {
 
-template <typename T, int DIM, int SHIFT> struct EigenProjection {
-
-  using Val = FTensor::Tensor1<FTensor::PackPtr<T *, SHIFT>, DIM>;
-  using Vec = FTensor::Tensor2<FTensor::PackPtr<T *, SHIFT>, DIM, DIM>;
+  using Val = FTensor::Tensor1<FTensor::PackPtr<T *, Shift>, Dim>;
+  using Vec = FTensor::Tensor2<FTensor::PackPtr<T *, Shift>, Dim, Dim>;
   using Fun = boost::function<double(const double)>;
 
-  template <int a, int i> static inline T N(Vec &vec) { return vec(a, i); }
+  template <int a, int i> static inline T N(Vec &t_vec) { return t_vec(a, i); }
 
-  template <int a> static inline T L(Val &&val) { return val(a); }
+  template <int a> static inline T L(Val &&t_val) { return t_val(a); }
 
-  template <int a, int b> static inline T F(Val &val) {
-    return static_cast<T>(1) / (F<a>(val) - F<b>(val));
+  template <int a, int b> static inline T F(Val &t_val) {
+    return static_cast<T>(1) / (F<a>(t_val) - F<b>(t_val));
   }
 
-  template <int a, int b, int i, int j> static T M(Vec &vec) {
-    return N<a, i>(vec) * N<b, j>(vec);
+  template <int a, int b, int i, int j> static T M(Vec &t_vec) {
+    return N<a, i>(t_vec) * N<b, j>(t_vec);
   }
 
   template <int a, int b, int i, int j>
-  static inline T dFdN(Val &val, Vec &vec) {
-    return dFdNa<a, b, i, j>(val, vec) + dFdNb<a, b, i, j>(val, vec);
+  static inline T dFdN(Val &t_val, Vec &t_vec) {
+    return dFdNa<a, b, i, j>(t_val, t_vec) + dFdNb<a, b, i, j>(t_val, t_vec);
   }
 
   template <int a, int b, int i, int j, int k, int l>
-  static inline T G(Vec &vec) {
-    return M<a, i, k>(vec) * M<b, j, l>(vec) +
-           M<a, i, l>(vec) * M<b, j, k>(vec);
+  static inline T G(Vec &t_vec) {
+    return M<a, i, k>(t_vec) * M<b, j, l>(t_vec) +
+           M<a, i, l>(t_vec) * M<b, j, k>(t_vec);
   }
 
   template <int a, int i, int j, int k, int l>
-  static inline T d2M(Val &val, Vec &vec) {
+  static inline T d2M(Val &t_val, Vec &t_vec) {
     T ret = 0;
 
     boost::hana::for_each(
 
-        boost::hana::make_range(boost::hana::int_c<0>, boost::hana::int_c<DIM>),
+        boost::hana::make_range(boost::hana::int_c<0>, boost::hana::int_c<Dim>),
 
         [&](auto b) {
           if (a != b) {
-            ret += F<a, b>(val) * S<a, b, i, j, k, l>(val);
+            ret += F<a, b>(t_val) * S<a, b, i, j, k, l>(t_val);
           }
         }
 
@@ -71,18 +69,18 @@ template <typename T, int DIM, int SHIFT> struct EigenProjection {
   }
 
   template <int a, int i, int j, int k, int l, int m, int n>
-  static inline T dd4M(Val &val, Vec &vec) {
+  static inline T dd4M(Val &t_val, Vec &t_vec) {
     T ret = 0;
 
     boost::hana::for_each(
 
-        boost::hana::make_range(boost::hana::int_c<0>, boost::hana::int_c<DIM>),
+        boost::hana::make_range(boost::hana::int_c<0>, boost::hana::int_c<Dim>),
 
         [&](auto b) {
           if (a != b) {
-            ret +=
-                F<a, b>(val) * d2S<a, b, i, j, k, l, m, n>(val, vec) +
-                2 * dFdN<a, b, m, n>(val, vec) * S<a, b, i, j, k, l>(val, vec);
+            ret += F<a, b>(t_val) * d2S<a, b, i, j, k, l, m, n>(t_val, t_vec) +
+                   2 * dFdN<a, b, m, n>(t_val, t_vec) *
+                       S<a, b, i, j, k, l>(t_val, t_vec);
           }
         }
 
@@ -92,14 +90,14 @@ template <typename T, int DIM, int SHIFT> struct EigenProjection {
   }
 
   template <int NB, int i, int j>
-  static inline T reconstructMatrix(Val &val, Vec &vec, Fun f) {
+  static inline T reconstructMatrix(Val &t_val, Vec &t_vec, Fun f) {
     T ret = 0;
 
     boost::hana::for_each(
 
         boost::hana::make_range(boost::hana::int_c<0>, boost::hana::int_c<NB>),
 
-        [&](auto a) { ret += M<a, i, j>(vec) * f(L<a>(val)); }
+        [&](auto a) { ret += M<a, i, j>(t_vec) * f(L<a>(t_val)); }
 
     );
 
@@ -107,7 +105,7 @@ template <typename T, int DIM, int SHIFT> struct EigenProjection {
   }
 
   template <int NB, int i, int j, int k, int l>
-  static inline T firstMatrixDirective(Val &val, Vec &vec, Fun f, Fun d_f) {
+  static inline T firstMatrixDirective(Val &t_val, Vec &t_vec, Fun f, Fun d_f) {
     T ret = 0;
 
     boost::hana::for_each(
@@ -115,9 +113,9 @@ template <typename T, int DIM, int SHIFT> struct EigenProjection {
         boost::hana::make_range(boost::hana::int_c<0>, boost::hana::int_c<NB>),
 
         [&](auto a) {
-          ret +=
-              M<a, i, j>(vec) * M<a, k, l>(vec) * d_f(L<a>(val)) +
-              d2M<a, i, j, k, l>(val, vec) * f(L<a>(val)) / static_cast<T>(2);
+          ret += M<a, i, j>(t_vec) * M<a, k, l>(t_vec) * d_f(L<a>(t_val)) +
+                 d2M<a, i, j, k, l>(t_val, t_vec) * f(L<a>(t_val)) /
+                     static_cast<T>(2);
         }
 
     );
@@ -126,7 +124,7 @@ template <typename T, int DIM, int SHIFT> struct EigenProjection {
   }
 
   template <int NB, int i, int j, int k, int l, int m, int n>
-  static inline T secondMatrixDirective(Val &val, Vec &vec, Fun f, Fun d_f,
+  static inline T secondMatrixDirective(Val &t_val, Vec &t_vec, Fun f, Fun d_f,
                                         Fun dd_f) {
     T ret = 0;
 
@@ -137,18 +135,19 @@ template <typename T, int DIM, int SHIFT> struct EigenProjection {
         [&](auto a) {
           ret +=
 
-              d2M<a, i, j, m, n>(vec) * M<a, k, l>(vec) * d_f(L<a>(val)) /
+              d2M<a, i, j, m, n>(t_vec) * M<a, k, l>(t_vec) * d_f(L<a>(t_val)) /
                   static_cast<T>(2) +
 
-              M<a, i, j>(vec) * d2M<a, k, l, m, n>(vec) * d_f(L<a>(val)) /
+              M<a, i, j>(t_vec) * d2M<a, k, l, m, n>(t_vec) * d_f(L<a>(t_val)) /
                   static_cast<T>(2) +
 
-              M<a, i, j>(vec) * M<a, k, l>(vec) * dd_f(L<a>(val)) +
+              M<a, i, j>(t_vec) * M<a, k, l>(t_vec) * dd_f(L<a>(t_val)) +
 
-              dd4M<a, i, j, k, l, m, n>(val, vec) * f(L<a>(val)) /
+              dd4M<a, i, j, k, l, m, n>(t_val, t_vec) * f(L<a>(t_val)) /
                   static_cast<T>(4) +
 
-              d2M<a, i, j, k, l>(val, vec) * d_f(L<a>(val)) / static_cast<T>(2);
+              d2M<a, i, j, k, l>(t_val, t_vec) * d_f(L<a>(t_val)) /
+                  static_cast<T>(2);
         }
 
     );
@@ -156,37 +155,137 @@ template <typename T, int DIM, int SHIFT> struct EigenProjection {
     return ret;
   }
 
+  template <int NB> static inline auto getMat(Val &t_val, Vec &t_vec, Fun f) {
+    FTensor::Tensor2<T, Dim, Dim> t_A;
+
+    boost::hana::for_each(
+
+        boost::hana::make_range(boost::hana::int_c<0>,
+                                boost::hana::int_c<Dim * Dim>),
+
+        [&](constexpr auto s) {
+          constexpr int i = (s - s % Dim) / Dim;
+          constexpr int j = s - 3 * j;
+          t_A(i, j) = reconstructMatrix<NB, i, j>(t_val, t_vec, f);
+        }
+
+    );
+
+    return t_A;
+  }
+
+  template <int NB>
+  static inline auto getDiffMat(Val &t_val, Vec &t_vec, Fun f, Fun d_f,
+                                FTensor::Number<NB>) {
+    FTensor::Ddg<T, Dim, Dim> t_diff_A;
+
+    boost::hana::for_each(
+
+        boost::hana::make_range(boost::hana::int_c<0>,
+                                boost::hana::int_c<Dim * Dim * Dim * Dim>),
+
+        [&](auto s) {
+          constexpr int i = (s - s % (3 * 3 * 3)) / (3 * 3 * 3);
+          constexpr int j = (s - i * 3 * 3 * 3 - s % (3 * 3)) / (3 * 3);
+          constexpr int k = (s - i * 3 * 3 * 3 - j * 3 * 3 - s % 3) / 3;
+          constexpr int l = s - i * 3 * 3 * 3 - j * 3 * 3 - k * 3;
+
+          if (i >= j && k >= l) {
+            t_diff_A(i, j) =
+                firstMatrixDirective<NB, i, j, k, l>(t_val, t_vec, f, d_f);
+          }
+        }
+
+    );
+
+    return t_diff_A;
+  }
+
+  template <int NB, typename A>
+  static inline auto getDiffDiffMat(FTensor::Tensor2_symmetric<A, Dim> &t_s,
+                                    Val &t_val, Vec &t_vec, Fun f, Fun d_f,
+                                    Fun dd_f, FTensor::Number<NB>) {
+    FTensor::Ddg<T, Dim, Dim> t_diff_diff_a;
+    {
+      FTensor::Index<'i', Dim> i;
+      FTensor::Index<'j', Dim> j;
+      FTensor::Index<'k', Dim> k;
+      FTensor::Index<'l', Dim> l;
+      t_diff_A(i, j, k, l) = 0;
+    }
+
+    boost::hana::for_each(
+
+        boost::hana::make_range(boost::hana::int_c<0>,
+                                boost::hana::int_c<Dim * Dim * Dim * Dim>),
+
+        [&](auto s) {
+          constexpr int k = (s - s % (3 * 3 * 3)) / (3 * 3 * 3);
+          constexpr int l = (s - k * 3 * 3 * 3 - s % (3 * 3)) / (3 * 3);
+          constexpr int m = (s - k * 3 * 3 * 3 - l * 3 * 3 - s % 3) / 3;
+          constexpr int n = s - k * 3 * 3 * 3 - l * 3 * 3 - m * 3;
+
+          if (k >= l && m >= n) {
+
+            boost::hana::for_each(
+
+                boost::hana::make_range(boost::hana::int_c<0>,
+                                        boost::hana::int_c<Dim * Dim>),
+
+                [&](auto r) {
+                  constexpr int i = (s - s % Dim) / Dim;
+                  constexpr int j = s - 3 * j;
+
+                  double a;
+                  if (i >= j) {
+                    a = secondMatrixDirective<NB, i, j, k, l, m, n>(
+                        t_val, t_vec, f, d_f, dd_f);
+                    if (i != j)
+                      t_diff_diff_a(k, l, m, n) += 2 * a;
+                    else
+                      t_diff_diff_a(k, l, m, n) += a;
+                  }
+                }
+
+            );
+          }
+        }
+
+    );
+
+    return t_diff_diff_a;
+  }
+
 private:
   template <int a, int b, int i, int j>
-  static inline T dFdNa(Val &val, Vec &vec) {
-    return -M<a, a, i, j>(vec) /
-           ((L<a>(val) - L<b>(val)) * (L<a>(val) - L<b>(val)));
+  static inline T dFdNa(Val &t_val, Vec &t_vec) {
+    return -M<a, a, i, j>(t_vec) /
+           ((L<a>(t_val) - L<b>(t_val)) * (L<a>(t_val) - L<b>(t_val)));
   }
 
   template <int a, int b, int i, int j>
-  static inline T dFdNb(Val &val, Vec &vec) {
-    return M<b, b, i, j>(vec) /
-           ((L<a>(val) - L<b>(val)) * (L<a>(val) - L<b>(val)));
+  static inline T dFdNb(Val &t_val, Vec &t_vec) {
+    return M<b, b, i, j>(t_vec) /
+           ((L<a>(t_val) - L<b>(t_val)) * (L<a>(t_val) - L<b>(t_val)));
   }
 
   template <int a, int b, int i, int j, int k, int l>
-  static inline T S(Vec &vec) {
-    return G<a, b, i, j, k, l>(vec) + G<b, a, i, j, k, l>(vec);
+  static inline T S(Vec &t_vec) {
+    return G<a, b, i, j, k, l>(t_vec) + G<b, a, i, j, k, l>(t_vec);
   }
 
   template <int a, int b, int i, int j, int k, int l, int m, int n>
-  static inline T d2G(Val &val, Vec &vec) {
-    return d2M<a, i, k, n, m>(val, vec) * M<b, j, l>(vec) +
-           M<a, i, k>(vec) * d2M<b, j, l, m, n>(val, vec) +
-           d2M<a, i, l, m, n>(val, vec) * M<b, j, k>(val) +
-           M<a, i, l>(vec) * d2M<b, j, k, m, n>(val, vec);
+  static inline T d2G(Val &t_val, Vec &t_vec) {
+    return d2M<a, i, k, n, m>(t_val, t_vec) * M<b, j, l>(t_vec) +
+           M<a, i, k>(t_vec) * d2M<b, j, l, m, n>(t_val, t_vec) +
+           d2M<a, i, l, m, n>(t_val, t_vec) * M<b, j, k>(t_val) +
+           M<a, i, l>(t_vec) * d2M<b, j, k, m, n>(t_val, t_vec);
   }
 
   template <int a, int b, int i, int j, int k, int l, int m, int n>
-  static inline T d2S(Val &val, Vec &vec) {
-    return d2G<a, b, i, j, k, l, m, n>(val, vec) +
-           d2G<b, a, i, j, k, l, m, n>(val, vec);
+  static inline T d2S(Val &t_val, Vec &t_vec) {
+    return d2G<a, b, i, j, k, l, m, n>(t_val, t_vec) +
+           d2G<b, a, i, j, k, l, m, n>(t_val, t_vec);
   }
 };
 
-} // namespace MatrixFunction
