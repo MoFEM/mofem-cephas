5c3b9e936b4067a76e399f1db56fa1a0701faac4
Lukasz Kaczmarczyk <likask@wp.pl>
2020-10-18 11:26:23 +0100

    fix logging

--- mofem/atom_tests/matrix_function.cpp
+++ mofem/atom_tests/matrix_function.cpp
@@ -48,40 +48,47 @@ int main(int argc, char *argv[]) {
 
       FTensor::Tensor1<double, 3> t_L{0.873555, 2.00794, 3.11851};
 
-      cout << "Diff A" << endl;
-      cout << t_A << endl;
-
       auto print_ddg = [](auto &t) {
         for (int ii = 0; ii != 3; ++ii)
           for (int jj = 0; jj <= ii; ++jj)
             for (int kk = 0; kk != 3; ++kk)
               for (int ll = 0; ll <= kk; ++ll)
-                cout << ii + 1 << " " << jj + 1 << " " << kk + 1 << " "
-                     << ll + 1 << " : " << t(ii, jj, kk, ll) << endl;
+                MOFEM_LOG("ATOM_TEST", Sev::inform)
+                    << ii + 1 << " " << jj + 1 << " " << kk + 1 << " " << ll + 1
+                    << " : " << t(ii, jj, kk, ll);
       };
 
-      cout << "Diff d2m 0" << endl;
+      auto print_mat = [](auto &t) {
+        for (int ii = 0; ii != 3; ++ii)
+          for (int jj = 0; jj <= ii; ++jj)
+            MOFEM_LOG("ATOM_TEST", Sev::inform)
+                << ii + 1 << " " << jj + 1 << " : " << t(ii, jj);
+      };
+
+      MOFEM_LOG("ATOM_TEST", Sev::inform) << "Diff A";
+      print_mat(t_A);
+
       auto t_d2m_0 = EigenProjection<double, double>::getD2M<0, 3>(t_L, t_N);
+      MOFEM_LOG("ATOM_TEST", Sev::inform) << "Diff d2m 0";
       print_ddg(t_d2m_0);
 
-      cout << "Diff d2m 1" << endl;
       auto t_d2m_1 = EigenProjection<double, double>::getD2M<1, 3>(t_L, t_N);
+      MOFEM_LOG("ATOM_TEST", Sev::inform) << "Diff d2m 1";
       print_ddg(t_d2m_1);
 
-      cout << "Diff d2m 2" << endl;
       auto t_d2m_2 = EigenProjection<double, double>::getD2M<2, 3>(t_L, t_N);
+      MOFEM_LOG("ATOM_TEST", Sev::inform) << "Diff d2m 2";
       print_ddg(t_d2m_2);
 
       auto f = [](double v) { return v; };
 
-      cout << "Reconstruct mat" << endl;
       auto t_b = EigenProjection<double, double>::getMat<3>(t_L, t_N, f);
-
-      cout << t_b << endl;
+      MOFEM_LOG("ATOM_TEST", Sev::inform) << "Reconstruct mat";
+      print_mat(t_b);
 
       auto t_d = EigenProjection<double, double>::getDiffMat<3>(t_L, t_N, f, f);
 
-      cout << "Diff" << endl;
+      MOFEM_LOG("ATOM_TEST", Sev::inform) << "Diff";
       print_ddg(t_d);
 
       FTensor::Tensor2<double, 3, 3> t_S{
@@ -95,9 +102,8 @@ int main(int argc, char *argv[]) {
       auto t_dd = EigenProjection<double, double>::getDiffDiffMat(
           t_L, t_N, f, f, f, t_S, FTensor::Number<3>());
 
-      cout << "Diff Diff" << endl;;
+      MOFEM_LOG("ATOM_TEST", Sev::inform) << "Diff Diff";
       print_ddg(t_dd);
- 
     }
   }
   CATCH_ERRORS;
