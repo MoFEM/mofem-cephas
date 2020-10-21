bc55ada81a67b18a5e369a42fbc43b2bce699f03
Lukasz Kaczmarczyk <likask@wp.pl>
2020-10-21 17:59:20 +0100

    refactor test

--- mofem/atom_tests/matrix_function.cpp
+++ mofem/atom_tests/matrix_function.cpp
@@ -268,17 +268,42 @@ int main(int argc, char *argv[]) {
         }
 
         {
-          auto t_b = EigenProjection<double, double, 3>::getMat(t_L, t_N, f);
-          auto t_d = EigenProjection<double, double, 3>::getDiffMat(t_L, t_N,
-                                                                       f, d_f);
-          FTensor::Tensor2_symmetric<double, 3> t_c;
-          t_c(i, j) =
-              (t_d(i, j, k, l) * t_b(k, l)) - (t_one(i, j, k, l) * t_b(k, l));
-          print_mat(t_c);
-          auto norm2_t_c = t_c(i, j) * t_c(i, j);
+
+          auto t_d =
+              EigenProjection<double, double, 3>::getDiffMat(t_L, t_N, f, d_f);
+
+          constexpr auto t_kd = FTensor::Kronecker_Delta<double>();
+          FTensor::Tensor4<double, 3, 3, 3, 3> t_d_a;
+          t_d_a(i, j, k, l) = 0;
+
+          for (int ii = 0; ii != 3; ++ii)
+            for (int jj = 0; jj != 3; ++jj)
+              for (int kk = 0; kk != 3; ++kk)
+                for (int ll = 0; ll != 3; ++ll) {
+
+                  auto diff = [&](auto ii, auto jj, auto kk, auto ll) {
+                    return t_kd(ii, kk) * t_kd(jj, ll);
+                  };
+
+                  t_d_a(ii, jj, kk, ll) =
+                      (diff(ii, jj, kk, ll) + diff(ii, jj, ll, kk)) / 2.;
+                }
+
+          MOFEM_LOG("ATOM_TEST", Sev::verbose) << "t_d_a";
+          print_ddg(t_d_a, "hand ");
+          MOFEM_LOG("ATOM_TEST", Sev::verbose) << "t_d";
+          print_ddg(t_d, "code ");
+
+          for (int ii = 0; ii != 3; ++ii)
+            for (int jj = 0; jj != 3; ++jj)
+              for (int kk = 0; kk != 3; ++kk)
+                for (int ll = 0; ll != 3; ++ll)
+                  t_d_a(ii, jj, kk, ll) -= t_d(ii, jj, kk, ll);
+
+          double nrm2_t_d_a = get_norm_t4(t_d_a);
           MOFEM_LOG("ATOM_TEST", Sev::inform)
-              << "Derivative should be unity matrix " << norm2_t_c;
-          if (norm2_t_c > eps)
+              << "Direvarive hand calculation minus code " << nrm2_t_d_a;
+          if (nrm2_t_d_a > eps)
             SETERRQ(PETSC_COMM_SELF, MOFEM_ATOM_TEST_INVALID,
                     "This norm should be zero");
         }
@@ -461,6 +486,7 @@ int main(int argc, char *argv[]) {
           }
         }
       }
+
     }
   }
   CATCH_ERRORS;
