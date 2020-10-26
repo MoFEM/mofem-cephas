453c39077a101ead610b5a82348278e1bad7a467
Lukasz Kaczmarczyk <likask@wp.pl>
2020-10-26 12:34:47 +0000

    refactor code

--- mofem/atom_tests/matrix_function.cpp
+++ mofem/atom_tests/matrix_function.cpp
@@ -730,53 +730,20 @@ int main(int argc, char *argv[]) {
     // Testing two same eigen values
     {
 
-      int info;
-      double wkopt;
-      double w[3];
-
       std::array<double, 9> a{0.1, 0.,  0,
 
                               0.,  0.1, 0.,
 
                               0.0, 0.,  4.};
 
-      FTensor::Tensor2<double, 3, 3> t_a{
-
-          a[0], a[1], a[2],
-
-          a[3], a[4], a[5],
-
-          a[6], a[7], a[8]};
-
       auto f = [](double v) { return v; };
       auto d_f = [](double v) { return 1; };
       auto dd_f = [](double v) { return 0; };
 
-      /* Query and allocate the optimal workspace */
-      int lwork = -1;
-      info = lapack_dsyev('V', 'U', 3, a.data(), 3, w, &wkopt, lwork);
-      if (info > 0)
-        SETERRQ(PETSC_COMM_SELF, MOFEM_ATOM_TEST_INVALID,
-                "The algorithm failed to compute eigenvalues.");
-      lwork = (int)wkopt;
-      std::vector<double> work(lwork);
-      /* Solve eigenproblem */
-      info = lapack_dsyev('V', 'U', 3, a.data(), 3, w, &*work.begin(), lwork);
-      if (info > 0)
-        SETERRQ(PETSC_COMM_SELF, MOFEM_ATOM_TEST_INVALID,
-                "The algorithm failed to compute eigenvalues.");
-
-      FTensor::Tensor2<double, 3, 3> t_eig_vecs{
-
-          a[0 * 3 + 0], a[0 * 3 + 1], a[0 * 3 + 2],
-
-          a[2 * 3 + 0], a[2 * 3 + 1], a[2 * 3 + 2],
-
-          a[1 * 3 + 0], a[1 * 3 + 1], a[1 * 3 + 2]};
-
-      // FTensor::Tensor1<double, 3> t_eig_vals{w[0], w[2], w[1]};
-
-      FTensor::Tensor1<double, 3> t_eig_vals{w[0], w[2], w[1]};
+      auto tuple = run_lapack(a);
+      auto &t_a = std::get<0>(tuple);
+      auto &t_eig_vecs = std::get<1>(tuple);
+      auto &t_eig_vals = std::get<2>(tuple);
 
       // cerr << t_eig_vecs << endl;
       // cerr << t_eig_vals << endl;
