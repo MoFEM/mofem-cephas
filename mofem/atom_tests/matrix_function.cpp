628714309d767d7de96d884417e6ed9f6f04fe76
Lukasz Kaczmarczyk <likask@wp.pl>
2020-10-26 13:11:27 +0000

    reefactor code

--- mofem/atom_tests/matrix_function.cpp
+++ mofem/atom_tests/matrix_function.cpp
@@ -128,6 +128,19 @@ int main(int argc, char *argv[]) {
                   (diff(ii, jj, kk, ll) + diff(ii, jj, ll, kk)) / 2.;
             }
 
+      constexpr double eps = 1e-4;
+      for (int ii = 0; ii != 3; ++ii)
+        for (int jj = 0; jj != 3; ++jj)
+          for (int kk = 0; kk != 3; ++kk)
+            for (int ll = 0; ll != 3; ++ll) {
+
+              if (std::abs(t_d_a(ii, jj, kk, ll) - t_d(ii, jj, kk, ll)) > eps)
+                MOFEM_LOG("ATOM_TEST", Sev::error)
+                    << "Error " << ii << " " << jj << " " << kk << " " << ll
+                    << " " << t_d_a(ii, jj, kk, ll) << " "
+                    << t_d(ii, jj, kk, ll);
+            }
+
       for (int ii = 0; ii != 3; ++ii)
         for (int jj = 0; jj != 3; ++jj)
           for (int kk = 0; kk != 3; ++kk)
@@ -161,6 +174,20 @@ int main(int argc, char *argv[]) {
                 t_d_a(ii, jj, kk, ll) +=
                     (diff(ii, jj, kk, ll, zz) + diff(ii, jj, ll, kk, zz)) / 2.;
               }
+
+      constexpr double eps = 1e-4;
+      for (int ii = 0; ii != 3; ++ii)
+        for (int jj = 0; jj != 3; ++jj)
+          for (int kk = 0; kk != 3; ++kk)
+            for (int ll = 0; ll != 3; ++ll) {
+
+              if (std::abs(t_d_a(ii, jj, kk, ll) - t_d(ii, jj, kk, ll)) > eps)
+                MOFEM_LOG("ATOM_TEST", Sev::error)
+                    << "Error " << ii << " " << jj << " " << kk << " " << ll
+                    << " " << t_d_a(ii, jj, kk, ll) << " "
+                    << t_d(ii, jj, kk, ll);
+
+            }
 
       for (int ii = 0; ii != 3; ++ii)
         for (int jj = 0; jj != 3; ++jj)
@@ -774,48 +801,11 @@ int main(int argc, char *argv[]) {
 
         auto t_d = EigenProjection<double, double, 1>::getDiffMat(
             t_eig_vals, t_eig_vecs, f, d_f);
-
-        constexpr auto t_kd = FTensor::Kronecker_Delta<double>();
-        FTensor::Tensor4<double, 3, 3, 3, 3> t_d_a;
-        t_d_a(i, j, k, l) = 0;
-
-        for (int ii = 0; ii != 3; ++ii)
-          for (int jj = 0; jj != 3; ++jj)
-            for (int kk = 0; kk != 3; ++kk)
-              for (int ll = 0; ll != 3; ++ll) {
-
-                auto diff = [&](auto ii, auto jj, auto kk, auto ll) {
-                  return t_kd(ii, kk) * t_kd(jj, ll);
-                };
-
-                t_d_a(ii, jj, kk, ll) =
-                    (diff(ii, jj, kk, ll) + diff(ii, jj, ll, kk)) / 2.;
-              }
-
-        // MOFEM_LOG("ATOM_TEST", Sev::verbose) << "t_d_a";
-        // print_ddg(t_d_a, "hand ");
-        // MOFEM_LOG("ATOM_TEST", Sev::verbose) << "t_d";
-        // print_ddg(t_d, "code ");
-
-        for (int ii = 0; ii != 3; ++ii)
-          for (int jj = 0; jj != 3; ++jj)
-            for (int kk = 0; kk != 3; ++kk)
-              for (int ll = 0; ll != 3; ++ll) {
-
-                if (std::abs(t_d_a(ii, jj, kk, ll) - t_d(ii, jj, kk, ll)) > eps)
-                  MOFEM_LOG("ATOM_TEST", Sev::error)
-                      << "Error " << ii << " " << jj << " " << kk << " " << ll
-                      << " " << t_d_a(ii, jj, kk, ll) << " "
-                      << t_d(ii, jj, kk, ll);
-
-                t_d_a(ii, jj, kk, ll) -= t_d(ii, jj, kk, ll);
-              }
-
+        auto t_d_a = get_diff_matrix(t_d);
         MOFEM_LOG("ATOM_TEST", Sev::verbose) << "t_d_a";
         print_ddg(t_d_a, "hand ");
         MOFEM_LOG("ATOM_TEST", Sev::verbose) << "t_d";
         print_ddg(t_d, "code ");
-
         double nrm2_t_d_a = get_norm_t4(t_d_a);
         MOFEM_LOG("ATOM_TEST", Sev::inform)
             << "Direvarive hand calculation minus code " << nrm2_t_d_a;
