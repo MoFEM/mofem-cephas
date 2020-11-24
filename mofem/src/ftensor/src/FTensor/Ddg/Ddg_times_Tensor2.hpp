df56ba13ae8a7675ea683b4a83b0e2148d8b0171
Lukasz Kaczmarczyk <likask@wp.pl>
2020-11-24 20:27:34 +0000

    add operation

--- mofem/src/ftensor/src/FTensor/Ddg/Ddg_times_Tensor2.hpp
+++ mofem/src/ftensor/src/FTensor/Ddg/Ddg_times_Tensor2.hpp
@@ -154,6 +154,56 @@ namespace FTensor
                                   i, j>(TensorExpr(a, b));
   }
 
+  /* A(i,j,k,l)*B(i,j) */
+
+  template <class A, class B, class T, class U, int Dim01, int Dim23, char i,
+            char j, char k, char l>
+  class Ddg_times_Tensor2_01 {
+    Ddg_Expr<A, T, Dim01, Dim23, i, j, k, l> iterA;
+    Tensor2_Expr<B, U, Dim01, Dim01, i, j> iterB;
+
+    template <int Current_Dim0, int Current_Dim1>
+    typename promote<T, U>::V eval(const int N1, const int N2,
+                                   const Number<Current_Dim0> &,
+                                   const Number<Current_Dim1> &) const {
+      return iterA(Current_Dim0 - 1, Current_Dim1 - 1, N1, N2) *
+                 iterB(Current_Dim0 - 1, Current_Dim1 - 1) +
+             eval(N1, N2, Number<Current_Dim0 - 1>(), Number<Current_Dim1>());
+    }
+    template <int Current_Dim1>
+    typename promote<T, U>::V eval(const int N1, const int N2,
+                                   const Number<1> &,
+                                   const Number<Current_Dim1> &) const {
+      return iterA(0, Current_Dim1 - 1, N1, N2) * iterB(0, Current_Dim1 - 1) +
+             eval(N1, N2, Number<Dim01>(), Number<Current_Dim1 - 1>());
+    }
+    typename promote<T, U>::V eval(const int N1, const int N2,
+                                   const Number<1> &, const Number<1> &) const {
+      return iterA(0, 0, N1, N2) * iterB(0, 0);
+    }
+
+  public:
+    Ddg_times_Tensor2_01(const Ddg_Expr<A, T, Dim01, Dim23, i, j, k, l> &a,
+                         const Tensor2_Expr<B, U, Dim01, Dim01, i, j> &b)
+        : iterA(a), iterB(b) {}
+    typename promote<T, U>::V operator()(const int N1, const int N2) const {
+      return eval(N1, N2, Number<Dim01>(), Number<Dim01>());
+    }
+  };
+
+  template <class A, class B, class T, class U, int Dim01, int Dim23, char i,
+            char j, char k, char l>
+  Tensor2_symmetric_Expr<
+      Ddg_times_Tensor2_01<A, B, T, U, Dim01, Dim23, i, j, k, l>,
+      typename promote<T, U>::V, Dim23, k, l>
+  operator*(const Ddg_Expr<A, T, Dim01, Dim23, i, j, k, l> &a,
+            const Tensor2_Expr<B, U, Dim01, Dim01, i, j> &b) {
+    using TensorExpr =
+        Ddg_times_Tensor2_01<A, B, T, U, Dim01, Dim23, i, j, k, l>;
+    return Tensor2_symmetric_Expr<TensorExpr, typename promote<T, U>::V, Dim23,
+                                  k, l>(TensorExpr(a, b));
+  }
+
   /* This operatores will yield tensor 2 */
 
   /* A(i,j,k,l)*B(j,l) */
