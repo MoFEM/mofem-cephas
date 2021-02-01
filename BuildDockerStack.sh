16089b2b67b076cb85a4cbeab0fbe461b33520a0
Lukasz Kaczmarczyk <likask@wp.pl>
2021-02-01 19:22:23 +0000

    update docker

--- BuildDockerStack.sh
+++ BuildDockerStack.sh
@@ -2,26 +2,31 @@
 
 mkdir -p empty
 
+export VERSION=0.10.2
+
 # Create environment
-#docker build -t likask/mofem-spack-env \
-#  -f Dockerfile-spack-env . 
+docker build -t likask/mofem-spack-env -f Dockerfile-spack-env empty/
+docker tag likask/mofem-spack-env:latest likask/mofem-spack-env:$VERSION
 
 # Build core and users modules
-docker build -t likask/mofem-jupyter-um \
-  -f Dockerfile-spack-build . 
+docker build -t likask/mofem-spack-mofem -f Dockerfile-spack-mofem . 
+docker tag likask/mofem-spack-mofem:latest ikask/mofem-spack-mofem:$VERSION
 
 # Install softmech module
-docker build -t likask/mofem-jupyter-softmech \
-  -f Dockerfile-spack-jupyter-softmech empty/ 
-
+docker build -t likask/mofem-jupyter-softmech -f Dockerfile-spack-softmech empty/ 
+docker tag likask/mofem-softmech:latest likask/mofem-softmech:$VERSION 
 # Install other modules ...
 
-# Create volume
-docker build -t likask/mofem-spack-build \
-  -f Dockerfile-spack-volume empty
 
-# Make Jupyter
-docker build -t likask/mofem-spack-jupyter \
-  -f Dockerfile-spack-jupyter empty
+# Tag last module
+docker tag likask/mofem-jupyter-softmech:latest likask/mofem-intermidiate:latest
+
+# Volume
+docker build -t likask/mofem-spack-build -f Dockerfile-spack-volume empty
+docker tag likask/mofem-spack-build:latest likask/mofem-spack-build:$VERSION 
+
+# Jupyter
+docker build -t likask/mofem-spack-jupyter -f Dockerfile-spack-jupyter empty
+docker tag likask/likask/mofem-spack-jupyter:latest likask/likask/mofem-spack-jupyter:$VERSION 
 
   
