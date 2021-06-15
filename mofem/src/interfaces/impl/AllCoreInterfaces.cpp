0e6985e9854adb7c534cf1f0be42aa0f1119cc89
Lukasz Kaczmarczyk <lukasz.kaczmarczyk@glasgow.ac.uk>
2021-06-15 12:34:37 +0100

    makd verion methods static

--- mofem/src/interfaces/impl/AllCoreInterfaces.cpp
+++ mofem/src/interfaces/impl/AllCoreInterfaces.cpp
@@ -4,6 +4,7 @@ const static int debug = 0;
 
 #include <MoFEM.hpp>
 
+#include "impl/UnknownInterface.cpp"
 #include "impl/MatrixManager.cpp"
 #include "impl/ISManager.cpp"
 #include "impl/VecManager.cpp"
