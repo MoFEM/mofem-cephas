c6bd39087388220a30e3080d6d2ba7d8d0a75dc3
Lukasz Kaczmarczyk <lukasz.kaczmarczyk@glasgow.ac.uk>
2021-08-20 00:18:30 +0100

    remove obsolete line

--- mofem/atom_tests/hcurl_divergence_operator_2d_embaded_in_3d.cpp
+++ mofem/atom_tests/hcurl_divergence_operator_2d_embaded_in_3d.cpp
@@ -225,8 +225,6 @@ int main(int argc, char *argv[]) {
       double flux = 0;
       EdgeEle fe_edge(m_field);
       fe_edge.getRuleHook = rule;
-      // fe_edge.getOpPtrVector().push_back(
-      //     new OpSetContravariantPiolaTransformOnEdge2D());
       fe_edge.getOpPtrVector().push_back(new OpFlux(flux));
       CHKERR m_field.loop_finite_elements("TEST_PROBLEM", "EDGE_FE", fe_edge);
       return flux;
