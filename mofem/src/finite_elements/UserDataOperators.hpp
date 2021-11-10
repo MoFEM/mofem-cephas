d4c84c8124c544e238b85c2320c9eb3884805784
Ignatios Athanasiadis <ignatiosathanasiadis@Ignatioss-MacBook-Pro-3.local>
2021-11-10 15:49:41 +0000

    adding trace evaluateor [skip ci]

--- mofem/src/finite_elements/UserDataOperators.hpp
+++ mofem/src/finite_elements/UserDataOperators.hpp
@@ -2891,6 +2891,65 @@ OpInvertMatrix<DIM>::doWorkImpl(int side, EntityType type,
 
 /**@}*/
 
+/** \brief Calculates the trace of an input matrix
+
+\ingroup mofem_forces_and_sources
+
+*/
+
+template <int DIM>
+struct OpCalculateTraceFromMat : public ForcesAndSourcesCore::UserDataOperator {
+
+  OpCalculateTraceFromMat(boost::shared_ptr<MatrixDouble> in_ptr,
+                 boost::shared_ptr<VectorDouble> out_ptr)
+      : ForcesAndSourcesCore::UserDataOperator(NOSPACE), inPtr(in_ptr),
+        outPtr(out_ptr) {}
+
+  MoFEMErrorCode doWork(int side, EntityType type,
+                        DataForcesAndSourcesCore::EntData &data);
+
+private:
+  boost::shared_ptr<MatrixDouble> inPtr;
+  boost::shared_ptr<VectorDouble> outPtr;
+
+  // MoFEMErrorCode doWorkImpl(int side, EntityType type,
+  //                           DataForcesAndSourcesCore::EntData &data,
+  //                           const FTensor::Number<DIM> &);
+
+  // MoFEMErrorCode doWorkImpl(int side, EntityType type,
+  //                           DataForcesAndSourcesCore::EntData &data,
+  //                           const FTensor::Number<2> &);
+};
+
+template <int DIM>
+MoFEMErrorCode
+OpCalculateTraceFromMat<DIM>::doWork(int side, EntityType type,
+                                DataForcesAndSourcesCore::EntData &data) {
+  MoFEMFunctionBegin;
+
+  if (!inPtr)
+    SETERRQ(PETSC_COMM_SELF, MOFEM_DATA_INCONSISTENCY,
+            "Pointer for inPtr matrix not allocated");
+
+  const auto nb_integration_pts = inPtr->size2();
+  FTensor::Index<'i', DIM> i;
+  // Invert jacobian
+  if (outPtr) {
+    outPtr->resize(nb_integration_pts, false);
+    auto t_in = getFTensor2FromMat<DIM, DIM>(*inPtr);
+    auto t_out = getFTensor0FromVec(*outPtr);
+
+    for (size_t gg = 0; gg != nb_integration_pts; ++gg) {
+      t_out = t_in(i, i);
+      ++t_in;
+      ++t_out;
+    }
+  }
+
+  MoFEMFunctionReturn(0);
+}
+
+
 } // namespace MoFEM
 
 #endif // __USER_DATA_OPERATORS_HPP__
