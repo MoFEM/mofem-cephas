25ebb7696ad09c0bedc56ec3bccbd2db71033da8
Lukasz Kaczmarczyk <lukasz.kaczmarczyk@glasgow.ac.uk>
2021-12-22 20:21:46 +0000

    refactor field blas interface

--- mofem/src/interfaces/FieldBlas.hpp
+++ mofem/src/interfaces/FieldBlas.hpp
@@ -40,8 +40,74 @@ struct FieldBlas : public UnknownInterface {
 
   ~FieldBlas() = default;
 
-  typedef boost::function<MoFEMErrorCode(double &, const double)>
-      TwoFieldFunction;
+  /**
+   * @brief function to set a field value
+   *
+   */
+  using OneFieldFunctionOnValues =
+      boost::function<double(const double field_val)>;
+
+  /**
+   * @brief
+   *
+   */
+  using OneFieldFunctionOnEntities = boost::function<MoFEMErrorCode(
+      boost::shared_ptr<FieldEntity> field_entity_ptr)>;
+
+  /**
+   * @brief
+   * \param y_field_value_reference field "y_field" values
+   * \param x_field_value
+   */
+  using TwoFieldFunctionOnValues = boost::function<double(
+      const double y_field_value_reference, const double x_field_value)>;
+
+  /**
+   * @brief
+   * \param y_field_entity_ptr pointer to second "y_field" field entities
+   * \param x_field_entity_ptr pointer to first "x_field" field entities
+   */
+  using TwoFieldFunctionOnEntities = boost::function<MoFEMErrorCode(
+      boost::shared_ptr<FieldEntity> y_field_entity_ptr,
+      const boost::shared_ptr<FieldEntity> x_field_entity_ptr)>;
+
+  /** \brief filed lambda
+     * \ingroup mofem_field_algebra
+     *
+     * Do calculation on two fields and save result to field fy
+     *
+     * \code
+     * FIXME
+     * \endcode
+     *
+     * \param function f(double &x, double)
+     * \param field_name name of field
+     * \param ents_ptr execute lambda function only on entities given by pointer
+     to range
+     *
+     */
+  MoFEMErrorCode fieldLambdaOnEntities(OneFieldFunctionOnEntities lambda,
+                                       const std::string field_name,
+                                       Range *ents_ptr = nullptr);
+
+  /** \brief filed lambda
+   * \ingroup mofem_field_algebra
+   *
+   * Do calculation on two fields and save result to field fy
+   *
+   * \code
+   * FIXME
+   * \endcode
+   *
+   * \param function f(double &x, double)
+   * \param field_name name of field
+   * \param ents_ptr execute lambda function only on entities given by
+   pointer to range
+   *
+   */
+  MoFEMErrorCode fieldLambdaOnValues(OneFieldFunctionOnValues lambda,
+                                     const std::string field_name,
+                                     Range *ents_ptr = nullptr);
 
   /** \brief filed lambda
    * \ingroup mofem_field_algebra
@@ -58,23 +124,62 @@ struct FieldBlas : public UnknownInterface {
       MoFEMFunctionReturnHot(0);
     }
    };
-   CHKERR m_fiel.getInterface<FieldBlas>()->fieldLambda(Axpy(aLpha),
+   CHKERR m_field.getInterface<FieldBlas>()->fieldLambda(Axpy(aLpha),
    field_name_x, field_name_y);
    * \endcode
    *
    * \param function f(double &x, double)
    * \param field_name_x name of field_x
    * \param field_name_y name of field_y
-   * \param error_if_missing throw error if entity/dof exist in field_x but not
-   * on field_y \param create_if_missing creat dof in field_y from field_x if it
-   * is not database
+   * \param error_if_missing throw error if missing entities of field y
+   * \param ents_ptr execute lambda function only on entities given by pointer
+   to range
    *
    */
-  MoFEMErrorCode fieldLambda(TwoFieldFunction lambda,
-                             const std::string field_name_x,
-                             const std::string field_name_y,
-                             bool error_if_missing = false,
-                             bool creat_if_missing = false);
+  MoFEMErrorCode fieldLambdaOnValues(TwoFieldFunctionOnValues lambda,
+                                     const std::string field_name_x,
+                                     const std::string field_name_y,
+                                     bool error_if_missing = false,
+                                     Range *ents_ptr = nullptr);
+
+  /** \brief filed lambda
+   * \ingroup mofem_field_algebra
+   *
+   * Do calculation on two fields and save result to field fy
+   *
+   * \code
+   * FIXME
+   * \endcode
+   *
+   * \param function f(double &x, double)
+   * \param field_name_x name of field_x
+   * \param field_name_y name of field_y
+   * \param error_if_missing throw error if missing entities of field y
+   * \param ents_ptr execute lambda function only on entities given by pointer
+   to range
+   *
+   */
+  MoFEMErrorCode fieldLambdaOnEntities(TwoFieldFunctionOnEntities lambda,
+                                       const std::string field_name_x,
+                                       const std::string field_name_y,
+                                       bool error_if_missing = false,
+                                       Range *ents_ptr = nullptr);
+
+  /**
+   * @deprecated This is with obsolete last option
+   *
+   * @param lambda
+   * @param field_name_x
+   * @param field_name_y
+   * @param error_if_missing
+   * @param create_if_missing
+   * @return DEPRECATED
+   */
+  DEPRECATED MoFEMErrorCode fieldLambda(TwoFieldFunctionOnValues lambda,
+                                        const std::string field_name_x,
+                                        const std::string field_name_y,
+                                        bool error_if_missing,
+                                        bool create_if_missing);
 
   /** \brief axpy fields
    * \ingroup mofem_field_algebra
@@ -85,14 +190,33 @@ struct FieldBlas : public UnknownInterface {
    * \param field_name_x name of field_x
    * \param field_name_y name of field_y
    * \param error_if_missing throw error if entity/dof exist in field_x but not
-   * on field_y \param create_if_missing creat dof in field_y from fiedl_x if it
-   * is not database
+   * \param ents_ptr execute lambda function only on entities given by pointer
+   * on field_y
    *
    */
   MoFEMErrorCode fieldAxpy(const double alpha, const std::string field_name_x,
                            const std::string field_name_y,
                            bool error_if_missing = false,
-                           bool creat_if_missing = false);
+                           Range *ents_ptr = nullptr);
+
+  /** \deprecated axpy fields
+   * \ingroup mofem_field_algebra
+   *
+   * field_y = field_y + alpha*field_x
+   *
+   * \param alpha
+   * \param field_name_x name of field_x
+   * \param field_name_y name of field_y
+   * \param error_if_missing throw error if entity/dof exist in field_x but not
+   * on field_y \param create_if_missing creat dof in field_y from field_x if it
+   * is not database
+   *
+   */
+  DEPRECATED MoFEMErrorCode fieldAxpy(const double alpha,
+                                      const std::string field_name_x,
+                                      const std::string field_name_y,
+                                      bool error_if_missing,
+                                      bool create_if_missing);
 
   /** \brief copy and scale fields
    * \ingroup mofem_field_algebra
@@ -103,19 +227,37 @@ struct FieldBlas : public UnknownInterface {
    * \param field_name_x name of field_x
    * \param field_name_y name of field_y
    * \param error_if_missing throw error if entity/dof exist in field_x but not
-   * on field_y \param create_if_missing creat dof in field_y from fiedl_x if it
+   * \param ents_ptr execute lambda function only on entities given by pointer
+   * on field_y \param create_if_missing creat dof in field_y from field_x if it
    * is not database
    *
    */
   MoFEMErrorCode fieldCopy(const double alpha, const std::string field_name_x,
                            const std::string field_name_y,
                            bool error_if_missing = false,
-                           bool creat_if_missing = false);
+                           Range *ents_ptr = nullptr);
 
-  typedef boost::function<MoFEMErrorCode(VectorAdaptor &&field_data,
-                                         double *xcoord, double *ycoord,
-                                         double *zcoord)>
-      VertexCoordsFunction;
+  DEPRECATED MoFEMErrorCode fieldCopy(const double alpha,
+                                      const std::string field_name_x,
+                                      const std::string field_name_y,
+                                      bool error_if_missing,
+                                      bool create_if_missing);
+
+  /** \brief field scale
+   * \ingroup mofem_field_algebra
+   * 
+   * \param scale field by value
+   * \param field_name name of field
+   * \param error_if_missing throw error if missing entities of field y
+   * \param ents_ptr execute lambda function only on entities given by
+   pointer to range
+   */
+  MoFEMErrorCode fieldScale(const double alpha, const std::string field_name,
+                            Range *ents_ptr = nullptr);
+
+  using VertexCoordsFunction =
+      boost::function<MoFEMErrorCode(VectorAdaptor &&field_data, double *xcoord,
+                                     double *ycoord, double *zcoord)>;
 
   /** \brief Set DOFs on vertices using user function
    * \ingroup mofem_field_algebra
@@ -134,7 +276,8 @@ struct FieldBlas : public UnknownInterface {
    * CHKERR m_field.getInterface<FieldBlas>()->setVertexDofs(set_distance,
    * "DISP"); \endcode
    *
-   * \note Function works both ways, using it coordinates can be set from field.
+   * \note Function works both ways, using it coordinates can be set from
+   * field.
    *
    * \param lambda function evaluating field at points
    * \param field_name  is a field name
@@ -146,7 +289,7 @@ struct FieldBlas : public UnknownInterface {
                                const std::string field_name,
                                Range *verts = nullptr);
 
-  /** \brief scale field
+  /** \deprecated scale field
    * \ingroup mofem_field_algebra
    *
    * \param val is a set parameter
@@ -180,27 +323,8 @@ struct FieldBlas : public UnknownInterface {
    */
   MoFEMErrorCode setField(const double val, const std::string field_name);
 
-  /** \brief set field
-   * \ingroup mofem_field_algebra
-   *
-   * field_y = val
-   *
-   * \param val
-   * \param entity type
-   * \param on enties
-   * \param field_name
-   *
-   */
-  MoFEMErrorCode fieldScale(const double alpha, const std::string field_name);
 };
 
 } // namespace MoFEM
 
 #endif // __FIELD_BLAS_HPP__
-
-/**
- * \defgroup mofem_field_algebra Field Basic Algebra
- * \brief Basic algebraic operation on fields
- *
- * \ingroup mofem
- */
