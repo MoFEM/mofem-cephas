b970e31da13b1e5fecfbe8f4b8584da32a147a4a
Lukasz Kaczmarczyk <lukasz.kaczmarczyk@glasgow.ac.uk>
2022-02-08 20:51:51 +0000

    work on field evaluaror

--- mofem/atom_tests/field_evaluator.cpp
+++ mofem/atom_tests/field_evaluator.cpp
@@ -25,6 +25,65 @@ using namespace MoFEM;
 
 static char help[] = "testing field evaluator\n\n";
 
+/**
+ * @brief Operator used to check consistency between local coordinates
+ * and global cooridnates for integrated points and evaluated points
+ *
+ */
+template <typename OP> struct MyOp : public OP {
+
+  MatrixShallowArrayAdaptor<double> evalPoints;
+
+  MyOp(std::array<double, 12> &eval_points)
+      : OP("FIELD1", OP::OPROW),
+        evalPoints(
+            getMatrixAdaptor(eval_points.data(), eval_points.size() / 3, 3)) {}
+
+  MoFEMErrorCode doWork(int side, EntityType type,
+                        DataForcesAndSourcesCore::EntData &data) {
+    MoFEMFunctionBegin;
+    if (type == MBVERTEX) {
+
+      std::cout << "Integration pts" << std::endl;
+      std::cout << OP::getGaussPts() << endl;
+
+      std::cout << "Global coordinates " << endl;
+      std::cout << OP::getCoordsAtGaussPts() << std::endl;
+
+      for (int gg = 0; gg != OP::getCoordsAtGaussPts().size1(); ++gg) {
+        int pt_number = OP::getGaussPts()(3, gg);
+
+        std::cout << "gg " << gg << std::endl;
+        std::cout << "pt " << pt_number << std::endl;
+
+        ublas::matrix_row<MatrixDouble> coord_at_gauss_pt(
+            OP::getCoordsAtGaussPts(), gg);
+        ublas::matrix_row<MatrixShallowArrayAdaptor<double>> eval_coord(
+            evalPoints, pt_number);
+
+        std::cout << "coord_at_gauss_pt ";
+        std::cout << coord_at_gauss_pt << std::endl;
+
+        std::cout << "eval_coord ";
+        std::cout << eval_coord << std::endl;
+
+        double error = norm_2(coord_at_gauss_pt - eval_coord);
+
+        if (error > 1e-12)
+          SETERRQ2(PETSC_COMM_SELF, MOFEM_ATOM_TEST_INVALID,
+                   "Difference at %d error = %3.4e", pt_number, error);
+      }
+    }
+    MoFEMFunctionReturn(0);
+  }
+};
+
+// Lambda function is used to set integration rule to -1, that indicates
+// that finite element instace will use non-standard integration points.
+auto get_rule = [](int order_row, int order_col, int order_data) {
+  return -1;
+};
+
 int main(int argc, char *argv[]) {
 
   MoFEM::Core::Initialize(&argc, &argv, (char *)0, help);
@@ -62,101 +121,86 @@ int main(int argc, char *argv[]) {
       FieldEvaluatorInterface *field_eval_ptr;
       CHKERR m_field.getInterface(field_eval_ptr);
 
-      std::array<double, 3> point = {0, 0, 0};
-      const double dist = 0.1;
-
-      std::array<double, 12> eval_points = {0.0,  0.0, 0.0, 0.0, 0.01, 0.0,
-                                            -0.1, 0.0, 0.0, 0.1, 0.0,  0.0};
 
       auto dm = simple_interface->getDM();
       const MoFEM::Problem *prb_ptr;
       CHKERR DMMoFEMGetProblemPtr(dm, &prb_ptr);
 
-      using VolEle = VolumeElementForcesAndSourcesCore;
-      using VolOp = VolumeElementForcesAndSourcesCore::UserDataOperator;
+      if (simple_interface->getDim() == 3) {
 
-      /**
-       * @brief Operator used to check consistency between local coordinates and
-       * global cooridnates for integrated points and evaluated points
-       *
-       */
-      struct MyOp : public VolOp {
+        std::array<double, 3> point = {0, 0, 0};
+        const double dist = 0.1;
 
-        MatrixShallowArrayAdaptor<double> evalPoints;
+        std::array<double, 12> eval_points = {0.0,  0.0, 0.0, 0.0, 0.01, 0.0,
+                                              -0.1, 0.0, 0.0, 0.1, 0.0,  0.0};
 
-        MyOp(std::array<double, 12> &eval_points)
-            : VolOp("FIELD1", OPROW),
-              evalPoints(getMatrixAdaptor(eval_points.data(),
-                                          eval_points.size() / 3, 3)) {}
+        using VolEle = VolumeElementForcesAndSourcesCore;
+        using VolOp = VolEle::UserDataOperator;
 
-        MoFEMErrorCode doWork(int side, EntityType type,
-                              DataForcesAndSourcesCore::EntData &data) {
-          MoFEMFunctionBegin;
-          if (type == MBVERTEX) {
+        // Get pointer of FieldEvaluator data. Note finite element and method
+        // set integrating points is destroyed when this pointer is releases
+        auto data = field_eval_ptr->getData<VolEle>();
 
-            std::cout << "Integration pts" << std::endl;
-            std::cout << getGaussPts() << endl;
+        // Set operators and integration rule
+        if (auto fe_method = data->feMethodPtr.lock()) {
+          fe_method->getRuleHook = get_rule;
+          fe_method->getOpPtrVector().push_back(new MyOp<VolOp>(eval_points));
+        } else
+          SETERRQ(PETSC_COMM_SELF, MOFEM_DATA_INCONSISTENCY,
+                  "Pointer to element does not exists");
 
-            std::cout << "Global coordinates " << endl;
-            std::cout << getCoordsAtGaussPts() << std::endl;
+        // Build tree for particular element
+        CHKERR field_eval_ptr->buildTree3D(data,
+                                           simple_interface->getDomainFEName());
+        // Set points to set on finite elements
+        data->setEvalPoints(eval_points.data(), eval_points.size() / 3);
 
-            for (int gg = 0; gg != getCoordsAtGaussPts().size1(); ++gg) {
-              int pt_number = getGaussPts()(3, gg);
+        // Evaluate points on finite elements
+        CHKERR field_eval_ptr->evalFEAtThePoint3D(
+            &point[0], dist, prb_ptr->getName(),
+            simple_interface->getDomainFEName(), data, m_field.get_comm_rank(),
+            m_field.get_comm_rank(), nullptr, MF_EXIST, VERY_NOISY);
+      }
 
-              std::cout << "gg " << gg << std::endl;
-              std::cout << "pt " << pt_number << std::endl;
+      if (simple_interface->getDim() == 2) {
 
-              ublas::matrix_row<MatrixDouble> coord_at_gauss_pt(
-                  getCoordsAtGaussPts(), gg);
-              ublas::matrix_row<MatrixShallowArrayAdaptor<double>> eval_coord(
-                  evalPoints, pt_number);
+        std::array<double, 3> point = {0, 0, 0};
+        const double dist = 0.1;
 
-              std::cout << "coord_at_gauss_pt ";
-              std::cout << coord_at_gauss_pt << std::endl;
+        std::array<double, 12> eval_points = {
+          0.0,  0.0, 0.0, 
+          0.0, 0.01, 0.0,
+          -0.1, 0.0, 0.0, 
+          0.1, 0.0,  0.0};
 
-              std::cout << "eval_coord ";
-              std::cout << eval_coord << std::endl;
+        using FaceEle = FaceElementForcesAndSourcesCore;
+        using FaceOp = FaceEle::UserDataOperator;
 
-              double error = norm_2(coord_at_gauss_pt - eval_coord);
+        // Get pointer of FieldEvaluator data. Note finite element and method
+        // set integrating points is destroyed when this pointer is releases
+        auto data = field_eval_ptr->getData<FaceEle>();
 
-              if (error > 1e-12)
-                SETERRQ2(PETSC_COMM_SELF, MOFEM_ATOM_TEST_INVALID,
-                         "Difference at %d error = %3.4e", pt_number, error);
-            }
-          }
-          MoFEMFunctionReturn(0);
-        }
-      };
+        // Set operators and integration rule
+        if (auto fe_method = data->feMethodPtr.lock()) {
+          fe_method->getRuleHook = get_rule;
+          fe_method->getOpPtrVector().push_back(new MyOp<FaceOp>(eval_points));
+        } else
+          SETERRQ(PETSC_COMM_SELF, MOFEM_DATA_INCONSISTENCY,
+                  "Pointer to element does not exists");
 
-      // Lambda function is used to set integration rule to -1, that indicates
-      // that finite element instace will use non-standard integration points.
-      auto get_rule = [&](int order_row, int order_col, int order_data) {
-        return -1;
-      };
+        // Build tree for particular element
+        CHKERR field_eval_ptr->buildTree2D(data,
+                                           simple_interface->getDomainFEName());
+        // Set points to set on finite elements
+        data->setEvalPoints(eval_points.data(), eval_points.size() / 3);
 
-      // Get pointer of FieldEvaluator data. Note finite element and method
-      // set integrating points is destroyed when this pointer is releases
-      auto data = field_eval_ptr->getData<VolEle>();
-      
-      // Set operators and integration rule
-      if (auto fe_method = data->feMethodPtr.lock()) {
-        fe_method->getRuleHook = get_rule;
-        fe_method->getOpPtrVector().push_back(new MyOp(eval_points));
-      } else
-        SETERRQ(PETSC_COMM_SELF, MOFEM_DATA_INCONSISTENCY,
-                "Pointer to element does not exists");
+        // Evaluate points on finite elements
+        CHKERR field_eval_ptr->evalFEAtThePoint2D(
+            &point[0], dist, prb_ptr->getName(),
+            simple_interface->getDomainFEName(), data, m_field.get_comm_rank(),
+            m_field.get_comm_rank(), nullptr, MF_EXIST, VERY_NOISY);
+      }
 
-      // Build tree for particular element
-      CHKERR field_eval_ptr->buildTree3D(data,
-                                         simple_interface->getDomainFEName());
-      // Set points to set on finite elements
-      data->setEvalPoints(eval_points.data(), eval_points.size() / 3);
-
-      // Evaluate points on finite elements
-      CHKERR field_eval_ptr->evalFEAtThePoint3D(
-          &point[0], dist, prb_ptr->getName(),
-          simple_interface->getDomainFEName(), data, m_field.get_comm_rank(),
-          m_field.get_comm_rank(), nullptr, MF_EXIST, VERY_NOISY);
     }
   }
   CATCH_ERRORS;
--- mofem/src/interfaces/impl/FieldEvaluator.cpp
+++ mofem/src/interfaces/impl/FieldEvaluator.cpp
@@ -52,19 +52,32 @@ MoFEMErrorCode FieldEvaluatorInterface::query_interface(
   MoFEMFunctionReturnHot(0);
 }
 
+template <int D>
 MoFEMErrorCode
-FieldEvaluatorInterface::buildTree3D(boost::shared_ptr<SetPtsData> spd_ptr,
-                                     const std::string finite_element) {
+FieldEvaluatorInterface::buildTree(boost::shared_ptr<SetPtsData> spd_ptr,
+                                   const std::string finite_element) {
   CoreInterface &m_field = cOre;
   MoFEMFunctionBegin;
   EntityHandle fe_meshset;
   fe_meshset = m_field.get_finite_element_meshset(finite_element);
-  Range entities_3d;
-  CHKERR m_field.get_moab().get_entities_by_dimension(fe_meshset, 3,
-                                                      entities_3d, true);
+  Range entities;
+  CHKERR m_field.get_moab().get_entities_by_dimension(fe_meshset, D, entities,
+                                                      true);
   spd_ptr->treePtr.reset(new AdaptiveKDTree(&m_field.get_moab()));
-  CHKERR spd_ptr->treePtr->build_tree(entities_3d, &spd_ptr->rooTreeSet);
+  CHKERR spd_ptr->treePtr->build_tree(entities, &spd_ptr->rooTreeSet);
   MoFEMFunctionReturn(0);
+}
+
+MoFEMErrorCode
+FieldEvaluatorInterface::buildTree3D(boost::shared_ptr<SetPtsData> spd_ptr,
+                                     const std::string finite_element) {
+  return buildTree<3>(spd_ptr, finite_element);
+}
+
+MoFEMErrorCode
+FieldEvaluatorInterface::buildTree2D(boost::shared_ptr<SetPtsData> spd_ptr,
+                                     const std::string finite_element) {
+  return buildTree<2>(spd_ptr, finite_element);
 }
 
 MoFEMErrorCode
@@ -75,53 +88,77 @@ FieldEvaluatorInterface::SetPts::operator()(ForcesAndSourcesCore *fe_raw_ptr,
 
   if (auto data_ptr = dataPtr.lock()) {
 
-    decltype(data_ptr->nbEvalPoints) nbEvalPoints = data_ptr->nbEvalPoints;
-    decltype(data_ptr->verb) verb = data_ptr->verb;
-
-    decltype(data_ptr->shapeFunctions) &shapeFunctions =
-        data_ptr->shapeFunctions;
-    decltype(data_ptr->evalPointEntityHandle) &evalPointEntityHandle =
-        data_ptr->evalPointEntityHandle;
+    const auto nb_eval_points = data_ptr->nbEvalPoints;
+    const auto &shape_functions = data_ptr->shapeFunctions;
+    const auto &eval_pointentity_handle = data_ptr->evalPointEntityHandle;
 
     if (auto fe_ptr = data_ptr->feMethodPtr.lock()) {
 
-      VolumeElementForcesAndSourcesCore &fe =
-          static_cast<VolumeElementForcesAndSourcesCore &>(*fe_ptr);
+      auto &fe = static_cast<ForcesAndSourcesCore &>(*fe_ptr);
 
+#ifndef NDEBUG
       if (fe_ptr.get() != fe_raw_ptr)
         SETERRQ(PETSC_COMM_SELF, MOFEM_DATA_INCONSISTENCY, "Wrong FE ptr");
+#endif
 
-      MatrixDouble &gauss_pts = fe.gaussPts;
-      gauss_pts.resize(4, nbEvalPoints, false);
-      gauss_pts.clear();
+      const auto fe_ent = fe.numeredEntFiniteElementPtr->getEnt();
+      const auto fe_type = type_from_handle(fe_ent);
+      const auto fe_dim = moab::CN::Dimension(fe_type);
 
-      FTensor::Tensor1<FTensor::PackPtr<double *, 4>, 4> t_shape = {
-          &shapeFunctions(0, 0), &shapeFunctions(0, 1), &shapeFunctions(0, 2),
-          &shapeFunctions(0, 3)};
-      FTensor::Tensor1<FTensor::PackPtr<double *, 1>, 3> t_gauss_pts = {
-          &gauss_pts(0, 0), &gauss_pts(1, 0), &gauss_pts(2, 0)};
+      auto &gauss_pts = fe.gaussPts;
 
-      int nb_gauss_pts = 0;
-      for (int nn = 0; nn != nbEvalPoints; ++nn) {
-        if (evalPointEntityHandle[nn] ==
-            fe.numeredEntFiniteElementPtr->getEnt()) {
-          for (const int i : {0, 1, 2}) {
-            t_gauss_pts(i) = t_shape(i + 1);
-            gauss_pts(3, nb_gauss_pts) = nn;
+      if (fe_dim == 3) {
+        gauss_pts.resize(4, nb_eval_points, false);
+        FTensor::Tensor1<FTensor::PackPtr<const double *, 4>, 4> t_shape{
+            &shape_functions(0, 0), &shape_functions(0, 1),
+            &shape_functions(0, 2), &shape_functions(0, 3)};
+        FTensor::Tensor1<FTensor::PackPtr<double *, 1>, 3> t_gauss_pts{
+            &gauss_pts(0, 0), &gauss_pts(1, 0), &gauss_pts(2, 0)};
+
+        int nb_gauss_pts = 0;
+        for (int nn = 0; nn != nb_eval_points; ++nn) {
+          if (eval_pointentity_handle[nn] == fe_ent) {
+            for (const int i : {0, 1, 2}) {
+              t_gauss_pts(i) = t_shape(i + 1);
+              gauss_pts(3, nb_gauss_pts) = nn;
+            }
+            ++t_gauss_pts;
+            ++nb_gauss_pts;
           }
-          ++t_gauss_pts;
-          ++nb_gauss_pts;
+          ++t_shape;
         }
-        ++t_shape;
+        gauss_pts.resize(4, nb_gauss_pts, true);
+      } else if (fe_dim == 2) {
+        gauss_pts.resize(3, nb_eval_points, false);
+        FTensor::Tensor1<FTensor::PackPtr<const double *, 3>, 3> t_shape{
+            &shape_functions(0, 0), &shape_functions(0, 1),
+            &shape_functions(0, 2)};
+        FTensor::Tensor1<FTensor::PackPtr<double *, 1>, 2> t_gauss_pts{
+            &gauss_pts(0, 0), &gauss_pts(1, 0)};
+        int nb_gauss_pts = 0;
+        for (int nn = 0; nn != nb_eval_points; ++nn) {
+          if (eval_pointentity_handle[nn] == fe_ent) {
+            for (const int i : {0, 1, 2}) {
+              t_gauss_pts(i) = t_shape(i + 1);
+              gauss_pts(2, nb_gauss_pts) = nn;
+            }
+            ++t_gauss_pts;
+            ++nb_gauss_pts;
+          }
+          ++t_shape;
+        }
+        gauss_pts.resize(3, nb_gauss_pts, true);
+      } else {
+        SETERRQ(PETSC_COMM_SELF, MOFEM_NOT_IMPLEMENTED,
+                "Dimension not implemented");
       }
 
-      if (verb >= VERY_NOISY)
-        std::cout << "nbEvalOPoints / nbGaussPts: " << nbEvalPoints << " / "
-                  << nb_gauss_pts << std::endl;
-      gauss_pts.resize(4, nb_gauss_pts, true);
-
-      if (verb >= VERY_NOISY)
-        std::cout << "gauss pts: " << gauss_pts << std::endl;
+#ifndef NDEBUG
+      MOFEM_LOG("SELF", Sev::noisy)
+          << "nbEvalOPoints / nbGau_sspt_s: " << nbEvalPoints << " / "
+          << nb_gauss_pts;
+      MOFEM_LOG("SELF", Sev::noisy) << "gauss pts: " << gauss_pts;
+#endif
 
     } else
       SETERRQ(PETSC_COMM_SELF, MOFEM_DATA_INCONSISTENCY,
@@ -134,7 +171,8 @@ FieldEvaluatorInterface::SetPts::operator()(ForcesAndSourcesCore *fe_raw_ptr,
   MoFEMFunctionReturn(0);
 }
 
-MoFEMErrorCode FieldEvaluatorInterface::evalFEAtThePoint3D(
+template <int D>
+MoFEMErrorCode FieldEvaluatorInterface::evalFEAtThePoint(
     const double *const point, const double distance, const std::string problem,
     const std::string finite_element, boost::shared_ptr<SetPtsData> data_ptr,
     int lower_rank, int upper_rank, boost::shared_ptr<CacheTuple> cache_ptr,
@@ -146,7 +184,7 @@ MoFEMErrorCode FieldEvaluatorInterface::evalFEAtThePoint3D(
   CHKERR data_ptr->treePtr->distance_search(point, distance, leafs_out);
   Range tree_ents;
   for (auto lit : leafs_out)
-    CHKERR m_field.get_moab().get_entities_by_dimension(lit, 3, tree_ents,
+    CHKERR m_field.get_moab().get_entities_by_dimension(lit, D, tree_ents,
                                                         true);
   if (verb >= NOISY)
     MOFEM_LOG("SELF", Sev::noisy) << "tree entities: " << tree_ents;
@@ -155,39 +193,84 @@ MoFEMErrorCode FieldEvaluatorInterface::evalFEAtThePoint3D(
   std::fill(data_ptr->evalPointEntityHandle.begin(),
             data_ptr->evalPointEntityHandle.end(), 0);
 
-  for (auto tet : tree_ents) {
+  std::vector<double> local_coords;
+  std::vector<double> shape;
 
+  auto nb_eval_points = data_ptr->nbEvalPoints;
+
+  for (auto tet : tree_ents) {
     const EntityHandle *conn;
     int num_nodes;
     CHKERR m_field.get_moab().get_connectivity(tet, conn, num_nodes, true);
-    std::array<double, 12> coords;
-    CHKERR m_field.get_moab().get_coords(conn, num_nodes, coords.data());
 
-    for (int n = 0; n != data_ptr->nbEvalPoints; ++n) {
+    if constexpr (D == 3) {
 
-      std::array<double, 3> local_coords;
+      local_coords.resize(3 * data_ptr->nbEvalPoints);
+      shape.resize(4 * data_ptr->nbEvalPoints);
+
+      std::array<double, 12> coords;
+      CHKERR m_field.get_moab().get_coords(conn, num_nodes, coords.data());
+
       CHKERR Tools::getLocalCoordinatesOnReferenceFourNodeTet(
-          coords.data(), &data_ptr->evalPoints[3 * n], 1, local_coords.data());
+          coords.data(), &data_ptr->evalPoints[0], nb_eval_points,
+          &local_coords[0]);
+      CHKERR Tools::shapeFunMBTET<3>(&shape[0], &local_coords[0],
+                                     &local_coords[1], &local_coords[2],
+                                     nb_eval_points);
 
-      std::array<double, 4> shape;
-      CHKERR Tools::shapeFunMBTET<3>(shape.data(), &local_coords[0],
-                                     &local_coords[1], &local_coords[2], 1);
+      FTensor::Index<'i', 4> i4;
+      FTensor::Tensor1<FTensor::PackPtr<double *, 4>, 4> t_shape{
+          &shape[0], &shape[1], &shape[2], &shape[3]};
+      FTensor::Tensor1<FTensor::PackPtr<double *, 4>, 4> t_shape_data{
+          &data_ptr->shapeFunctions(0, 0), &data_ptr->shapeFunctions(0, 1),
+          &data_ptr->shapeFunctions(0, 2), &data_ptr->shapeFunctions(0, 3)};
 
-      const double eps = data_ptr->eps;
-      if (shape[0] >= 0 - eps && shape[0] <= 1 + eps &&
+      FTensor::Index<'j', 3> j3;
+      FTensor::Tensor1<FTensor::PackPtr<double *, 3>, 3> t_local{
+          &local_coords[0], &local_coords[1], &local_coords[2]};
+      FTensor::Tensor1<FTensor::PackPtr<double *, 3>, 3> t_local_data{
+          &(data_ptr->localCoords(0, 0)), &(data_ptr->localCoords(0, 1)),
+          &(data_ptr->localCoords(0, 2))};
 
-          shape[1] >= 0 - eps && shape[1] <= 1 + eps &&
 
-          shape[2] >= 0 - eps && shape[2] <= 1 + eps &&
+      for (int n = 0; n != data_ptr->nbEvalPoints; ++n) {
 
-          shape[3] >= 0 - eps && shape[3] <= 1 + eps) {
+        const double eps = data_ptr->eps;
+        if (t_shape(0) >= 0 - eps && t_shape(0) <= 1 + eps &&
 
-        std::copy(shape.begin(), shape.end(), &data_ptr->shapeFunctions(n, 0));
-        std::copy(local_coords.begin(), local_coords.end(),
-                  &data_ptr->localCoords(n, 0));
-        data_ptr->evalPointEntityHandle[n] = tet;
+            t_shape(1) >= 0 - eps && t_shape(1) <= 1 + eps &&
+
+            t_shape(2) >= 0 - eps && t_shape(2) <= 1 + eps &&
+
+            t_shape(3) >= 0 - eps && t_shape(3) <= 1 + eps) {
+
+          data_ptr->evalPointEntityHandle[n] = tet;
+          t_shape_data(i4) = t_shape(i4);
+          t_local_data(j3) = t_local(j3);
+        }
+
+        ++t_shape;
+        ++t_shape_data;
+        ++t_local;
+        ++t_local_data; 
+
       }
+
     }
+
+    if constexpr (D == 2) {
+
+      std::array<double, 9> coords;
+      CHKERR m_field.get_moab().get_coords(conn, num_nodes, coords.data());
+
+      for (int n = 0; n != data_ptr->nbEvalPoints; ++n) {
+
+
+      }
+
+    }
+
+
   }
 
   const Problem *prb_ptr;
@@ -198,7 +281,7 @@ MoFEMErrorCode FieldEvaluatorInterface::evalFEAtThePoint3D(
   Range in_tets;
   in_tets.insert_list(data_ptr->evalPointEntityHandle.begin(),
                       data_ptr->evalPointEntityHandle.end());
-  in_tets = in_tets.subset_by_dimension(3);
+  in_tets = in_tets.subset_by_dimension(D);
 
   if (verb >= NOISY)
     MOFEM_LOG("SELF", Sev::noisy) << "in tets: " << in_tets << endl;
@@ -247,6 +330,24 @@ MoFEMErrorCode FieldEvaluatorInterface::evalFEAtThePoint3D(
             "Pointer to element does not exists");
 
   MoFEMFunctionReturn(0);
+}
+
+MoFEMErrorCode FieldEvaluatorInterface::evalFEAtThePoint3D(
+    const double *const point, const double distance, const std::string problem,
+    const std::string finite_element, boost::shared_ptr<SetPtsData> data_ptr,
+    int lower_rank, int upper_rank, boost::shared_ptr<CacheTuple> cache_ptr,
+    MoFEMTypes bh, VERBOSITY_LEVELS verb) {
+  return evalFEAtThePoint<3>(point, distance, problem, finite_element, data_ptr,
+                             lower_rank, upper_rank, cache_ptr, bh, verb);
+}
+
+MoFEMErrorCode FieldEvaluatorInterface::evalFEAtThePoint2D(
+    const double *const point, const double distance, const std::string problem,
+    const std::string finite_element, boost::shared_ptr<SetPtsData> data_ptr,
+    int lower_rank, int upper_rank, boost::shared_ptr<CacheTuple> cache_ptr,
+    MoFEMTypes bh, VERBOSITY_LEVELS verb) {
+  return evalFEAtThePoint<2>(point, distance, problem, finite_element, data_ptr,
+                             lower_rank, upper_rank, cache_ptr, bh, verb);
 }
 
 } // namespace MoFEM
