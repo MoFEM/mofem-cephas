86087351db6d4a2cee5b0c4faa58bcc39a1a31bb
Lukasz Kaczmarczyk <lukasz.kaczmarczyk@glasgow.ac.uk>
2022-04-06 13:45:58 +0100

    more fixes for hetergenous order

--- mofem/src/approximation/impl/EdgeQuadHexPolynomials.cpp
+++ mofem/src/approximation/impl/EdgeQuadHexPolynomials.cpp
@@ -276,7 +276,7 @@ MoFEMErrorCode MoFEM::DemkowiczHexAndQuad::H1_FaceShapeFunctions_ONQUAD(
     int *faces_nodes, int *p, double *N, double *diffN, double *faceN,
     double *diff_faceN, int nb_integration_pts) {
 
-  if (p[0] > 1) {
+  if (p[0] > 1 && p[1] > 1) {
     const int nb_dofs = (p[0] - 1) * (p[1] - 1);
 
     const int n0 = faces_nodes[0];
