72be60b963f9183cea1d59cb5773eacf7e412f08
Lukasz Kaczmarczyk <lukasz.kaczmarczyk@glasgow.ac.uk>
2022-05-24 22:50:17 +0100

    add ptr_dqueue

--- mofem/include/Includes.hpp
+++ mofem/include/Includes.hpp
@@ -115,6 +115,7 @@
 
 #include <boost/ptr_container/ptr_vector.hpp>
 #include <boost/ptr_container/ptr_map.hpp>
+#include <boost/ptr_container/ptr_deque.hpp>
 #include <boost/scoped_ptr.hpp>
 #include <boost/scoped_array.hpp>
 #include <boost/weak_ptr.hpp>
