78248e4a2085c76681d88e1bb20d63c6ed5f2f45
Lukasz Kaczmarczyk <lukasz.kaczmarczyk@glasgowa.ac.uk>
2022-06-30 08:38:04 +0100

    fix bug in comments

--- BuildDockerStack.sh
+++ BuildDockerStack.sh
@@ -8,19 +8,19 @@ export VERSION=0.13.1
 #docker build -t likask/mofem-spack-env -f Dockerfile-spack-env empty/
 #docker tag likask/mofem-spack-env:latest likask/mofem-spack-env:$VERSION
 
-Build core and users modules
+#Build core and users modules
 docker build -t likask/mofem-spack-mofem -f Dockerfile-spack-mofem . 
 docker tag likask/mofem-spack-mofem:latest likask/mofem-spack-mofem:$VERSION
 docker tag likask/mofem-spack-mofem:latest likask/mofem-intermidiate:latest
 
-Install softmech module
+#Install softmech module
 docker build -t likask/mofem-spack-softmech -f Dockerfile-spack-softmech empty/ 
 docker tag likask/mofem-spack-softmech:latest likask/mofem-spack-softmech:$VERSION 
 docker tag likask/mofem-spack-softmech:latest likask/mofem-intermidiate:latest
 
 # Install other modules ...
 
-Jupyter
+#Jupyter
 docker build -t likask/mofem-spack-jupyter -f Dockerfile-spack-jupyter empty
 docker tag likask/mofem-spack-jupyter:latest likask/mofem-spack-jupyter:$VERSION 
 docker tag likask/mofem-spack-jupyter:latest likask/mofem-intermidiate:latest
