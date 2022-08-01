1c2ac83e2c65b7aaf22d85b7fa52ae3daccae1ed
Karol Lewandowski <k.lewandowski.1@research.gla.ac.uk>
2022-08-01 18:46:33 +0100

    first version of cuda MPI functions

--- mofem/src/interfaces/impl/MatrixManager.cpp
+++ mofem/src/interfaces/impl/MatrixManager.cpp
@@ -559,6 +559,10 @@ MatrixManager::MatrixManager(const MoFEM::Core &core)
                         &MOFEM_EVENT_createMPIAIJWithArrays);
   PetscLogEventRegister("MatrixManagerCreateMPIAdjWithArrays", 0,
                         &MOFEM_EVENT_createMPIAdjWithArrays);
+  PetscLogEventRegister("MatrixManagerCreateMPIAIJCUSPARSEWithArrays", 0,
+                        &MOFEM_EVENT_createMPIAIJCUSPARSEWithArrays);
+  PetscLogEventRegister("MatrixManagerCreateSeqAIJCUSPARSEWithArrays", 0,
+                        &MOFEM_EVENT_createSeqAIJCUSPARSEWithArrays);
   PetscLogEventRegister("MatrixManagerCreateSeqAIJWithArrays", 0,
                         &MOFEM_EVENT_createSeqAIJWithArrays);
   PetscLogEventRegister("MatrixManagerCheckMPIAIJWithArraysMatrixFillIn", 0,
@@ -598,6 +602,117 @@ MoFEMErrorCode MatrixManager::createMPIAIJWithArrays<PetscGlobalIdx_mi_tag>(
       nb_col_dofs, &*i_vec.begin(), &*j_vec.begin(), PETSC_NULL, Aij);
 
   PetscLogEventEnd(MOFEM_EVENT_createMPIAIJWithArrays, 0, 0, 0, 0);
+  MoFEMFunctionReturn(0);
+}
+
+template <>
+MoFEMErrorCode
+MatrixManager::createMPIAIJCUSPARSEWithArrays<PetscGlobalIdx_mi_tag>(
+    const std::string name, Mat *Aij, int verb) {
+  MoFEMFunctionBegin;
+
+  MoFEM::CoreInterface &m_field = cOre;
+  CreateRowComressedADJMatrix *core_ptr =
+      static_cast<CreateRowComressedADJMatrix *>(&cOre);
+  PetscLogEventBegin(MOFEM_EVENT_createMPIAIJCUSPARSEWithArrays, 0, 0, 0, 0);
+
+  auto problems_ptr = m_field.get_problems();
+  auto &prb = problems_ptr->get<Problem_mi_tag>();
+  auto p_miit = prb.find(name);
+  if (p_miit == prb.end()) {
+    SETERRQ1(m_field.get_comm(), MOFEM_NOT_FOUND,
+             "problem < %s > is not found (top tip: check spelling)",
+             name.c_str());
+  }
+
+  std::vector<int> i_vec, j_vec;
+  j_vec.reserve(10000);
+  CHKERR core_ptr->createMatArrays<PetscGlobalIdx_mi_tag>(
+      p_miit, MATAIJCUSPARSE, i_vec, j_vec, false, verb);
+
+  int nb_row_dofs = p_miit->getNbDofsRow();
+  int nb_col_dofs = p_miit->getNbDofsCol();
+  int nb_local_dofs_row = p_miit->getNbLocalDofsRow();
+  int nb_local_dofs_col = p_miit->getNbLocalDofsCol();
+
+  auto get_layout = [&]() {
+    int start_ranges, end_ranges;
+    PetscLayout layout;
+    CHKERR PetscLayoutCreate(m_field.get_comm(), &layout);
+    CHKERR PetscLayoutSetBlockSize(layout, 1);
+    CHKERR PetscLayoutSetLocalSize(layout, nb_local_dofs_col);
+    CHKERR PetscLayoutSetUp(layout);
+    CHKERR PetscLayoutGetRange(layout, &start_ranges, &end_ranges);
+    CHKERR PetscLayoutDestroy(&layout);
+    return std::make_pair(start_ranges, end_ranges);
+  };
+
+  auto get_nnz = [&](auto &d_nnz, auto &o_nnz) {
+    MoFEMFunctionBeginHot;
+    auto layout = get_layout();
+    int j = 0;
+    for (int i = 0; i != nb_local_dofs_row; ++i) {
+      for (; j != i_vec[i + 1]; ++j) {
+        if (j_vec[j] < layout.second && j_vec[j] >= layout.first)
+          ++(d_nnz[i]);
+        else
+          ++(o_nnz[i]);
+      }
+    }
+    MoFEMFunctionReturnHot(0);
+  };
+
+  std::vector<int> d_nnz(nb_local_dofs_row, 0), o_nnz(nb_local_dofs_row, 0);
+  CHKERR get_nnz(d_nnz, o_nnz);
+
+  // CHKERR MatCreate(m_field.get_comm(), Aij);
+  // CHKERR MatSetSizes(*Aij, nb_local_dofs_row, nb_local_dofs_col, nb_row_dofs,
+  //                    nb_col_dofs);
+  // CHKERR MatSetType(*Aij, MATMPIAIJ);
+  // CHKERR MatMPIAIJSetPreallocation(*Aij, 0, &*d_nnz.begin(), 0,
+  //                                  &*o_nnz.begin());
+
+  // PetscLogEventEnd(MOFEM_EVENT_createMPIAIJ, 0, 0, 0, 0);
+
+  // CHKERR ::MatCreateMPIAIJWithArrays(
+  //     m_field.get_comm(), nb_local_dofs_row, nb_local_dofs_col, nb_row_dofs,
+  //     nb_col_dofs, &*i_vec.begin(), &*j_vec.begin(), PETSC_NULL, Aij);
+
+  // CHKERR ::MatCreateMPIAIJWithArrays(
+  //     MPI_Comm comm, PetscInt m, PetscInt n, PetscInt M, PetscInt N,
+  //     const PetscInt i[], const PetscInt j[], const PetscScalar a[], Mat
+  //     *mat);
+
+  // CHKERR ::MatCreateMPIAIJCUSPARSE(MPI_Comm comm, PetscInt m, PetscInt n,
+  //                               PetscInt M, PetscInt N, PetscInt d_nz,
+  //                               const PetscInt d_nnz[], PetscInt o_nz,
+  //                               const PetscInt o_nnz[], Mat *A);
+
+#ifdef PETSC_HAVE_CUDA
+
+  CHKERR ::MatCreateAIJCUSPARSE(m_field.get_comm(), nb_local_dofs_row,
+                                nb_local_dofs_col, nb_row_dofs, nb_col_dofs,
+                                PETSC_DEFAULT, &*d_nnz.begin(), PETSC_DEFAULT,
+                                &*o_nnz.begin(), Aij);
+#endif
+
+  PetscLogEventEnd(MOFEM_EVENT_createMPIAIJCUSPARSEWithArrays, 0, 0, 0, 0);
+
+  MoFEMFunctionReturn(0);
+}
+
+template <>
+MoFEMErrorCode
+MatrixManager::createSeqAIJCUSPARSEWithArrays<PetscLocalIdx_mi_tag>(
+    const std::string name, Mat *Aij, int verb) {
+  MoFEMFunctionBegin;
+
+#ifdef PETSC_HAVE_CUDA
+  // CHKERR ::MatCreateSeqAIJCUSPARSE(MPI_Comm comm, PetscInt m, PetscInt n,
+  //                                        PetscInt nz, const PetscInt nnz[],
+  //                                        Mat *A)
+#endif
+
   MoFEMFunctionReturn(0);
 }
 
