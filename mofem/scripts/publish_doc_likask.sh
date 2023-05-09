da8b803be1489d09a34d9d5def2ef77f9ce12ebe
Karol Lewandowski <k.lewandowski.1@research.gla.ac.uk>
2023-05-09 19:44:31 +0200

    remove files

--- mofem/scripts/publish_doc_likask.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-RSYNC=/usr/bin/rsync
-PROJECT_SOURCE_DIR=/Users/karollewandowski/moFEM/mofem-cephas/mofem
-BINDIR=/Users/karollewandowski/moFEM/lib
-
-$RSYNC -avz --delete html "campus\lk58p"@userweb.eng.gla.ac.uk:/home/CAMPUS/lk58p/html/lukasz.kaczmarczyk/MoFem
