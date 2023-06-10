a55f5d99a962b599fa1cd1b98b3695d5754a5018
Lukasz Kaczmarczyk <lukasz.kaczmarczyk@glasgowa.ac.uk>
2023-06-10 21:52:50 +0100

    refactor code

--- mofem/atom_tests/tensor_divergence_operator_2d.cpp
+++ mofem/atom_tests/tensor_divergence_operator_2d.cpp
@@ -124,154 +124,169 @@ int main(int argc, char *argv[]) {
     // CHKERR bc_mng->removeBlockDOFsOnEntities(simple->getProblemName(), "SYM",
     //                                          "SIGMA", 0, 0, true);
 
-
     // get operators tester
     auto opt = m_field.getInterface<OperatorsTester>(); // get interface to
                                                         // OperatorsTester
-    auto pip = m_field.getInterface<PipelineManager>(); // get interface to
-                                                        // pipeline manager
+    auto pip_mng = m_field.getInterface<PipelineManager>(); // get interface to
+                                                            // pipeline manager
 
-    pip->getOpDomainLhsPipeline().clear();
-    pip->getOpDomainRhsPipeline().clear();
+    pip_mng->getOpDomainLhsPipeline().clear();
+    pip_mng->getOpDomainRhsPipeline().clear();
 
-    pip->setDomainLhsIntegrationRule([](int, int, int o) { return 2 * o; });
-    pip->setDomainLhsIntegrationRule([](int, int, int o) { return 2 * o; });
+    pip_mng->setDomainLhsIntegrationRule([](int, int, int o) { return 2 * o; });
+    pip_mng->setDomainLhsIntegrationRule([](int, int, int o) { return 2 * o; });
+    pip_mng->setBoundaryLhsIntegrationRule(
+        [](int, int, int o) { return 2 * o; });
 
     auto x = opt->setRandomFields(simple->getDM(),
                                   {{"U", {-1, 1}}, {"SIGMA", {-1, 1}}});
     CHKERR DMoFEMMeshToLocalVector(simple->getDM(), x, ADD_VALUES,
                                    SCATTER_REVERSE);
 
-    using OpMixDivURhs = FormsIntegrators<DomainEleOp>::Assembly<A>::LinearForm<
-        GAUSS>::OpMixDivTimesU<3, SPACE_DIM, SPACE_DIM>;
-    using OpMixLambdaGradURhs = FormsIntegrators<DomainEleOp>::Assembly<
-        PETSC>::LinearForm<I>::OpMixTensorTimesGradU<SPACE_DIM>;
-    using OpMixLambdaGradURhs = FormsIntegrators<DomainEleOp>::Assembly<
-        PETSC>::LinearForm<I>::OpMixTensorTimesGradU<SPACE_DIM>;
-    using OpMixUTimesDivLambdaRhs = FormsIntegrators<DomainEleOp>::Assembly<
-        PETSC>::LinearForm<I>::OpMixVecTimesDivLambda<SPACE_DIM>;
-
-    using OpMixUTimesLambdaRhs = FormsIntegrators<DomainEleOp>::Assembly<
-        PETSC>::LinearForm<I>::OpGradTimesTensor<1, SPACE_DIM, SPACE_DIM>;
-
-    using OpMixNormalLambdaURhs = FormsIntegrators<BoundaryEleOp>::Assembly<
-        PETSC>::LinearForm<I>::OpNormalMixVecTimesVectorField<SPACE_DIM>;
-    using OpUTimeTractionRhs = FormsIntegrators<BoundaryEleOp>::Assembly<
-        PETSC>::LinearForm<I>::OpBaseTimesVector<1, SPACE_DIM, 1>;
-
-    auto pip_mng = m_field.getInterface<PipelineManager>();
-    // integration rule
-    auto rule = [&](int, int, int p) { return 2 * p + 1; };
-    CHKERR pip_mng->setDomainRhsIntegrationRule(rule);
-    CHKERR pip_mng->setBoundaryRhsIntegrationRule(rule);
-
-    auto beta_domain = [](double x, double y, double z) { return 1.; };
-    auto beta_bdy = [](double x, double y, double z) { return -1; };
-
-    auto ops_rhs_interior = [&](auto &pip) {
+    auto test_consistency_of_domain_and_bdy_integrals = [&]() {
       MoFEMFunctionBegin;
-      CHKERR AddHOOps<SPACE_DIM, SPACE_DIM, SPACE_DIM>::add(pip, {H1, HDIV},
-                                                            "GEOMETRY");
-      auto u_ptr = boost::make_shared<MatrixDouble>();
-      auto grad_u_ptr = boost::make_shared<MatrixDouble>();
-      pip.push_back(new OpCalculateVectorFieldValues<SPACE_DIM>("U", u_ptr));
-      pip.push_back(new OpCalculateVectorFieldGradient<SPACE_DIM, SPACE_DIM>(
-          "U", grad_u_ptr));
+      using OpMixDivURhs = FormsIntegrators<DomainEleOp>::Assembly<
+          A>::LinearForm<GAUSS>::OpMixDivTimesU<3, SPACE_DIM, SPACE_DIM>;
+      using OpMixLambdaGradURhs = FormsIntegrators<DomainEleOp>::Assembly<
+          PETSC>::LinearForm<I>::OpMixTensorTimesGradU<SPACE_DIM>;
+      using OpMixLambdaGradURhs = FormsIntegrators<DomainEleOp>::Assembly<
+          PETSC>::LinearForm<I>::OpMixTensorTimesGradU<SPACE_DIM>;
+      using OpMixUTimesDivLambdaRhs = FormsIntegrators<DomainEleOp>::Assembly<
+          PETSC>::LinearForm<I>::OpMixVecTimesDivLambda<SPACE_DIM>;
 
-      pip.push_back(new OpMixDivURhs("SIGMA", u_ptr, beta_domain));
-      pip.push_back(new OpMixLambdaGradURhs("SIGMA", grad_u_ptr, beta_domain));
+      using OpMixUTimesLambdaRhs = FormsIntegrators<DomainEleOp>::Assembly<
+          PETSC>::LinearForm<I>::OpGradTimesTensor<1, SPACE_DIM, SPACE_DIM>;
 
-      auto sigma_ptr = boost::make_shared<MatrixDouble>();
-      auto sigma_div_ptr = boost::make_shared<MatrixDouble>();
-      pip.push_back(new OpCalculateHVecTensorDivergence<SPACE_DIM, SPACE_DIM>(
-          "SIGMA", sigma_div_ptr));
-      pip.push_back(new OpCalculateHVecTensorField<SPACE_DIM, SPACE_DIM>(
-          "SIGMA", sigma_ptr));
-      pip.push_back(new OpMixUTimesDivLambdaRhs("U", sigma_div_ptr));
-      pip.push_back(new OpMixUTimesLambdaRhs("U", sigma_ptr));
+      using OpMixNormalLambdaURhs = FormsIntegrators<BoundaryEleOp>::Assembly<
+          PETSC>::LinearForm<I>::OpNormalMixVecTimesVectorField<SPACE_DIM>;
+      using OpUTimeTractionRhs = FormsIntegrators<BoundaryEleOp>::Assembly<
+          PETSC>::LinearForm<I>::OpBaseTimesVector<1, SPACE_DIM, 1>;
+
+      // integration rule
+      auto rule = [&](int, int, int p) { return 2 * p + 1; };
+      CHKERR pip_mng->setDomainRhsIntegrationRule(rule);
+      CHKERR pip_mng->setBoundaryRhsIntegrationRule(rule);
+
+      auto beta_domain = [](double x, double y, double z) { return 1.; };
+      auto beta_bdy = [](double x, double y, double z) { return -1; };
+
+      auto ops_rhs_interior = [&](auto &pip) {
+        MoFEMFunctionBegin;
+        CHKERR AddHOOps<SPACE_DIM, SPACE_DIM, SPACE_DIM>::add(pip, {H1, HDIV},
+                                                              "GEOMETRY");
+        auto u_ptr = boost::make_shared<MatrixDouble>();
+        auto grad_u_ptr = boost::make_shared<MatrixDouble>();
+        pip.push_back(new OpCalculateVectorFieldValues<SPACE_DIM>("U", u_ptr));
+        pip.push_back(new OpCalculateVectorFieldGradient<SPACE_DIM, SPACE_DIM>(
+            "U", grad_u_ptr));
+
+        pip.push_back(new OpMixDivURhs("SIGMA", u_ptr, beta_domain));
+        pip.push_back(
+            new OpMixLambdaGradURhs("SIGMA", grad_u_ptr, beta_domain));
+
+        auto sigma_ptr = boost::make_shared<MatrixDouble>();
+        auto sigma_div_ptr = boost::make_shared<MatrixDouble>();
+        pip.push_back(new OpCalculateHVecTensorDivergence<SPACE_DIM, SPACE_DIM>(
+            "SIGMA", sigma_div_ptr));
+        pip.push_back(new OpCalculateHVecTensorField<SPACE_DIM, SPACE_DIM>(
+            "SIGMA", sigma_ptr));
+        pip.push_back(new OpMixUTimesDivLambdaRhs("U", sigma_div_ptr));
+        pip.push_back(new OpMixUTimesLambdaRhs("U", sigma_ptr));
+
+        MoFEMFunctionReturn(0);
+      };
+
+      auto ops_rhs_boundary = [&](auto &pip) {
+        MoFEMFunctionBegin;
+        CHKERR AddHOOps<SPACE_DIM - 1, SPACE_DIM, SPACE_DIM>::add(pip, {HDIV},
+                                                                  "GEOMETRY");
+        auto u_ptr = boost::make_shared<MatrixDouble>();
+        pip.push_back(new OpCalculateVectorFieldValues<SPACE_DIM>("U", u_ptr));
+        auto traction_ptr = boost::make_shared<MatrixDouble>();
+        pip.push_back(new OpCalculateHVecTensorTrace<SPACE_DIM, BoundaryEleOp>(
+            "SIGMA", traction_ptr));
+
+        // We have to integrate on curved face geometry, thus integration weight
+        // have to adjusted.
+        pip.push_back(new OpSetHOWeightsOnSubDim<SPACE_DIM>());
+        pip.push_back(new OpMixNormalLambdaURhs("SIGMA", u_ptr, beta_bdy));
+        pip.push_back(new OpUTimeTractionRhs("U", traction_ptr, beta_bdy));
+
+        MoFEMFunctionReturn(0);
+      };
+
+      CHKERR ops_rhs_interior(pip_mng->getOpDomainRhsPipeline());
+      CHKERR ops_rhs_boundary(pip_mng->getOpBoundaryRhsPipeline());
+
+      auto f = createDMVector(simple->getDM());
+      pip_mng->getDomainRhsFE()->f = f;
+      pip_mng->getBoundaryRhsFE()->f = f;
+
+      CHKERR VecZeroEntries(f);
+
+      CHKERR DMoFEMLoopFiniteElements(simple->getDM(),
+                                      simple->getDomainFEName(),
+                                      pip_mng->getDomainRhsFE());
+      CHKERR DMoFEMLoopFiniteElements(simple->getDM(),
+                                      simple->getBoundaryFEName(),
+                                      pip_mng->getBoundaryRhsFE());
+
+      CHKERR VecAssemblyBegin(f);
+      CHKERR VecAssemblyEnd(f);
+
+      double f_nrm2;
+      CHKERR VecNorm(f, NORM_2, &f_nrm2);
+
+      MOFEM_LOG("ATOM", Sev::inform) << "f_norm2 = " << f_nrm2;
+      if (std::fabs(f_nrm2) > 1e-10)
+        SETERRQ(PETSC_COMM_WORLD, MOFEM_ATOM_TEST_INVALID, "Test failed");
 
       MoFEMFunctionReturn(0);
     };
 
-    auto ops_rhs_boundary = [&](auto &pip) {
-      MoFEMFunctionBegin;
-      CHKERR AddHOOps<SPACE_DIM - 1, SPACE_DIM, SPACE_DIM>::add(pip, {HDIV},
-                                                                "GEOMETRY");
-      auto u_ptr = boost::make_shared<MatrixDouble>();
-      pip.push_back(new OpCalculateVectorFieldValues<SPACE_DIM>("U", u_ptr));
-      auto traction_ptr = boost::make_shared<MatrixDouble>();
-      pip.push_back(new OpCalculateHVecTensorTrace<SPACE_DIM, BoundaryEleOp>(
-          "SIGMA", traction_ptr));
-
-      // We have to integrate on curved face geometry, thus integration weight
-      // have to adjusted.
-      pip.push_back(new OpSetHOWeightsOnSubDim<SPACE_DIM>());
-      pip.push_back(new OpMixNormalLambdaURhs("SIGMA", u_ptr, beta_bdy));
-      pip.push_back(new OpUTimeTractionRhs("U", traction_ptr, beta_bdy));
-
-      MoFEMFunctionReturn(0);
-    };
-
-    CHKERR ops_rhs_interior(pip_mng->getOpDomainRhsPipeline());
-    CHKERR ops_rhs_boundary(pip_mng->getOpBoundaryRhsPipeline());
-
-    auto f = createDMVector(simple->getDM());
-    pip_mng->getDomainRhsFE()->f = f;
-    pip_mng->getBoundaryRhsFE()->f = f;
-
-    CHKERR VecZeroEntries(f);
-
-    CHKERR DMoFEMLoopFiniteElements(simple->getDM(), simple->getDomainFEName(),
-                                    pip_mng->getDomainRhsFE());
-    CHKERR DMoFEMLoopFiniteElements(simple->getDM(),
-                                    simple->getBoundaryFEName(),
-                                    pip_mng->getBoundaryRhsFE());
-
-    CHKERR VecAssemblyBegin(f);
-    CHKERR VecAssemblyEnd(f);
-
-    double f_nrm2;
-    CHKERR VecNorm(f, NORM_2, &f_nrm2);
-
-    MOFEM_LOG("ATOM", Sev::inform) << "f_norm2 = " << f_nrm2;
-    if (std::fabs(f_nrm2) > 1e-10)
-      SETERRQ(PETSC_COMM_WORLD, MOFEM_ATOM_TEST_INVALID, "Test failed");
+    CHKERR test_consistency_of_domain_and_bdy_integrals();
 
     // Testing Lhs domain operators
 
-    using OpMixDivULhs = FormsIntegrators<DomainEleOp>::Assembly<
-        PETSC>::BiLinearForm<I>::OpMixDivTimesVec<SPACE_DIM>;
-    using OpLambdaGraULhs = FormsIntegrators<DomainEleOp>::Assembly<
-        PETSC>::BiLinearForm<I>::OpMixTensorTimesGrad<SPACE_DIM>;
-
-    auto op_lhs_domain = [&](auto &pip) {
+    auto test_lhs_ops = [&]() {
       MoFEMFunctionBegin;
-      CHKERR AddHOOps<SPACE_DIM, SPACE_DIM, SPACE_DIM>::add(pip, {H1, HDIV},
-                                                            "GEOMETRY");
+      using OpMixDivULhs = FormsIntegrators<DomainEleOp>::Assembly<
+          PETSC>::BiLinearForm<I>::OpMixDivTimesVec<SPACE_DIM>;
+      using OpLambdaGraULhs = FormsIntegrators<DomainEleOp>::Assembly<
+          PETSC>::BiLinearForm<I>::OpMixTensorTimesGrad<SPACE_DIM>;
 
-      auto unity = []() { return 1; };
-      pip.push_back(new OpMixDivULhs("SIGMA", "U", unity, true));
-      pip.push_back(new OpLambdaGraULhs("SIGMA", "U", unity, true));
+      auto op_lhs_domain = [&](auto &pip) {
+        MoFEMFunctionBegin;
+        CHKERR AddHOOps<SPACE_DIM, SPACE_DIM, SPACE_DIM>::add(pip, {H1, HDIV},
+                                                              "GEOMETRY");
+
+        auto unity = []() { return 1; };
+        pip.push_back(new OpMixDivULhs("SIGMA", "U", unity, true));
+        pip.push_back(new OpLambdaGraULhs("SIGMA", "U", unity, true));
+        MoFEMFunctionReturn(0);
+      };
+
+      CHKERR op_lhs_domain(pip_mng->getOpDomainLhsPipeline());
+
+      auto diff_x = opt->setRandomFields(simple->getDM(),
+                                         {{"U", {-1, 1}}, {"SIGMA", {-1, 1}}});
+      constexpr double eps = 1e-6;
+      auto diff_res = opt->checkCentralFiniteDifference(
+          simple->getDM(), simple->getDomainFEName(), pip_mng->getDomainRhsFE(),
+          pip_mng->getDomainLhsFE(), x, SmartPetscObj<Vec>(),
+          SmartPetscObj<Vec>(), diff_x, 0, 1, eps);
+
+      // Calculate norm of difference between directive calculated from finite
+      // difference, and tangent matrix.
+      double fnorm_res;
+      CHKERR VecNorm(diff_res, NORM_2, &fnorm_res);
+      MOFEM_LOG_C("ATOM", Sev::inform, "Test Lhs OPs %3.4e", fnorm_res);
       MoFEMFunctionReturn(0);
     };
 
-    CHKERR op_lhs_domain(pip_mng->getOpDomainLhsPipeline());
+    CHKERR test_lhs_ops();
+}
+CATCH_ERRORS;
 
-    auto diff_x = opt->setRandomFields(simple->getDM(),
-                                       {{"U", {-1, 1}}, {"SIGMA", {-1, 1}}});
-    constexpr double eps = 1e-6;
-    auto diff_res = opt->checkCentralFiniteDifference(
-        simple->getDM(), simple->getDomainFEName(), pip->getDomainRhsFE(),
-        pip->getDomainLhsFE(), x, SmartPetscObj<Vec>(), SmartPetscObj<Vec>(),
-        diff_x, 0, 1, eps);
-
-    // Calculate norm of difference between directive calculated from finite
-    // difference, and tangent matrix.
-    double fnorm_res;
-    CHKERR VecNorm(diff_res, NORM_2, &fnorm_res);
-    MOFEM_LOG_C("ATOM", Sev::inform, "Test Lhs OPs %3.4e", fnorm_res);
-  }
-  CATCH_ERRORS;
-
-  CHKERR MoFEM::Core::Finalize();
+CHKERR MoFEM::Core::Finalize();
 }
