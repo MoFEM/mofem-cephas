80fddb7b2641607d750be316932e725dc30261f7
Lukasz Kaczmarczyk <63849e6f489de2f7f464f50d@bots.bitbucket.org>
2023-06-13 13:40:03 +0000

    update test with hetogenous h-div base

--- mofem/atom_tests/tensor_divergence_operator.cpp
+++ mofem/atom_tests/tensor_divergence_operator.cpp
@@ -121,8 +121,31 @@ int main(int argc, char *argv[]) {
 
     // set fields order, i.e. for most first cases order is sufficient.
     CHKERR simple->setFieldOrder("U", order);
-    CHKERR simple->setFieldOrder("SIGMA", order);
+    // CHKERR simple->setFieldOrder("SIGMA", order);
     CHKERR simple->setFieldOrder("GEOMETRY", 2);
+
+    auto get_skin = [&]() {
+      Range body_ents;
+      CHKERR m_field.get_moab().get_entities_by_dimension(0, SPACE_DIM,
+                                                         body_ents);
+      Skinner skin(&m_field.get_moab());
+      Range skin_ents;
+      CHKERR skin.find_skin(0, body_ents, false, skin_ents);
+      return skin_ents;
+    };
+
+    auto filter_true_skin = [&](auto skin) {
+      Range boundary_ents;
+      ParallelComm *pcomm =
+          ParallelComm::get_pcomm(&m_field.get_moab(), MYPCOMM_INDEX);
+      CHKERR pcomm->filter_pstatus(skin, PSTATUS_SHARED | PSTATUS_MULTISHARED,
+                                   PSTATUS_NOT, -1, &boundary_ents);
+      return boundary_ents;
+    };
+
+    auto boundary_ents = filter_true_skin(get_skin());
+    CHKERR simple->setFieldOrder("SIGMA", 0);
+    CHKERR simple->setFieldOrder("SIGMA", order - 1, &boundary_ents);
 
     // setup problem
     CHKERR simple->setUp();
