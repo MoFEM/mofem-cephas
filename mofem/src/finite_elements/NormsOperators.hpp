3e7fdaf59e63df810999bd86fb51ac25163bfb41
AdrianaKul <kulikova.adriana@gmail.com>
2023-09-03 14:27:28 +0000

    OpCalcDifferenceTensor 0 1 2 to calculate difference between VectorDouble or MatrixDouble

--- mofem/src/finite_elements/NormsOperators.hpp
+++ mofem/src/finite_elements/NormsOperators.hpp
@@ -3,8 +3,8 @@
 
 */
 
-#ifndef __NORM_OPERATORS_HPP__
-#define __NORM_OPERATORS_HPP__
+#ifndef __NORM_OPERATORS_HPP___
+#define __NORM_OPERATORS_HPP___
 
 namespace MoFEM {
 
@@ -86,6 +86,92 @@ protected:
   const int iNdex;
 };
 
+/** \brief Get difference between input VectorDouble and VectorDouble
+ * intended for Tensor0 fields
+ *
+ */
+
+struct OpCalcDifferenceTensor0 : public ForcesAndSourcesCore::UserDataOperator {
+
+  OpCalcDifferenceTensor0(const std::string field_name,
+                          boost::shared_ptr<VectorDouble> data_1_ptr,
+                          boost::shared_ptr<VectorDouble> data_2_ptr,
+                          boost::shared_ptr<VectorDouble> result_ptr);
+
+  /**
+   * \brief calculate values of scalar field at integration points
+   * @param  side side entity number
+   * @param  type side entity type
+   * @param  data entity data
+   * @return      error code
+   */
+  MoFEMErrorCode doWork(int side, EntityType type,
+                        EntitiesFieldData::EntData &data);
+
+protected:
+  boost::shared_ptr<VectorDouble> data1Ptr;
+  boost::shared_ptr<VectorDouble> data2Ptr;
+  boost::shared_ptr<VectorDouble> resultPtr;
+};
+
+/** \brief Get difference between input MatrixDouble and MatrixDouble
+ * intended for Tensor1 fields
+ *
+ */
+
+template <int DIM>
+struct OpCalcDifferenceTensor1 : public ForcesAndSourcesCore::UserDataOperator {
+
+  OpCalcDifferenceTensor1(const std::string field_name,
+                          boost::shared_ptr<MatrixDouble> data_1_ptr,
+                          boost::shared_ptr<MatrixDouble> data_2_ptr,
+                          boost::shared_ptr<MatrixDouble> result_ptr);
+
+  /**
+   * \brief calculate values of scalar field at integration points
+   * @param  side side entity number
+   * @param  type side entity type
+   * @param  data entity data
+   * @return      error code
+   */
+  MoFEMErrorCode doWork(int side, EntityType type,
+                        EntitiesFieldData::EntData &data);
+
+protected:
+  boost::shared_ptr<MatrixDouble> data1Ptr;
+  boost::shared_ptr<MatrixDouble> data2Ptr;
+  boost::shared_ptr<MatrixDouble> resultPtr;
+};
+
+/** \brief Get difference between input MatrixDouble and MatrixDouble
+ * intended for Tensor1 fields
+ *
+ */
+
+template <int DIM_1, int DIM_2>
+struct OpCalcDifferenceTensor2 : public ForcesAndSourcesCore::UserDataOperator {
+
+  OpCalcDifferenceTensor2(const std::string field_name,
+                          boost::shared_ptr<MatrixDouble> data_1_ptr,
+                          boost::shared_ptr<MatrixDouble> data_2_ptr,
+                          boost::shared_ptr<MatrixDouble> result_ptr);
+
+  /**
+   * \brief calculate values of scalar field at integration points
+   * @param  side side entity number
+   * @param  type side entity type
+   * @param  data entity data
+   * @return      error code
+   */
+  MoFEMErrorCode doWork(int side, EntityType type,
+                        EntitiesFieldData::EntData &data);
+
+protected:
+  boost::shared_ptr<MatrixDouble> data1Ptr;
+  boost::shared_ptr<MatrixDouble> data2Ptr;
+  boost::shared_ptr<MatrixDouble> resultPtr;
+};
+
 } // namespace MoFEM
 
-#endif // __NORM_OPERATORS_HPP__
+#endif // __NORM_OPERATORS_HPP___
--- mofem/src/finite_elements/impl/NormsOperators.cpp
+++ mofem/src/finite_elements/impl/NormsOperators.cpp
@@ -9,6 +9,10 @@ template struct MoFEM::OpCalcNormL2Tensor1<2>;
 template struct MoFEM::OpCalcNormL2Tensor1<3>;
 template struct MoFEM::OpCalcNormL2Tensor2<2, 2>;
 template struct MoFEM::OpCalcNormL2Tensor2<3, 3>;
+template struct MoFEM::OpCalcDifferenceTensor1<2>;
+template struct MoFEM::OpCalcDifferenceTensor1<3>;
+template struct MoFEM::OpCalcDifferenceTensor2<2, 2>;
+template struct MoFEM::OpCalcDifferenceTensor2<3, 3>;
 
 namespace MoFEM {
 
@@ -142,6 +146,128 @@ OpCalcNormL2Tensor2<DIM_1, DIM_2>::doWork(int side, EntityType type,
   }
 
   CHKERR VecSetValue(dataVec, iNdex, norm_on_element, ADD_VALUES);
+
+  MoFEMFunctionReturn(0);
+}
+
+OpCalcDifferenceTensor0::OpCalcDifferenceTensor0(
+    const std::string field_name, boost::shared_ptr<VectorDouble> data_1_ptr,
+    boost::shared_ptr<VectorDouble> data_2_ptr,
+    boost::shared_ptr<VectorDouble> result_ptr)
+    : ForcesAndSourcesCore::UserDataOperator(
+          field_name, ForcesAndSourcesCore::UserDataOperator::OPROW),
+      data1Ptr(data_1_ptr), data2Ptr(data_2_ptr), resultPtr(result_ptr) {
+  if (!data1Ptr || !data2Ptr)
+    THROW_MESSAGE("Pointer is not set");
+}
+
+MoFEMErrorCode
+OpCalcDifferenceTensor0::doWork(int side, EntityType type,
+                                EntitiesFieldData::EntData &data) {
+  MoFEMFunctionBegin;
+
+  // get number of integration points
+  const auto nb_integration_points = getGaussPts().size2();
+  // resize resulting pointer
+  resultPtr->resize(nb_integration_points);
+  // get values
+  auto t_data_1 = getFTensor0FromVec(*data1Ptr);
+  auto t_data_2 = getFTensor0FromVec(*data2Ptr);
+  auto t_result = getFTensor0FromVec(*resultPtr);
+
+  // loop over integration points
+  for (int gg = 0; gg != nb_integration_points; gg++) {
+    // get difference
+    t_result = t_data_1 - t_data_2;
+    // move to another data values
+    ++t_data_1;
+    ++t_data_2;
+    ++t_result;
+  }
+
+  MoFEMFunctionReturn(0);
+}
+
+template <int DIM>
+OpCalcDifferenceTensor1<DIM>::OpCalcDifferenceTensor1(
+    const std::string field_name, boost::shared_ptr<MatrixDouble> data_1_ptr,
+    boost::shared_ptr<MatrixDouble> data_2_ptr,
+    boost::shared_ptr<MatrixDouble> result_ptr)
+    : ForcesAndSourcesCore::UserDataOperator(
+          field_name, ForcesAndSourcesCore::UserDataOperator::OPROW),
+      data1Ptr(data_1_ptr), data2Ptr(data_2_ptr), resultPtr(result_ptr) {
+  if (!data1Ptr || !data2Ptr)
+    THROW_MESSAGE("Pointer is not set");
+}
+
+template <int DIM>
+MoFEMErrorCode
+OpCalcDifferenceTensor1<DIM>::doWork(int side, EntityType type,
+                                     EntitiesFieldData::EntData &data) {
+  MoFEMFunctionBegin;
+
+  // Declare FTensor index
+  FTensor::Index<'i', DIM> i;
+  // get number of integration points
+  const auto nb_integration_points = getGaussPts().size2();
+  // resize resulting pointer
+  resultPtr->resize(DIM, nb_integration_points);
+  // get values
+  auto t_data_1 = getFTensor1FromMat<DIM>(*data1Ptr);
+  auto t_data_2 = getFTensor1FromMat<DIM>(*data2Ptr);
+  auto t_result = getFTensor1FromMat<DIM>(*resultPtr);
+
+  // loop over integration points
+  for (int gg = 0; gg != nb_integration_points; gg++) {
+    // get difference
+    t_result(i) = t_data_1(i) - t_data_2(i);
+    // move to another data values
+    ++t_data_1;
+    ++t_data_2;
+    ++t_result;
+  }
+
+  MoFEMFunctionReturn(0);
+}
+
+template <int DIM_1, int DIM_2>
+OpCalcDifferenceTensor2<DIM_1, DIM_2>::OpCalcDifferenceTensor2(
+    const std::string field_name, boost::shared_ptr<MatrixDouble> data_1_ptr,
+    boost::shared_ptr<MatrixDouble> data_2_ptr,
+    boost::shared_ptr<MatrixDouble> result_ptr)
+    : ForcesAndSourcesCore::UserDataOperator(
+          field_name, ForcesAndSourcesCore::UserDataOperator::OPROW),
+      data1Ptr(data_1_ptr), data2Ptr(data_2_ptr), resultPtr(result_ptr) {
+  if (!data1Ptr || !data2Ptr)
+    THROW_MESSAGE("Pointer is not set");
+}
+
+template <int DIM_1, int DIM_2>
+MoFEMErrorCode OpCalcDifferenceTensor2<DIM_1, DIM_2>::doWork(
+    int side, EntityType type, EntitiesFieldData::EntData &data) {
+  MoFEMFunctionBegin;
+
+  // Declare FTensor index
+  FTensor::Index<'i', DIM_1> i;
+  FTensor::Index<'j', DIM_2> j;
+  // get number of integration points
+  const auto nb_integration_points = getGaussPts().size2();
+  // resize resulting pointer
+  resultPtr->resize(DIM_1 * DIM_2, nb_integration_points);
+  // get values
+  auto t_data_1 = getFTensor2FromMat<DIM_1, DIM_2>(*data1Ptr);
+  auto t_data_2 = getFTensor2FromMat<DIM_1, DIM_2>(*data2Ptr);
+  auto t_result = getFTensor2FromMat<DIM_1, DIM_2>(*resultPtr);
+
+  // loop over integration points
+  for (int gg = 0; gg != nb_integration_points; gg++) {
+    // get difference
+    t_result(i, j) = t_data_1(i, j) - t_data_2(i, j);
+    // move to another data values
+    ++t_data_1;
+    ++t_data_2;
+    ++t_result;
+  }
 
   MoFEMFunctionReturn(0);
 }
