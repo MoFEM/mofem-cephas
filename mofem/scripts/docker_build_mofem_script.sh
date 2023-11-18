d7c089a2f62c20b56f5b80fe178db04718533aa8
Lukasz Kaczmarczyk <lukasz.kaczmarczyk@glasgowa.ac.uk>
2023-11-18 13:45:32 +0000

    remove obsolete files

--- mofem/scripts/docker_build_mofem_script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo MOFEM_SRC_DIR $MOFEM_SRC_DIR
-echo MOFEM_INSTALL_DIR $MOFEM_INSTALL_DIR
-echo MOFEM_BUILD_DIR $MOFEM_BUILD_DIR
-
-source ${MOFEM_SRC_DIR}/scripts/docker_build_core_lib_script.sh
-source ${MOFEM_SRC_DIR}/scripts/docker_build_users_modules_script.sh
