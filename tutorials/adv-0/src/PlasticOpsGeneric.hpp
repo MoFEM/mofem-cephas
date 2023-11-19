883bfa3a183b7396fd9e3cc19a5145b378437845
Lukasz Kaczmarczyk <638395893e79f12e57223b9a@bots.bitbucket.org>
2023-11-19 22:19:54 +0000

    alternative c function

--- tutorials/adv-0/src/PlasticOpsGeneric.hpp
+++ tutorials/adv-0/src/PlasticOpsGeneric.hpp
@@ -275,7 +275,7 @@ inline double constrain_abs(double x) {
 
 inline double w(double eqiv, double dot_tau, double f, double sigma_y,
                 double sigma_Y) {
-  return (f - sigma_y) / sigma_Y + cn1 * (eqiv);
+  return (f - sigma_y) / sigma_Y + cn1 * (dot_tau);
 };
 
 /**
@@ -292,19 +292,19 @@ c_n \sigma_y \dot{\tau} - \frac{1}{2}\left\{c_n\sigma_y \dot{\tau} +
 inline double constraint(double eqiv, double dot_tau, double f, double sigma_y,
                          double abs_w, double vis_H, double sigma_Y) {
   return vis_H * dot_tau +
-         (sigma_Y / 2) * ((cn0 * (dot_tau - eqiv) + cn1 * (eqiv) -
+         (sigma_Y / 2) * ((cn0 * (dot_tau - eqiv) + cn1 * (dot_tau) -
                            (f - sigma_y) / sigma_Y) -
                           abs_w);
 };
 
 inline double diff_constrain_ddot_tau(double sign, double eqiv, double dot_tau,
                                       double vis_H, double sigma_Y) {
-  return vis_H + (sigma_Y / 2) * (cn0);
+  return vis_H + (sigma_Y / 2) * (cn0 + cn1 * (1 - sign));
 };
 
 inline double diff_constrain_deqiv(double sign, double eqiv, double dot_tau,
                                    double sigma_Y) {
-  return (sigma_Y / 2) * (-cn0 + cn1 * (1 - sign));
+  return (sigma_Y / 2) * (-cn0);
 };
 
 inline auto diff_constrain_df(double sign) { return (-1 - sign) / 2; };
