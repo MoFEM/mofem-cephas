7f9d53d58ba79e8aa809f7a4e598cdfb2c06655d
Lukasz Kaczmarczyk <63849e6f489de2f7f464f50d@bots.bitbucket.org>
2023-12-10 18:13:06 +0000

    add hanliding ho on curved surface

--- mofem/atom_tests/hcurl_divergence_operator_2d_embaded_in_3d.cpp
+++ mofem/atom_tests/hcurl_divergence_operator_2d_embaded_in_3d.cpp
@@ -6,8 +6,6 @@
  *
  */
 
-
-
 #include <MoFEM.hpp>
 
 using namespace MoFEM;
@@ -191,24 +189,7 @@ int main(int argc, char *argv[]) {
       double div = 0;
       FaceEle fe_face(m_field);
       fe_face.getRuleHook = rule;
-
-      auto jac_ptr = boost::make_shared<MatrixDouble>();
-      auto inv_jac_ptr = boost::make_shared<MatrixDouble>();
-      auto det_ptr = boost::make_shared<VectorDouble>();
-
-      fe_face.getOpPtrVector().push_back(
-          new OpCalculateHOJacForFaceEmbeddedIn3DSpace(jac_ptr));
-      fe_face.getOpPtrVector().push_back(new OpMakeHdivFromHcurl());
-      fe_face.getOpPtrVector().push_back(
-          new OpSetContravariantPiolaTransformOnFace2DEmbeddedIn3DSpace(
-              jac_ptr));
-
-      fe_face.getOpPtrVector().push_back(
-          new OpInvertMatrix<3>(jac_ptr, det_ptr, inv_jac_ptr));
-
-      fe_face.getOpPtrVector().push_back(
-          new OpSetInvJacHcurlFaceEmbeddedIn3DSpace(inv_jac_ptr));
-      fe_face.getOpPtrVector().push_back(new OpSetHOWeightsOnFace());
+      CHKERR AddHOOps<2, 2, 3>::add(fe_face.getOpPtrVector(), {HDIV});
       fe_face.getOpPtrVector().push_back(new OpDivergence(div));
       CHKERR m_field.loop_finite_elements("TEST_PROBLEM", "FACE_FE", fe_face);
       return div;
