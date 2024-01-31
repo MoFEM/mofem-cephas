155d7f7ccec3835cd223b54d6b70aee9cb554d25
CallumRuncie <callum.runcie@glasgow.ac.uk>
2024-01-31 15:19:29 +0000

    update sdf files

--- tutorials/adv-1/sdf_files/sdf_hertz.py
+++ tutorials/adv-1/sdf_files/sdf_hertz.py
@@ -19,44 +19,21 @@ def hess_sdf(delta_t, t, x, y, z, tx, ty, tz, block_id):
 
 class Sphere:
 	def sDF(r, xc, yc, zc, x, y, z):
-		dx = np.subtract(x, xc)
-		dy = np.subtract(y, yc)
-		dz = np.subtract(z, zc)
-		a = dx**2 + dy**2 + dz**2
-		gap = np.sqrt(a) - r
-		return gap
+		return np.sqrt((x - xc)**2 + (y - yc)**2 + (z - zc)**2) - r
 
 	def gradSdf(xc, yc, zc, x, y, z):
-		a = (x-xc)**2 + (y-yc)**2 + (z-zc)**2
-		c_val = np.sqrt(a)
-		c_val_A = 1./c_val
-		c_val_dx = c_val_A * (x-xc)
-		c_val_dy = c_val_A * (y-yc)
-		c_val_dz = c_val_A * (z-zc)
-
-		c_val_dx = c_val_dx.reshape((-1,1))
-		c_val_dy = c_val_dy.reshape((-1,1))
-		c_val_dz = c_val_dz.reshape((-1,1))
-		grad_array = np.hstack([c_val_dx,c_val_dy,c_val_dz])
-		return grad_array
+		c_val_A = 1./np.sqrt((x-xc)**2 + (y-yc)**2 + (z-zc)**2)
+		return np.hstack([(c_val_A * (x-xc)).reshape((-1,1)), (c_val_A * (y-yc)).reshape((-1,1)), (c_val_A * (z-zc)).reshape((-1,1))])
 
 	def hessSdf(xc, yc, zc, x, y, z):
-		x = x-xc
-		y = y-yc
-		z = z-zc
-		Hxx = -x**2/(x**2 + y**2 + z**2)**(3/2) + 1/np.sqrt(x**2 + y**2 + z**2)
-		Hzx = -x*z/(x**2 + y**2 + z**2)**(3/2)
-		Hxy = -x*y/(x**2 + y**2 + z**2)**(3/2)
-		Hyy = -y**2/(x**2 + y**2 + z**2)**(3/2) + 1/np.sqrt(x**2 + y**2 + z**2)
-		Hzy = -y*z/(x**2 + y**2 + z**2)**(3/2)
-		Hzz = -z**2/(x**2 + y**2 + z**2)**(3/2) + 1/np.sqrt(x**2 + y**2 + z**2)
+		x, y, z = x-xc, y-yc, z-zc
+		denom = (x**2 + y**2 + z**2)**(3/2)
+		sqrt_denom = np.sqrt(x**2 + y**2 + z**2)
+		Hxx = -x**2/denom + 1/sqrt_denom
+		Hzx = -x*z/denom
+		Hxy = -x*y/denom
+		Hyy = -y**2/denom + 1/sqrt_denom
+		Hzy = -y*z/denom
+		Hzz = -z**2/denom + 1/sqrt_denom
 		# xx, yx, zx, yy, zy, zz
-		Hxx = Hxx.reshape((-1,1))
-		Hzx = Hzx.reshape((-1,1))
-		Hxy = Hxy.reshape((-1,1))
-		Hyy = Hyy.reshape((-1,1))
-		Hzy = Hzy.reshape((-1,1))
-		Hzz = Hzz.reshape((-1,1))
-		hess_array = np.hstack([Hxx, Hxy, Hzx, Hyy, Hzy, Hzz])
-
-		return hess_array
+		return np.hstack([Hxx.reshape((-1,1)), Hxy.reshape((-1,1)), Hzx.reshape((-1,1)), Hyy.reshape((-1,1)), Hzy.reshape((-1,1)), Hzz.reshape((-1,1))])
