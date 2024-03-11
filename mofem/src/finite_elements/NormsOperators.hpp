98a6518212c449539cc71579b9abe0e7f887c37f
AdrianaKul <kulikova.adriana@gmail.com>
2024-03-11 12:27:48 +0000

    operators for getting values from funstions

--- mofem/src/finite_elements/NormsOperators.hpp
+++ mofem/src/finite_elements/NormsOperators.hpp
@@ -19,7 +19,7 @@ struct OpCalcNormL2Tensor0 : public ForcesAndSourcesCore::UserDataOperator {
                       boost::shared_ptr<VectorDouble> diff_data_ptr = nullptr);
 
   /**
-   * \brief calculate values of scalar field at integration points
+   * \brief calculate norm of scalar values at integration points
    * @param  side side entity number
    * @param  type side entity type
    * @param  data entity data
@@ -46,7 +46,7 @@ struct OpCalcNormL2Tensor1 : public ForcesAndSourcesCore::UserDataOperator {
                       boost::shared_ptr<MatrixDouble> diff_data_ptr = nullptr);
 
   /**
-   * \brief calculate values of scalar field at integration points
+   * \brief calculate norm of vector values at integration points
    * @param  side side entity number
    * @param  type side entity type
    * @param  data entity data
@@ -73,7 +73,7 @@ struct OpCalcNormL2Tensor2 : public ForcesAndSourcesCore::UserDataOperator {
                       boost::shared_ptr<MatrixDouble> diff_data_ptr = nullptr);
 
   /**
-   * \brief calculate values of scalar field at integration points
+   * \brief calculate norm of tensor values at integration points
    * @param  side side entity number
    * @param  type side entity type
    * @param  data entity data
@@ -87,6 +87,58 @@ protected:
   boost::shared_ptr<MatrixDouble> diffDataPtr;
   SmartPetscObj<Vec> dataVec;
   const int iNdex;
+};
+
+typedef boost::function<double(const double, const double, const double)>
+    ScalarFunc;
+
+typedef boost::function<VectorDouble(const double, const double, const double)>
+    VectorFunc;
+
+/** \brief Get values from scalar function at integration points and save them
+ * to VectorDouble for Tensor0
+ *
+ */
+
+struct OpGetTensor0fromFunc : public ForcesAndSourcesCore::UserDataOperator {
+
+  OpGetTensor0fromFunc(boost::shared_ptr<VectorDouble> data_ptr,
+                       ScalarFunc scalar_function);
+
+  /**
+   * \brief calculate values of scalar function at integration points
+   * @param  side side entity number
+   * @param  type side entity type
+   * @param  data entity data
+   * @return      error code
+   */
+  MoFEMErrorCode doWork(int side, EntityType type,
+                        EntitiesFieldData::EntData &data);
+
+protected:
+  ScalarFunc sFunc;
+  boost::shared_ptr<VectorDouble> dataPtr;
+};
+
+template <int SPACE_DIM, int BASE_DIM>
+struct OpGetTensor1fromFunc : public ForcesAndSourcesCore::UserDataOperator {
+
+  OpGetTensor1fromFunc(boost::shared_ptr<MatrixDouble> data_ptr,
+                       VectorFunc vector_function);
+
+  /**
+   * \brief calculate values of vector function at integration points
+   * @param  side side entity number
+   * @param  type side entity type
+   * @param  data entity data
+   * @return      error code
+   */
+  MoFEMErrorCode doWork(int side, EntityType type,
+                        EntitiesFieldData::EntData &data);
+
+private:
+  VectorFunc vFunc;
+  boost::shared_ptr<MatrixDouble> dataPtr;
 };
 
 } // namespace MoFEM
--- mofem/src/finite_elements/impl/NormsOperators.cpp
+++ mofem/src/finite_elements/impl/NormsOperators.cpp
@@ -35,7 +35,7 @@ MoFEMErrorCode OpCalcNormL2Tensor0::doWork(int side, EntityType type,
   // get integration weights
   auto t_w = getFTensor0IntegrationWeight();
   // get values
-  auto t_data = getFTensor0FromVec(*dataPtr);
+  auto t_data = getFTensor0FromVec(*diffDataPtr);
   // initialise double to store norm values
   double norm_on_element = 0.;
   // loop over integration points
@@ -142,7 +142,7 @@ OpCalcNormL2Tensor2<DIM_1, DIM_2>::doWork(int side, EntityType type,
   // get integration weights
   auto t_w = getFTensor0IntegrationWeight();
   // get vector values
-  auto t_data = getFTensor2FromMat<DIM_1, DIM_2>(*dataPtr);
+  auto t_data = getFTensor2FromMat<DIM_1, DIM_2>(*diffDataPtr);
   // initialise double to store norm values
   double norm_on_element = 0.;
   // loop over integration points
@@ -162,9 +162,86 @@ OpCalcNormL2Tensor2<DIM_1, DIM_2>::doWork(int side, EntityType type,
   MoFEMFunctionReturn(0);
 }
 
+OpGetTensor0fromFunc::OpGetTensor0fromFunc(
+    boost::shared_ptr<VectorDouble> data_ptr, ScalarFunc scalar_function)
+    : ForcesAndSourcesCore::UserDataOperator(
+          NOSPACE, ForcesAndSourcesCore::UserDataOperator::OPSPACE),
+      dataPtr(data_ptr), sFunc(scalar_function) {}
+
+MoFEMErrorCode OpGetTensor0fromFunc::doWork(int side, EntityType type,
+                                            EntitiesFieldData::EntData &data) {
+  MoFEMFunctionBegin;
+
+  // get number of integration points
+  const int nb_integration_pts = getGaussPts().size2();
+  // resize dataPtr to store values at integration points
+  dataPtr->resize(nb_integration_pts);
+  // get values
+  auto t_val = getFTensor0FromVec(*dataPtr);
+
+  // get coordinates at integration point
+  auto t_coords = getFTensor1CoordsAtGaussPts();
+  // loop over integration points
+  for (int gg = 0; gg != nb_integration_pts; ++gg) {
+    // set value at integration point to value of scalar function at the
+    // coordinates
+    t_val = sFunc(t_coords(0), t_coords(1), t_coords(2));
+    // move to another value
+    ++t_val;
+    // move to another coordinates
+    ++t_coords;
+  }
+
+  MoFEMFunctionReturn(0);
+}
+
+template <int SPACE_DIM, int BASE_DIM>
+OpGetTensor1fromFunc<SPACE_DIM, BASE_DIM>::OpGetTensor1fromFunc(
+    boost::shared_ptr<MatrixDouble> data_ptr, VectorFunc vector_function)
+    : ForcesAndSourcesCore::UserDataOperator(
+          NOSPACE, ForcesAndSourcesCore::UserDataOperator::OPSPACE),
+      dataPtr(data_ptr), vFunc(vector_function) {}
+
+template <int SPACE_DIM, int BASE_DIM>
+MoFEMErrorCode OpGetTensor1fromFunc<SPACE_DIM, BASE_DIM>::doWork(
+    int side, EntityType type, EntitiesFieldData::EntData &data) {
+  MoFEMFunctionBegin;
+
+  // Declare FTensor index set to space dimension
+  FTensor::Index<'i', SPACE_DIM> i;
+  // get number of integration points
+  const int nb_integration_pts = getGaussPts().size2();
+  // resize dataPtr to store values at integration points
+  dataPtr->resize(BASE_DIM, nb_integration_pts);
+  // get values
+  auto t_val = getFTensor1FromMat<BASE_DIM>(*(dataPtr));
+
+  // get coordinates at integration point
+  auto t_coords = getFTensor1CoordsAtGaussPts();
+  // loop over integration points
+  for (int gg = 0; gg != nb_integration_pts; ++gg) {
+    // get function values of vector function at the coordinates
+    auto func_val = vFunc(t_coords(0), t_coords(1), t_coords(2));
+    // translate function values to tensor
+    auto t_func_val = getFTensor1FromArray<SPACE_DIM, SPACE_DIM>(func_val);
+    // set values at integration point to function values at the coordinates
+    t_val(i) = t_func_val(i);
+
+    // move to another value
+    ++t_val;
+    // move to another coordinates
+    ++t_coords;
+  }
+
+  MoFEMFunctionReturn(0);
+}
+
 template struct OpCalcNormL2Tensor1<2>;
 template struct OpCalcNormL2Tensor1<3>;
-template struct MoFEM::OpCalcNormL2Tensor2<2, 2>;
-template struct MoFEM::OpCalcNormL2Tensor2<3, 3>;
+template struct OpCalcNormL2Tensor2<2, 2>;
+template struct OpCalcNormL2Tensor2<3, 3>;
+template struct OpGetTensor1fromFunc<2, 2>;
+template struct OpGetTensor1fromFunc<2, 3>;
+template struct OpGetTensor1fromFunc<3, 3>;
 
 } // namespace MoFEM
