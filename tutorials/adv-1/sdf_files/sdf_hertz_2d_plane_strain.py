00d4017f110a962025cbbff6396627f4464f6093
Andrei Shvarts <andrei.shvarts@glasgow.ac.uk>
2024-03-14 19:58:11 +0000

    work in progress

---
+++ tutorials/adv-1/sdf_files/sdf_hertz_2d_plane_strain.py
@@ -0,0 +1,32 @@
+import math
+import numpy as np
+
+d = 0.01 # indentation depth
+#d = 0.09705 # indentation depth
+
+xc = 0
+yc = 0
+zc = 0
+
+def sdf(delta_t, t, x, y, z, tx, ty, tz, block_id):
+    return yPlane.sDF(xc, yc - d * t, zc, x, y, z)
+
+def grad_sdf(delta_t, t, x, y, z, tx, ty, tz, block_id):
+    return yPlane.gradSdf(xc, yc - d * t, zc, x, y, z)
+
+def hess_sdf(delta_t, t, x, y, z, tx, ty, tz, block_id):
+    return yPlane.hessSdf(xc, yc - d * t, zc, x, y, z)
+
+class yPlane:
+	def sDF(xc, yc, zc, x, y, z):
+		return np.subtract(yc, y)
+
+	def gradSdf(xc, yc, zc, x, y, z):
+		dx = np.zeros_like(x).reshape((-1, 1))
+		dy = np.full_like(y, -1).reshape((-1, 1))
+		dz = np.zeros_like(z).reshape((-1, 1))
+		return np.hstack([dx, dy, dz])
+	
+	def hessSdf(xc, yc, zc, x, y, z):
+		zeros = np.zeros_like(x).reshape((-1, 1))
+		return np.hstack([zeros for _ in range(6)])  # xx, yx, zx, yy, zy, zz
