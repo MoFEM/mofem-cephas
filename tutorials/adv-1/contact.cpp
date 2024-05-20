843306ab8dc80c541bc20ef9e7f1c3834a455ea4
ross.williams <Ross.Williams@glasgow.ac.uk>
2024-05-20 16:55:53 +0000

    Removed formula for sigma_order

--- tutorials/adv-1/contact.cpp
+++ tutorials/adv-1/contact.cpp
@@ -229,11 +229,10 @@ MoFEMErrorCode Contact::setupProblem() {
                             PETSC_NULL);
 
   MOFEM_LOG("CONTACT", Sev::inform) << "Order " << order;
-  if (contact_order == 2)
-    contact_order = order;
-  MOFEM_LOG("CONTACT", Sev::inform) << "Contact order " << contact_order;
-  if (sigma_order == 1)
-    sigma_order = std::max(order, contact_order) - 1;
+  
+  if (contact_order != 2)
+    MOFEM_LOG("CONTACT", Sev::inform) << "Contact order " << contact_order;
+    
   MOFEM_LOG("CONTACT", Sev::inform) << "Sigma order " << sigma_order;
 
   MOFEM_LOG("CONTACT", Sev::inform) << "Geom order " << geom_order;
