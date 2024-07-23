759d2a58bb87f484291c435fc18e7325adc916b4
Lukasz Kaczmarczyk <lukasz.kaczmarczyk@glasgowa.ac.uk>
2024-07-23 14:45:38 -0700

    add tag for side dof map

--- mofem/src/multi_indices/TagMultiIndices.hpp
+++ mofem/src/multi_indices/TagMultiIndices.hpp
@@ -94,6 +94,8 @@ struct Composite_SeriesID_And_Step_mi_tag {};
 struct Composite_SeriesName_And_Step_mi_tag {};
 struct Composite_SeriesName_And_Time_mi_tag {};
 
+struct type_side_mi_tag {};
+
 } // namespace MoFEM
 
 #endif // __TAGMULTIINDICES_HPP__
