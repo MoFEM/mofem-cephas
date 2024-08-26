0fcc475807b3554f0e73298071b314c4cd478cec
karol41 <inz.karolewandowski@gmail.com>
2024-08-26 09:55:51 +0100

    working on libs

--- mofem/tutorials/cor-6/doc/AddDocumentation.cmake
+++ mofem/tutorials/cor-6/doc/AddDocumentation.cmake
@@ -1,7 +1,7 @@
 #
 
 # copy dox/figures to html directory created by doxygen
-add_custom_target(simple_elasticity
+add_custom_target(simple_elasticity_doc
   ${CMAKE_COMMAND} -E copy_directory
   ${ADD_DOC_DIRECTORY}/figures ${PROJECT_BINARY_DIR}/html)
-add_dependencies(doc simple_elasticity)
+add_dependencies(doc simple_elasticity_doc)
