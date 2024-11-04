44b05d678ac366ab04d6d9c79c84a07a8588238c
CallumRuncie <callum.runcie@glasgow.ac.uk>
2024-11-04 20:20:40 +0000

    adding ctest

--- mofem/tools/read_vtk.cpp
+++ mofem/tools/read_vtk.cpp
@@ -1,32 +1,31 @@
-/** \file reading_vtk.cpp
+/** \file read_vtk.cpp
 
-  \brief Reading vtk files
+  \brief Tool to read mesh files as vtk files assuming Boundary conditions
+  are set with a field named "BOUNDARY_CONDITIONS"
 
 */
-
-
 
 #include <MoFEM.hpp>
 using namespace MoFEM;
 
 struct VtkInterface {
-    
-    VtkInterface(MoFEM::Interface &m_field, moab::Interface &moab) : mField(m_field), mOab(moab) {}
 
-    MoFEMErrorCode readVtk();
+  VtkInterface(MoFEM::Interface &m_field, moab::Interface &moab)
+      : mField(m_field), mOab(moab) {}
+
+  MoFEMErrorCode readVtk();
 
 private:
-    MoFEM::Interface &mField;
-    moab::Interface &mOab;
-    Simple *simpleInterface;
+  MoFEM::Interface &mField;
+  moab::Interface &mOab;
+  Simple *simpleInterface;
 
-    MoFEMErrorCode readMesh();
-    MoFEMErrorCode getBoundaryConditions();
-    MoFEMErrorCode setBoundaryConditions();
-    MoFEMErrorCode getMaterialProperties();
-    MoFEMErrorCode writeOutput();
-
-
+  MoFEMErrorCode readMesh();
+  MoFEMErrorCode getBoundaryConditions();
+  MoFEMErrorCode setBoundaryConditions();
+  MoFEMErrorCode getMaterialProperties();
+  MoFEMErrorCode writeOutput();
+  MoFEMErrorCode checkResults();
 };
 
 MoFEMErrorCode VtkInterface::readVtk() {
@@ -36,9 +35,9 @@ MoFEMErrorCode VtkInterface::readVtk() {
   CHKERR setBoundaryConditions();
   CHKERR getMaterialProperties();
   CHKERR writeOutput();
+  CHKERR checkResults();
   MoFEMFunctionReturn(0);
 }
-
 
 MoFEMErrorCode VtkInterface::readMesh() {
   MoFEMFunctionBegin;
@@ -60,16 +59,26 @@ MoFEMErrorCode VtkInterface::getBoundaryConditions() {
                              MB_TAG_CREAT | MB_TAG_SPARSE);
 
   std::vector<std::tuple<int, std::string, std::string>> conditions = {
-      {1, "FIX_ALL", "FIX_ALL"},      {2, "FIX_X", "FIX_X"},
-      {3, "FIX_Y", "FIX_Y"},          {4, "FIX_Z", "FIX_Z"},
-      {5, "FIX_X_2", "DISP_X"},       {6, "FIX_Y_2", "DISP_Y"},
-      {7, "FIX_Z_2", "DISP_Z"},       {8, "FORCE_X", "FORCE_X"},
-      {9, "FORCE_Y", "FORCE_Y"},      {10, "FORCE_Z", "FORCE_Z"},
-      {11, "VEL_X", "VEL_X"},         {12, "VEL_Y", "VEL_Y"},
-      {13, "VEL_Z", "VEL_Z"},         {14, "ACCL_X", "ACCL_X"},
-      {15, "ACCL_Y", "ACCL_Y"},       {16, "ACCL_Z", "ACCL_Z"},
-      {17, "TEMP", "TEMP"},           {18, "PRESSURE", "PRESSURE"},
-      {19, "HEAT_FLUX", "HEAT_FLUX"}, {20, "CONTACT", "CONTACT"},
+      {1, "FIX_ALL", "FIX_ALL"},
+      {2, "FIX_X", "FIX_X"},
+      {3, "FIX_Y", "FIX_Y"},
+      {4, "FIX_Z", "FIX_Z"},
+      {5, "FIX_X_2", "DISP_X"},
+      {6, "FIX_Y_2", "DISP_Y"},
+      {7, "FIX_Z_2", "DISP_Z"},
+      {8, "FORCE_X", "FORCE_X"},
+      {9, "FORCE_Y", "FORCE_Y"},
+      {10, "FORCE_Z", "FORCE_Z"},
+      {11, "VEL_X", "VEL_X"},
+      {12, "VEL_Y", "VEL_Y"},
+      {13, "VEL_Z", "VEL_Z"},
+      {14, "ACCL_X", "ACCL_X"},
+      {15, "ACCL_Y", "ACCL_Y"},
+      {16, "ACCL_Z", "ACCL_Z"},
+      {17, "TEMP", "TEMP"},
+      {18, "PRESSURE", "PRESSURE"},
+      {19, "HEAT_FLUX", "HEAT_FLUX"},
+      {20, "CONTACT", "CONTACT"},
       {50, "TIE_MATRIX", "TIE_MATRIX"}};
 
   for (auto &condition : conditions) {
@@ -123,22 +132,22 @@ MoFEMErrorCode VtkInterface::getBoundaryConditions() {
 }
 
 MoFEMErrorCode VtkInterface::setBoundaryConditions() {
-    MoFEMFunctionBegin;
-    // Add meshsets if config file provided
-    MeshsetsManager *meshsets_interface_ptr;
-    CHKERR mField.getInterface(meshsets_interface_ptr);
-    CHKERR meshsets_interface_ptr->setMeshsetFromFile();
+  MoFEMFunctionBegin;
+  // Add meshsets if config file provided
+  MeshsetsManager *meshsets_interface_ptr;
+  CHKERR mField.getInterface(meshsets_interface_ptr);
+  CHKERR meshsets_interface_ptr->setMeshsetFromFile();
 
-    MOFEM_LOG_CHANNEL("WORLD");
-    MOFEM_LOG_TAG("WORLD", "read_vtk")
-    MOFEM_LOG("WORLD", Sev::inform)
-        << "Print all meshsets (old and added from meshsets "
-           "configurational file)";
-    for (auto cit = meshsets_interface_ptr->getBegin();
-         cit != meshsets_interface_ptr->getEnd(); cit++)
-      MOFEM_LOG("WORLD", Sev::inform) << *cit;
+  MOFEM_LOG_CHANNEL("WORLD");
+  MOFEM_LOG_TAG("WORLD", "read_vtk")
+  MOFEM_LOG("WORLD", Sev::inform)
+      << "Print all meshsets (old and added from meshsets "
+         "configurational file)";
+  for (auto cit = meshsets_interface_ptr->getBegin();
+       cit != meshsets_interface_ptr->getEnd(); cit++)
+    MOFEM_LOG("WORLD", Sev::inform) << *cit;
 
-      MoFEMFunctionReturn(0);
+  MoFEMFunctionReturn(0);
 }
 
 MoFEMErrorCode VtkInterface::getMaterialProperties() {
@@ -167,6 +176,67 @@ MoFEMErrorCode VtkInterface::writeOutput() {
   MoFEMFunctionReturn(0);
 }
 
+MoFEMErrorCode VtkInterface::checkResults() {
+  MoFEMFunctionBegin;
+  PetscBool atom_test = PETSC_FALSE;
+  PetscOptionsGetBool(PETSC_NULL, "", "-atom_test", &atom_test, PETSC_NULL);
+  if (atom_test) {
+    // check mesh has been read correctly
+    Range ents;
+    CHKERR mOab.get_entities_by_dimension(0, 3, ents);
+    if (ents.size() != 125) {
+      SETERRQ3(
+          PETSC_COMM_SELF, MOFEM_ATOM_TEST_INVALID,
+          "atom test %d failed: Wrong number of elements %d, should be 125",
+          atom_test, ents.size(), 125);
+    }
+    // check boundary conditions have been set correctly
+    MeshsetsManager *meshsets_manager_ptr;
+    CHKERR mField.getInterface(meshsets_manager_ptr);
+    // iterate meshsets
+    for (_IT_CUBITMESHSETS_BY_SET_TYPE_FOR_LOOP_(mField, BLOCKSET, it)) {
+      Range ents;
+      CHKERR mField.get_moab().get_entities_by_dimension(it->meshset, 2, ents,
+                                                         true);
+      if (it->getMeshsetId() == 1) {
+        if (it->getName() != "FIX_ALL") {
+          SETERRQ3(PETSC_COMM_SELF, MOFEM_ATOM_TEST_INVALID,
+                   "atom test %d failed: Wrong name of meshset %d, should be "
+                   "FIX_ALL is %s",
+                   atom_test, it->getMeshsetId(), it->getName());
+        }
+        if (ents.size() != 25) {
+          SETERRQ3(PETSC_COMM_SELF, MOFEM_ATOM_TEST_INVALID,
+                   "atom test %d failed: Wrong number of entities in meshset "
+                   "%d with %d, should be 25",
+                   atom_test, it->getMeshsetId(), ents.size());
+        }
+      } else if (it->getMeshsetId() == 2) {
+        if (it->getName() != "FIX_X") {
+          SETERRQ3(PETSC_COMM_SELF, MOFEM_ATOM_TEST_INVALID,
+                   "atom test %d failed: Wrong name of meshset %d, should be "
+                   "FIX_X is %s",
+                   atom_test, it->getMeshsetId(), it->getName());
+        }
+        if (ents.size() != 25) {
+          SETERRQ3(PETSC_COMM_SELF, MOFEM_ATOM_TEST_INVALID,
+                   "atom test %d failed: Wrong number of entities in meshset "
+                   "%d with %d, should be 25",
+                   atom_test, it->getMeshsetId(), ents.size());
+        }
+      } else if (it->getMeshsetId() == 100) {
+        if (it->getName() != "ADOLCMAT") {
+          SETERRQ3(PETSC_COMM_SELF, MOFEM_ATOM_TEST_INVALID,
+                   "atom test %d failed: Wrong name of meshset %d, should be "
+                   "ADOLCMAT is %s",
+                   atom_test, it->getMeshsetId(), it->getName());
+        }
+      }
+    }
+  }
+  MoFEMFunctionReturn(0);
+}
+
 static char help[] = "...\n\n";
 
 int main(int argc, char *argv[]) {
@@ -186,7 +256,6 @@ int main(int argc, char *argv[]) {
 
     char mesh_out_file[255] = "out.h5m";
 
-    int time_step = 0;
     CHKERR PetscOptionsBegin(m_field.get_comm(), "", "Read vtk tool", "none");
     CHKERR PetscOptionsString("-output_file", "output mesh file name", "",
                               "out.h5m", mesh_out_file, 255, PETSC_NULL);
@@ -195,10 +264,6 @@ int main(int argc, char *argv[]) {
 
     VtkInterface read_vtk(m_field, moab);
     CHKERR read_vtk.readVtk();
-
-
-
-
   }
   CATCH_ERRORS;
 
