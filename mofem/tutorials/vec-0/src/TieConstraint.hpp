675b0b1b3ea0b964c745e43de0f5df1d812571f5
CallumRuncie <callum.runcie@glasgow.ac.uk>
2024-12-03 18:04:26 +0000

    adding reaction calculation based on LM

--- mofem/tutorials/vec-0/src/TieConstraint.hpp
+++ mofem/tutorials/vec-0/src/TieConstraint.hpp
@@ -21,7 +21,7 @@ struct OpTieTermConstraintRhs
       : OpBase(lambda_name, lambda_name, OpBase::OPROW, tie_faces_ptr),
         uPtr(u_ptr), tieCoord(tie_coord), tieDirection(tie_direction) {
     std::fill(&(doEntities[MBEDGE]), &(doEntities[MBMAXTYPE]), false);
-    doEntities[MBEDGE] = true;
+    //doEntities[MBEDGE] = true;
     doEntities[MBTRI] = true;
     doEntities[MBQUAD] = true;
   }
@@ -43,17 +43,32 @@ struct OpTieTermConstraintRhs
     auto t_coords = OpBase::getFTensor1CoordsAtGaussPts();
     // get displacement
     auto t_u = getFTensor1FromMat<SPACE_DIM>(*uPtr);
+    // get normal
+    FTensor::Tensor1<double, SPACE_DIM> t_Normal;
+    t_Normal(0) = -1.0;
+    t_Normal(1) = 0.0;
+    t_Normal(2) = 0.0;
+    
+
     for (int gg = 0; gg != nb_integration_pts; gg++) {
       const auto alpha = t_w * vol;
       ++t_w;
-      FTensor::Tensor1<double, SPACE_DIM> t_delta_current;
-      t_delta_current(i) =
-          (t_coords(i) + t_u(i)) - (tieCoord(i) + (tieDirection(i) * time));
+      //std::cout << "!!!!!!!!!!!!!!!!!!!!!!!! testing normals print" << std::endl;
+      // FTensor::Tensor1<double, SPACE_DIM> t_delta_current;
+      // t_delta_current(i) =
+      //     (t_coords(i) + t_u(i)) - (tieCoord(i) + (tieDirection(i) * time));
+      // ++t_u;
+      // FTensor::Tensor1<double, SPACE_DIM> t_delta_initial;
+      // t_delta_initial(i) = t_coords(i) - tieCoord(i);
+      // ++t_coords;
+      // auto g = alpha * (t_delta_current.l2() - t_delta_initial.l2());
+      double tieDistance = 10.0;
+
+      auto g = alpha * (t_Normal(i) * t_u(i) - tieDistance * time);
       ++t_u;
-      FTensor::Tensor1<double, SPACE_DIM> t_delta_initial;
-      t_delta_initial(i) = t_coords(i) - tieCoord(i);
       ++t_coords;
-      auto g = alpha * (t_delta_current.l2() - t_delta_initial.l2());
+//      ++t_Normal;
+
       int rr = 0;
       for (; rr != OpBase::nbRows; ++rr) {
         OpBase::locF[rr] += t_row_base * g;
@@ -85,7 +100,7 @@ struct OpTieTermConstraintLhs
       : OpBase(lambda_name, col_field_name, OpBase::OPROWCOL, tie_faces_ptr),
         uPtr(u_ptr), tieCoord(tie_coord), tieDirection(tie_direction) {
     std::fill(&(doEntities[MBEDGE]), &(doEntities[MBMAXTYPE]), false);
-    doEntities[MBEDGE] = true;
+    //doEntities[MBEDGE] = true;
     doEntities[MBTRI] = true;
     doEntities[MBQUAD] = true;
     this->assembleTranspose = true;
@@ -111,16 +126,26 @@ struct OpTieTermConstraintLhs
     auto t_coords = OpBase::getFTensor1CoordsAtGaussPts();
     // get displacement
     auto t_u = getFTensor1FromMat<SPACE_DIM>(*uPtr);
+    // get normal
+    FTensor::Tensor1<double, SPACE_DIM> t_Normal;
+    t_Normal(0) = -1.0;
+    t_Normal(1) = 0.0;
+    t_Normal(2) = 0.0;
+
     for (int gg = 0; gg != nb_integration_pts; gg++) {
       const auto alpha = t_w * vol;
       ++t_w;
-      FTensor::Tensor1<double, SPACE_DIM> t_delta_current;
-      t_delta_current(i) =
-          (t_coords(i) + t_u(i)) - (tieCoord(i) + (tieDirection(i) * time));
-      ++t_u;
-      ++t_coords;
+      // FTensor::Tensor1<double, SPACE_DIM> t_delta_current;
+      // t_delta_current(i) =
+      //     (t_coords(i) + t_u(i)) - (tieCoord(i) + (tieDirection(i) * time));
+      // ++t_u;
+      // ++t_coords;
+      // FTensor::Tensor1<double, SPACE_DIM> t_tangent;
+      // t_tangent(i) = alpha * (t_delta_current(i) / t_delta_current.l2());
       FTensor::Tensor1<double, SPACE_DIM> t_tangent;
-      t_tangent(i) = alpha * (t_delta_current(i) / t_delta_current.l2());
+      t_tangent(i) = alpha * (t_Normal(i));
+      //++t_Normal;
+
       int rr = 0;
       for (; rr != OpBase::nbRows; ++rr) {
         auto t_col_base = col_data.getFTensor0N(gg, 0);
@@ -162,7 +187,7 @@ struct OpTieTermConstraintRhs_du
         lambdaPtr(lambda_ptr), tieCoord(tie_coord),
         tieDirection(tie_direction) {
     std::fill(&(doEntities[MBEDGE]), &(doEntities[MBMAXTYPE]), false);
-    doEntities[MBEDGE] = true;
+    //doEntities[MBEDGE] = true;
     doEntities[MBTRI] = true;
     doEntities[MBQUAD] = true;
   }
@@ -189,17 +214,24 @@ struct OpTieTermConstraintRhs_du
 
     auto &nf = OpBase::locF;
 
+    FTensor::Tensor1<double, SPACE_DIM> t_Normal;
+    t_Normal(0) = -1.0;
+    t_Normal(1) = 0.0;
+    t_Normal(2) = 0.0;
+    //std::cout << "t_Normal: " << t_Normal(0) << " " << t_Normal(1) << " " << t_Normal(2) << std::endl;
+    double tieDistance = 10.0;
+
     for (int gg = 0; gg != nb_integration_pts; gg++) {
       const auto alpha = t_w * vol;
       ++t_w;
       FTensor::Tensor1<double, SPACE_DIM> t_du;
-      t_du(i) = t_u(i) + (t_coords(i) - tieCoord(i) - (tieDirection(i) * time));
+      t_du(i) = (t_Normal(i));
       ++t_u;
       ++t_coords;
       
       auto t_nf = getFTensor1FromPtr<DIM>(&nf[0]);
       FTensor::Tensor1<double, SPACE_DIM> g;
-      g(i) = alpha * t_lambda * (t_du(i) / t_du.l2());
+      g(i) = alpha * t_lambda * (t_du(i));
       ++t_lambda;
       int rr = 0;
       for (; rr != OpBase::nbRows / DIM; ++rr) {
@@ -219,4 +251,92 @@ private:
   boost::shared_ptr<VectorDouble> lambdaPtr;
   FTensor::Tensor1<double, 3> tieCoord;
   FTensor::Tensor1<double, 3> tieDirection;
+};
+
+// operator to calculate total reaction force
+template <int DIM> struct OpCalculateTotalTieReactionForce : public FormsIntegrators<BoundaryEleOp>::Assembly<A>::OpBase {
+
+  using OpBase = FormsIntegrators<BoundaryEleOp>::Assembly<A>::OpBase;
+
+  OpCalculateTotalTieReactionForce(
+      std::string lambda_name, boost::shared_ptr<VectorDouble> lambda_ptr,
+      boost::shared_ptr<SmartPetscObj<Vec>> total_reaction_ptr,
+      boost::shared_ptr<Range> tie_faces_ptr)
+      : OpBase(lambda_name, lambda_name, OpBase::OPROW, tie_faces_ptr),
+        lambdaPtr(lambda_ptr), totalReactionPtr(total_reaction_ptr), tieFacesPtr(tie_faces_ptr) {
+    std::fill(&(doEntities[MBEDGE]), &(doEntities[MBMAXTYPE]), false);
+    // doEntities[MBEDGE] = true;
+    //doEntities[MBTRI] = true;
+    //doEntities[MBQUAD] = true;
+  }
+
+  MoFEMErrorCode doWork(int side, EntityType type,
+                        EntitiesFieldData::EntData &data) {
+    MoFEMFunctionBegin;
+    if (tieFacesPtr->find(getNumeredEntFiniteElementPtr()->getEnt()) ==
+        tieFacesPtr->end()) {
+      MoFEMFunctionReturnHot(0);
+    }
+    //std::cout<< "Entity id = "<< getNumeredEntFiniteElementPtr()->getEnt() << std::endl;
+    FTENSOR_INDEX(SPACE_DIM, i);
+    FTENSOR_INDEX(SPACE_DIM, j);
+
+    double sum_reaction = 0.0;
+    FTensor::Tensor1<double, 3> t_sum_reaction{0.0, 0.0, 0.0};
+    
+
+    auto nb_integration_pts = OpBase::getGaussPts().size2();
+    // get element volume
+    const double vol = OpBase::getMeasure();
+    // get base function on rows
+    auto t_row_base = data.getFTensor0N();
+    // get integration weights
+    auto t_w = OpBase::getFTensor0IntegrationWeight();
+    // get coordinate at integration points
+    auto t_coords = OpBase::getFTensor1CoordsAtGaussPts();
+    // get lambda
+    auto t_lambda = getFTensor0FromVec(*lambdaPtr);
+    // get stress
+    //auto t_stress = getFTensor2FromMat<SPACE_DIM, SPACE_DIM>(*stressPtr);
+    // get normal
+    //auto t_normal = OpBase::getFTensor1NormalsAtGaussPts();
+
+    for (int gg = 0; gg != nb_integration_pts; gg++) {
+      const auto alpha = t_w * vol;
+      ++t_w;
+
+      
+      auto g = alpha * t_lambda;
+      ++t_lambda;
+
+      //t_sum_reaction(i) += alpha * t_stress(i, j) * t_normal(i);
+
+
+      //int rr = 0;
+      //for (; rr != OpBase::nbRows / DIM; ++rr) {
+        sum_reaction += g;
+        //++t_row_base;
+      //}
+
+      //for (; rr < OpBase::nbRowBaseFunctions; ++rr)
+      //  ++t_row_base;
+    }
+
+    constexpr int ind[] = {1, 2, 3};
+    constexpr int ind0[] = {0};
+    // std::cout << "lambdaPtr: " << *lambdaPtr << std::endl;
+    // std::cout << "sum_reaction: " << sum_reaction << std::endl;
+    // std::cout << "t_sum_reaction: " << t_sum_reaction(0) << " " << t_sum_reaction(1) << " " << t_sum_reaction(2) << std::endl;
+    // std::cout << "total_reaction_ptr: " << *totalReactionPtr << std::endl;
+    // set reaction from LM 
+    CHKERR VecSetValues(*totalReactionPtr, 1, ind0, &sum_reaction, ADD_VALUES);
+    // set reaction from stress
+    CHKERR VecSetValues(*totalReactionPtr, 3, ind, &t_sum_reaction(0), ADD_VALUES);
+
+    MoFEMFunctionReturn(0);
+  }
+  boost::shared_ptr<SmartPetscObj<Vec>> totalReactionPtr;
+  boost::shared_ptr<VectorDouble> lambdaPtr;
+  boost::shared_ptr<MatrixDouble> stressPtr;
+  boost::shared_ptr<Range> tieFacesPtr;
 };
