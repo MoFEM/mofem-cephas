d7b26ebbe8a8ee2cf8a108847d77e0776ad74025
Andrei Shvarts <andrei.shvarts@glasgow.ac.uk>
2024-12-29 20:47:45 +0000

    add base class, get param from command line

--- mofem/src/interfaces/MFrontInterface.hpp
+++ mofem/src/interfaces/MFrontInterface.hpp
@@ -13,9 +13,21 @@
 // #include <MoFEM.hpp>
 #include "UnknownInterface.hpp"
 
+#include <MGIS/Behaviour/Behaviour.hxx>
+#include <MGIS/Behaviour/BehaviourData.hxx>
+// #include "MGIS/Behaviour/Integrate.hxx"
+#include "MGIS/LibrariesManager.hxx"
+
+using namespace mgis;
+using namespace mgis::behaviour;
+
 namespace MoFEM {
 
-// using EntData = MoFEM::EntitiesFieldData::EntData;
+class MFrontInterfaceBase : public UnknownInterface {
+public:
+  virtual ~MFrontInterfaceBase() = default;
+  virtual MoFEMErrorCode getCommandLineParameters() = 0;
+};
 
 enum ModelHypothesis { TRIDIMENSIONAL, PLANESTRAIN, AXISYMMETRICAL };
 
@@ -57,21 +69,20 @@ template <> struct MFrontEleType<AXISYMMETRICAL> {
   static constexpr int SPACE_DIM = 2;
 };
 
-  #ifndef SCHUR_ASSEMBLE
-    #define SCHUR_ASSEMBLE 0
-  #endif
+#ifndef SCHUR_ASSEMBLE
+  #define SCHUR_ASSEMBLE 0
+#endif
 
 constexpr AssemblyType MFRONT_AT =
     (SCHUR_ASSEMBLE) ? AssemblyType::BLOCK_SCHUR
                      : AssemblyType::PETSC; //< selected assembly type
 
 template <ModelHypothesis H>
-struct MFrontInterface : public UnknownInterface {
+struct MFrontInterface : public MFrontInterfaceBase {
 
   MoFEMErrorCode query_interface(boost::typeindex::type_index type_index,
                                  UnknownInterface **iface) const;
 
-  MoFEM::Core &cOre;
   MFrontInterface(const MoFEM::Core &core);
 
   using DomainEle = typename MFrontEleType<H>::DomainEle;
@@ -93,7 +104,7 @@ struct MFrontInterface : public UnknownInterface {
           template BiLinearForm<GAUSS>::template OpGradSymTensorGrad<1, DIM,
                                                                      DIM, 0>;
 
-  // MoFEMErrorCode getCommandLineParameters();
+  MoFEMErrorCode getCommandLineParameters() override;
 
   // MoFEMErrorCode setUpdateElementVariablesOperators();
 
@@ -104,19 +115,274 @@ struct MFrontInterface : public UnknownInterface {
 
   // MoFEMErrorCode testOperators();
 
-  // MoFEMErrorCode updateElementVariables(SmartPetscObj<DM> dm, string fe_name);
+  // MoFEMErrorCode updateElementVariables(SmartPetscObj<DM> dm, string
+  // fe_name);
 
   // MoFEMErrorCode postProcessElement(int step, SmartPetscObj<DM> dm,
   //                                   string fe_name);
 
-  // MoFEMErrorCode setMonitorPtr(boost::shared_ptr<MoFEM::FEMethod> monitor_ptr) {
+  // MoFEMErrorCode setMonitorPtr(boost::shared_ptr<MoFEM::FEMethod>
+  // monitor_ptr) {
   //   MoFEMFunctionBeginHot;
   //   monitorPtr = monitor_ptr;
   //   MoFEMFunctionReturnHot(0);
   // }
 
-private:
-  // MoFEM::Interface &mField;
+enum DataTags { RHS = 0, LHS };
+
+struct BlockData {
+  int iD;
+  int oRder;
+
+  bool isFiniteStrain;
+  string behaviourPath;
+  string behaviourName;
+
+  boost::shared_ptr<Behaviour> mGisBehaviour;
+  BehaviourDataView bView;
+  boost::shared_ptr<BehaviourData> behDataPtr;
+
+  int sizeIntVar;
+  int sizeExtVar;
+  int sizeGradVar;
+  int sizeStressVar;
+
+  vector<double> params;
+
+  double dIssipation;
+  double storedEnergy;
+  double externalVariable;
+
+  Range eNts;
+
+  BlockData()
+      : oRder(-1), isFiniteStrain(false), behaviourPath("src/libBehaviour.so"),
+        behaviourName("IsotropicLinearHardeningPlasticity") {
+    dIssipation = 0;
+    storedEnergy = 0;
+    externalVariable = 0;
+  }
+
+  inline MoFEMErrorCode setTag(DataTags tag) {
+    MoFEMFunctionBeginHot;
+    if (tag == RHS) {
+      behDataPtr->K[0] = 0;
+    } else {
+      behDataPtr->K[0] = 5;
+    }
+    MoFEMFunctionReturnHot(0);
+  }
+
+  MoFEMErrorCode setBlockBehaviourData(bool set_params_from_blocks) {
+    MoFEMFunctionBeginHot;
+    if (mGisBehaviour) {
+
+      auto &mgis_bv = *mGisBehaviour;
+
+      sizeIntVar = getArraySize(mgis_bv.isvs, mgis_bv.hypothesis);
+      sizeExtVar = getArraySize(mgis_bv.esvs, mgis_bv.hypothesis);
+      sizeGradVar = getArraySize(mgis_bv.gradients, mgis_bv.hypothesis);
+      sizeStressVar =
+          getArraySize(mgis_bv.thermodynamic_forces, mgis_bv.hypothesis);
+
+      behDataPtr = boost::make_shared<BehaviourData>(BehaviourData{mgis_bv});
+      bView = make_view(*behDataPtr);
+      const int total_number_of_params = mgis_bv.mps.size();
+      // const int total_number_of_params = mgis_bv.mps.size() +
+      // mgis_bv.params.size() + mgis_bv.iparams.size() +
+      // mgis_bv.usparams.size();
+
+      if (set_params_from_blocks) {
+
+        if (params.size() < total_number_of_params)
+          SETERRQ2(PETSC_COMM_SELF, MOFEM_DATA_INCONSISTENCY,
+                   "Not enough parameters supplied for this block. We have %d "
+                   "provided where %d are necessary for this block",
+                   params.size(), total_number_of_params);
+
+        for (int dd = 0; dd < total_number_of_params; ++dd) {
+          setMaterialProperty(behDataPtr->s0, dd, params[dd]);
+          setMaterialProperty(behDataPtr->s1, dd, params[dd]);
+        }
+      }
+
+      if (isFiniteStrain) {
+        behDataPtr->K[0] = 0; // no tangent
+        behDataPtr->K[1] = 2; // PK1
+        behDataPtr->K[2] = 2; // PK1
+      } else {
+        behDataPtr->K[0] = 0; // no tangent
+        behDataPtr->K[1] = 0; // cauchy
+      }
+
+      for (auto &mb : {&behDataPtr->s0, &behDataPtr->s1}) {
+        mb->dissipated_energy = dIssipation;
+        mb->stored_energy = storedEnergy;
+        setExternalStateVariable(*mb, 0, externalVariable);
+      }
+    }
+
+    MoFEMFunctionReturnHot(0);
+  }
+};
+
+  struct CommonData {
+
+    MoFEM::Interface &mField;
+    boost::shared_ptr<MatrixDouble> mGradPtr;
+    boost::shared_ptr<MatrixDouble> mStressPtr;
+    boost::shared_ptr<MatrixDouble> mFullStrainPtr;
+    boost::shared_ptr<MatrixDouble> mFullStressPtr;
+
+    boost::shared_ptr<MatrixDouble> mPrevGradPtr;
+    boost::shared_ptr<MatrixDouble> mPrevStressPtr;
+
+    boost::shared_ptr<MatrixDouble> mDispPtr;
+    boost::shared_ptr<MatrixDouble> materialTangentPtr;
+    boost::shared_ptr<MatrixDouble> mFullTangentPtr;
+    boost::shared_ptr<MatrixDouble> internalVariablePtr;
+
+    std::map<int, BlockData> setOfBlocksData;
+    std::map<EntityHandle, int> blocksIDmap;
+
+    MatrixDouble lsMat;
+    MatrixDouble gaussPts;
+    MatrixDouble myN;
+
+    Tag internalVariableTag;
+    Tag stressTag;
+    Tag gradientTag;
+
+    CommonData(MoFEM::Interface &m_field) : mField(m_field) {}
+
+    MoFEMErrorCode setBlocks(int dim) {
+      MoFEMFunctionBegin;
+      string block_name = "MFRONT";
+      for (_IT_CUBITMESHSETS_BY_SET_TYPE_FOR_LOOP_(mField, BLOCKSET, it)) {
+        if (it->getName().compare(0, block_name.size(), block_name) == 0) {
+          std::vector<double> block_data;
+          // FIXME: TODO: maybe this should be set only from the command line!!!
+          CHKERR it->getAttributes(block_data);
+          const int id = it->getMeshsetId();
+          EntityHandle meshset = it->getMeshset();
+          CHKERR mField.get_moab().get_entities_by_dimension(
+              meshset, dim, setOfBlocksData[id].eNts, true);
+          for (auto ent : setOfBlocksData[id].eNts)
+            blocksIDmap[ent] = id;
+
+          setOfBlocksData[id].iD = id;
+          setOfBlocksData[id].params.resize(block_data.size());
+
+          for (int n = 0; n != block_data.size(); n++)
+            setOfBlocksData[id].params[n] = block_data[n];
+        }
+      }
+
+      MoFEMFunctionReturn(0);
+    }
+
+    MoFEMErrorCode getInternalVar(const EntityHandle fe_ent,
+                                  const int nb_gauss_pts, const int var_size,
+                                  const int grad_size, const int stress_size,
+                                  bool is_large_strain = false) {
+      MoFEMFunctionBegin;
+
+      auto mget_tag_data = [&](Tag &m_tag,
+                               boost::shared_ptr<MatrixDouble> &m_mat,
+                               const int &m_size, bool is_def_grad = false) {
+        MoFEMFunctionBeginHot;
+
+        double *tag_data;
+        int tag_size;
+        rval = mField.get_moab().tag_get_by_ptr(
+            m_tag, &fe_ent, 1, (const void **)&tag_data, &tag_size);
+
+        if (rval != MB_SUCCESS || tag_size != m_size * nb_gauss_pts) {
+          m_mat->resize(nb_gauss_pts, m_size, false);
+          m_mat->clear();
+          // initialize deformation gradient properly
+          if (is_def_grad && is_large_strain)
+            for (int gg = 0; gg != nb_gauss_pts; ++gg) {
+              (*m_mat)(gg, 0) = 1;
+              (*m_mat)(gg, 1) = 1;
+              (*m_mat)(gg, 2) = 1;
+            }
+          void const *tag_data2[] = {&*m_mat->data().begin()};
+          const int tag_size2 = m_mat->data().size();
+          CHKERR mField.get_moab().tag_set_by_ptr(m_tag, &fe_ent, 1, tag_data2,
+                                                  &tag_size2);
+        } else {
+          MatrixAdaptor tag_vec = MatrixAdaptor(
+              nb_gauss_pts, m_size,
+              ublas::shallow_array_adaptor<double>(tag_size, tag_data));
+
+          *m_mat = tag_vec;
+        }
+
+        MoFEMFunctionReturnHot(0);
+      };
+
+      CHKERR mget_tag_data(internalVariableTag, internalVariablePtr, var_size);
+      CHKERR mget_tag_data(stressTag, mPrevStressPtr, grad_size);
+      CHKERR mget_tag_data(gradientTag, mPrevGradPtr, stress_size, true);
+
+      MoFEMFunctionReturn(0);
+    }
+
+    MoFEMErrorCode setInternalVar(const EntityHandle fe_ent) {
+      MoFEMFunctionBegin;
+
+      auto mset_tag_data = [&](Tag &m_tag,
+                               boost::shared_ptr<MatrixDouble> &m_mat) {
+        MoFEMFunctionBeginHot;
+        void const *tag_data[] = {&*m_mat->data().begin()};
+        const int tag_size = m_mat->data().size();
+        CHKERR mField.get_moab().tag_set_by_ptr(m_tag, &fe_ent, 1, tag_data,
+                                                &tag_size);
+        MoFEMFunctionReturnHot(0);
+      };
+
+      CHKERR mset_tag_data(internalVariableTag, internalVariablePtr);
+      CHKERR mset_tag_data(stressTag, mPrevStressPtr);
+      CHKERR mset_tag_data(gradientTag, mPrevGradPtr);
+
+      MoFEMFunctionReturn(0);
+    }
+
+    MoFEMErrorCode createTags() {
+      MoFEMFunctionBegin;
+      double def_val = 0.0;
+      const int default_length = 0;
+      CHKERR mField.get_moab().tag_get_handle(
+          "_INTERNAL_VAR", default_length, MB_TYPE_DOUBLE, internalVariableTag,
+          MB_TAG_CREAT | MB_TAG_VARLEN | MB_TAG_SPARSE, PETSC_NULL);
+      CHKERR mField.get_moab().tag_get_handle(
+          "_STRESS_TAG", default_length, MB_TYPE_DOUBLE, stressTag,
+          MB_TAG_CREAT | MB_TAG_VARLEN | MB_TAG_SPARSE, PETSC_NULL);
+      CHKERR mField.get_moab().tag_get_handle(
+          "_GRAD_TAG", default_length, MB_TYPE_DOUBLE, gradientTag,
+          MB_TAG_CREAT | MB_TAG_VARLEN | MB_TAG_SPARSE, PETSC_NULL);
+
+      MoFEMFunctionReturn(0);
+    }
+
+    MoFEMErrorCode clearTags() {
+      MoFEMFunctionBegin;
+      double zero = 0;
+      for (auto &[id, data] : setOfBlocksData) {
+        CHKERR mField.get_moab().tag_clear_data(internalVariableTag, data.eNts,
+                                                &zero);
+        CHKERR mField.get_moab().tag_clear_data(stressTag, data.eNts, &zero);
+        CHKERR mField.get_moab().tag_clear_data(gradientTag, data.eNts, &zero);
+      }
+      MoFEMFunctionReturn(0);
+    }
+  };
+
+  private:
+
+  MoFEM::Core &cOre;
+
   string optionsPrefix;
 
   SmartPetscObj<DM> dM;
@@ -138,13 +404,15 @@ private:
   // boost::shared_ptr<PostProcDomainOnRefinedMesh> postProcFe;
   // boost::shared_ptr<DomainEle> updateIntVariablesElePtr;
 
-  string positionField;
-  string meshNodeField;
+  // string positionField;
+  // string meshNodeField;
 
   //   moab::Core mb_postGauss;
-  // boost::shared_ptr<moab::Interface> moabGaussIntPtr;
+  boost::shared_ptr<moab::Interface> moabGaussIntPtr;
 
-  // boost::shared_ptr<MoFEM::FEMethod> monitorPtr;
+  boost::shared_ptr<MoFEM::FEMethod> monitorPtr;
+
+  boost::shared_ptr<CommonData> commonDataPtr;
 
   // MoFEMErrorCode
   // setIntegrationRule(boost::shared_ptr<ForcesAndSourcesCore> fe_ptr);
--- mofem/src/interfaces/impl/MFrontInterface.cpp
+++ mofem/src/interfaces/impl/MFrontInterface.cpp
@@ -7,7 +7,7 @@
 
 #ifdef WITH_MGIS
 
-#include <MFrontInterface.hpp>
+  #include <MFrontInterface.hpp>
 
 namespace MoFEM {
 
@@ -18,6 +18,15 @@ template struct MFrontInterface<PLANESTRAIN>;
 template <ModelHypothesis H>
 MFrontInterface<H>::MFrontInterface(const MoFEM::Core &core)
     : cOre(const_cast<MoFEM::Core &>(core)) {
+
+  isFiniteKinematics = true;
+  saveGauss = PETSC_FALSE;
+  saveVolume = PETSC_TRUE;
+  testJacobian = PETSC_FALSE;
+  randomFieldScale = 1.0;
+  optionsPrefix = "mf_";
+  monitorPtr = nullptr;
+  fieldBase = NOBASE;
 
   // if (!LogManager::checkIfChannelExist("FieldEvaluatorWorld")) {
   //   auto core_log = logging::core::get();
@@ -50,6 +59,226 @@ MFrontInterface<H>::query_interface(boost::typeindex::type_index type_index,
   *iface = const_cast<MFrontInterface<H> *>(this);
   MoFEMFunctionReturnHot(0);
 }
+
+template <ModelHypothesis H>
+MoFEMErrorCode MFrontInterface<H>::getCommandLineParameters() {
+  MoFEMFunctionBegin;
+  // isQuasiStatic = PETSC_FALSE;
+  // if (oRder == -1)
+  //   oRder = 2;
+  CHKERR PetscOptionsBegin(PETSC_COMM_WORLD, optionsPrefix.c_str(), "", "none");
+
+  CHKERR PetscOptionsBool("-save_gauss", "save gauss pts (internal variables)",
+                          "", saveGauss, &saveGauss, PETSC_NULL);
+  CHKERR PetscOptionsBool("-save_volume", "save results on a volumetric mesh",
+                          "", saveVolume, &saveVolume, PETSC_NULL);
+
+  CHKERR PetscOptionsBool("-test_jacobian", "test Jacobian (LHS matrix)", "",
+                          testJacobian, &testJacobian, PETSC_NULL);
+  CHKERR PetscOptionsReal("-random_field_scale",
+                          "scale for the finite difference jacobian", "",
+                          randomFieldScale, &randomFieldScale, PETSC_NULL);
+
+  if (saveGauss)
+    moabGaussIntPtr = boost::shared_ptr<moab::Interface>(new moab::Core());
+
+  MoFEM::Interface &m_field = cOre;
+  commonDataPtr = boost::make_shared<CommonData>(m_field);
+
+  commonDataPtr->setBlocks(DIM);
+  commonDataPtr->createTags();
+
+  commonDataPtr->mGradPtr = boost::make_shared<MatrixDouble>();
+  commonDataPtr->mStressPtr = boost::make_shared<MatrixDouble>();
+  commonDataPtr->mFullStrainPtr = boost::make_shared<MatrixDouble>();
+  commonDataPtr->mFullStressPtr = boost::make_shared<MatrixDouble>();
+  commonDataPtr->mDispPtr = boost::make_shared<MatrixDouble>();
+  commonDataPtr->mPrevGradPtr = boost::make_shared<MatrixDouble>();
+  commonDataPtr->mPrevStressPtr = boost::make_shared<MatrixDouble>();
+  commonDataPtr->materialTangentPtr = boost::make_shared<MatrixDouble>();
+  commonDataPtr->mFullTangentPtr = boost::make_shared<MatrixDouble>();
+  commonDataPtr->internalVariablePtr = boost::make_shared<MatrixDouble>();
+
+  if (commonDataPtr->setOfBlocksData.empty())
+    MOFEM_LOG("WORLD", Sev::inform)
+        << "No blocksets on the mesh have been provided for MFront (e.g. "
+           "MFRONT_MAT_1)";
+
+  auto check_lib_finite_strain = [&](const std::string &lib,
+                                     const std::string &beh_name, bool &flag) {
+    MoFEMFunctionBeginHot;
+
+    ifstream f(lib.c_str());
+    if (!f.good())
+      MOFEM_LOG("WORLD", Sev::error)
+          << "Problem with the behaviour path: " << lib;
+
+    auto &lm = LibrariesManager::get();
+    flag = bool(lm.getBehaviourType(lib, beh_name) == 2) &&
+           (lm.getBehaviourKinematic(lib, beh_name) == 3);
+    MoFEMFunctionReturnHot(0);
+  };
+
+  auto op = FiniteStrainBehaviourOptions{};
+  op.stress_measure = FiniteStrainBehaviourOptions::PK1;
+  op.tangent_operator = FiniteStrainBehaviourOptions::DPK1_DF;
+
+  for (auto &block : commonDataPtr->setOfBlocksData) {
+    const int &id = block.first;
+    auto &lib_path = block.second.behaviourPath;
+    auto &name = block.second.behaviourName;
+    const string param_name = "-block_" + to_string(id);
+    const string param_path = "-lib_path_" + to_string(id);
+    const string param_from_blocks = "-my_params_" + to_string(id);
+    PetscBool set_from_blocks = PETSC_FALSE;
+    char char_name[255];
+    PetscBool is_param;
+
+    CHKERR PetscOptionsBool(param_from_blocks.c_str(),
+                            "set parameters from blocks", "", set_from_blocks,
+                            &set_from_blocks, PETSC_NULL);
+
+    CHKERR PetscOptionsString(param_name.c_str(), "name of the behaviour", "",
+                              "IsotropicLinearHardeningPlasticity", char_name,
+                              255, &is_param);
+    if (is_param)
+      name = string(char_name);
+    string default_lib_path =
+        "src/libBehaviour." + string(DEFAULT_LIB_EXTENSION);
+    CHKERR PetscOptionsString(
+        param_path.c_str(), "path to the behaviour library", "",
+        default_lib_path.c_str(), char_name, 255, &is_param);
+    if (is_param)
+      lib_path = string(char_name);
+    auto &mgis_bv_ptr = block.second.mGisBehaviour;
+    bool is_finite_strain = false;
+
+    cout << "Loading behaviour: " << name << " from " << lib_path << endl;
+
+    CHKERR check_lib_finite_strain(lib_path, name, is_finite_strain);
+
+    mgis::behaviour::Hypothesis h;
+    switch (H) {
+    case TRIDIMENSIONAL:
+      h = mgis::behaviour::Hypothesis::TRIDIMENSIONAL;
+      CHKERR PetscPrintf(PETSC_COMM_WORLD,
+                         "MFront material model: TRIDIMENSIONAL \n");
+      break;
+    case PLANESTRAIN:
+      h = mgis::behaviour::Hypothesis::PLANESTRAIN;
+      CHKERR PetscPrintf(PETSC_COMM_WORLD,
+                         "MFront material model: PLANESTRAIN \n");
+      break;
+    case AXISYMMETRICAL:
+      h = mgis::behaviour::Hypothesis::AXISYMMETRICAL;
+      CHKERR PetscPrintf(PETSC_COMM_WORLD,
+                         "MFront material model: AXISYMMETRICAL \n");
+      break;
+    default:
+      break;
+    }
+
+    if (is_finite_strain) {
+      mgis_bv_ptr = boost::make_shared<Behaviour>(load(op, lib_path, name, h));
+      block.second.isFiniteStrain = true;
+    } else
+      mgis_bv_ptr = boost::make_shared<Behaviour>(load(lib_path, name, h));
+
+    CHKERR block.second.setBlockBehaviourData(set_from_blocks);
+    for (size_t dd = 0; dd < mgis_bv_ptr->mps.size(); ++dd) {
+      double my_param = 0;
+      PetscBool is_set = PETSC_FALSE;
+      string param_cmd = "-param_" + to_string(id) + "_" + to_string(dd);
+      CHKERR PetscOptionsScalar(param_cmd.c_str(), "parameter from cmd", "",
+                                my_param, &my_param, &is_set);
+      if (!is_set)
+        continue;
+      setMaterialProperty(block.second.behDataPtr->s0, dd, my_param);
+      setMaterialProperty(block.second.behDataPtr->s1, dd, my_param);
+    }
+
+    int nb = 0;
+
+    // PRINT PROPERLY WITH SHOWING WHAT WAS ASSIGNED BY THE USER!!!
+    CHKERR PetscPrintf(PETSC_COMM_WORLD, "%s behaviour loaded on block %d. \n",
+                       mgis_bv_ptr->behaviour.c_str(), block.first);
+    if (is_finite_strain)
+      CHKERR PetscPrintf(PETSC_COMM_WORLD, "Finite Strain Kinematics \n");
+    else
+      CHKERR PetscPrintf(PETSC_COMM_WORLD, "Small Strain Kinematics \n");
+
+    CHKERR PetscPrintf(PETSC_COMM_WORLD, "Internal variables: \n");
+    for (const auto &is : mgis_bv_ptr->isvs)
+      CHKERR PetscPrintf(PETSC_COMM_WORLD, ": %s\n", is.name.c_str());
+    CHKERR PetscPrintf(PETSC_COMM_WORLD, "External variables: \n");
+    for (const auto &es : mgis_bv_ptr->esvs)
+      CHKERR PetscPrintf(PETSC_COMM_WORLD, ": %s\n", es.name.c_str());
+
+    auto it = block.second.behDataPtr->s0.material_properties.begin();
+    CHKERR PetscPrintf(PETSC_COMM_WORLD, "Material properties: \n");
+    for (const auto &mp : mgis_bv_ptr->mps)
+      CHKERR PetscPrintf(PETSC_COMM_WORLD, "%d : %s = %g\n", nb++,
+                         mp.name.c_str(), *it++);
+
+    CHKERR PetscPrintf(PETSC_COMM_WORLD, "Real parameters: \n");
+    for (auto &p : mgis_bv_ptr->params)
+      CHKERR PetscPrintf(PETSC_COMM_WORLD, "%d : %s\n", nb++, p.c_str());
+    CHKERR PetscPrintf(PETSC_COMM_WORLD, "Integer parameters: \n");
+    for (auto &p : mgis_bv_ptr->iparams)
+      CHKERR PetscPrintf(PETSC_COMM_WORLD, "%d : %s\n", nb++, p.c_str());
+    CHKERR PetscPrintf(PETSC_COMM_WORLD, "Unsigned short parameters: \n");
+    for (auto &p : mgis_bv_ptr->usparams)
+      CHKERR PetscPrintf(PETSC_COMM_WORLD, "%d : %s\n", nb++, p.c_str());
+  }
+
+  ierr = PetscOptionsEnd();
+  CHKERRQ(ierr);
+
+  auto check_behaviours_kinematics = [&](bool &is_finite_kin) {
+    MoFEMFunctionBeginHot;
+    is_finite_kin =
+        commonDataPtr->setOfBlocksData.begin()->second.isFiniteStrain;
+    for (auto &block : commonDataPtr->setOfBlocksData) {
+      if (block.second.isFiniteStrain != is_finite_kin)
+        SETERRQ(PETSC_COMM_SELF, MOFEM_DATA_INCONSISTENCY,
+                "All used MFront behaviours have to be of same kinematics "
+                "(small or "
+                "large strains)");
+    }
+    MoFEMFunctionReturnHot(0);
+  };
+
+  CHKERR check_behaviours_kinematics(isFiniteKinematics);
+
+  auto get_base = [&]() {
+    Range domain_ents;
+    MoFEM::Interface &m_field = cOre;
+    CHKERR m_field.get_moab().get_entities_by_dimension(0, DIM, domain_ents,
+                                                       true);
+    if (domain_ents.empty())
+      CHK_THROW_MESSAGE(MOFEM_NOT_FOUND, "Empty mesh");
+    const auto type = type_from_handle(domain_ents[0]);
+    switch (type) {
+    case MBQUAD:
+      return DEMKOWICZ_JACOBI_BASE;
+    case MBHEX:
+      return DEMKOWICZ_JACOBI_BASE;
+    case MBTRI:
+      return AINSWORTH_LEGENDRE_BASE;
+    case MBTET:
+      return AINSWORTH_LEGENDRE_BASE;
+    default:
+      CHK_THROW_MESSAGE(MOFEM_NOT_FOUND, "Element type not handled");
+    }
+    return NOBASE;
+  };
+
+  fieldBase = get_base();
+  MOFEM_LOG("WORLD", Sev::inform)
+      << "MFront approximation base " << ApproximationBaseNames[fieldBase];
+
+  MoFEMFunctionReturn(0);
+};
 
 } // namespace MoFEM
 
