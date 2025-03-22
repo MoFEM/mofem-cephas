22d61c99820ecaf3c70b8e51d808c34e3e6f488e
Andrei Shvarts <andrei.shvarts@glasgow.ac.uk>
2025-03-22 20:24:41 +0000

    address Karol's comments

--- mofem/tools/mesh_data_transfer.cpp
+++ mofem/tools/mesh_data_transfer.cpp
@@ -1,6 +1,14 @@
 /** \file mesh_data_transfer.cpp
 
-  \brief Transfer data from source mesh to target mesh using MOAB
+  \brief Transfer data from source mesh to target mesh
+
+  The data is transferred from tags on 3D elements of the source mesh to tags on
+  3D elements in the target mesh. This code uses MBCoupler functionality of MOAB
+  and is based on mbcoupler_test.cpp from MOAB.
+
+  For this tool to work, source and target files should be partitioned using
+  mbpart tool from MOAB, e.g.
+  mbpart -z PHG -t 4 sslv116_hex.cub sslv116_hex_4mp.h5m
 
 */
 
@@ -28,9 +36,9 @@ int main(int argc, char *argv[]) {
 
     int interp_order = 1;
     int interp_tag_len = 9;
-
-    PetscBool set_source_data = PETSC_FALSE;
+        
     PetscBool use_target_verts = PETSC_TRUE;
+    PetscBool set_source_data = PETSC_FALSE;
     PetscBool atom_test = PETSC_FALSE;
 
     double toler = 5.e-10;
@@ -60,17 +68,33 @@ int main(int argc, char *argv[]) {
     ierr = PetscOptionsEnd();
     CHKERRQ(ierr);
 
-    Coupler::Method method = Coupler::CONSTANT;
-
-    if (interp_order == 1) {
+    Coupler::Method method;
+    switch (interp_order) {
+    case 0:
+      method = Coupler::CONSTANT;
+      break;
+    case 1:
       method = Coupler::LINEAR_FE;
+      break;
+    default:
+      SETERRQ(PETSC_COMM_WORLD, MOFEM_NOT_IMPLEMENTED,
+              "Unsupported interpolation order");
     }
 
-    std::vector<std::string> meshFiles(2);
-    meshFiles[0] = string(mesh_source_file);
-    meshFiles[1] = string(mesh_target_file);
+    if (atom_test && atom_test != 1) {
+      SETERRQ1(PETSC_COMM_WORLD, MOFEM_NOT_IMPLEMENTED, "Unknown test number: %d",
+               atom_test);
+    }
+    if (interp_tag_len != 1 && interp_tag_len != 3 && interp_tag_len != 9) {
+      SETERRQ1(PETSC_COMM_WORLD, MOFEM_NOT_IMPLEMENTED,
+              "Unsupported interpolation tag length: %d", interp_tag_len);
+    }
 
-    std::string readOpts, writeOpts;
+    std::vector<std::string> mesh_files(2);
+    mesh_files[0] = string(mesh_source_file);
+    mesh_files[1] = string(mesh_target_file);
+
+    std::string read_opts, write_opts;
 
     int nprocs, rank;
     ierr = MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
@@ -78,7 +102,7 @@ int main(int argc, char *argv[]) {
     ierr = MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     CHKERRQ(ierr);
 
-    readOpts =
+    read_opts =
         (nprocs > 1
              ? "PARALLEL=READ_PART;PARTITION=PARALLEL_PARTITION;PARTITION_"
                "DISTRIBUTE;"
@@ -86,28 +110,23 @@ int main(int argc, char *argv[]) {
              : "PARALLEL=READ_PART;PARTITION=PARALLEL_PARTITION;PARTITION_"
                "DISTRIBUTE;"
                "PARALLEL_RESOLVE_SHARED_ENTS");
+    write_opts = (nprocs > 1 ? "PARALLEL=WRITE_PART" : "");
 
-    writeOpts = (nprocs > 1 ? "PARALLEL=WRITE_PART" : "");
+    std::vector<ParallelComm *> pcs(mesh_files.size());
+    std::vector<EntityHandle> roots(mesh_files.size());
 
-    std::vector<ParallelComm *> pcs(meshFiles.size());
-
-    EntityHandle *roots =
-        (EntityHandle *)malloc(sizeof(EntityHandle) * meshFiles.size());
-
-    moab::Interface *mbImpl = &moab;
-
-    for (unsigned int i = 0; i < meshFiles.size(); i++) {
-      pcs[i] = new ParallelComm(mbImpl, MPI_COMM_WORLD);
+    for (unsigned int i = 0; i < mesh_files.size(); i++) {
+      pcs[i] = new ParallelComm(&moab, MPI_COMM_WORLD);
       int index = pcs[i]->get_id();
-      std::string newReadopts;
-      std::ostringstream extraOpt;
-      extraOpt << ";PARALLEL_COMM=" << index;
-      newReadopts = readOpts + extraOpt.str();
+      std::string newread_opts;
+      std::ostringstream extra_opt;
+      extra_opt << ";PARALLEL_COMM=" << index;
+      newread_opts = read_opts + extra_opt.str();
+  
+      CHKERR moab.create_meshset(MESHSET_SET, roots[i]);
 
-      CHKERR mbImpl->create_meshset(MESHSET_SET, roots[i]);
-
-      CHKERR mbImpl->load_file(meshFiles[i].c_str(), &roots[i],
-                               newReadopts.c_str());
+      CHKERR moab.load_file(mesh_files[i].c_str(), &roots[i],
+                               newread_opts.c_str());
     }
 
     Range src_elems, targ_elems, targ_verts;
@@ -116,37 +135,42 @@ int main(int argc, char *argv[]) {
     Tag interp_tag;
     double def_val[9];
     bzero(def_val, 9 * sizeof(double));
-    CHKERR mbImpl->tag_get_handle(iterp_tag_name, interp_tag_len,
+    CHKERR moab.tag_get_handle(iterp_tag_name, interp_tag_len,
                                   MB_TYPE_DOUBLE, interp_tag,
                                   MB_TAG_CREAT | MB_TAG_DENSE, &def_val);
 
-    // Set source tag to create sslv116 test mesh
+    // Set data on the source mesh for SSLV116 test 
     if (set_source_data) {
       std::vector<double> spos;
       spos.resize(3 * src_elems.size());
-      CHKERR mbImpl->get_coords(src_elems, &spos[0]);
+      CHKERR moab.get_coords(src_elems, &spos[0]);
 
       std::vector<double> stress(9 * src_elems.size(), 0.0);
       auto t_stress = getFTensor2FromPtr<3, 3>(&stress[0]);
 
+      // temperature distribution for SSLV116 test
+      // see https://code-aster.org/V2/doc/v14/en/man_v/v3/v3.04.116.pdf
       auto temperature = [](double z) -> double {
         return (z < -1)   ? (120.0 / -9.0) * (z + 10) + 250
                : (z <= 2) ? (50.0 / -3.0) * (z + 1) + 130
                           : (170.0 / 8.0) * (z - 2) + 80;
       };
 
+      // material properties for SSLV116 test 
       double young_modulus = 2e11;
       double poisson_ratio = 0.3;
-      double bulk_modulus_K = young_modulus / (3 * (1 - 2 * poisson_ratio));
-      double shear_modulus_G = young_modulus / (2 * (1 + poisson_ratio));
       double alpha = 1e-5;
       double temp_0 = 250;
+
+      double bulk_modulus_K = young_modulus / (3 * (1 - 2 * poisson_ratio));
+      double shear_modulus_G = young_modulus / (2 * (1 + poisson_ratio));
 
       FTensor::Index<'i', 3> i;
       FTensor::Index<'j', 3> j;
       FTensor::Index<'k', 3> k;
       FTensor::Index<'l', 3> l;
 
+      // computing elasticity matrix
       MatrixDouble mat_D_ptr;
       mat_D_ptr.resize(6, 6, false);
       auto t_D = getFTensor4DdgFromPtr<3, 3, 0>(&*(mat_D_ptr.data().begin()));
@@ -155,6 +179,7 @@ int main(int argc, char *argv[]) {
                         (bulk_modulus_K - (2. / 3.) * shear_modulus_G) *
                             t_kd(i, j) * t_kd(k, l);
 
+      // computing thermal stress field (const on each element)
       for (auto ielem = 0; ielem < src_elems.size(); ++ielem) {
         double z = spos[3 * ielem + 2];
         double temp = temperature(z);
@@ -163,13 +188,13 @@ int main(int argc, char *argv[]) {
         ++t_stress;
       }
 
-      CHKERR mbImpl->tag_set_data(interp_tag, src_elems, &stress[0]);
+      CHKERR moab.tag_set_data(interp_tag, src_elems, &stress[0]);
     }
 
-    Coupler mbc(mbImpl, pcs[0], src_elems, 0, true);
+    Coupler mbc(&moab, pcs[0], src_elems, 0, true);
 
     std::vector<double> vpos; // the positions we are interested in
-    int numPointsOfInterest = 0;
+    int num_pts = 0;
 
     Range tmp_verts;
 
@@ -178,7 +203,7 @@ int main(int argc, char *argv[]) {
     if (!use_target_verts) {
       targ_verts = targ_elems;
     } else {
-      CHKERR mbImpl->get_adjacencies(targ_elems, 0, false, targ_verts,
+      CHKERR moab.get_adjacencies(targ_elems, 0, false, targ_verts,
                                      moab::Interface::UNION);
     }
 
@@ -187,64 +212,67 @@ int main(int argc, char *argv[]) {
     targ_verts = subtract(targ_verts, tmp_verts);
 
     // get position of these entities; these are the target points
-    numPointsOfInterest = (int)targ_verts.size();
+    num_pts = (int)targ_verts.size();
     vpos.resize(3 * targ_verts.size());
-    CHKERR mbImpl->get_coords(targ_verts, &vpos[0]);
+    CHKERR moab.get_coords(targ_verts, &vpos[0]);
 
     // Locate those points in the source mesh
-    std::cout << "rank " << pcs[0]->proc_config().proc_rank()
-              << " points of interest: " << numPointsOfInterest << "\n";
-    CHKERR mbc.locate_points(&vpos[0], numPointsOfInterest, 0, toler);
+    CHKERR mbc.locate_points(&vpos[0], num_pts, 0, toler);
 
     std::vector<double> source_data(interp_tag_len * src_elems.size(), 0.0);
-    std::vector<double> target_data(interp_tag_len * numPointsOfInterest, 0.0);
+    std::vector<double> target_data(interp_tag_len * num_pts, 0.0);
 
-    CHKERR mbImpl->tag_get_data(interp_tag, src_elems, &source_data[0]);
+    CHKERR moab.tag_get_data(interp_tag, src_elems, &source_data[0]);
 
     Tag scalar_tag, adj_count_tag;
     double def_scl = 0;
     string scalar_tag_name = string(iterp_tag_name) + "_COMP";
-    CHKERR mbImpl->tag_get_handle(scalar_tag_name.c_str(), 1, MB_TYPE_DOUBLE,
+    CHKERR moab.tag_get_handle(scalar_tag_name.c_str(), 1, MB_TYPE_DOUBLE,
                                   scalar_tag, MB_TAG_CREAT | MB_TAG_DENSE,
                                   &def_scl);
 
     string adj_count_tag_name = "ADJ_COUNT";
     double def_adj = 0;
-    CHKERR mbImpl->tag_get_handle(adj_count_tag_name.c_str(), 1, MB_TYPE_DOUBLE,
+    CHKERR moab.tag_get_handle(adj_count_tag_name.c_str(), 1, MB_TYPE_DOUBLE,
                                   adj_count_tag, MB_TAG_CREAT | MB_TAG_DENSE,
                                   &def_adj);
 
     std::vector<double> source_data_scalar(src_elems.size());
 
+    // MBCoupler functionality supports only scalar meshes. For the case of
+    // vector or tensor tags we need to save each component as a scalar tag
     for (int itag = 0; itag < interp_tag_len; itag++) {
 
       for (int ielem = 0; ielem < src_elems.size(); ielem++) {
         source_data_scalar[ielem] = source_data[itag + ielem * interp_tag_len];
       }
 
-      CHKERR mbImpl->tag_set_data(scalar_tag, src_elems,
+      CHKERR moab.tag_set_data(scalar_tag, src_elems,
                                   &source_data_scalar[0]);
 
-      if (interp_order) {
+      if (interp_order == 1) {
+        // In case of linear interpolation, data needs to be saved on source
+        // mesh vertices. Here we compute average value of the data on the
+        // vertices from all adjacent 3D elements
 
         Range src_verts;
-        CHKERR mbImpl->get_connectivity(src_elems, src_verts, true);
+        CHKERR moab.get_connectivity(src_elems, src_verts, true);
 
-        CHKERR mbImpl->tag_clear_data(scalar_tag, src_verts, &def_val);
-        CHKERR mbImpl->tag_clear_data(adj_count_tag, src_verts, &def_adj);
+        CHKERR moab.tag_clear_data(scalar_tag, src_verts, &def_val);
+        CHKERR moab.tag_clear_data(adj_count_tag, src_verts, &def_adj);
 
         for (auto &tet : src_elems) {
           double tet_data = 0;
-          CHKERR mbImpl->tag_get_data(scalar_tag, &tet, 1, &tet_data);
+          CHKERR moab.tag_get_data(scalar_tag, &tet, 1, &tet_data);
 
           Range adj_verts;
-          CHKERR mbImpl->get_connectivity(&tet, 1, adj_verts, true);
+          CHKERR moab.get_connectivity(&tet, 1, adj_verts, true);
 
           std::vector<double> adj_vert_data(adj_verts.size(), 0.0);
           std::vector<double> adj_vert_count(adj_verts.size(), 0.0);
 
-          CHKERR mbImpl->tag_get_data(scalar_tag, adj_verts, &adj_vert_data[0]);
-          CHKERR mbImpl->tag_get_data(adj_count_tag, adj_verts,
+          CHKERR moab.tag_get_data(scalar_tag, adj_verts, &adj_vert_data[0]);
+          CHKERR moab.tag_get_data(adj_count_tag, adj_verts,
                                       &adj_vert_count[0]);
 
           for (int ivert = 0; ivert < adj_verts.size(); ivert++) {
@@ -252,11 +280,12 @@ int main(int argc, char *argv[]) {
             adj_vert_count[ivert] += 1;
           }
 
-          CHKERR mbImpl->tag_set_data(scalar_tag, adj_verts, &adj_vert_data[0]);
-          CHKERR mbImpl->tag_set_data(adj_count_tag, adj_verts,
+          CHKERR moab.tag_set_data(scalar_tag, adj_verts, &adj_vert_data[0]);
+          CHKERR moab.tag_set_data(adj_count_tag, adj_verts,
                                       &adj_vert_count[0]);
         }
 
+        // We need to reduce tags for the parallel case
         std::vector<Tag> tags;
         tags.push_back(scalar_tag);
         tags.push_back(adj_count_tag);
@@ -265,43 +294,46 @@ int main(int argc, char *argv[]) {
         std::vector<double> src_vert_data(src_verts.size(), 0.0);
         std::vector<double> src_vert_adj_count(src_verts.size(), 0.0);
 
-        CHKERR mbImpl->tag_get_data(scalar_tag, src_verts, &src_vert_data[0]);
-        CHKERR mbImpl->tag_get_data(adj_count_tag, src_verts,
+        CHKERR moab.tag_get_data(scalar_tag, src_verts, &src_vert_data[0]);
+        CHKERR moab.tag_get_data(adj_count_tag, src_verts,
                                     &src_vert_adj_count[0]);
 
         for (int ivert = 0; ivert < src_verts.size(); ivert++) {
           src_vert_data[ivert] /= src_vert_adj_count[ivert];
         }
-        CHKERR mbImpl->tag_set_data(scalar_tag, src_verts, &src_vert_data[0]);
+        CHKERR moab.tag_set_data(scalar_tag, src_verts, &src_vert_data[0]);
       }
 
-      std::vector<double> target_data_scalar(numPointsOfInterest, 0.0);
+      std::vector<double> target_data_scalar(num_pts, 0.0);
       CHKERR mbc.interpolate(method, scalar_tag_name, &target_data_scalar[0]);
 
-      for (int ielem = 0; ielem < numPointsOfInterest; ielem++) {
+      for (int ielem = 0; ielem < num_pts; ielem++) {
         target_data[itag + ielem * interp_tag_len] = target_data_scalar[ielem];
       }
     }
 
-    CHKERR mbImpl->tag_delete(scalar_tag);
-    CHKERR mbImpl->tag_delete(adj_count_tag);
+    CHKERR moab.tag_delete(scalar_tag);
+    CHKERR moab.tag_delete(adj_count_tag);
 
     // Use original tag
-    CHKERR mbImpl->tag_set_data(interp_tag, targ_verts, &target_data[0]);
+    CHKERR moab.tag_set_data(interp_tag, targ_verts, &target_data[0]);
 
     if (use_target_verts) {
+      // If the data was interpolated to target mesh vertices, we need to
+      // average the vertex values for each 3D element and save on its tag
 
+      // We need to reduce tags for the parallel case
       std::vector<Tag> tags;
       tags.push_back(interp_tag);
       pcs[1]->reduce_tags(tags, tags, MPI_SUM, targ_verts);
 
       for (auto &tet : targ_elems) {
         Range adj_verts;
-        CHKERR mbImpl->get_connectivity(&tet, 1, adj_verts, true);
+        CHKERR moab.get_connectivity(&tet, 1, adj_verts, true);
 
         std::vector<double> adj_vert_data(adj_verts.size() * interp_tag_len,
                                           0.0);
-        CHKERR mbImpl->tag_get_data(interp_tag, adj_verts, &adj_vert_data[0]);
+        CHKERR moab.tag_get_data(interp_tag, adj_verts, &adj_vert_data[0]);
 
         std::vector<double> tet_data(interp_tag_len, 0.0);
         for (int itag = 0; itag < interp_tag_len; itag++) {
@@ -310,11 +342,12 @@ int main(int argc, char *argv[]) {
           }
           tet_data[itag] /= adj_verts.size();
         }
-        CHKERR mbImpl->tag_set_data(interp_tag, &tet, 1, &tet_data[0]);
+        CHKERR moab.tag_set_data(interp_tag, &tet, 1, &tet_data[0]);
       }
     }
 
     if (atom_test) {
+      // Check the integrated stress for the SSLV116 test
 
       auto compute_tet_volume = [](const std::vector<double> &vpos) -> double {
         // Extract vertices
@@ -344,12 +377,12 @@ int main(int argc, char *argv[]) {
       for (auto &tet : targ_elems) {
 
         std::vector<double> tet_data(interp_tag_len, 0.0);
-        CHKERR mbImpl->tag_get_data(interp_tag, &tet, 1, &tet_data[0]);
+        CHKERR moab.tag_get_data(interp_tag, &tet, 1, &tet_data[0]);
 
         Range tmp_verts;
-        CHKERR mbImpl->get_connectivity(&tet, 1, tmp_verts);
+        CHKERR moab.get_connectivity(&tet, 1, tmp_verts);
         std::vector<double> vpos(3 * tmp_verts.size());
-        CHKERR mbImpl->get_coords(tmp_verts, &vpos[0]);
+        CHKERR moab.get_coords(tmp_verts, &vpos[0]);
         double vol = compute_tet_volume(vpos);
 
         for (int itag = 0; itag < interp_tag_len; itag++) {
@@ -390,25 +423,23 @@ int main(int argc, char *argv[]) {
       }
     }
 
-    Range partSets;
+    Range part_sets;
     // Only save the target mesh
-    partSets.insert((EntityHandle)roots[1]);
-    std::string newwriteOpts;
-    std::ostringstream extraOpt;
+    part_sets.insert(roots[1]);
+    std::string new_write_opts;
+    std::ostringstream extra_opt;
     if (nprocs > 1) {
       int index = pcs[1]->get_id();
-      extraOpt << ";PARALLEL_COMM=" << index;
+      extra_opt << ";PARALLEL_COMM=" << index;
     }
-    newwriteOpts = writeOpts + extraOpt.str();
-    CHKERR mbImpl->write_file(mesh_out_file, NULL, newwriteOpts.c_str(),
-                              partSets);
+    new_write_opts = write_opts + extra_opt.str();
+    CHKERR moab.write_file(mesh_out_file, NULL, new_write_opts.c_str(),
+                              part_sets);
     if (0 == rank) {
       std::cout << "Wrote file " << mesh_out_file << std::endl;
     }
 
-    free(roots);
-
-    for (unsigned int i = 0; i < meshFiles.size(); i++)
+    for (unsigned int i = 0; i < mesh_files.size(); i++)
       delete pcs[i];
   }
   CATCH_ERRORS;
