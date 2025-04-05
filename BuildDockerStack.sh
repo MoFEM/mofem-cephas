6112a92e08b13329c718ab7f66ff9ae38e5cad37
Andrei Shvarts <Andrei.Shvarts@glasgow.ac.uk>
2025-04-05 10:11:43 +0000

    docker for mos

--- BuildDockerStack.sh
+++ BuildDockerStack.sh
@@ -2,16 +2,17 @@
 
 mkdir -p empty
 
-export VERSION=Workshop2023
+export ACCOUNT=agshvarts
+export VERSION=MOS2025
 
 # Create environment
-docker build -t likask/mofem-spack-env -f Dockerfile-spack-env empty/
-docker tag likask/mofem-spack-env:latest likask/mofem-spack-env:$VERSION
+docker build -t $ACCOUNT/mofem-spack-env -f Dockerfile-spack-env empty/
+docker tag $ACCOUNT/mofem-spack-env:latest $ACCOUNT/mofem-spack-env:$VERSION
 
 #Build core and users modules
-docker build -t likask/mofem-spack-mofem -f Dockerfile-spack-mofem empty/
-docker tag likask/mofem-spack-mofem:latest likask/mofem-spack-mofem:$VERSION
-docker tag likask/mofem-spack-mofem:latest likask/mofem-intermidiate:latest
+docker build -t $ACCOUNT/mofem-spack-mofem -f Dockerfile-spack-mofem empty/
+docker tag $ACCOUNT/mofem-spack-mofem:latest $ACCOUNT/mofem-spack-mofem:$VERSION
+# docker tag $ACCOUNT/mofem-spack-mofem:latest $ACCOUNT/mofem-intermidiate:latest
 
 #Install softmech module
 #docker build -t likask/mofem-spack-softmech -f Dockerfile-spack-softmech empty/ 
@@ -21,17 +22,17 @@ docker tag likask/mofem-spack-mofem:latest likask/mofem-intermidiate:latest
 # Install other modules ...
 
 #Jupyter
-docker build -t likask/mofem-spack-jupyter -f Dockerfile-spack-jupyter empty
-docker tag likask/mofem-spack-jupyter:latest likask/mofem-spack-jupyter:$VERSION 
-docker tag likask/mofem-spack-jupyter:latest likask/mofem-intermidiate:latest
+# docker build -t likask/mofem-spack-jupyter -f Dockerfile-spack-jupyter empty
+# docker tag likask/mofem-spack-jupyter:latest likask/mofem-spack-jupyter:$VERSION 
+# docker tag likask/mofem-spack-jupyter:latest likask/mofem-intermidiate:latest
 
 # Labs indentation
 #dodocker build -t likask/mofem-spack-jupyter-labs-indentation:VERSION-new -f Dockerfile-spack-jupyter-labs-indentation .
 
 # Hub
-docker build -t likask/mofem-spack-jupyterhub -f Dockerfile-spack-jupyterhub jupyter
-docker tag likask/mofem-spack-jupyterhub:latest likask/mofem-spack-jupyterhub:$VERSION
-docker tag likask/mofem-spack-jupyterhub:latest likask/mofem-intermidiate:latest
+# docker build -t likask/mofem-spack-jupyterhub -f Dockerfile-spack-jupyterhub jupyter
+# docker tag likask/mofem-spack-jupyterhub:latest likask/mofem-spack-jupyterhub:$VERSION
+# docker tag likask/mofem-spack-jupyterhub:latest likask/mofem-intermidiate:latest
 
 # Labs indentation
 #docker build -t likask/mofem-spack-jupyterhub-labs-indentation:VERSION-new -f Dockerfile-spack-jupyterhub-labs-indentation .
